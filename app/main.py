921618a1bfb9a9f6a6d15597c4a197f6df7f2968
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-08 20:13:56 +0200

    fix complуxity O(n^2) -> O(n)

--- app/main.py
+++ app/main.py
@@ -1,72 +1,97 @@
 class Dictionary:
+    LOAD_FACTOR = 2 / 3
+    RESIZE = 2
+
     def __init__(self):
         self.initial_capacity = 8
         self.storage = [[] for _ in range(self.initial_capacity)]
         self.length = 0
-        self.load_factor = 2 / 3
-        self.resize = 2
 
-    def is_resize(self):
-        return True if self.length == int(
-            self.initial_capacity * self.load_factor
-        ) else False
+    def resize(self):
+        saved_storage = self.storage[:]
+        load = int(self.initial_capacity * self.LOAD_FACTOR)
 
-    def check_resize(self):
-        save_storage = self.storage[:]
-        re_size = int(self.initial_capacity * self.load_factor)
-        if self.length == re_size:
+        if load == self.length:
             self.length = 0
-            self.initial_capacity *= self.resize
+            self.initial_capacity *= self.RESIZE
             self.storage = [[] for _ in range(self.initial_capacity)]
-        for sub_list in save_storage:
-            for el in sub_list:
-                self.__setitem__(el[0], el[1])
+        for item in saved_storage:
+            if len(item) != 0:
+                self.__setitem__(item[0], item[1])
 
     def __setitem__(self, key, value):
-        if self.is_resize():
-            self.check_resize()
+        if self.length == int(self.initial_capacity * self.LOAD_FACTOR):
+            self.resize()
 
-        index = hash(key) % self.initial_capacity
-        for ele in self.storage[index]:
-            if key == ele[0]:
-                ele[1] = value
-                break
+        hashed_value = hash(key)
+
+        index = hashed_value % self.initial_capacity
+
+        if len(self.storage[index]) != 0:
+            if key == self.storage[index][0] and\
+                    hashed_value == self.storage[index][2]:
+                self.storage[index][1] = value
+            else:
+                while True:
+                    index += 1
+                    if index == self.initial_capacity:
+                        index = 0
+                        continue
+                    elif len(self.storage[index]) == 0:
+                        self.length += 1
+                        self.storage[index] = [key, value, hashed_value]
+                        break
         else:
-            self.storage[index].append([key, value])
+            self.storage[index] = [key, value, hashed_value]
             self.length += 1
 
     def __getitem__(self, key):
-        index = hash(key) % self.initial_capacity
-        for ele in self.storage[index]:
-            if ele[0] == key:
-                return ele[1]
-        raise KeyError
+        hashed_value = hash(key)
+        index = hashed_value % self.initial_capacity
 
-    def __delitem__(self, key):
-        index = hash(key) % self.initial_capacity
-        for sub_lst in self.storage[index]:
-            if key == sub_lst[0]:
-                self.storage[index].remove(sub_lst)
-                self.length -= 1
-                return
-        raise KeyError
+        if key == self.storage[index][0] and\
+                hashed_value == self.storage[index][2]:
+            return self.storage[index][1]
+        else:
+            counter = 0
+            while counter <= self.initial_capacity:
+                index += 1
+                if index == self.initial_capacity:
+                    index = 0
+                    continue
+                if key == self.storage[index][0] and\
+                        hashed_value == self.storage[index][2]:
+                    return self.storage[index][1]
+                counter += 1
+            raise KeyError
 
     def __len__(self):
         return self.length
 
-    def __contains__(self, key):
-        index = hash(key) % self.initial_capacity
-        for el in self.storage[index]:
-            if el[0] == key:
-                return True
-        return False
+    def __delitem__(self, key):
+        hashed_value = hash(key)
+        index = hashed_value % self.initial_capacity
 
-    def __iter__(self):
-        for sub_list in self.storage:
-            if not sub_list:
-                continue
-            for key in sub_list:
-                yield key[0]
+        for _ in self.storage[index]:
+            if key == self.storage[index][0] and\
+                    hashed_value == self.storage[index][2]:
+                self.storage[index] = []
+                self.length -= 1
+                return
+            else:
+                counter = 0
+                while counter <= self.initial_capacity:
+                    index += 1
+                    if index == self.initial_capacity:
+                        index = 0
+                        continue
+                    if key == self.storage[index][0] and\
+                            hashed_value == self.storage[index][2]:
+                        self.storage[index] = []
+                        self.length -= 1
+                        return
+                    counter += 1
+        raise KeyError
 
     def get(self, key):
         try:
@@ -81,12 +106,35 @@ class Dictionary:
             raise KeyError
 
     def clear(self):
-        for el in range(len(self.storage)):
-            self.storage[el] = []
-        self.length = 0
+        self.storage = [[] for _ in range(self.initial_capacity)]
 
-    def update(self, upd_lst):
-        try:
-            return self.__setitem__(upd_lst[0], upd_lst[1])
-        except KeyError:
-            raise KeyError
+    def __contains__(self, key):
+        hashed_value = hash(key)
+        index = hashed_value % self.initial_capacity
+
+        if hashed_value > self.initial_capacity:
+            return False
+
+        if key == self.storage[index][0] and\
+                hashed_value == self.storage[index][2]:
+            return True
+        else:
+            counter = 0
+            while counter <= self.initial_capacity:
+                index += 1
+                if index == self.initial_capacity:
+                    index = 0
+                    continue
+                if key == self.storage[index][0] and\
+                        hashed_value == self.storage[index][2]:
+                    return True
+                counter += 1
+        return False
+
+    def __iter__(self):
+        for item in self.storage:
+            if len(item) != 0:
+                yield item
+
+    def update(self, upd_data):
+        return self.__setitem__(upd_data[0], upd_data[1])
