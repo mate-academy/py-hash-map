35214d0fd990d7f853aa869351d914a39ed5eeaf
Denys Lipin <lipin.transstream@gmail.com>
2022-02-08 19:40:59 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,2 +1,48 @@
 class Dictionary:
-    pass
+    def __init__(self):
+        self.capacity = 0
+        self.my_dict_length = 8
+        self.my_dict = [[] for _ in range(self.my_dict_length)]
+
+    def __setitem__(self, key, value):
+        if self.capacity == int(self.my_dict_length * 2 / 3):
+            self.resize()
+        hash_ = hash(key)
+        index = hash_ % self.my_dict_length
+        if not self.my_dict[index]:
+            self.my_dict[index] = [hash_, key, value]
+            self.capacity += 1
+        elif self.my_dict[index][1] == key:
+            self.my_dict[index][2] = value
+        else:
+            index = (index + 1) % self.my_dict_length
+            while True:
+                if not self.my_dict[index]:
+                    self.my_dict[index] = [hash_, key, value]
+                    self.capacity += 1
+                    break
+                if self.my_dict[index][1] == key:
+                    self.my_dict[index][2] = value
+                    break
+                index = (index + 1) % self.my_dict_length
+
+    def __getitem__(self, key):
+        hash_ = hash(key)
+        index = hash_ % self.my_dict_length
+        for _ in range(self.my_dict_length):
+            if self.my_dict[index][1] == key:
+                return self.my_dict[index][2]
+            index = (index + 1) % self.my_dict_length
+        else:
+            raise KeyError(f"Key: {key} not in dictionary!")
+
+    def __len__(self):
+        return self.capacity
+
+    def resize(self):
+        self.capacity = 0
+        self.my_dict_length *= 2
+        old_dict = [item for item in self.my_dict if item]
+        self.my_dict = [[] for _ in range(self.my_dict_length)]
+        for (hash_, key, value) in old_dict:
+            self.__setitem__(key, value)
