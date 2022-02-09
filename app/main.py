b994e6961f8df051dc6ab9ed453d44e1d5eb567f
Zikkolion <nmadaraosnova835@gmail.com>
2022-02-09 11:38:37 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,2 +1,56 @@
+import math
+
+
 class Dictionary:
-    pass
+    RESIZE = 2
+    LOAD_FACTOR = 2 / 3
+
+    def __init__(self, capacity=8):
+        self.capacity = capacity
+        self.ls = [None for _ in range(self.capacity)]
+        self.len = 0
+        self.filled = 0
+
+    def __setitem__(self, key, value):
+        if self.filled == math.ceil(self.capacity * self.LOAD_FACTOR):
+            self.resize()
+
+        hash_value = hash(key)
+        index = hash_value % self.capacity
+
+        if self.ls[index] is None:
+            self.ls[index] = [key, hash_value, value]
+            self.filled += 1
+        else:
+            if key == self.ls[index][0]:
+                self.ls[index][2] = value
+                return
+            while True:
+                index += 1
+                index %= self.capacity
+                if self.ls[index] is not None:
+                    self.filled += 1
+                    self.ls[index] = [key, hash_value, value]
+                    break
+
+    def __getitem__(self, key):
+        hash_value = hash(key)
+        index = hash_value % self.capacity
+        while self.ls[index] is not None:
+            cell_key, hash_key, value = self.ls[index]
+            if cell_key == key:
+                return value
+            index += 1
+        raise KeyError
+
+    def resize(self):
+        copy_ls = self.ls[:]
+        self.filled = 0
+        self.capacity *= self.RESIZE
+        self.ls = [None for _ in range(self.capacity)]
+        for item in copy_ls:
+            if item is not None:
+                self.__setitem__(item[0], item[2])
+
+    def __len__(self):
+        return self.filled
