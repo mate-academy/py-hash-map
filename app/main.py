ea8d8495e73ac542b9ab6c676ab1fab74c84a1b5
Ihor Olifirenko <igoroliphirenko@gmail.com>
2022-02-09 14:43:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,55 @@
 class Dictionary:
-    pass
+
+    INITIAL_CAPACITY = 8
+    LOAD_FACTOR = 2 / 3
+    RESIZE_FACTOR = 2
+
+    def __init__(self):
+        self.elements = [None] * self.INITIAL_CAPACITY
+        self.size = 0
+        self.capacity = self.INITIAL_CAPACITY
+
+    def resize(self):
+        current_elements = [node for node in self.elements if node is not None]
+        self.capacity *= self.RESIZE_FACTOR
+        self.elements = [None] * self.capacity
+        self.size = 0
+        for node in current_elements:
+            self.__setitem__(node[0], node[1])
+
+    def __setitem__(self, key, value):
+        hash_ = hash(key)
+        index = hash_ % len(self.elements)
+        node = [key, value, hash_]
+
+        while self.elements[index] is not None:
+            if self.elements[index][0] == key \
+                    and self.elements[index][2] == hash_:
+                self.elements[index][1] = value
+                return
+            index = (index + 1) % len(self.elements)
+
+        if (self.size + 1) > self.capacity * self.LOAD_FACTOR:
+            self.resize()
+
+        self.elements[index] = node
+        self.size += 1
+
+    def __getitem__(self, item):
+        hash_ = hash(item)
+        index = hash_ % len(self.elements)
+
+        while self.elements[index] is not None:
+            if self.elements[index][2] == hash_ \
+                    and self.elements[index][0] == item:
+                return self.elements[index][1]
+
+            index = (index + 1) % len(self.elements)
+
+        raise KeyError(f"Key {item} does not exist")
+
+    def __repr__(self):
+        return str(self.elements)
+
+    def __len__(self):
+        return self.size
