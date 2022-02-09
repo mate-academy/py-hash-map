5a81217cc4f42634510f743e54ccb627746cad8d
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-09 19:05:26 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -53,8 +53,8 @@ class Dictionary:
                 return
             index = (index + 1) % self._capacity
 
-        if (self._size + 1) > Dictionary._LOAD_FACTOR * self._capacity:
-            self._resize()
-
         self._hashtable[index] = Node(hash_, key, value)
         self._size += 1
+
+        if (self._size + 1) > Dictionary._LOAD_FACTOR * self._capacity:
+            self._resize()
