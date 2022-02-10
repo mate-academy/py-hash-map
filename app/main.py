68da53b72dfb66e0aff8f48d090e02a1f845f90e
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-10 16:24:50 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -15,7 +15,9 @@ class Dictionary:
     _RESIZE_MULT_COEF = 2
 
     def __init__(self):
-        self._table: List[Optional[Node]] = [None for _ in range(Dictionary._DEFAULT_lENGHT)]
+        self._table: \
+            List[Optional[Node]] = \
+            [None for _ in range(Dictionary._DEFAULT_lENGHT)]
         self._size = 0
         self._capacity = Dictionary._DEFAULT_lENGHT
 
@@ -65,28 +67,3 @@ class Dictionary:
 
     def __len__(self):
         return self._size
-
-
-if __name__ == "__main__":
-    d = Dictionary()
-    c = Dictionary()
-    # print(d.__dict__)
-    # d.__setitem__(4, 6)
-    # print(d.__getitem__(4))
-    # print(d.__dict__)
-    d.__setitem__(c, 57)
-    print(d.__getitem__(c))
-    d.__setitem__("d", 5)
-    d.__setitem__(2, "llkj")
-    d.__setitem__("d", 6667)
-    d.__setitem__("Element 682", 578)
-    print(d.__getitem__("Element 682"))
-    d.__setitem__("dd", 5787)
-    d.__setitem__(78, "llkoo")
-    d.__setitem__("d-sdsjs", 66674)
-    d.__setitem__(43434, 5)
-    d.__setitem__("ll8", "llkooi")
-    d.__setitem__("d-sjsss", 66679)
-    print(d.__dict__)
-    print(len(d))
-    print(hash('Element 682'))
