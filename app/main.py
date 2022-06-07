90194bbc6284f6ec5c590466120920d57594af56
Marko Khodan <khodanmarko@gmail.com>
2022-06-07 17:48:46 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class Dictionary:
 
     def __init__(self):
         self.container = [None] * self.INITIAL_CAPACITY
-        self.cells = 0
+        self.elements = 0
         self.length = len(self.container)
 
     def resize(self):
@@ -19,7 +19,7 @@ class Dictionary:
         self.INITIAL_CAPACITY *= self.RESIZE
         self.container = [None] * self.INITIAL_CAPACITY
         self.length *= 2
-        self.cells = 0
+        self.elements = 0
         for cell in old_container:
             if cell:
                 self.__setitem__(cell.key, cell.value)
@@ -28,14 +28,15 @@ class Dictionary:
         key_hash = hash(key)
         index = key_hash % self.length
 
-        if self.cells == round(self.length * self.LOAD_FACTOR):
+        if self.elements == round(self.length * self.LOAD_FACTOR):
             self.resize()
 
         if not self.container[index]:
             self.container[index] = self.Node(key, value, key_hash)
-            self.cells += 1
+            self.elements += 1
 
-        elif self.container[index].key == key:
+        elif self.container[index].key_hash == key_hash and \
+                self.container[index].key == key:
             self.container[index].value = value
 
         else:
@@ -43,7 +44,7 @@ class Dictionary:
             while True:
                 if not self.container[index]:
                     self.container[index] = self.Node(key, value, key_hash)
-                    self.cells += 1
+                    self.elements += 1
                     break
                 elif self.container[index].key == key:
                     self.container[index].value = value
@@ -54,10 +55,11 @@ class Dictionary:
         key_hash = hash(key)
         index = key_hash % self.length
         while self.container[index] is not None:
-            if self.container[index].key == key:
+            if self.container[index].key_hash == key_hash \
+                    and self.container[index].key == key:
                 return self.container[index].value
             index = (index + 1) % self.length
         raise KeyError
 
     def __len__(self):
-        return self.cells
+        return self.elements
