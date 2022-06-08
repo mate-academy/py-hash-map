89174852596418c6e569c9fd154d208449ca5b2a
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-06-08 21:38:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -7,10 +7,17 @@ class Dictionary:
         self.table[hash(key) % self.size] = [hash(key), key, value]
 
     def __getitem__(self, key):
-        if self.table[hash(key) % self.size][2]:
+        if self.table[hash(key) % self.size] is not None:
             return self.table[hash(key) % self.size][2]
         else:
             raise KeyError
 
     def __len__(self):
         return self.table
+
+
+a = Dictionary()
+
+for i in range(1000):
+    a.__setitem__(str(i), i)
+print(a.__getitem__('564'))
