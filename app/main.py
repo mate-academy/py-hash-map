d5eda1be664049c48d82b1327f37d85e08b1020b
Serhii Leonenko <leonenko3149@gmail.com>
2022-06-08 17:35:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,111 @@
 class Dictionary:
-    pass
+
+    _DEFAULT_CAPACITY = 8
+    _PERMITTED_VOLUME = 2 / 3
+
+    def __init__(self):
+        self._capacity = self._DEFAULT_CAPACITY
+        self._size = 0
+        self._array = [None] * self._capacity
+
+    def __setitem__(self, key, value):
+        index = self._index(key)
+
+        while self._array[index] is not None:
+            current_key = self._array[index][0]
+            if key == current_key:
+                self._array[index] = (current_key, value)
+                return
+            index += 1
+
+        self._array[index] = (key, value)
+        self._size += 1
+
+        if self._size >= self._capacity * self._PERMITTED_VOLUME:
+            self._resize()
+
+    def __getitem__(self, search_key):
+        index = self._index(search_key)
+
+        while self._array[index] is not None:
+            key, value = self._array[index]
+            if key == search_key:
+                return value
+            index += 1
+
+        raise KeyError
+
+    def __len__(self):
+        return self._size
+
+    def _resize(self):
+        included_elements = [
+            element
+            for element in self._array
+            if element
+        ]
+        self._capacity *= 2
+        self._size = 0
+        self._array = [None] * self._capacity
+
+        for key, value in included_elements:
+            self.__setitem__(key=key, value=value)
+
+    def _index(self, key):
+        return hash(key) % self._capacity
+
+    def __repr__(self):
+        return "{" + ", ".join(
+            f"{str(element[0])}: {str(element[1])}"
+            for element in self._array
+            if element is not None
+        ) + "}"
+
+    def clear(self):
+        self._capacity = self._DEFAULT_CAPACITY
+        self._size = 0
+        self._array = [None] * self._capacity
+
+    def __delitem__(self, del_key):
+        self.pop(del_key)
+        return
+
+    def pop(self, pop_key):
+        index = self._index(pop_key)
+
+        while self._array[index] is not None:
+            key, value = self._array[index]
+            if key == pop_key:
+                self._array[index] = None
+                return value
+            index += 1
+
+        raise KeyError
+
+    def __iter__(self):
+        self.current_position = 0
+        self._non_empty_arr = [
+            element for element
+            in self._array
+            if element is not None
+        ]
+        print(self._non_empty_arr)
+        return self
+
+    def __next__(self):
+        if self.current_position >= len(self._non_empty_arr):
+            raise StopIteration
+
+        return_value = self._non_empty_arr[self.current_position]
+        self.current_position += 1
+
+        return return_value
+
+    def update(self, other):
+        if not isinstance(other, Dictionary):
+            raise ValueError
+
+        for element in other._array:
+            if element is not None:
+                key, value = element
+                self.__setitem__(key, value)
--- app/point.py
+++ app/point.py
@@ -1,3 +1,6 @@
+from app.main import Dictionary
+
+
 class Point:
     def __init__(self, x, y):
         self._x = x
@@ -14,7 +17,7 @@ class Point:
         # which is NOT a best practice, but you will be able to predict
         # a hash value by coordinates of the point and its index
         # in the hashtable as well
-        return hash((self.x, self.y))
+        return self.x + self.y
 
     @property
     def x(self):
@@ -23,3 +26,43 @@ class Point:
     @property
     def y(self):
         return self._y
+
+
+if __name__ == "__main__":
+    d = Dictionary()
+    print(d)
+
+    a = Point(2, 3)
+    b = Point(3, 2)
+
+    d[a] = "Point(2, 3)"
+
+    d[b] = "Point(3, 2)"
+
+    for el in d:
+        print(el)
+
+    print(d)
+    print("key:", a, "value:", d[a])
+
+    d[a] = 123
+
+    print(d)
+    print("key:", a, "value:", d[a])
+    print("id:", id(d))
+
+    d1 = Dictionary()
+
+    d1[a] = 222
+    d1[b] = 333
+    d1["c"] = 444
+
+    d.update(d1)
+    print("after update", d)
+
+    del d[a]
+    print("after del", d)
+
+    d.clear()
+    print(d)
+    print("id:", id(d))
