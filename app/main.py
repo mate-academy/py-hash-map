e8d87f25ecf80e3418e461781d93ef6e123b78eb
Taras Nester <tarasnester414@gmail.com>
2022-06-08 13:39:30 +0300

    refactoring del method, added check hash in first

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-from app.point import Point
+class IsDeleted:
+    def __str__(self):
+        return "deleted"
 
 
 class Dictionary:
     _BASE_CAPACITY = 8
     _LOAD_FACTOR = 2 / 3
     _RESIZE_FACTOR = 2
+    _Is_Deleted = IsDeleted()
 
     def clear(self):
         self._filled_slots = 0
@@ -33,43 +36,44 @@ class Dictionary:
             try:
                 iterable.keys()
             except AttributeError:
-                for k in iterable:
-                    self[k] = iterable[k]
+                for key in iterable:
+                    self[key] = iterable[key]
             else:
-                for k, v in iterable.items():
-                    self[k] = v
+                for key, value in iterable.items():
+                    self[key] = value
         else:
-            for k in kwargs:
-                self[k] = kwargs[k]
+            for key in kwargs:
+                self[key] = kwargs[key]
 
     def _create_hash_table(self):
-        self._hash_table = [
-            [None, None]
-            for i in range(self._capacity)
-        ]
+        self._hash_table = [None for _ in range(self._capacity)]
 
     def _resize_hash_table(self):
         self._capacity *= self._RESIZE_FACTOR
         old_table = self._hash_table
         self.clear()
-        for key, value in old_table:
-            if key is not None:
-                self.__setitem__(key, value)
+        for item in old_table:
+            if item is not None and item != self._Is_Deleted:
+                self.__setitem__(item[1], item[2])
 
     def __delitem__(self, key):
         if self[key]:
-            hash_index = hash(key) % self._capacity
-            self._hash_table[hash_index] = [None, None]
+            index = hash(key) % self._capacity
+            self._hash_table[index] = self._Is_Deleted
             self._filled_slots -= 1
         else:
             raise KeyError("Key not found")
 
     def __getitem__(self, item):
         index = hash(item) % self._capacity
-        while self._hash_table[index][1] is not None:
-            key, value = self._hash_table[index]
-            if key == item:
-                return value
+        while self._hash_table[index] is not None:
+            if item == self._Is_Deleted:
+                index += 1
+                continue
+            hash_, key, value = self._hash_table[index]
+            if hash_ == hash(item):
+                if key == item:
+                    return value
             index += 1
             if index >= self._capacity - 1:
                 index = 0
@@ -82,8 +86,7 @@ class Dictionary:
 
     def __iter__(self):
         self._returned_iter_count = 0
-        self._current_iter_index = 0
-
+        self._iter_index = 0
         return self
 
     def __len__(self):
@@ -91,48 +94,50 @@ class Dictionary:
 
     def __next__(self):
         if self._returned_iter_count < self._filled_slots:
-            while self._hash_table[self._current_iter_index][0] is None:
-                self._current_iter_index += 1
-            value = self._hash_table[self._current_iter_index][0]
+            while True:
+                item = self._hash_table[self._iter_index]
+                if item is None or item == self._Is_Deleted:
+                    self._iter_index += 1
+                else:
+                    break
+            value = self._hash_table[self._iter_index][1]
             self._returned_iter_count += 1
-            self._current_iter_index += 1
+            self._iter_index += 1
             return value
         else:
             raise StopIteration
 
     def __repr__(self):
         result = []
-        num = 0
         for item in self._hash_table:
-            line = f"({num} {item[0]} {item[1]})"
-            result.append(line)
-            num += 1
+            result.append(str(item))
         res_str = ", ".join(result)
         return "{" + res_str + "}"
 
     def __setitem__(self, key, value):
         index = hash(key) % self._capacity
 
-        # find index of next free slot
-        while self._hash_table[index][0] is not None:
-            if self._hash_table[index][0] == key:
-                self._filled_slots += 1
+        while self._hash_table[index] is not None:
+            if self._hash_table[index] == self._Is_Deleted:
                 break
+            if self._hash_table[index][0] == hash(key):
+                if self._hash_table[index][1] == key:
+                    break
             index += 1
             if index >= self._capacity - 1:
                 index = 0
+        else:
+            self._filled_slots += 1
 
-        self._hash_table[index][0] = key
-        self._hash_table[index][1] = value
-        self._filled_slots += 1
+        self._hash_table[index] = (hash(key), key, value)
         if self._filled_slots > self._capacity * self._LOAD_FACTOR:
             self._resize_hash_table()
 
     def __str__(self):
         result = []
-
-        for key, value in self._hash_table:
-            if key is not None:
+        for item in self._hash_table:
+            if item is not None and item != self._Is_Deleted:
+                _, key, value = item
                 if isinstance(key, str):
                     key = f"'{key}'"
                 if isinstance(value, str):
@@ -142,12 +147,3 @@ class Dictionary:
 
         result_str = ", ".join(result)
         return "{" + result_str + "}"
-
-
-items = [("one", 1), ("two", 22), (145, -1), (Point(1, 1), "A")]
-dictionary = Dictionary()
-for key, value in items:
-    dictionary[key] = value
-print(repr(dictionary))
-for key, value in items:
-    print(dictionary[key], value)
