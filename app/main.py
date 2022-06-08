63a4d3bfb993270d633ac073cb220c9174238ace
Dima Svirsa <august4smile@gmail.com>
2022-06-08 18:02:07 +0300

    better names, KeyError check added

--- app/main.py
+++ app/main.py
@@ -2,38 +2,41 @@ class Dictionary:
     def __init__(self):
         self._capacity = 8
         self._storage = [None for _ in range(self._capacity)]
-        self._flow = 0
+        self._current_load = 0
 
     def __setitem__(self, key_, value_):
         key_hash = hash(key_)
         index = key_hash % self._capacity
         while self._storage[index] is not None:
             if self._storage[index][0] == key_hash:
-                self._flow -= 1
+                self._current_load -= 1
                 break
             else:
                 index = (index + 1) % self._capacity
         self._storage[index] = [key_hash, value_, key_]
-        self._flow += 1
-        if self._flow > self._capacity * 2 / 3:
-            self._refactor()
+        self._current_load += 1
+        if self._current_load > self._capacity * 2 / 3:
+            self._extend_capacity()
 
     def __getitem__(self, key_):
         key_hash = hash(key_)
         index = key_hash % self._capacity
+        check_index = key_hash % self._capacity
         while self._storage[index] is not None:
             if self._storage[index][0] == key_hash:
                 return self._storage[index][1]
             index = (index + 1) % self._capacity
+            if check_index == index:
+                raise KeyError(f"Key: {key_} not in dictionary!")
 
     def __len__(self):
-        return self._flow
+        return self._current_load
 
-    def _refactor(self):
+    def _extend_capacity(self):
         self._capacity *= 2
         self.old_storage = self._storage
         self._storage = [None for _ in range(self._capacity)]
-        self._flow = 0
+        self._current_load = 0
         for element in self.old_storage:
             if element is not None:
                 self.__setitem__(element[2], element[1])
