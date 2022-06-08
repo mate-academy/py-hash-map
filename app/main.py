e236b470a344494fb9dfe7629e1de86915328060
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-06-08 20:28:03 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,29 +4,26 @@ class Dictionary:
         self.size = 8
         self.table = [None for _ in range(self.size)]
 
-    def __setitem__(self, key, value, hsh=None):
-        if hsh is None:
-            hsh = hash(key)
-        i = hsh % self.size
-        while True:
-            if not self.table[i]:
-                self.table[i] = [hsh, key, value]
-                self.volume += 1
-                break
+    def __setitem__(self, key, value):
+        i = hash(key) % self.size
+        if self.table[i] is None:
+            self.table[i] = [hash(key), key, value]
+            self.volume += 1
+        else:
             if self.table[i][1] == key:
                 self.table[i][2] = value
-                break
-            i = (i + 1) % self.size
         if self.volume > int(self.size * 2 / 3):
             self.resize()
 
     def __getitem__(self, key):
         i = hash(key) % self.size
         for _ in range(self.size):
-            if self.table[i][1] == key:
-                return self.table[i][2]
-            i = (i + 1) % self.size
-        raise KeyError
+            if self.table[i] is not None:
+                if self.table[i][1] == key:
+                    return self.table[i][2]
+                i = (i + 1) % self.size
+            else:
+                raise KeyError('Not found')
 
     def resize(self):
         self.volume = 0
@@ -34,7 +31,8 @@ class Dictionary:
         prev_dict = [cell for cell in self.table if cell]
         self.table = [None for _ in range(self.size)]
         for hsh, key, value in prev_dict:
-            self.__setitem__(key, value, hsh)
+            i = hsh % self.size
+            self.table[i] = [hsh, key, value]
 
     def __len__(self):
         return self.volume
