da527d751825d1b4092015b6a643f76134efde1c
bohdan <bodyan1108@gmail.com>
2022-06-09 15:19:52 +0300

    Updated solution with Danny's advice

--- app/main.py
+++ app/main.py
@@ -2,79 +2,55 @@ import math
 
 
 class Dictionary:
+    THRESHOLD = 8
+    LOAD_FACTOR = 2 / 3
+    RESIZE_COEFFICIENT = 2
+
     def __init__(self):
-        self.__storage = [None] * 8
-        self.__capacity = 8
-        self.__load_factor = 2 / 3
-        self.__key_index = []
-        self.__length = 0
+        self.capacity = Dictionary.THRESHOLD
+        self.length = 0
+        self.hash_table: list[tuple | None] = [None] * 8
 
     def __setitem__(self, key, value):
-        for element in self.__key_index:
-            if element[0] == key:
-                for index, item in enumerate(self.__storage):
-                    if element[1] == index:
-                        self.__storage[index] = value
-                        return
+        hash_index = hash(key) % self.capacity
 
-        count = 0
-        for char in self.__storage:
-            if char:
-                count += 1
+        while self.hash_table[hash_index] is not None:
+            if self.hash_table[hash_index][0] == key:
+                self.hash_table[hash_index] = (key, value)
+                return
 
-        if count == math.floor(self.__capacity * self.__load_factor):
+            hash_index = (hash_index + 1) % self.capacity
+
+        self.length += 1
+        if self.length > math.floor(self.capacity * Dictionary.LOAD_FACTOR):
             self.__resize_dict()
+            self.__setitem__(key=key, value=value)
 
-        self.__add_to_dict(key, value)
+        self.hash_table[hash_index] = (key, value)
 
     def __resize_dict(self):
-        self.__length = 0
-        values = self.__storage
-        self.__capacity *= 2
-        self.__storage = [None] * self.__capacity
+        self.length = 0
+        self.capacity *= Dictionary.RESIZE_COEFFICIENT
+        elements = [el for el in self.hash_table if el is not None]
+        self.hash_table = [None] * self.capacity
 
-        for index, value in enumerate(values):
-            if value is not None:
-                for item_link in self.__key_index:
-                    if index == item_link[1]:
-                        self.__add_to_dict(item_link[0], value)
-                        self.__key_index.remove(item_link)
-                        break
-
-    def __add_to_dict(self, key, value):
-        index = hash(key) % self.__capacity
-
-        while True:
-            if index >= self.__capacity:
-                index = 0
-
-            if self.__storage[index] is None:
-                self.__storage[index] = value
-                self.__key_index.append((key, index))
-                self.__length += 1
-                break
-            index += 1
+        for el in elements:
+            self.__setitem__(key=el[0], value=el[1])
 
     def __getitem__(self, key):
-        index = 0
-        flag = False
-        for elem in self.__key_index:
-            if key in elem:
-                index = elem[1]
-                flag = True
-                break
-        if flag:
-            return self.__storage[index]
+        hash_index = hash(key) % self.capacity
+
+        while self.hash_table[hash_index] is not None:
+            if self.hash_table[hash_index][0] == key:
+                return self.hash_table[hash_index][1]
+
+            hash_index = (hash_index + 1) % self.capacity
+
         raise KeyError("No keys!")
 
     def __len__(self):
-        return self.__length
+        return self.length
 
     def __repr__(self):
-        result = []
-
-        for index in range(len(self.__key_index)):
-            result.append((self.__key_index[index][0],
-                           self.__storage[self.__key_index[index][1]]))
-
+        result = {el for el in self.hash_table if el is not None}
         return f"{result}"
