b0126100a2370ab0b02ad2c9c6ffc62541aecb76
Flashmobber <ufinaid@gmail.com>
2022-06-09 14:13:10 +0300

    Some improvements

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ class Dictionary:
     def __init__(self):
         self.__capacity = 8
         self.__threshold = 2 / 3
-        self.__resize_step = 2
+        self.__resize_step = 3
         self.__cells = 0
-        self.__hash_table = [None for _ in range(self.__capacity)]
+        self.__hash_table = [None] * self.__capacity
 
     def __setitem__(self, key, value):
         if self.__cells > self.__capacity * self.__threshold:
@@ -17,33 +17,28 @@ class Dictionary:
             if self.__hash_table[index][2] == cell_hash:
                 if self.__hash_table[index][0] == key:
                     break
-            index += 1
-            if index >= self.__capacity - 1:
-                index = 0
+            index = (index + 1) % self.__capacity
         else:
             self.__cells += 1
 
         self.__hash_table[index] = (key, value, cell_hash)
 
-    def __getitem__(self, key_):
-        cell_hash = hash(key_)
+    def __getitem__(self, key):
+        cell_hash = hash(key)
         index = cell_hash % self.__capacity
         while self.__hash_table[index] is not None:
-            key, value, c_hash = self.__hash_table[index]
-            if c_hash == cell_hash:
-                if key == key_:
-                    return value
-            index += 1
-            if index >= self.__capacity - 1:
-                index = 0
-        raise KeyError(f"Key: {key_} not found.")
+            if self.__hash_table[index][2] == cell_hash:
+                if self.__hash_table[index][0] == key:
+                    return self.__hash_table[index][1]
+            index = (index + 1) % self.__capacity
+        raise KeyError(f"Key: {key} not found.")
 
     def __len__(self):
         return self.__cells
 
     def clear(self):
         self.__cells = 0
-        self.__hash_table = [None for _ in range(self.__capacity)]
+        self.__hash_table = [None] * self.__capacity
 
     def __delitem__(self, key):
         if self[key]:
