ec325d56829dae403f1d040bb87d8175f70261ad
Artem Toporchan <gydratvar@gmail.com>
2022-06-19 12:36:39 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -10,24 +10,17 @@ class Dictionary:
         hash_f = hash(key)
         index = hash_f % self._size
 
-        if not self._hash_table[index]:
-            self._hash_table[index] = [key, hash_f, value]
-            self._elements_num += 1
-        elif self._hash_table[index][0] == key:
-            self._hash_table[index][2] = value
-        else:
+        while True:
+            if not self._hash_table[index]:
+                self._hash_table[index] = [key, hash_f, value]
+                self._elements_num += 1
+
+                break
+            elif self._hash_table[index][0] == key:
+                self._hash_table[index][2] = value
+
+                break
             index = (index + 1) % self._size
-            while True:
-                if not self._hash_table[index]:
-                    self._hash_table[index] = [key, hash_f, value]
-                    self._elements_num += 1
-
-                    break
-                elif self._hash_table[index][0] == key:
-                    self._hash_table[index][2] = value
-
-                    break
-                index = (index + 1) % self._size
 
         if self._elements_num == int(self._size * self.threshold):
             self.resize()
@@ -47,13 +40,12 @@ class Dictionary:
         hash_f = hash(key)
         index = hash_f % self._size
 
-        while self._hash_table[index] is not None:
-            try:
-                if self._hash_table[index][0] == key:
-                    return self._hash_table[index][2]
-                index = (index + 1) % self._size
-            except IndexError:
-                raise KeyError(f"No key {key} in dictionary")
+        while self._hash_table[index]:
+            if self._hash_table[index][0] == key:
+                return self._hash_table[index][2]
+            index = (index + 1) % self._size
+
+        raise KeyError(f"No key {key} in dictionary")
 
     def __len__(self):
         return self._elements_num
