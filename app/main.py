0f340b5c1c297ae722f2a965629f52a3c35f9a29
Anton Lasko <anton.lasko1993@gmail.com>
2022-06-20 15:02:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,26 @@
 class Dictionary:
-    pass
+
+    def __init__(self, init=None):
+        if init is not None:
+            self.__dict__.update(init)
+
+    def __setitem__(self, key, item):
+        self.__dict__[key] = item
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def clear(self):
+        return self.__dict__.clear()
+
+    def __delitem__(self, key):
+        del self.__dict__[key]
+
+    def pop(self, *args):
+        return self.__dict__.pop(*args)
+
+    def __iter__(self):
+        return iter(self.__dict__)
