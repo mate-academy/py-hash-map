70b8ffdd1c1f11df726f2039490f76b75b42ab98
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-07-06 18:04:45 +0300

    'Fix'

--- app/main.py
+++ app/main.py
@@ -1,58 +1,43 @@
 class Dictionary:
     def __init__(self):
-        self.capacity = 0
-        self.dict_length = 8
-        self.my_dict = [None for _ in range(self.dict_length)]
+        self.capacity = 8
+        self.length = 0
+        self.hash_table = [[] for _ in range(self.capacity)]
 
-    def __setitem__(self, key, value, hash_=None):
-        if hash_ is None:
-            hash_ = hash(key)
-        index = hash_ % self.dict_length
-        while True:
-            if not self.my_dict[index]:
-                self.my_dict[index] = [hash_, key, value]
-                self.capacity += 1
+    def __setitem__(self, key, value):
+        key_hash = hash(key)
+        index = key_hash % self.capacity
+        while self.hash_table[index]:
+            if self.hash_table[index][0] == key_hash and\
+                    self.hash_table[index][1] == key:
+                self.hash_table[index][2] = value
+                self.length -= 1
                 break
-            if self.my_dict[index][1] == key:
-                self.my_dict[index][2] = value
-                break
-            index = (index + 1) % self.dict_length
-        if self.capacity > int(self.dict_length * 2 / 3):
+            else:
+                index = (index + 1) % self.capacity
+        self.hash_table[index] = [key_hash, key, value]
+        self.length += 1
+        if int(self.capacity * (2 / 3)) < self.length:
             self.resize()
 
     def __getitem__(self, key):
-        hash_ = hash(key)
-        index = hash_ % self.dict_length
-        for _ in range(self.dict_length):
-            if self.my_dict[index][1] == key:
-                return self.my_dict[index][2]
-            index = (index + 1) % self.dict_length
-        else:
-            raise KeyError(f"Key: {key} not in dictionary!")
+        key_hash = hash(key)
+        index = key_hash % self.capacity
+        while self.hash_table[index]:
+            if self.hash_table[index][0] == key_hash and \
+                    self.hash_table[index][1] == key:
+                return self.hash_table[index][2]
+            index = (index + 1) % self.capacity
+        raise KeyError
 
     def __len__(self):
-        return self.capacity
+        return self.length
 
     def resize(self):
-        self.capacity = 0
-        self.dict_length *= 2
-        old_dict = [item for item in self.my_dict if item]
-        self.my_dict = [None for _ in range(self.dict_length)]
-        for hash_, key, value in old_dict:
-            self.__setitem__(key, value, hash_)
-
-    def clear(self):
-        self.capacity = 0
-        self.my_dict = [None for _ in range(self.dict_length)]
-
-    def __delitem__(self, key):
-        hash_ = hash(key)
-        index = hash_ % self.dict_length
-        for _ in range(self.dict_length):
-            if self.my_dict[index][1] == key:
-                self.my_dict[index] = None
-                self.capacity -= 1
-                break
-            index = (index + 1) % self.dict_length
-        else:
-            raise KeyError(f"Key: {key} not in dictionary!")
+        self.capacity *= 2
+        old_hash_table = self.hash_table
+        self.hash_table = [[] for _ in range(self.capacity)]
+        self.length = 0
+        for element in old_hash_table:
+            if element:
+                self.__setitem__(element[1], element[2])
