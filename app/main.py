9d1718c760b35637b7778406ec0ae26fba8715f1
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-07-08 22:20:09 +0300

    One more update

--- app/main.py
+++ app/main.py
@@ -5,60 +5,47 @@ class Dictionary:
 
     def __setitem__(self, key, value):
         self._resize()
-        index = hash(key) % self.capacity
+        hash_ = hash(key)
+        index = hash_ % self.capacity
         while self.hash_table[index]:
-            if self.hash_table[index][0] == hash(key) or \
-                    self.hash_table[index][0] == key:
-                self.hash_table[index] = [hash(key), key, value]
+            if self.hash_table[index][0] == hash_ and \
+                    self.hash_table[index][1] == key:
+
+                self.hash_table[index] = [hash_, key, value]
                 break
-            else:
-                index = (index + 1) % self.capacity
-        else:
-            self.hash_table[index] = [hash(key), key, value]
+
+            index = (index + 1) % self.capacity
+
+        self.hash_table[index] = [hash_, key, value]
 
     def __getitem__(self, key):
-        index = hash(key) % self.capacity
+        hash_ = hash(key)
+        index = hash_ % self.capacity
         while self.hash_table[index]:
             if self.hash_table[index][1] == key and\
-                    self.hash_table[index][0] == hash(key):
+                    self.hash_table[index][0] == hash_:
                 return self.hash_table[index][2]
-            else:
-                index = (index + 1) % self.capacity
-        else:
-            raise KeyError
+            index = (index + 1) % self.capacity
+        raise KeyError
 
     def __len__(self):
         temp_ls = []
-        for i in self.hash_table:
-            if i:
-                temp_ls.append(i)
+        for item in self.hash_table:
+            if item:
+                temp_ls.append(item)
         return len(temp_ls)
 
     def _resize(self):
         size_to_resize = round((self.capacity / 3) * 2)
         if len(self) >= size_to_resize:
             temp_list = []
+
             for item in self.hash_table:
                 if item:
                     temp_list.append(item)
+
             self.capacity *= 2
             self.hash_table = [[] for _ in range(self.capacity)]
+
             for el in temp_list:
                 self[el[1]] = el[2]
-
-
-if __name__ == '__main__':
-    dct = Dictionary()
-    dct[1] = 11
-    dct[2] = 12
-    dct[3] = 13
-    dct[4] = 14
-    dct[5] = 15
-    dct[5] = 555
-    # dct[6] = 16
-    # dct[7] = 17
-    # dct[8] = 18
-    # dct[9] = 19
-    print(dct.hash_table)
-
-
