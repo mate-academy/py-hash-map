9032b90a86e74fe8af3d78c7cab8ad1f3015c87b
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-07-11 23:09:41 +0100

    Solution_fixed3

--- app/main.py
+++ app/main.py
@@ -8,32 +8,35 @@ class Dictionary:
     def __setitem__(self, key, value):
         if self.length >= 2 / 3 * self.capacity:
             self.resize()
-        index = hash(key) % self.capacity
+        hash_ = hash(key)
+        index = hash_ % self.capacity
         while self.storage[index]:
             if self.storage[index][0] == key \
-                    and self.storage[index][1] == hash(key):
+                    and self.storage[index][1] == hash_:
                 self.storage[index][2] = value
                 return
             index = (index + 1) % self.capacity
-        self.storage[index] = [key, hash(key), value]
+        self.storage[index] = [key, hash_, value]
         self.length += 1
 
     def __len__(self):
         return self.length
 
     def resize(self):
-        existing_elements = [element for element in self.storage if element]
+        existing_elements = self.storage.copy()
         self.storage = [[] for _ in range(self.capacity * 2)]
         self.capacity *= 2
         self.length = 0
         for element in existing_elements:
-            self.__setitem__(element[0], element[2])
+            if element:
+                self.__setitem__(element[0], element[2])
 
     def __getitem__(self, key):
-        index = hash(key) % self.capacity
+        hash_ = hash(key)
+        index = hash_ % self.capacity
         while self.storage[index]:
             if self.storage[index][0] == key \
-                    and self.storage[index][1] == hash(key):
+                    and self.storage[index][1] == hash_:
                 return self.storage[index][2]
             index = (index + 1) % self.capacity
         raise KeyError
