ce1cc1962739eef6f2255d49e6052fae2f73bee0
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-07-11 13:57:26 +0300

    Changed length, deleted useless loop in resize

--- app/main.py
+++ app/main.py
@@ -2,16 +2,18 @@ class Dictionary:
     def __init__(self):
         self.hash_table = [[] for _ in range(8)]
         self.capacity = 8
+        self.length = 0
 
     def __setitem__(self, key, value):
         self._resize()
+        self.length += 1
         hash_ = hash(key)
         index = hash_ % self.capacity
         while self.hash_table[index]:
             if self.hash_table[index][0] == hash_ and \
                     self.hash_table[index][1] == key:
-
                 self.hash_table[index] = [hash_, key, value]
+                self.length -= 1
                 break
 
             index = (index + 1) % self.capacity
@@ -29,23 +31,16 @@ class Dictionary:
         raise KeyError
 
     def __len__(self):
-        temp_ls = []
-        for item in self.hash_table:
-            if item:
-                temp_ls.append(item)
-        return len(temp_ls)
+        return self.length
 
     def _resize(self):
         size_to_resize = round((self.capacity / 3) * 2)
         if len(self) >= size_to_resize:
-            temp_list = []
-
-            for item in self.hash_table:
-                if item:
-                    temp_list.append(item)
-
+            self.length = 0
+            temp_list = self.hash_table.copy()
             self.capacity *= 2
             self.hash_table = [[] for _ in range(self.capacity)]
 
             for el in temp_list:
-                self[el[1]] = el[2]
+                if el:
+                    self[el[1]] = el[2]
