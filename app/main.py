8f1d65275cc8fad640d92c5adc968de89cd054cc
Dmytro Hushchyk <107995555+DHushchyk@users.noreply.github.com>
2022-07-27 14:25:34 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,27 +6,26 @@ class Dictionary:
         self.dict = [[] for _ in range(self.capacity)]
 
     def __setitem__(self, key, value):
-        self.key = key
-        self.value = value
         hash_ = hash(key)
         index_ = hash_ % self.capacity
         while self.dict[index_]:
             if self.dict[index_][0] == key \
-                    and self.dict[index_][2] == hash(key):
-                self.dict[index_] = [self.key, self.value, hash_]
+                    and self.dict[index_][2] == hash_:
+                self.dict[index_] = [key, value, hash_]
                 self.length -= 1
                 break
             index_ = (index_ + 1) % self.capacity
-        self.dict[index_] = [self.key, self.value, hash_]
+        self.dict[index_] = [key, value, hash_]
         self.length += 1
         if self.length == int(self.capacity * (2 / 3)):
             self.resize()
 
     def __getitem__(self, key):
-        index_ = hash(key) % self.capacity
+        hash_ = hash(key)
+        index_ = hash_ % self.capacity
         while self.dict[index_]:
             if self.dict[index_][0] == key\
-                    and self.dict[index_][2] == hash(key):
+                    and self.dict[index_][2] == hash_:
                 return self.dict[index_][1]
             index_ = (index_ + 1) % self.capacity
         raise KeyError
@@ -34,6 +33,15 @@ class Dictionary:
     def resize(self):
         self.capacity *= 2
         all_items = self.dict
+        self.dict = [[] for _ in range(self.capacity)]
+        self.length = 0
+        for item in all_items:
+            if item:
+                self.__setitem__(item[0], item[1])
+
+    def __len__(self):
+        return self.length
+
         self.dict = [[] for _ in range(self.capacity)]
         self.length = 0
         for item in all_items:
