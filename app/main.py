561be0cbfa169db089c91cb0a73da266d09891f8
Roman <coderc770@gmail.com>
2022-07-29 19:07:48 +0300

    Fixed_new

--- app/main.py
+++ app/main.py
@@ -5,53 +5,61 @@ class Dictionary:
         self.hash_table = [[] for _ in range(self.capacity)]
 
     def __setitem__(self, key, value):
+        for element in self.items:
+            if element[0] == key:
+                self.items.remove(element)
+                break
+
         self.items.append([key, value])
+
         threshold = int((self.capacity * 2 / 3))
         size = len(self.items)
+
         if size == threshold:
-            self.capacity *= 2
-            self.hash_table = [[] for _ in range(self.capacity)]
-            for element in self.items:
-                self.hashing(hash(element[0]) % self.capacity,
-                             element[1],
-                             element[0])
-            return
+            self.resize()
 
         hash_ = hash(key) % self.capacity
         self.hashing(hash_, value, key)
 
-    def __getitem__(self, item):
-        n = 0
-        while True:
-            if len(self.hash_table[n]) > 0:
-                if self.hash_table[n][0] == item:
-                    return self.hash_table[n][1]
-            n += 1
-            if n >= len(self.hash_table):
+    def resize(self):
+        self.capacity *= 2
+        self.hash_table = [[] for _ in range(self.capacity)]
+        for element in self.items:
+            self.hashing(hash(element[0]) % self.capacity,
+                         element[1],
+                         element[0])
+        return
+
+    def __getitem__(self, key):
+        index_item = hash(key) % self.capacity
+        if len(self.hash_table[index_item]) == 0:
+            raise KeyError
+
+        while self.hash_table[index_item][0] != key:
+            index_item = (index_item + 1) % self.capacity
+            if index_item >= len(self.hash_table):
                 raise KeyError
+
+        if self.hash_table[index_item][0] == key \
+                and hash(self.hash_table[index_item][0]) == hash(key):
+            return self.hash_table[index_item][1]
 
     def __len__(self):
         return self.items.__len__()
 
     def hashing(self, hash_, value, key_):
-        n = hash_
         while True:
-            if len(self.hash_table[n]) == 0:
-                self.hash_table[n] = [key_, value, hash_]
-                # n += 1
+            if len(self.hash_table[hash_]) == 0:
+                self.hash_table[hash_] = [key_, value, hash_]
                 break
-            elif self.hash_table[n][0] == key_:
-                for element in self.items:
-                    if element[0] == key_:
-                        self.items.remove(element)
-                        break
-                self.hash_table[n] = [key_, value, hash_]
+            elif self.hash_table[hash_][0] == key_:
+                self.hash_table[hash_] = [key_, value, hash_]
                 break
-            if n == self.capacity - 1:
+            if hash_ == self.capacity - 1:
                 for res_index in range(self.capacity):
                     if not self.hash_table[res_index]:
                         self.hash_table[res_index] = [key_, value, hash_]
                         break
-            n += 1
-            if n >= len(self.hash_table):
+            hash_ += 1
+            if hash_ >= len(self.hash_table):
                 break
