a4d2b9c12ce63e1cc8365e588b8947824e8ac925
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-08-06 10:29:56 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,44 +2,57 @@ class Dictionary:
     def __init__(self):
         self.capacity = 8
         self.length = 0
-        self.threshold = int(self.capacity * 2 / 3)
         self.hash_list = [None for _ in range(self.capacity)]
 
-    def __len__(self):
-        return self.length
-
-    def __setitem__(self, key, value):
-        if self.length == self.threshold:
-            self.resize()
-        hash_ = hash(key)
+    def __setitem__(self, key, value, hash_=None):
+        if hash_ is None:
+            hash_ = hash(key)
         index_ = hash_ % self.capacity
         while True:
             if not self.hash_list[index_]:
-                self.hash_list[index_] = [key, hash_, value]
+                self.hash_list[index_] = [hash_, key, value]
                 self.length += 1
                 break
-            if self.hash_list[index_][0] == key and\
-                    self.hash_list[index_][1] == hash_:
+            if self.hash_list[index_][1] == key:
                 self.hash_list[index_][2] = value
                 break
             index_ = (index_ + 1) % self.capacity
-
-    def resize(self):
-        self.capacity *= 2
-        self.threshold = int(self.capacity * 2 / 3)
-        self.length = 0
-        old_list = self.hash_list
-        self.hash_list = [None for _ in range(self.capacity)]
-        for item in old_list:
-            if item:
-                self.__setitem__(item[0], item[2])
+        if self.length > int(self.capacity * 2 / 3):
+            self.resize()
 
     def __getitem__(self, key):
         hash_ = hash(key)
         index_ = hash_ % self.capacity
-        while self.hash_list[index_]:
-            if self.hash_list[index_][1] == hash_ and\
-                    self.hash_list[index_][0] == key:
+        for _ in range(self.capacity):
+            if self.hash_list[index_][1] == key:
                 return self.hash_list[index_][2]
             index_ = (index_ + 1) % self.capacity
+        else:
+            raise KeyError(f"Key: {key} is not in dictionary!")
+
+    def __len__(self):
+        return self.length
+
+    def resize(self):
+        self.capacity *= 2
+        self.length = 0
+        old_list = [item for item in self.hash_list if item]
+        self.hash_list = [None for _ in range(self.capacity)]
+        for hash_, key, value in old_list:
+            self.__setitem__(key, value, hash_)
+
+    def clear(self):
+        self.capacity = 0
+        self.hash_list = [None for _ in range(self.capacity)]
+
+    def __delitem__(self, key):
+        hash_ = hash(key)
+        index_ = hash_ % self.capacity
+        for _ in range(self.capacity):
+            if self.hash_list[index_][1] == key:
+                self.hash_list[index_] = None
+                self.capacity -= 1
+                break
+            index_ = (index_ + 1) % self.capacity
+        else:
             raise KeyError(f"Key: {key} is not in dictionary!")
