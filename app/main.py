8a197d50cc6bde5c57782e375319de5b7c21eed9
Andrii Padalko <a.padalko@gmail.com>
2022-08-10 12:11:09 +0300

    Solution using by while

--- app/main.py
+++ app/main.py
@@ -1,51 +1,59 @@
-import random
-
-
 class Dictionary:
 
     def __init__(self):
         self.capacity = 8
         self.length = 0
-        self.hash_table = [[] for _ in range(self.capacity)]
-        self.free_indexes = [_ for _ in range(self.capacity)]
         self.t_hold = int(self.capacity * (2 / 3))
+        self.hash_table = [[] for _ in range(self.capacity)]
 
-    def adding_item_simple(self, key, value, hash_index):
-        self.hash_table[hash_index].extend([key, value])
-        self.length += 1
-        self.free_indexes.remove(hash_index)
-
-    def adding_item_complex(self, key, value):
+    def adding_item(self, key, value):
         hash_index = hash(key) % self.capacity
         if not self.hash_table[hash_index]:
-            self.adding_item_simple(key, value, hash_index)
+            self.hash_table[hash_index].extend([key, value])
+            self.length += 1
         else:
-            if key in [item[0] for item in self.hash_table if item]:
-                elt = [itm for itm in self.hash_table if itm and itm[0] == key]
-                self.hash_table[self.hash_table.index(elt[0])] = [key, value]
-            else:
-                hash_index = random.choice(self.free_indexes)
-                self.adding_item_simple(key, value, hash_index)
+            i = 0
+            while i < len(self.hash_table):
+                if self.hash_table[i]:
+                    if self.hash_table[i][0] == key:
+                        self.hash_table[i] = [key, value]
+                        break
+                    else:
+                        i += 1
+                else:
+                    i += 1
+            if i == self.capacity:
+                i = 0
+                while self.hash_table[i]:
+                    i += 1
+                self.hash_table[i].extend([key, value])
+                self.length += 1
 
     def __setitem__(self, key, value):
         if self.length < self.t_hold:
-            self.adding_item_complex(key, value)
+            self.adding_item(key, value)
         else:
             temporary_storage = self.hash_table
+            print(temporary_storage)
             self.capacity *= 2
             self.length = 0
             self.hash_table = [[] for _ in range(self.capacity)]
-            self.free_indexes = [_ for _ in range(self.capacity)]
             self.t_hold = int(self.capacity * (2 / 3))
             for item in temporary_storage:
                 if item:
-                    self.adding_item_complex(item[0], item[1])
-            self.adding_item_complex(key, value)
+                    self.adding_item(item[0], item[1])
+            self.adding_item(key, value)
 
     def __getitem__(self, key):
-        value = [itm[1] for itm in self.hash_table if itm and itm[0] == key]
-        if value:
-            return value[0]
+        i = 0
+        while i <= len(self.hash_table):
+            try:
+                if self.hash_table[i][0] == key:
+                    return self.hash_table[i][1]
+                else:
+                    i += 1
+            except IndexError:
+                i += 1
         raise KeyError
 
     def __len__(self):
