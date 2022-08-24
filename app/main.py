7cde5e4f8060a18ee59096d6ebf751eb8b8c44e4
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-08-24 21:23:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -25,15 +25,49 @@ class Dictionary:
         return
 
     def __getitem__(self, key):
-        hash_key = hash(key)
-        index = hash_key % self.capacity
-        while self.hash_table[index] is not None:
-            if self.hash_table[index][0] == key:
-                return self.hash_table[index][2]
-            index = (index + 1) % self.capacity
-        raise KeyError(f"No Key {key} in dict!")
+        return self.hash_table[self.find_item(key)][2]
 
     def __len__(self):
         return len(
             [element for element in self.hash_table if element is not None]
         )
+
+    def clear(self):
+        for elem in self.hash_table:
+            elem = None
+
+    def __delitem__(self, key):
+        self.hash_table[self.find_item(key)] = None
+
+    def get(self, key):
+        return self.__getitem__(key)
+
+    def pop(self, key):
+        i = self.find_item(key)
+        result = self.hash_table[i][2]
+        self.__delitem__(self.hash_table[i][0])
+        return result
+
+    def update(self, items: list):
+        self.__setitem__(items[0], items[1])
+
+    def __iter__(self):
+        self.iter_i = 0
+        return self
+
+    def __next__(self):
+        if self.iter_i < self.__len__():
+            table = [elem for elem in self.hash_table if elem is not None]
+            result = table[self.iter_i][2]
+            self.iter_i += 1
+            return result
+        raise StopIteration
+
+    def find_item(self, key):
+        hash_key = hash(key)
+        index = hash_key % self.capacity
+        while self.hash_table[index] is not None:
+            if self.hash_table[index][0] == key:
+                return index
+            index = (index + 1) % self.capacity
+        raise KeyError(f"No Key {key} in dict!")
