ae42f0d7b182d7d82bdf5ee60091242f17dd1b46
Inna Kushnir <innakushnir63@gmail.com>
2022-08-26 18:40:54 +0300

    Solut

--- app/main.py
+++ app/main.py
@@ -12,36 +12,28 @@ class Dictionary:
             self.resize()
 
         index_ = int(hash(key) % self.capacity)
-        if self.base_data[index_] is None:
-            self.base_data[index_] = [key, hash(key), value]
 
-        elif self.base_data[index_][0] == key:
-            self.base_data[index_] = [key, hash(key), value]
-            self.length -= 1
-        else:
+        while self.base_data[index_] is not None:
 
-            while self.base_data[index_] is not None:
+            if self.base_data[index_][0] == key and\
+                    self.base_data[index_][1] == hash(key):
+                self.base_data[index_] = [key, hash(key), value]
+                self.length -= 1
+                break
+            index_ = (index_ + 1) % self.capacity
 
-                if self.base_data[index_][0] == key and\
-                        self.base_data[index_][1] == hash(key):
-                    self.base_data[index_] = [key, hash(key), value]
-                    self.length -= 1
-                    break
-                index_ = (index_ + 1) % self.capacity
-
-            self.base_data[index_] = [key, hash(key), value]
+        self.base_data[index_] = [key, hash(key), value]
         self.length += 1
 
     def __getitem__(self, key):
-        if self.length != 0:
-            index_ = hash(key) % self.capacity
-            while self.base_data[index_] is not None\
-                    and (index_ < self.capacity):
-                if self.base_data[index_][0] == key and \
-                        self.base_data[index_][1] == hash(key):
-                    return self.base_data[index_][2]
+        index_ = hash(key) % self.capacity
+        while self.base_data[index_] is not None\
+                and (index_ < self.capacity):
+            if self.base_data[index_][0] == key and \
+                    self.base_data[index_][1] == hash(key):
+                return self.base_data[index_][2]
 
-                index_ = (index_ + 1) % self.capacity
+            index_ = (index_ + 1) % self.capacity
 
         raise KeyError(f"Not Key {key} ")
 
@@ -56,7 +48,7 @@ class Dictionary:
             if member is not None:
                 data_.append(member)
         self.base_data = [
-            None for i in range(self.capacity)]
+            None for _ in range(self.capacity)]
         self.length = 0
         for member in data_:
             self.__setitem__(member[0], member[2])
@@ -65,40 +57,23 @@ class Dictionary:
         self.init()
 
     def __delitem__(self, item):
-        i = 0
+        i = hash(item) % self.capacity
         while self.base_data[i] is not None:
-            if self.base_data[i][2] == item:
+            if self.base_data[i][0] == item:
                 self.base_data[i] = None
             i = (i + 1) % self.capacity
 
     def pop(self, key):
-        index_ = hash(key) % self.capacity
-        while self.base_data[index_] is not None:
-            if self.base_data[index_][0] == key and\
-                    self.base_data[index_][1] == hash(key):
-                self.base_data[index_] = None
-                return
-            index_ = (index_ + 1) % self.capacity
-        return None
+        self.delitem(key)
 
     def get(self, key):
-        if self.length != 0:
-            index_ = hash(key) % self.capacity
-            while self.base_data[index_] is not None\
-                    and (index_ < self.capacity):
-                if self.base_data[index_][0] == key and \
-                        self.base_data[index_][1] == hash(key):
-                    return self.base_data[index_][2]
-
-                index_ = (index_ + 1) % self.capacity
-
+        self.getitem(key)
         return None
 
-    @staticmethod
-    def update(**kwargs):
-        if kwargs:
-            for member in kwargs:
-                member.setitem()
+    def update(self, **kwargs):
+        for member in kwargs:
+            member.setitem()
+        return self.base_data
 
     def __iter__(self):
         if self.length != 0:
