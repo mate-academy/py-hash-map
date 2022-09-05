70ac570b927b4a3dedd1ca887bb688ac593f93f7
yaroslav-demchenko <dastex1411@gmail.com>
2022-09-05 12:04:10 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,45 +3,55 @@ class Dictionary:
     def __init__(self):
         self.capacity = 8
         self.load_factor = 2 / 3
+        self.length = 0
         self.buckets = self.create_table()
 
     def __setitem__(self, key, value):
         hash_code = hash(key)
         index = int(hash_code % self.capacity)
-        fill_cell = self.calc_fill_cell()
-        if self.capacity / fill_cell > self.load_factor:
-            self.resize()
-        while self.buckets[index] is not None:
-            index = (index + 1) % self.capacity
-        self.buckets[index] = [hash_code, key, value]
+        if self.length > 0:
+            if self.length / self.capacity > self.load_factor:
+                self.resize()
+        if self.buckets[index][0] is not None:
+            if self.buckets[index][1] == key:
+                self.buckets[index][2] = value
+            else:
+                while self.buckets[index][0] is not None:
+                    index = (index + 1) % self.capacity
+                self.buckets[index] = [hash_code, key, value]
+                self.length += 1
+        else:
+            self.buckets[index] = [hash_code, key, value]
+            self.length += 1
 
     def __getitem__(self, input_key):
         hash_code = hash(input_key)
         index = hash_code % self.capacity
-        while self.buckets[index] is not None:
-            hash_x, key, value = self.buckets[index]
-            if hash_x == hash_code and input_key == key:
-                return self.buckets[index][2]
+        while True:
+            try:
+                if self.buckets[index][0] is not None:
+                    hash_x, key, value = self.buckets[index]
+                    if hash_x == hash_code and input_key == key:
+                        return self.buckets[index][2]
+            except IndexError:
+                raise KeyError(input_key)
+            if index % self.capacity == 0:
+                break
             index = (index + 1) % self.capacity
+        raise KeyError(input_key)
 
     def __len__(self):
-        return self.calc_fill_cell()
+        return self.length
 
     def resize(self):
         self.capacity *= 2
         copy_elements = self.buckets.copy()
         self.buckets = self.create_table()
+        self.length = 0
         for cell in copy_elements:
-            self.__setitem__(cell[1], cell[2])
+            if cell[0] is not None:
+                self.__setitem__(cell[1], cell[2])
         del copy_elements
 
     def create_table(self):
-        buckets = [[None] for _ in range(self.capacity)]
-        return buckets
-
-    def calc_fill_cell(self):
-        res = 0
-        for elem in self.buckets:
-            if elem is not None:
-                res += 1
-        return res
+        return [[None] for _ in range(self.capacity)]
