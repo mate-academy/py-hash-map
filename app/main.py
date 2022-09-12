e35c643f78c4c0b7267ebd81e67a2de51126af7c
Viktor Beniukh <superbigbenjamin@gmail.com>
2022-09-12 20:16:25 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -6,36 +6,46 @@ class Dictionary:
         self._size = 0
         self._hash_table = [None for _ in range(self._capacity)]
 
-    def __getitem__(self, item):
-        index_ = hash(item) % self._capacity
-
-        while self._hash_table[index_] is not None:
-            key, value = self._hash_table[index_]
-            if key == item:
-                return value
-            index_ = (index_ + 1) % self._capacity
-
-        raise KeyError(f"Key {item} not found in dictionary")
-
-    def __setitem__(self, key, value):
+    def __getitem__(self, key):
         index_ = hash(key) % self._capacity
 
-        while self._hash_table[index_] is not None:
-            current_key, _ = self._hash_table[index_]
-            if key == current_key:
-                self._hash_table[index_] = (current_key, value)
-                break
+        while self._hash_table[index_]:
+            if self._hash_table[index_][0] == hash(key) \
+                    and self._hash_table[index_][1] == key:
+                return self._hash_table[index_][2]
             index_ = (index_ + 1) % self._capacity
 
-        if self._hash_table[index_] is None:
-            self._hash_table[index_] = (key, value)
-            self._size += 1
+        raise KeyError(f"Key {key} not found in dictionary")
 
+    def __setitem__(self, key, value):
         if self._size >= self._threshold:
             self._resize()
 
+        index_ = hash(key) % self._capacity
+
+        while True:
+            if not self._hash_table[index_]:
+                self._hash_table[index_] = [hash(key), key, value]
+                self._size += 1
+            if self._hash_table[index_][0] == hash(key) \
+                    and self._hash_table[index_][1] == key:
+                self._hash_table[index_][2] = value
+                break
+            index_ = (index_ + 1) % self._capacity
+
     def __delitem__(self, key):
-        self.pop(key)
+        index_ = hash(key) % self._capacity
+
+        while self._hash_table[index_]:
+            if self._hash_table[index_][0] == hash(key) and \
+                    self._hash_table[index_][1] == key:
+                value = self._hash_table[index_][2]
+                self._hash_table[index_] = None
+                self._size -= 1
+                return value
+            index_ = (index_ + 1) % self._capacity
+
+        raise KeyError(f"Key {key} not found in dictionary")
 
     def __len__(self):
         return self._size
@@ -52,7 +62,6 @@ class Dictionary:
 
         node = self._array[self._it]
         self._it += 1
-
         return node
 
     def get(self, key, value=None):
@@ -64,27 +73,30 @@ class Dictionary:
     def _resize(self):
         old_data = [elements for elements in self._hash_table if elements]
         self._capacity *= 2
-        self._threshold = self._capacity * 2 / 3
         self._size = 0
+        self._threshold = self._capacity * 2 / 3
         self._hash_table = [None] * self._capacity
 
-        for key, value in old_data:
-            self.__setitem__(key=key, value=value)
+        for elements in old_data:
+            if elements:
+                self.__setitem__(key=elements[1], value=elements[2])
 
-    def pop(self, key_pop):
-        index_ = hash(key_pop) % self._capacity
+    def pop(self, key):
+        try:
+            value = self.get(key)
+            self.__delitem__(key)
+            return value
+        except KeyError:
+            return f"Key {key} not found in dictionary"
 
-        while self._hash_table[index_] is not None:
-            key, value = self._hash_table[index_]
-            if key == key_pop:
-                self._hash_table[index_] = None
-                return value
-            index_ = (index_ + 1) % self._capacity
+    def update(self, other):
+        if not isinstance(other, Dictionary):
+            raise ValueError
 
-        raise KeyError(f"Key {key_pop} not found in dictionary")
-
-    def update(self, key, value):
-        self.__setitem__(key, value)
+        for key, value in other._hash_table:
+            if len(other._hash_table) != 0:
+                self.__setitem__(key, value)
 
     def clear(self):
-        return self._hash_table.clear()
+        self._hash_table = [None for _ in self._hash_table]
+        self._size = 0
