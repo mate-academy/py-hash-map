ffb75c62bd7f0d6407b9599190e7914e0884bbb9
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-09-11 21:00:13 -0300

    I created a main2 with resize but this won't pass some tests

--- app/main.py
+++ app/main.py
@@ -1,2 +1,26 @@
 class Dictionary:
-    pass
+    def __init__(self, size=1000):
+        self.storage = [[] for _ in range(size)]
+        self.size = size
+        self.length = 0
+
+    def __setitem__(self, key, value):
+        storage_idx = hash(key) % self.size
+        for ele in self.storage[storage_idx]:
+            if key == ele[0]:
+                ele[1] = value
+                break
+        else:
+            self.storage[storage_idx].append([key, value])
+            self.length += 1
+
+    def __getitem__(self, key):
+        storage_idx = hash(key) % self.size
+        for ele in self.storage[storage_idx]:
+            if ele[0] == key:
+                return ele[1]
+
+        raise KeyError('Key {} dont exist'.format(key))
+
+    def __len__(self):
+        return self.length
