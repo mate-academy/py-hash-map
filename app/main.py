44eeb1106b20cb005ba131c4171d742999b67c91
Paul Starodub <amorallex@gmail.com>
2022-09-20 14:33:41 +0300

    Solo_

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ class Dictionary:
         self.capacity = 8
         self.length = 0
         self.load = int(self.capacity * 2 / 3)
-        self.tab = [[] for _ in range(self.capacity)]
+        self.table = [[] for _ in range(self.capacity)]
 
     def __setitem__(self, key, value):
         # preparing index of tab
@@ -11,29 +11,30 @@ class Dictionary:
         index = hashed_key % self.capacity
         while True:
             # if new key length increases
-            if not len(self.tab[index]):
-                self.tab[index] = [key, value, hashed_key]
+            if self.table[index] == []:
+                self.table[index] = [key, value, hashed_key]
                 self.length += 1
                 break
                 # if key in dictionary
-            if self.tab[index][0] == key and self.tab[index][2] == hashed_key:
-                self.tab[index][1] = value
+            if self.table[index][2] == hashed_key\
+                    and self.table[index][0] == key:
+                self.table[index][1] = value
                 break
             index = (index + 1) % self.capacity
         if self.length == self.load:
             self.resize()
 
     def resize(self):
-        # getting new capacity and new length of tab
+        # getting new capacity and new length of table
         self.capacity *= 2
         self.length = 0
         self.load = int(self.capacity * 2 / 3)
         # preparing old tab for iteration
-        prep_tab = self.tab
-        # creating new tab
-        self.tab = [[] for _ in range(self.capacity)]
+        prep_table = self.table
+        # creating new table
+        self.table = [[] for _ in range(self.capacity)]
         # check
-        for element in prep_tab:
+        for element in prep_table:
             if len(element):
                 self.__setitem__(element[0], element[1])
 
@@ -43,23 +44,23 @@ class Dictionary:
     def __getitem__(self, key):
         # getting index of element
         index = hash(key) % self.capacity
-        element = self.tab[index]
+        element = self.table[index]
         while True:
             try:
-                if element[0] == key and element[2] == hash(key):
+                if element[2] == hash(key) and element[0] == key:
                     return element[1]
             except IndexError:
                 raise KeyError
             index = (index + 1) % self.capacity
-            element = self.tab[index]
+            element = self.table[index]
 
     def clear(self):
         self.capacity = 8
         self.length = 0
         self.load = int(self.capacity * 2 / 3)
-        self.tab = [[] for _ in range(self.capacity)]
+        self.table = [[] for _ in range(self.capacity)]
 
     def __iter__(self):
         self.it = 0
-        self.arr = [element for element in self.tab if len(element)]
+        self.arr = [element for element in self.table if len(element)]
         return self
