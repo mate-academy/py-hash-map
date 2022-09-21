53f75e04be20ff29bb542c5a2b746b1e69697b58
Paul Starodub <amorallex@gmail.com>
2022-09-21 17:45:19 +0300

    Taxk_

--- app/main.py
+++ app/main.py
@@ -6,16 +6,13 @@ class Dictionary:
         self.table = [[] for _ in range(self.capacity)]
 
     def __setitem__(self, key, value):
-        # preparing index of tab
         hashed_key = hash(key)
         index = hashed_key % self.capacity
         while True:
-            # if new key length increases
             if self.table[index] == []:
                 self.table[index] = [key, value, hashed_key]
                 self.length += 1
                 break
-                # if key in dictionary
             if self.table[index][2] == hashed_key\
                     and self.table[index][0] == key:
                 self.table[index][1] = value
@@ -25,15 +22,11 @@ class Dictionary:
             self.resize()
 
     def resize(self):
-        # getting new capacity and new length of table
         self.capacity *= 2
         self.length = 0
         self.load = int(self.capacity * 2 / 3)
-        # preparing old tab for iteration
         prep_table = self.table
-        # creating new table
         self.table = [[] for _ in range(self.capacity)]
-        # check
         for element in prep_table:
             if len(element):
                 self.__setitem__(element[0], element[1])
@@ -42,15 +35,14 @@ class Dictionary:
         return self.length
 
     def __getitem__(self, key):
-        # getting index of element
         index = hash(key) % self.capacity
         element = self.table[index]
+        list_actual_keys = [_[0] for _ in self.table if _ != []]
+        if key not in list_actual_keys:
+            raise KeyError
         while True:
-            try:
-                if element[2] == hash(key) and element[0] == key:
-                    return element[1]
-            except IndexError:
-                raise KeyError
+            if element[2] == hash(key) and element[0] == key:
+                return element[1]
             index = (index + 1) % self.capacity
             element = self.table[index]
 
@@ -60,7 +52,34 @@ class Dictionary:
         self.load = int(self.capacity * 2 / 3)
         self.table = [[] for _ in range(self.capacity)]
 
-    def __iter__(self):
-        self.it = 0
-        self.arr = [element for element in self.table if len(element)]
-        return self
+    def __delitem__(self, key):
+        list_actual_keys = [_[0] for _ in self.table if _ != []]
+        if key not in list_actual_keys:
+            raise KeyError
+        index = hash(key) % self.capacity
+        element = self.table[index]
+        while True:
+            if element[2] == hash(key) and element[0] == key:
+                self.table[index] = []
+                self.length -= 1
+                break
+            index = (index + 1) % self.capacity
+            element = self.table[index]
+
+    def get(self, key, value=None):
+        list_actual_keys = [_[0] for _ in self.table if _ != []]
+        if key not in list_actual_keys:
+            return value
+        if key in list_actual_keys:
+            return self.__getitem__(key)
+
+    def pop(self, __key, default=None):
+        list_actual_keys = [_[0] for _ in self.table if _ != []]
+        if __key in list_actual_keys:
+            result = self.__getitem__(__key)
+            self.__delitem__(__key)
+            return result
+        if __key not in list_actual_keys and default is not None:
+            return default
+        if __key not in list_actual_keys and default is None:
+            raise KeyError
