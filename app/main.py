cb082b1ac78141cce2ea7efbf7780863869aa49b
Vlad Milkov <vladmilkov@gmail.com>
2022-09-23 15:48:09 +0300

    Removed hash_table_index from class attributes. Moved block of logic from __delitem__, pop, get, __getitem__ to method find_cell_by_hash_and_key

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 class Dictionary:
-    def set_hash_table_index(self, key_hash):
-        self._hash_table_index = key_hash % self.capacity
+    def get_hash_table_index_for(self, key_hash):
+        return key_hash % self.capacity
 
-    def increase_hash_table_index_by_one(self):
-        self._hash_table_index += 1
-        self._hash_table_index %= self.capacity
+    def increase_hash_table_index_by_one(self, hash_table_index):
+        hash_table_index += 1
+        hash_table_index %= self.capacity
+        return hash_table_index
 
     def return_threshold(self):
         return int(self.capacity * 2 / 3)
@@ -12,48 +13,55 @@ class Dictionary:
     def setup_hash_table(self):
         return [[] for _ in range(self.capacity)]
 
-    def clear_hash_table_cell(self):
-        self.hash_table[self._hash_table_index] = []
-
-    def hash_table_index_for_key_hash(self, key_hash):
-        return key_hash % self.capacity
+    def clear_hash_table_cell(self, hash_table_index):
+        self.hash_table[hash_table_index] = []
 
     def write_to_hash_table(self, hash_table_index, item):
         self.hash_table[hash_table_index] = item
 
-    def is_hash_table_cell_empty(self):
-        if self.hash_table[self._hash_table_index]:
+    def is_hash_table_cell_empty(self, hash_table_index):
+        if self.hash_table[hash_table_index]:
             return False
         return True
 
-    def are_keys_equal(self, key):
-        if key == self.hash_table[self._hash_table_index][0]:
+    def are_keys_equal(self, hash_table_index, key):
+        if key == self.hash_table[hash_table_index][0]:
             return True
         return False
 
     def update_hash_table_value(self, hash_table_index, item):
         self.hash_table[hash_table_index][2] = item[2]
 
+    def find_cell_by_hash_and_key(self, hash_table_index, key):
+        if not self.is_hash_table_cell_empty(hash_table_index):
+            while True:
+                if self.are_keys_equal(hash_table_index, key):
+                    return hash_table_index
+
+                hash_table_index = self.increase_hash_table_index_by_one(
+                    hash_table_index)
+
+        raise KeyError
+
     def extend_hash_table(self):
         temp_values = self.hash_table.copy()
+
         self.capacity *= 2
         self.threshold = self.return_threshold()
-
-        print(f"capacity: {self.capacity}, threshold: {self.threshold}")
-
         self.hash_table = self.setup_hash_table()
 
         # fill new hash table
         for item in temp_values:
             if item:
-                self.set_hash_table_index(item[1])
+                hash_table_index = self.get_hash_table_index_for(item[1])
 
                 while True:
-                    if self.is_hash_table_cell_empty():
-                        self.write_to_hash_table(self._hash_table_index, item)
+                    if self.is_hash_table_cell_empty(hash_table_index):
+                        self.write_to_hash_table(hash_table_index, item)
                         break
 
-                    self.increase_hash_table_index_by_one()
+                    hash_table_index = self.increase_hash_table_index_by_one(
+                        hash_table_index)
 
     def __init__(self):
         self.capacity = 8
@@ -65,35 +73,31 @@ class Dictionary:
     def __setitem__(self, key, value):
         key_hash = hash(key)
         item = [key, key_hash, value]
-        self.set_hash_table_index(key_hash)
+        hash_table_index = self.get_hash_table_index_for(key_hash)
 
         while True:
-            if self.is_hash_table_cell_empty():
-                self.write_to_hash_table(self._hash_table_index, item)
+            if self.is_hash_table_cell_empty(hash_table_index):
+                self.write_to_hash_table(hash_table_index, item)
                 self._size += 1
                 break
 
-            if self.are_keys_equal(key):
-                self.update_hash_table_value(self._hash_table_index, item)
+            if self.are_keys_equal(hash_table_index, key):
+                self.update_hash_table_value(hash_table_index, item)
                 break
 
-            self.increase_hash_table_index_by_one()
+            hash_table_index = self.increase_hash_table_index_by_one(
+                hash_table_index)
 
         if self._size == self.threshold:
             self.extend_hash_table()
 
     def __getitem__(self, key):
-        self.set_hash_table_index(hash(key))
+        hash_table_index = self.get_hash_table_index_for(hash(key))
 
-        if not self.is_hash_table_cell_empty():
-            while True:
-                if self.are_keys_equal(key):
-                    return self.hash_table[self._hash_table_index][2]
-                    break
+        target_cell_hash = self.find_cell_by_hash_and_key(
+            hash_table_index, key)
 
-                self.increase_hash_table_index_by_one()
-        else:
-            raise KeyError
+        return self.hash_table[target_cell_hash][2]
 
     def __len__(self):
         return self._size
@@ -102,44 +106,35 @@ class Dictionary:
         self.hash_table = self.setup_hash_table()
 
     def __delitem__(self, key):
-        self.set_hash_table_index(hash(key))
+        hash_table_index = self.get_hash_table_index_for(hash(key))
 
-        if not self.is_hash_table_cell_empty():
-            while True:
-                if self.are_keys_equal(key):
-                    self.clear_hash_table_cell()
-                    self._size -= 1
-                    break
-
-                self.increase_hash_table_index_by_one()
-        else:
-            raise KeyError
+        target_cell_hash = self.find_cell_by_hash_and_key(
+            hash_table_index, key)
+        self.clear_hash_table_cell(target_cell_hash)
+        self._size -= 1
 
     def get(self, key, default=None):
-        self.set_hash_table_index(hash(key))
+        hash_table_index = self.get_hash_table_index_for(hash(key))
+        try:
+            target_cell_hash = self.find_cell_by_hash_and_key(
+                hash_table_index, key)
 
-        if not self.is_hash_table_cell_empty():
-            while True:
-                if self.are_keys_equal(key):
-                    return self.hash_table[self._hash_table_index][2]
-
-                self.increase_hash_table_index_by_one()
-        else:
+            return self.hash_table[target_cell_hash][2]
+        except KeyError:
             return default
 
     def pop(self, key, default=None):
-        self.set_hash_table_index(hash(key))
+        hash_table_index = self.get_hash_table_index_for(hash(key))
 
-        if not self.is_hash_table_cell_empty():
-            while True:
-                if self.are_keys_equal(key):
-                    result = self.hash_table[self._hash_table_index][2]
-                    self.clear_hash_table_cell()
-                    self._size -= 1
-                    return result
+        try:
+            target_cell_hash = self.find_cell_by_hash_and_key(
+                hash_table_index, key)
 
-                self.increase_hash_table_index_by_one()
-        else:
+            result = self.hash_table[target_cell_hash][2]
+            self.clear_hash_table_cell(target_cell_hash)
+            self._size -= 1
+            return result
+        except KeyError:
             return default
 
     def __repr__(self):
