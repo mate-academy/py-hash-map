e1a285301b823266a25cd461d627698af1b4233b
Ivan Antonishak <jus1stored@gmail.com>
2022-09-28 00:25:20 +0300

    Fix 'get' method

--- app/main.py
+++ app/main.py
@@ -1,3 +1,30 @@
+class Point:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def __eq__(self, other):
+        if not isinstance(other, Point):
+            return False
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        # Change the implementation of the hash to debug your code.
+        # For example, you can return self.x + self.y as a hash
+        # which is NOT a best practice, but you will be able to predict
+        # a hash value by coordinates of the point and its index
+        # in the hashtable as well
+        return self.x + self.y
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+
 class Dictionary:
 
     def __init__(self):
@@ -49,5 +76,8 @@ class Dictionary:
         self.hash_table = [None for _ in range(self.capacity)]
         return self
 
-    def get(self, key):
-        return self.__getitem__(key)
+    def get(self, key, value="None"):
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return value
