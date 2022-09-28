9d7baf625b8f162b88e3d63a27b08a30b2bca411
Ivan Antonishak <jus1stored@gmail.com>
2022-09-28 16:13:02 +0300

    Fix 'if' statement

--- app/main.py
+++ app/main.py
@@ -1,30 +1,3 @@
-class Point:
-    def __init__(self, x, y):
-        self._x = x
-        self._y = y
-
-    def __eq__(self, other):
-        if not isinstance(other, Point):
-            return False
-        return self.x == other.x and self.y == other.y
-
-    def __hash__(self):
-        # Change the implementation of the hash to debug your code.
-        # For example, you can return self.x + self.y as a hash
-        # which is NOT a best practice, but you will be able to predict
-        # a hash value by coordinates of the point and its index
-        # in the hashtable as well
-        return self.x + self.y
-
-    @property
-    def x(self):
-        return self._x
-
-    @property
-    def y(self):
-        return self._y
-
-
 class Dictionary:
 
     def __init__(self):
@@ -43,8 +16,8 @@ class Dictionary:
                 self.hash_table[index] = [key, hash_, value]
                 self.size += 1
                 break
-            if self.hash_table[index][0] == key \
-                    and self.hash_table[index][1] == hash_:
+            if (self.hash_table[index][0] == key and
+                    self.hash_table[index][1] == hash_):
                 self.hash_table[index][2] = value
                 break
             index = (index + 1) % self.capacity
@@ -53,8 +26,8 @@ class Dictionary:
         hash_ = hash(key)
         index = hash_ % self.capacity
         while self.hash_table[index]:
-            if self.hash_table[index][0] == key \
-                    and self.hash_table[index][1] == hash_:
+            if (self.hash_table[index][0] == key and
+                    self.hash_table[index][1] == hash_):
                 return self.hash_table[index][2]
             index = (index + 1) % self.capacity
         raise KeyError(key)
