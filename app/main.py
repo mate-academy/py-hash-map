fa8c4fc7a3efde4dbf822bcbbb9bf3f8c128ab0f
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-09-29 11:18:24 +0300

    fix names of variable

--- app/main.py
+++ app/main.py
@@ -10,39 +10,39 @@ class Dictionary:
 
     def __getitem__(self, key):
         hash_key = hash(key)
-        get_ind = hash_key % self.capacity
+        get_index = hash_key % self.capacity
 
         while True:
-            if not self.table[get_ind]:
+            if not self.table[get_index]:
                 raise KeyError
 
             try:
-                if self.table[get_ind][0] == key and \
-                        self.table[get_ind][2] == hash_key:
-                    return self.table[get_ind][1]
+                if self.table[get_index][0] == key and \
+                        self.table[get_index][2] == hash_key:
+                    return self.table[get_index][1]
             except IndexError:
                 raise KeyError
-            get_ind = (get_ind + 1) % self.capacity
+            get_index = (get_index + 1) % self.capacity
 
     def __setitem__(self, key, value):
         if self.length == self.threshold:
             self.upgrade_table()
 
         hash_key = hash(key)
-        ind = hash_key % self.capacity
+        index = hash_key % self.capacity
 
         while True:
-            if not self.table[ind]:
-                self.table[ind] = [key, value, hash_key]
+            if not self.table[index]:
+                self.table[index] = [key, value, hash_key]
                 self.length += 1
                 break
 
-            if hash_key == self.table[ind][2] and \
-                    key == self.table[ind][0]:
-                self.table[ind][1] = value
+            if hash_key == self.table[index][2] and \
+                    key == self.table[index][0]:
+                self.table[index][1] = value
                 break
 
-            ind = (ind + 1) % self.capacity
+            index = (index + 1) % self.capacity
 
     def upgrade_table(self):
         copy_table = deepcopy(self.table)
