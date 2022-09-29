21e4f106ff7a509afe583a081a39b65834f41001
mltdll <ilya.vyplavin@gmail.com>
2022-09-29 14:11:07 +0300

    Solution?

--- app/main.py
+++ app/main.py
@@ -1,2 +1,114 @@
+from __future__ import annotations
+from typing import Any, Hashable
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.capacity = 8
+        self.threshold = 5
+        self.indices = [None] * self.capacity
+        self.nodes = []
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        if len(self.nodes) >= self.threshold:
+            self._resize()
+
+        key_hash = hash(key)
+        index, node_index = self._index_lookup(key_hash, key)
+
+        # Add new item
+        if node_index is None:
+            self.indices[index] = len(self.nodes)
+            self.nodes.append((key_hash, key, value))
+
+        # Reassign existing item
+        else:
+            self.nodes[node_index] = (key_hash, key, value)
+
+    def __getitem__(self, key: Hashable) -> Any:
+        key_hash = hash(key)
+        index, node_index = self._index_lookup(key_hash, key)
+
+        if node_index is None:
+            raise KeyError(key)
+
+        return self.nodes[node_index][2]
+
+    def __delitem__(self, key: Hashable) -> None:
+        key_hash = hash(key)
+        index, node_index = self._index_lookup(key_hash, key)
+
+        if node_index is None:
+            raise KeyError(key)
+
+        self.nodes.pop(node_index)
+        self.indices[index] = None
+
+    def __len__(self) -> int:
+        return len(self.nodes)
+
+    def __iter__(self):
+        return iter(self.keys())
+
+    def _resize(self):
+        # Increase size
+        self.capacity *= 2
+        self.threshold = int(self.capacity * 0.67)
+
+        # Store data temporarily
+        nodes = self.nodes
+
+        # Re-create nodes and indices with new capacity
+        self.nodes = []
+        self.indices = [None] * self.capacity
+
+        # Add old data to new nodes
+        for node in nodes:
+            self[node[1]] = node[2]
+
+    def _index_lookup(
+            self, key_hash: int, key: Hashable
+    ) -> tuple[int, int | None]:
+        index = key_hash % self.capacity
+
+        # No use iterating longer than self.capacity iterations
+        for _ in range(self.capacity):
+            node_index = self.indices[index]
+
+            # Not sure if it's okay to break lines like this
+            if node_index is None or (self.nodes[node_index][0] == key_hash
+                                      and self.nodes[node_index][1] == key):
+                return index, node_index
+
+            # Formula from CPython dict implementation :)
+            index = ((5 * index) + 1) % self.capacity
+
+    def clear(self):
+        # Breaking DRY, but calling self.__init__() wouldn't be better, right?
+        self.capacity = 8
+        self.threshold = 5
+        self.indices = [None] * self.capacity
+        self.nodes = []
+
+    def get(self, key: Hashable) -> Any:
+        try:
+            return self[key]
+        except KeyError:
+            return None
+
+    def pop(self, key: Hashable) -> Any:
+        value = self[key]
+        del self[key]
+        return value
+
+    def items(self) -> list[tuple]:
+        # Needed for update() method
+        return [(node[1], node[2]) for node in self.nodes]
+
+    def keys(self) -> list[Any]:
+        # Needed for __iter__() method
+        return [node[1] for node in self.nodes]
+
+    def update(self, other: Dictionary | dict) -> None:
+        for key, value in other.items():
+            self[key] = value
