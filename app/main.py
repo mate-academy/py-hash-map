9a036c32e8ce6579fad0d6df2e2258492d19d867
igor kosianenko <funkcionalkos@gmail.com>
2022-10-08 18:29:43 +0300

    rename 'lenght' and change deepcopy to copy

--- app/main.py
+++ app/main.py
@@ -5,13 +5,13 @@ from typing import Any, Hashable
 class Dictionary:
     def __init__(self) -> None:
         self.size = 8
-        self.lenght = 0
+        self.length = 0
         self.dict_ = [None for i in range(self.size)]
 
     def resize(self) -> None:
         if len(self) > (self.size * (2 / 3)):
-            self.lenght = 0
-            temp_ = deepcopy(self.dict_)
+            self.length = 0
+            temp_ = self.dict_.copy()
             self.size *= 2
             self.dict_ = [None for _ in range(self.size)]
             for el in temp_:
@@ -19,7 +19,7 @@ class Dictionary:
                     self[el[0]] = el[2]
 
     def __len__(self) -> int:
-        return self.lenght
+        return self.length
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
         hash_ = hash(key)
@@ -30,7 +30,7 @@ class Dictionary:
                 return
             index_ = (index_ + 1) % self.size
         self.dict_[index_] = [key, hash_, value]
-        self.lenght += 1
+        self.length += 1
         self.resize()
 
     def __getitem__(self, key: Hashable) -> None:
