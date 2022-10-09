418bd7f799db31e302e98d814d307bc9defefc82
Zakhar Podporinov <zp162162@gmail.com>
2022-10-10 00:21:49 +0300

    Fix remarks

--- app/main.py
+++ app/main.py
@@ -11,46 +11,41 @@ class DictItem:
 
 
 class Dictionary:
+    _capacity = 8
+    _load_factor = 2 / 3
+    _threshold = 5
+
     def __init__(self) -> None:
-        self._capacity = 8
-        self._load_factor = 2 / 3
-        self._threshold = int(self._capacity * self._load_factor)
         self._size = 0
-        self._source = [None for _ in range(self._capacity)]
+        self._store = [None for _ in range(self._capacity)]
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
         index = hash(key) % self._capacity
-        is_exist = False
-        while (item := self._source[index]) is not None:
+        key_exists = False
+        while (item := self._store[index]) is not None:
             if item.key == key:
-                is_exist = True
+                key_exists = True
                 break
-            if index == self._capacity - 1:
-                index = 0
-            else:
-                index += 1
-        self._source[index] = DictItem(key, value)
-        if not is_exist:
+            index = (index + 1) % self._capacity
+        self._store[index] = DictItem(key, value)
+        if not key_exists:
             self._size += 1
-        if self._size > self._threshold:
-            self._resize()
+            if self._size > self._threshold:
+                self._resize()
 
     def __getitem__(self, key: Hashable) -> Any:
         index = hash(key) % self._capacity
-        while (item := self._source[index]) is not None:
+        while (item := self._store[index]) is not None:
             if item.key == key:
                 return item.value
-            if index == self._capacity - 1:
-                index = 0
-            else:
-                index += 1
+            index = (index + 1) % self._capacity
         raise KeyError(key)
 
     def __delitem__(self, key: Hashable) -> None:
         index = hash(key) % self._capacity
-        if self._source[index] is None:
+        if self._store[index] is None:
             raise KeyError(key)
-        self._source[index] = None
+        self._store[index] = None
         self._size -= 1
 
     def __iter__(self) -> Dictionary:
@@ -58,12 +53,12 @@ class Dictionary:
         return self
 
     def __next__(self) -> Any:
-        while self._source[self.iter] is None:
+        while self._store[self.iter] is None:
             self.iter += 1
             if self.iter == self._capacity:
                 del self.iter
                 raise StopIteration
-        result = self._source[self.iter]
+        result = self._store[self.iter].key
         self.iter += 1
         return result
 
@@ -72,18 +67,17 @@ class Dictionary:
 
     def _resize(self) -> None:
         self._size = 0
-        items = [self._source[index]
-                 for index in range(self._capacity)
-                 if self._source[index] is not None]
+        items = self._store
         self._capacity *= 2
         self._threshold = int(self._capacity * self._load_factor)
-        self._source = [None for _ in range(self._capacity)]
+        self._store = [None for _ in range(self._capacity)]
         for item in items:
-            self[item.key] = item.value
+            if item is not None:
+                self[item.key] = item.value
 
     def clear(self) -> None:
         self._capacity = 8
-        self._source = [None for _ in range(self._capacity)]
+        self._store = [None for _ in range(self._capacity)]
 
     def pop(self, key: Hashable) -> Any:
         value = self[key]
@@ -91,7 +85,7 @@ class Dictionary:
         self._size -= 1
         return value
 
-    def get(self, key: Hashable, default: Any) -> Any:
+    def get(self, key: Hashable, default: Any = None) -> Any:
         try:
             return self[key]
         except KeyError:
@@ -100,7 +94,7 @@ class Dictionary:
     def update(self, other: Dictionary | dict) -> None:
         if isinstance(other, Dictionary):
             for index in range(other._capacity):
-                item = other._source[index]
+                item = other._store[index]
                 if item is not None:
                     self[item.key] = item.value
         elif isinstance(other, dict):
