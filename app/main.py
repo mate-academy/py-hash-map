a52de84bf4de0e9eebf94921ca237ed846a17bac
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-10-10 20:00:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,59 @@
+from typing import Any, Hashable
+
+
 class Dictionary:
-    pass
+    LOAD_FACTOR = 2 / 3
+
+    def __init__(self) -> None:
+        self.buckets = [[] for _ in range(8)]
+        self.size = 0
+        self.capacity = len(self.buckets)
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        index = self.count_index(key)
+
+        while self.buckets[index]:
+            key_, value_ = self.buckets[index]
+            if key == key_:
+                self.buckets[index] = (key, value)
+                return
+            index = (index + 1) % self.capacity
+
+        self.size += 1
+        self.buckets[index] = (key, value)
+        self.resize()
+
+    def __getitem__(self, key: Hashable) -> None:
+        index = self.count_index(key)
+
+        while self.buckets[index]:
+            key_, value = self.buckets[index]
+            if key == key_:
+                return value
+            index = (index + 1) % self.capacity
+
+        raise KeyError
+
+    def __len__(self) -> int:
+        return self.size
+
+    def resize(self) -> None:
+        threshold = self.capacity * self.LOAD_FACTOR
+
+        if self.size > threshold:
+            self.capacity *= 2
+            old_dictionary = self.buckets
+
+            self.buckets = [[] for _ in range(self.capacity)]
+
+            for element in old_dictionary:
+                if element:
+                    key, value = element
+                    self.__setitem__(key, value)
+                    self.size -= 1
+
+    def count_index(self, new_key: Hashable) -> int:
+        hashed_value = hash(new_key)
+        index = hashed_value % self.capacity
+
+        return index
