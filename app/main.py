904cdc61f21d988fcf8a3edcfbc37ab0a275e4c9
igor kosianenko <funkcionalkos@gmail.com>
2022-10-11 17:12:09 +0300

    So little fixes

--- app/main.py
+++ app/main.py
@@ -5,15 +5,15 @@ class Dictionary:
     def __init__(self) -> None:
         self.size = 8
         self.length = 0
-        self.dict_ = [None for i in range(self.size)]
+        self.hash_table = [None for _ in range(self.size)]
 
     def resize(self) -> None:
         if len(self) > (self.size * (2 / 3)):
             self.length = 0
-            temp_ = self.dict_.copy()
+            temp = self.hash_table
             self.size *= 2
-            self.dict_ = [None for _ in range(self.size)]
-            for el in temp_:
+            self.hash_table = [None for _ in range(self.size)]
+            for el in temp:
                 if el is not None:
                     self[el[0]] = el[2]
 
@@ -23,19 +23,19 @@ class Dictionary:
     def __setitem__(self, key: Hashable, value: Any) -> None:
         hash_ = hash(key)
         index_ = hash_ % self.size
-        while self.dict_[index_]:
-            if self.dict_[index_] and self.dict_[index_][0] == key:
-                self.dict_[index_] = [key, hash_, value]
+        while self.hash_table[index_]:
+            if self.hash_table[index_] and self.hash_table[index_][0] == key:
+                self.hash_table[index_] = [key, hash_, value]
                 return
             index_ = (index_ + 1) % self.size
-        self.dict_[index_] = [key, hash_, value]
+        self.hash_table[index_] = [key, hash_, value]
         self.length += 1
         self.resize()
 
     def __getitem__(self, key: Hashable) -> None:
         index_ = hash(key) % self.size
         for _ in range(self.size):
-            if self.dict_[index_] and self.dict_[index_][0] == key:
-                return self.dict_[index_][2]
+            if self.hash_table[index_] and self.hash_table[index_][0] == key:
+                return self.hash_table[index_][2]
             index_ = (index_ + 1) % self.size
         raise KeyError(f"{key}")
