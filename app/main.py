786e1e0586dada7f308a7ac3392c36e19813e632
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-10-11 20:41:40 +0300

    Added const value

--- app/main.py
+++ app/main.py
@@ -2,18 +2,19 @@ from typing import Hashable, Any
 
 
 class Dictionary:
+    LOAD_FACTOR = 2 / 3
+
     def __init__(self) -> None:
         self._size = 0
         self._capacity = 8
-        self._load_factor = 2 / 3
-        self._threshold = int(self._capacity * self._load_factor)
+        self._threshold = int(self._capacity * Dictionary.LOAD_FACTOR)
         self._hash_table = [[] for _ in range(self._capacity)]
 
     def resize(self) -> None:
         new_hash_table = self._hash_table.copy()
         self._capacity *= 2
         self._size = 0
-        self._threshold = int(self._capacity * self._load_factor)
+        self._threshold = int(self._capacity * Dictionary.LOAD_FACTOR)
         self._hash_table = [[] for _ in range(self._capacity)]
 
         for element in new_hash_table:
