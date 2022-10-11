6b62b684485b6e8c8b08b7ff7a66cba5a8bbb488
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-10-11 16:44:39 +0300

    Dictionary

--- app/main.py
+++ app/main.py
@@ -1,2 +1,55 @@
+from typing import Hashable, Any
+
+
 class Dictionary:
-    pass
+
+    CAPACITY = 8
+    LOAD_FACTOR = 2 / 3
+
+    def __init__(self) -> None:
+        self.threshold = int(self.CAPACITY * self.LOAD_FACTOR)
+        self.table = [[] for _ in range(self.CAPACITY)]
+        self.length = 0
+
+    def resize_table(self) -> None:
+        table_copy = self.table.copy()
+        self.length = 0
+        self.CAPACITY *= 2
+        self.threshold = int(self.CAPACITY * self.LOAD_FACTOR)
+        self.table = [[] for _ in range(self.CAPACITY)]
+        for x in table_copy:
+            if x:
+                self.__setitem__(x[0], x[1])
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        _hash = hash(key)
+        index = _hash % self.CAPACITY
+
+        if self.length == self.threshold:
+            self.resize_table()
+
+        while True:
+            if not self.table[index]:
+                self.table[index] = [key, value, _hash]
+                self.length += 1
+                return
+
+            if self.table[index][0] == key and \
+                    self.table[index][2] == _hash:
+                self.table[index][1] = value
+                return
+
+            index = (index + 1) % self.CAPACITY
+
+    def __getitem__(self, key: Hashable) -> Any:
+        _hash = hash(key)
+        index = _hash % self.CAPACITY
+
+        while self.table[index]:
+            if self.table[index][0] == key and self.table[index][2] == _hash:
+                return self.table[index][1]
+            index = (index + 1) % self.CAPACITY
+        raise KeyError
+
+    def __len__(self) -> int:
+        return self.length
