ebdb227533e5e6dc877bc58b24cbc568fb7c47f3
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-10-13 12:48:08 +0300

    getitem fixed

--- app/main.py
+++ app/main.py
@@ -5,14 +5,14 @@ class Dictionary:
     def __init__(self) -> None:
         self.size = 0
         self.capacity = 8
-        self.threshold = int(self.capacity * 0.666)
+        self.threshold = int(self.capacity * 2 / 3)
         self.hash_table = [None for _ in range(self.capacity)]
 
     def resize(self) -> None:
         old_hash_table = self.hash_table
         self.size = 0
         self.capacity *= 2
-        self.threshold = int(self.capacity * 0.666)
+        self.threshold = int(self.capacity * 2 / 3)
         self.hash_table = [None for _ in range(self.capacity)]
         for el in old_hash_table:
             if el:
@@ -37,10 +37,16 @@ class Dictionary:
             hash_index = (hash_index + 1) % self.capacity
 
     def __getitem__(self, key: Hashable) -> Any:
-        for el in self.hash_table:
-            if el and el[1] == key:
-                return el[2]
-        raise KeyError(key)
+
+        hash_key = hash(key)
+        hash_index = hash_key % self.capacity
+
+        while self.hash_table[hash_index]:
+            if (self.hash_table[hash_index][0] == hash_key
+                    and self.hash_table[hash_index][1] == key):
+                return self.hash_table[hash_index][2]
+            hash_index = (hash_index + 1) % self.capacity
+        raise KeyError(f"{key} key does not exist in falsified dictionary")
 
     def __len__(self) -> int:
         return self.size
