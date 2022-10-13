96578d4d026d91841d3d81247de76cf4ef37c487
Sergii Franchuk <Franchuksergei@gmail.com>
2022-10-13 15:46:58 +0300

    Rename variables

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Hashable
 
 
 class Dictionary:
@@ -6,47 +6,47 @@ class Dictionary:
         self.size = 0
         self._capacity = 8
         self.threshold = int(self._capacity / 3 * 2)
-        self.value = [[None, None, None]] * self._capacity
+        self.hash_table = [[None, None, None]] * self._capacity
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         if isinstance(key, (dict, set, list)):
             raise TypeError(f"unhashable type: '{type(key)}'")
 
         index = hash(key) % self._capacity
         while True:
 
-            if self.value[index][2] is None:
+            if self.hash_table[index][2] is None:
                 self.size += 1
-                self.value[index] = [key, value, hash(key)]
+                self.hash_table[index] = [key, value, hash(key)]
                 if self.size > self.threshold:
                     self._resize()
                 break
 
-            if self.value[index][0] == key:
-                self.value[index] = [key, value, hash(key)]
+            if self.hash_table[index][0] == key:
+                self.hash_table[index] = [key, value, hash(key)]
                 break
 
             index += 1
             if index == self._capacity:
                 index = 0
 
-    def __getitem__(self, item: Any) -> Any:
+    def __getitem__(self, item: Hashable) -> Any:
         if isinstance(item, (dict, set, list)):
             raise TypeError(f"unhashable type: '{type(item)}'")
 
         index = hash(item) % self._capacity
         start = index
-        if not self.value[index][0]:
+        if not self.hash_table[index][0]:
             raise KeyError(f"there is no such key {item}")
 
-        while self.value[index][0] != item:
+        while self.hash_table[index][0] != item:
             index += 1
             if index == self._capacity:
                 index = 0
             if index == start:
                 raise KeyError(f"there is no such key {item}")
 
-        return self.value[index][1]
+        return self.hash_table[index][1]
 
     def __len__(self) -> int:
         return self.size
@@ -54,15 +54,18 @@ class Dictionary:
     def _resize(self) -> None:
         self._capacity *= 2
         self.threshold = int(self._capacity / 3 * 2)
+
         new = [[None, None, None]] * self._capacity
-        for element in self.value:
+
+        for element in self.hash_table:
             if element[2] is None:
                 continue
             index = hash(element[0]) % self._capacity
+
             while new[index][2] is not None:
                 index += 1
                 if index == self._capacity:
                     index = 0
 
             new[index] = [element[0], element[1], hash(element[0])]
-        self.value = new
+        self.hash_table = new
