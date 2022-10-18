f2d4a5a67faba8a7244aa425f78324694f835afb
IvanGLS <gladiolus0707@gmail.com>
2022-10-18 21:02:19 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,2 +1,25 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self, init: Any = None) -> None:
+        if init is not None:
+            self.__dict__.update(init)
+
+    def __getitem__(self, key: Any) -> dict:
+        return self.__dict__[key]
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        self.__dict__[key] = value
+
+    def __delitem__(self, key: Any) -> None:
+        del self.__dict__[key]
+
+    def __contains__(self, key: Any) -> bool:
+        return key in self.__dict__
+
+    def __len__(self) -> int:
+        return len(self.__dict__)
+
+    def __repr__(self) -> str:
+        return repr(self.__dict__)
