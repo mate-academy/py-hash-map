8d4b8df39463edce52090b7e9fe71cc8f569832c
IvanGLS <gladiolus0707@gmail.com>
2022-10-20 13:26:07 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,24 +2,47 @@ from typing import Any
 
 
 class Dictionary:
-    def __init__(self, init: Any = None) -> None:
-        if init is not None:
-            self.__dict__.update(init)
-
-    def __getitem__(self, key: Any) -> dict:
-        return self.__dict__[key]
-
-    def __setitem__(self, key: Any, value: Any) -> None:
-        self.__dict__[key] = value
-
-    def __delitem__(self, key: Any) -> None:
-        del self.__dict__[key]
-
-    def __contains__(self, key: Any) -> bool:
-        return key in self.__dict__
+    def __init__(self) -> None:
+        self.capacity = 8
+        self.length = 0
+        self.bucket_size = int(self.capacity * 2 / 3)
+        self.hash_list = [[] for _ in range(self.capacity)]
 
     def __len__(self) -> int:
-        return len(self.__dict__)
+        return self.length
 
-    def __repr__(self) -> str:
-        return repr(self.__dict__)
+    def __setitem__(self, key: Any, value: Any) -> None:
+        if self.length == self.bucket_size:
+            self.resize()
+        hash_data = hash(key)
+        index_data = hash_data % self.capacity
+        while True:
+            if not self.hash_list[index_data]:
+                self.hash_list[index_data] = [key, hash_data, value]
+                self.length += 1
+                break
+            if self.hash_list[index_data][0] == key and\
+                    self.hash_list[index_data][1] == hash_data:
+                self.hash_list[index_data][2] = value
+                break
+            index_data = (index_data + 1) % self.capacity
+
+    def resize(self) -> None:
+        self.capacity *= 2
+        self.bucket_size = int(self.capacity * 2 / 3)
+        self.length = 0
+        old_data = self.hash_list
+        self.hash_list = [[] for _ in range(self.capacity)]
+        for item in old_data:
+            if item:
+                self.__setitem__(item[0], item[2])
+
+    def __getitem__(self, key: Any) -> list:
+        hash_data = hash(key)
+        index_data = hash_data % self.capacity
+        while self.hash_list[index_data]:
+            if self.hash_list[index_data][1] == hash_data \
+                    and self.hash_list[index_data][0] == key:
+                return self.hash_list[index_data][2]
+            index_data = (index_data + 1) % self.capacity
+        raise KeyError(key)
