ef783659df10d7bac1a9417bcce247895a3223f9
Bohdan2001007 <bogdan1991007@gmail.com>
2022-10-20 17:31:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,21 @@
 class Dictionary:
-    pass
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __repr__(self):
+        return repr(self.__dict__)
+
+    def __delitem__(self, key):
+        del self.__dict__[key]
+
+    def __contains__(self, item):
+        return item in self.__dict__
+
+    def __iter__(self):
+        return iter(self.__dict__)
