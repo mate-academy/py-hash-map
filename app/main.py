a1117cd8da6bdd78e5bfd20db08f9195b920b427
Nataliia <amirammina@gmail.com>
2022-10-28 11:46:51 +0300

    Added hash check

--- app/main.py
+++ app/main.py
@@ -3,41 +3,42 @@ class Dictionary:
         self.length = 0
         self.hash_size = 8
         self.hash_table = [[] for _ in range(self.hash_size)]
-        self.treshold = int(self.hash_size * 2 / 3)
+        self.threshold = int(self.hash_size * 2 / 3)
 
     def resize_hash(self) -> None:
         old_hash_table = self.hash_table
         self.hash_size *= 2
-        self.treshold = int(self.hash_size * 2 / 3)
+        self.threshold = int(self.hash_size * 2 / 3)
         self.length = 0
         self.hash_table = [[] for _ in range(self.hash_size)]
         for item in old_hash_table:
             if item:
-                self.__setitem__(item[0], item[1])
+                self.__setitem__(item[0], item[2])
 
     def __setitem__(self, key: object, value: object) -> None:
-        if self.length == self.treshold:
+        if self.length == self.threshold:
             self.resize_hash()
-        if isinstance(key, (list, dict)):
-            raise TypeError(f"unhashable type: {type(key)}")
-        index = hash(key) % self.hash_size
+        key_hash = hash(key)
+        index = key_hash % self.hash_size
+
         while True:
             if not self.hash_table[index]:
-                self.hash_table[index] = [key, value]
+                self.hash_table[index] = [key, key_hash, value]
                 self.length += 1
                 return
-            if key in self.hash_table[index]:
-                self.hash_table[index][1] = value
+            if self.hash_table[index][0] == key and \
+                    self.hash_table[index][1] == key_hash:
+                self.hash_table[index][2] = value
                 return
             index = (index + 1) % self.hash_size
 
     def __getitem__(self, key: object) -> object:
-        if self.length == self.treshold:
-            self.resize_hash()
-        index = hash(key) % self.hash_size
+        key_hash = hash(key)
+        index = key_hash % self.hash_size
         while self.hash_table[index]:
-            if key in self.hash_table[index]:
-                return self.hash_table[index][1]
+            if self.hash_table[index][0] == key and \
+                    self.hash_table[index][1] == key_hash:
+                return self.hash_table[index][2]
             index = (index + 1) % self.hash_size
         raise KeyError(f"Key {key} does not exist")
 
@@ -46,16 +47,16 @@ class Dictionary:
 
     def clear(self) -> None:
         self.hash_size = 8
-        self.treshold = int(self.hash_size * 2 / 3)
+        self.threshold = int(self.hash_size * 2 / 3)
         self.length = 0
         self.hash_table = [[] for _ in range(self.hash_size)]
 
     def __delitem__(self, key: object) -> None:
-        if self.length == self.treshold:
-            self.resize_hash()
-        index = hash(key) % self.hash_size
+        key_hash = hash(key)
+        index = key_hash % self.hash_size
         while self.hash_table[index]:
-            if key in self.hash_table[index]:
+            if self.hash_table[index][0] == key and \
+                    self.hash_table[index][1] == key_hash:
                 self.hash_table[index] = []
                 return
             index = (index + 1) % self.hash_size
