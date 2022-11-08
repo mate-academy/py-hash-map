5c1de04e6d116d9867ccf7cd4cc00bb140bba2b2
Denys Tereshchuk <terrya@ukr.net>
2022-11-08 16:10:50 +0400

    New solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
-from copy import deepcopy
+from copy import deepcopy, copy
 from dataclasses import dataclass
+from datetime import time, datetime
 from typing import Any, Union
 
 
 @dataclass
 class Dictionary:
     def __init__(self) -> None:
-        self.nodes = [[], [], [], [], [], [], [], []]
+        self.size = 8
+        self.load = 0
+        self.length = 0
+        self.nodes = [[] for i in range(self.size)]
 
     def resize(self) -> None:
-        temp = deepcopy(self.nodes)
-        size = len(self.nodes)
-        self.nodes = []
-        for _ in range(2 * size):
-            self.nodes.append([])
+        temp = copy(self.nodes)
+        self.load = 0
+        self.length = 0
+        self.size = 2 * self.size
+        self.nodes = [[] for i in range(self.size)]
         for node in temp:
             for item in node:
                 if item:
@@ -22,7 +26,7 @@ class Dictionary:
     def __setitem__(self,
                     key: Union[int, float, bool, str, tuple],
                     value: Any) -> None:
-        if self.nodes.count([]) <= 1 / 3 * len(self.nodes):
+        if self.load > 2 / 3 * self.size:
             self.resize()
         self.__write(key, value)
 
@@ -30,46 +34,52 @@ class Dictionary:
                 key: Union[int, float, bool, str, tuple],
                 value: Any) -> None:
         write = True
-        for item in self.nodes[hash(key) % len(self.nodes)]:
+        for item in self.nodes[hash(key) % self.size]:
             if key in item:
                 item[1] = value
                 write = False
                 break
         if write:
-            self.nodes[hash(key) % len(self.nodes)].append([key, value])
+            self.length += 1
+            if not self.nodes[hash(key) % self.size]:
+                self.load += 1
+            self.nodes[hash(key) % self.size].append([key, value])
 
-    def __getitem__(self, key: Union[int, float, bool, str, tuple]) -> Any:
-        for node in self.nodes[hash(key) % len(self.nodes)]:
+    def __getitem__(self,
+                    key: Union[int, float, bool, str, tuple]) -> Union[Any, KeyError]:
+        for node in self.nodes[hash(key) % self.size]:
             if key in node:
                 return node[1]
         raise KeyError
 
     def __len__(self) -> int:
-        result = 0
-        for node in self.nodes:
-            for item in node:
-                if item:
-                    result += 1
-        return result
+        return self.length
 
     def clear(self) -> None:
-        self.nodes = []
+        self.size = 8
+        self.load = 0
+        self.length = 0
+        self.nodes = [[] for i in range(self.size)]
 
-    def __delitem__(self, key: Any) -> None:
-        for node in self.nodes[hash(key) % len(self.nodes)]:
+    def __delitem__(self, key: Any) -> Union[None, KeyError]:
+        for node in self.nodes[hash(key) % self.size]:
             if key in node:
-                self.nodes[hash(key) % len(self.nodes)].remove(node)
+                self.nodes[hash(key) % self.size].remove(node)
+                self.length -= 1
+            else:
+                raise KeyError
 
     def get(self, key: Union[int, float, bool, str, tuple]) -> Any:
-        for node in self.nodes[hash(key) % len(self.nodes)]:
+        for node in self.nodes[hash(key) % self.size]:
             if key in node:
                 return node[1]
 
     def pop(self, key: Union[int, float, bool, str, tuple]) -> Any:
-        for node in self.nodes[hash(key) % len(self.nodes)]:
+        for node in self.nodes[hash(key) % self.size]:
             if key in node:
-                temp_value = deepcopy(node[1])
-                self.nodes[hash(key) % len(self.nodes)].remove(node)
+                temp_value = copy(node[1])
+                self.nodes[hash(key) % self.size].remove(node)
+                self.length -= 1
                 return temp_value
 
     def update(self, other: Any) -> None:
