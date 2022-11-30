8e3de4bda05bdb420050848d43e953f4f8b44484
Nailia Mamiedova <nailia.mamiedova.work@gmail.com>
2022-11-30 18:55:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,9 @@
 class Dictionary:
-    pass
+    def __setitem__(self, key, value) -> None:
+        self.__dict__[key] = value
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __len__(self):
+        return len(self.__dict__)
