ffa6a1c24f96dd97d16a939997caae814be68f15
Nailia Mamiedova <nailia.mamiedova.work@gmail.com>
2022-11-30 22:39:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
+from typing import Any
+
+
 class Dictionary:
-    def __setitem__(self, key, value) -> None:
+    def __setitem__(self, key: Any, value: Any) -> None:
         self.__dict__[key] = value
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Any) -> Any:
         return self.__dict__[key]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.__dict__)
