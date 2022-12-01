eea12f9d83f02f1b2609e890a4dc11b38c335c3b
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-12-01 15:53:20 +0200

    Pytest was passed

--- app/main.py
+++ app/main.py
@@ -1,2 +1,19 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+
+    def __setitem__(self, key: Any, item: Any) -> None:
+        self.__dict__[key] = item
+
+    def __getitem__(self, key: Any) -> dict:
+        return self.__dict__[key]
+
+    def __len__(self) -> int:
+        return len(self.__dict__)
+
+    def __delitem__(self, key: Any) -> None:
+        del self.__dict__
+
+    def __iter__(self) -> iter:
+        return iter(self.__dict__)
