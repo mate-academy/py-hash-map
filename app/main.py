4869bf1ff7e60c5889f887893d32455565c68aa6
Nailia Mamiedova <nailia.mamiedova.work@gmail.com>
2022-12-04 13:10:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,11 +2,46 @@ from typing import Any
 
 
 class Dictionary:
-    def __setitem__(self, key: Any, value: Any) -> None:
-        self.__dict__[key] = value
+    def __init__(self) -> None:
+        self.capacity = 8
+        self.threshold = int(self.capacity * 2 / 3)
+        self.hash_table = [[] for _ in range(self.capacity)]
+        self.storage = 0
 
-    def __getitem__(self, key: Any) -> Any:
-        return self.__dict__[key]
+    def __setitem__(self, key: (int, float, str, tuple, bool),
+                    value: Any) -> None:
+        if self.storage == self.threshold:
+            self.resize()
+        index = hash(key) % self.capacity
+        while True:
+            if not self.hash_table[index]:
+                self.hash_table[index] = [hash(key), key, value]
+                self.storage += 1
+                break
+            if self.hash_table[index][1] == key:
+                self.hash_table[index][2] = value
+                break
+            index = (index + 1) % self.capacity
+
+    def __getitem__(self, key: (int, float, str, tuple, bool)) -> Any:
+        index = hash(key) % self.capacity
+        while True:
+            try:
+                if self.hash_table[index][1] == key:
+                    return self.hash_table[index][2]
+            except IndexError:
+                raise KeyError
+            index = (index + 1) % self.capacity
 
     def __len__(self) -> int:
-        return len(self.__dict__)
+        return self.storage
+
+    def resize(self) -> None:
+        copy_hash_table = self.hash_table
+        self.storage = 0
+        self.capacity *= 2
+        self.threshold = int(self.capacity * 2 / 3)
+        self.hash_table = [[] for _ in range(self.capacity)]
+        for item in copy_hash_table:
+            if item:
+                self.__setitem__(item[1], item[2])
