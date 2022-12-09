ca58fc27ceaddc22c1a0b548640d93bb9639ba6d
Anastasiia <nastya.gnusova@gmail.com>
2022-12-09 15:03:43 +0200

    'Fixed_solution'

--- app/main.py
+++ app/main.py
@@ -5,19 +5,21 @@ class Dictionary:
     def __init__(self) -> None:
         self.size = 0
         self.capacity = 8
-        self.threshold = int(self.capacity * (2 / 3)) + 1
+        self.threshold = int(self.capacity * (2 / 3))
         self.hash_table = [[]] * self.capacity
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        if self.size == self.threshold:
+        if self.size > self.threshold:
             self.resize()
-        index = hash(key) % self.capacity
+        hash_key = hash(key)
+        index = hash_key % self.capacity
         while True:
             if not self.hash_table[index]:
-                self.hash_table[index] = [key, value]
+                self.hash_table[index] = [key, value, hash_key]
                 self.size += 1
                 break
-            if self.hash_table[index][0] == key:
+            if self.hash_table[index][2] == hash_key and\
+                    self.hash_table[index][0] == key:
                 self.hash_table[index][1] = value
                 break
             index = (index + 1) % self.capacity
@@ -26,7 +28,7 @@ class Dictionary:
         current_table = self.hash_table
         self.size = 0
         self.capacity *= 2
-        self.threshold = int(self.capacity * (2 / 3)) + 1
+        self.threshold = int(self.capacity * (2 / 3))
         self.hash_table = [[]] * self.capacity
         for item in current_table:
             if item:
@@ -35,10 +37,9 @@ class Dictionary:
     def __getitem__(self, key: Hashable) -> list:
         index = hash(key) % self.capacity
         while True:
-            try:
-                if self.hash_table[index][0] == key:
-                    return self.hash_table[index][1]
-            except IndexError:
+            if self.hash_table[index] and key == self.hash_table[index][0]:
+                return self.hash_table[index][1]
+            if not self.hash_table[index]:
                 raise KeyError(f"There is no item named {key} in the dict")
             index = (index + 1) % self.capacity
 
