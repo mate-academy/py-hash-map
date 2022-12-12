1f61db144eb23ba52a067f3788e13df2e98a0b0c
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-12-12 22:59:25 +0800

    Solution Maksym Maksimov

--- app/main.py
+++ app/main.py
@@ -1,21 +1,50 @@
+from typing import Any
+
+
 class Dictionary:
     def __init__(self) -> None:
         self.length = 0
         self.table_size = 8
         self.hash_table: list = [None] * self.table_size
 
-    def __setitem__(self, key, value):
+    def __setitem__(
+            self,
+            key: (int, float, bool, tuple, str),
+            value: Any
+    ) -> None:
         hashed_value = hash(key)
         index = hashed_value % self.table_size
         while self.hash_table[index] is not None:
-            print(f"The key {key} collided with {self.hash_table[index]}")
-            index = (index + 1) % len(self.hash_table)
+            saved_key, saved_hashed_value, saved_value = self.hash_table[index]
+            if saved_key == key and hashed_value == saved_hashed_value:
+                self.hash_table[index] = (key, hashed_value, value)
+                break
+            else:
+                print(f"The key {key} collided with {self.hash_table[index]}")
+                index = (index + 1) % len(self.hash_table)
+        if self.hash_table[index] is None:
+            self.hash_table[index] = (key, hashed_value, value)
+            self.length += 1
+            self.resize()
 
-        self.hash_table[index] = (key, hashed_value, value)
-        self.length += 1
-        self.resize()
+    def __getitem__(
+            self,
+            key: (int, float, bool, tuple, str)
+    ) -> Any:
+        hashed_value = hash(key)
+        index = hashed_value % self.table_size
+        while True:
+            try:
+                if self.hash_table[index][0] == key:
+                    return self.hash_table[index][2]
+            except TypeError:
+                raise KeyError
+            index = (index + 1) % self.table_size
 
-    def resize(self):
+    def __len__(self) -> int:
+        return self.length
+
+    def resize(self) -> None:
         if self.length > 2 / 3 * self.table_size:
             old_hash_table = []
             for container in self.hash_table:
@@ -26,22 +55,3 @@ class Dictionary:
             self.length = 0
             for key, hashed_value, value in old_hash_table:
                 self.__setitem__(key, value)
-
-    def __getitem__(self, key):
-        hashed_value = hash(key)
-        index = hashed_value % len(self.hash_table)
-        while self.hash_table[index] is not None:
-            saved_key, saved_hashed_value, saved_value = self.hash_table[index]
-            if key == saved_key and hashed_value == saved_hashed_value:
-                return saved_value
-            index = (index + 1) % len(self.hash_table)
-
-    def __len__(self):
-        return self.length
-
-
-new_dict = Dictionary()
-print(new_dict.__setitem__(2, "Hello"))
-print(new_dict.__getitem__(2))
-print(new_dict.hash_table)
-print(new_dict.__getitem__(3))
