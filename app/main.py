3d9e11fdbee1bd24aff1fe8a5f9457a8ec4184f3
Tetiana Kozakova <kleotan901@gmail.com>
2023-01-06 15:29:49 +0100

    fixed

--- app/main.py
+++ app/main.py
@@ -4,36 +4,44 @@ from app.point import Point
 
 
 class Dictionary:
-    def __init__(self) -> None:
-        self.length = 8
-        self.size = 0
-        self.hash_table = [(None,)] * self.length
 
-    def resize(self) -> None:
-        if self.size / self.length >= 0.625:
-            self.length *= 2
-            self.hash_table += [(None,)] * self.length
+    def __init__(self) -> None:
+        self.size = 0
+        self.hash_table = [[None]] * 8
+
+    def resize(self) -> list:
+        self.hash_table += [[None]] * len(self.hash_table)
+        for el in self.hash_table:
+            if el[0] is not None:
+                index = hash(el[0]) % len(self.hash_table)
+                self.hash_table.insert(index, self.hash_table.pop(
+                    self.hash_table.index(el)))
+                self.__setitem__(el[0], el[1])
+        return self.hash_table
 
     def __setitem__(
             self,
             key: Union[str, int, tuple, Point],
             value: Any
     ) -> list:
-        self.resize()
-        index = hash(key) % len(self.hash_table)
+        if self.size / len(self.hash_table) >= 0.625:
+            self.hash_table = self.resize()
+
+        len_table = len(self.hash_table)
+        index = hash(key) % len_table
         if self.hash_table[index][0] is None:
             self.hash_table[index] = [key, value]
             self.size += 1
             return self.hash_table
+
         for pairs in self.hash_table:
             if pairs[0] == key:
                 pairs[1] = value
                 return self.hash_table
 
-        next_index = (index + 1) % len(self.hash_table)
+        next_index = (index + 1) % len_table
         while self.hash_table[next_index][0] is not None:
-            next_index = (next_index + 1) % len(self.hash_table)
-
+            next_index = (next_index + 1) % len_table
         self.hash_table[next_index] = [key, value]
         self.size += 1
         return self.hash_table
@@ -47,10 +55,12 @@ class Dictionary:
     def __len__(self) -> int:
         return self.size
 
-    def __delitem__(self, key: Union[str, int, tuple, Point]) -> list:
+    def __delitem__(self, key: Union[str, int, tuple, Point]) -> None:
+        if self.size / len(self.hash_table) >= 0.625:
+            self.hash_table = self.resize()
         index = hash(key) % len(self.hash_table)
-        self.hash_table.pop(index)
-        self.hash_table.insert(index, (None,))
+        del self.hash_table[index]
+        self.hash_table.insert(index, [None])
 
     def clear(self) -> None:
         self.hash_table.clear()
@@ -60,5 +70,5 @@ class Dictionary:
     def pop(self, key: Union[str, int, tuple, Point]) -> Any:
         index = hash(key) % len(self.hash_table)
         del_el = self.hash_table.pop(index)
-        self.hash_table.insert(index, (None,))
+        self.hash_table.insert(index, [None])
         return del_el
