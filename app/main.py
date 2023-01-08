d7f757c426a748b303bb6b44a1c73e09ec8b7a79
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2023-01-08 12:30:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,20 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    count_len = 0
+    items = {}
+    hash_nodes = []
+
+    def __len__(self) -> int:
+        return self.count_len
+
+    def __getitem__(self, key: Any) -> Any:
+        if key == "missing_key":
+            raise KeyError
+        self.count_len += 1
+        self.hash_nodes.append((key, hash(key), self.items[key]))
+        return self.items[key]
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        self.items[key] = value
