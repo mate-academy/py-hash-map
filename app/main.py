8504774fe82f2c79245ea8f3e785fac05b1dd267
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-01-18 17:12:03 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -19,8 +19,8 @@ class Dictionary:
                 self.hash_table[index] = [hash_key, key, value]
                 self.length += 1
                 break
-            if self.hash_table[index][0] == hash_key\
-                    and self.hash_table[index][1] == key:
+            if (self.hash_table[index][0] == hash_key
+                    and self.hash_table[index][1] == key):
                 self.hash_table[index][2] = value
                 break
             index = (index + 1) % self.capacity
@@ -29,8 +29,8 @@ class Dictionary:
         hash_key = hash(key)
         index = hash_key % self.capacity
         while self.hash_table[index] is not None:
-            if self.hash_table[index][0] == hash_key\
-                    and self.hash_table[index][1] == key:
+            if (self.hash_table[index][0] == hash_key
+                    and self.hash_table[index][1] == key):
                 return self.hash_table[index][2]
             index = (index + 1) % self.capacity
         raise KeyError(f"{self.hash_table[index]} is None")
