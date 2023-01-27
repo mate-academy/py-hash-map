7100cf84658a6eaa127be87e0871a3185783564a
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-01-27 21:15:45 +0200

    Solution maybe its better

--- app/main.py
+++ app/main.py
@@ -8,6 +8,16 @@ class Dictionary:
         self.hash_table: list = [[None, None] for _ in range(8)]
         self.capacity = 8
 
+    def key_upp(self, key: int) -> int:
+        key += 1
+        if key == self.capacity:
+            key = 0
+        return key
+
+    def write(self, index: int, key: any, value: any) -> None:
+        self.hash_table[index][0] = key
+        self.hash_table[index][1] = value
+
     def resize(self) -> None:
         self.capacity *= 2
         old_hash_tabel = deepcopy(self.hash_table)
@@ -15,15 +25,14 @@ class Dictionary:
         for pair_key_value in old_hash_tabel:
             if pair_key_value[0] is None:
                 continue
-            new_key_hash = hash(pair_key_value[0]) % self.capacity
+            new_key = hash(pair_key_value[0]) % self.capacity
             for _ in range(self.capacity):
-                if self.hash_table[new_key_hash][0] is None:
-                    self.hash_table[new_key_hash][0] = pair_key_value[0]
-                    self.hash_table[new_key_hash][1] = pair_key_value[1]
+                if self.hash_table[new_key][0] is None:
+                    self.write(new_key, pair_key_value[0], pair_key_value[1])
                     break
-                new_key_hash += 1
-                if new_key_hash == self.capacity:
-                    new_key_hash = 0
+                new_key += 1
+                if new_key == self.capacity:
+                    new_key = 0
         del old_hash_tabel
 
     def __setitem__(self, key: any, value: any) -> None:
@@ -33,9 +42,7 @@ class Dictionary:
                 if self.hash_table[key_hash_rewrite][0] == key:
                     self.hash_table[key_hash_rewrite][1] = value
                     return
-                key_hash_rewrite += 1
-                if key_hash_rewrite == self.capacity:
-                    key_hash_rewrite = 0
+                key_hash_rewrite = self.key_upp(key_hash_rewrite)
 
         if self.length > self.capacity * 2 // 3:
             self.resize()
@@ -43,22 +50,17 @@ class Dictionary:
         key_hash = hash(key) % self.capacity
         for _ in range(self.capacity):
             if self.hash_table[key_hash][0] is None:
-                self.hash_table[key_hash][0] = key
-                self.hash_table[key_hash][1] = value
+                self.write(key_hash, key, value)
                 self.length += 1
                 break
-            key_hash += 1
-            if key_hash == self.capacity:
-                key_hash = 0
+            key_hash = self.key_upp(key_hash)
 
     def __getitem__(self, key: any) -> any:
         key_hash = hash(key) % self.capacity
         for _ in range(self.capacity):
             if self.hash_table[key_hash][0] == key:
                 return self.hash_table[key_hash][1]
-            key_hash += 1
-            if key_hash == self.capacity:
-                key_hash = 0
+            key_hash = self.key_upp(key_hash)
         raise KeyError
 
     def __len__(self) -> int:
