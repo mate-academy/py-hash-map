70556b4966b2ba065fb8049078558048ab1e5020
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-01-28 23:53:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from copy import deepcopy
-from typing import Any
+from typing import Any, Hashable
 
 
 class Dictionary:
@@ -15,7 +15,7 @@ class Dictionary:
             key = 0
         return key
 
-    def write(self, index: int, key: Any, value: Any) -> None:
+    def write(self, index: int, key: Hashable, value: Any) -> None:
         self.hash_table[index][0] = key
         self.hash_table[index][1] = value
 
@@ -23,21 +23,14 @@ class Dictionary:
         self.capacity *= 2
         old_hash_tabel = deepcopy(self.hash_table)
         self.hash_table = [[None, None] for _ in range(self.capacity)]
+        self.length = 0
         for pair_key_value in old_hash_tabel:
             if pair_key_value[0] is None:
                 continue
-            index = hash(pair_key_value[0]) % self.capacity
-            for _ in range(self.capacity):
-                if self.hash_table[index][0] is None:
-                    self.write(index, pair_key_value[0], pair_key_value[1])
-                    break
-                index += 1
-                if index == self.capacity:
-                    index = 0
+            self.__setitem__(pair_key_value[0], pair_key_value[1])
         del old_hash_tabel
 
     def __setitem__(self, key: Any, value: Any) -> None:
-
         if self.length > self.capacity * 2 // 3:
             self.resize()
 
@@ -47,10 +40,9 @@ class Dictionary:
                 self.write(index, key, value)
                 self.length += 1
                 break
-            else:
-                if self.hash_table[index][0] == key:
-                    self.write(index, key, value)
-                    break
+            if self.hash_table[index][0] == key:
+                self.write(index, key, value)
+                break
             index = self.key_upp(index)
 
     def __getitem__(self, key: Any) -> Any:
