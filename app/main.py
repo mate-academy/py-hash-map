4271bd57fc4279c1bacb4eedb5b82a6988569e92
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-01-28 19:48:45 +0200

    Solution 2.0

--- app/main.py
+++ app/main.py
@@ -1,20 +1,31 @@
+from __future__ import annotations
+from typing import Any, Union, Tuple
+
+
 class Dictionary:
     def __init__(self) -> None:
         self.capacity = 8
         self.threshold = int(self.capacity * 2 / 3)
         self.resize_multiplier = 2
         self.size = 0
-        self.hash_table = [[] for _ in range(self.capacity)]
+        self.hash_table = [[]] * self.capacity
 
     def __len__(self) -> int:
         return self.size
 
-    def _get_index_and_hash(self, key):
+    def _get_index_and_hash(
+            self,
+            key: Union[bool, int, float, str, tuple]
+    ) -> Tuple[int, int]:
         index = hash(key) % self.capacity
         hash_of_item = hash(key)
         return hash_of_item, index
 
-    def __setitem__(self, key, value):
+    def __setitem__(
+            self,
+            key: Union[bool, int, float, str, tuple],
+            value: Any
+    ) -> None:
         if self.size > self.threshold:
             self._resize()
         hash_of_item, index = self._get_index_and_hash(key)
@@ -24,27 +35,34 @@ class Dictionary:
                 self.hash_table[index] = node
                 self.size += 1
                 break
-            if self.hash_table[index][0] == key and \
-                    self.hash_table[index][1] == hash_of_item:
+            if (
+                    self.hash_table[index][0] == key
+                    and self.hash_table[index][1] == hash_of_item
+            ):
                 self.hash_table[index][2] = value
                 break
             index = (index + 1) % self.capacity
 
-    def _resize(self):
+    def _resize(self) -> None:
         self.capacity *= self.resize_multiplier
         self.threshold = int(self.capacity * 2 / 3)
         old_hash_list = self.hash_table
         self.size = 0
-        self.hash_table = [[] for _ in range(self.capacity)]
+        self.hash_table = [[]] * self.capacity
         for item in old_hash_list:
             if item:
                 self.__setitem__(item[0], item[2])
 
-    def __getitem__(self, key):
+    def __getitem__(
+            self,
+            key: [bool, int, float, str, list, dict]
+    ) -> Any:
         hash_of_item, index = self._get_index_and_hash(key)
         while self.hash_table[index]:
-            if self.hash_table[index][0] == key and \
-                    self.hash_table[index][1] == hash_of_item:
+            if (
+                    self.hash_table[index][0] == key
+                    and self.hash_table[index][1] == hash_of_item
+            ):
                 return self.hash_table[index][2]
             index = (index + 1) % self.capacity
         raise KeyError
