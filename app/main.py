8e17af69c60fcc6ae9b272edbaaa2ee64f854df8
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-01-29 20:20:50 +0200

    Senk for help

--- app/main.py
+++ app/main.py
@@ -21,16 +21,15 @@ class Dictionary:
 
     def resize(self) -> None:
         self.capacity *= 2
-        old_hash_tabel = deepcopy(self.hash_table)
+        old_hash_table = self.hash_table
         self.hash_table = [[None, None] for _ in range(self.capacity)]
         self.length = 0
-        for pair_key_value in old_hash_tabel:
-            if pair_key_value[0] is None:
-                continue
-            self.__setitem__(pair_key_value[0], pair_key_value[1])
-        del old_hash_tabel
+        for key, value in old_hash_table:
+            if key:
+                self.__setitem__(key, value)
+        del old_hash_table
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         if self.length > self.capacity * 2 // 3:
             self.resize()
 
@@ -45,7 +44,7 @@ class Dictionary:
                 break
             index = self.key_upp(index)
 
-    def __getitem__(self, key: Any) -> Any:
+    def __getitem__(self, key: Hashable) -> Any:
         index = hash(key) % self.capacity
         for _ in range(self.capacity):
             if self.hash_table[index][0] == key:
