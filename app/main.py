d320afc48cc4b0f676f4585105ab34a067abb7fc
Yehor Kornev <dmekkindom00@gmail.com>
2023-02-18 15:55:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,31 +1,47 @@
-from app.hashtable import Hashtable
+import pprint
 
 
 class Dictionary:
-    list_nodes = []
+    def __init__(self, elements):
+        self.len_bucket = len(elements)
+        self.bucket_size = 8
+        self.bucket_resize = self.bucket_size * 2/3
+        while self.len_bucket > self.bucket_resize:
+            self.bucket_size *= 2
+        self.buckets = [[] for i in range(self.bucket_size)]
+        self._assign_buckets(elements)
+        self.len_bucket = len([full_bucket for full_bucket in self.buckets if full_bucket is not None])
 
-    def __init__(self, *args, **kwargs):
-        Dictionary.list_nodes += [kwargs]
+    def _assign_buckets(self, elements):
+        self.buckets = [None] * self.bucket_size
+
+        for key, value in elements:
+            hashed_value = hash(key)
+            index = hashed_value % self.bucket_size
+
+            while self.buckets[index] is not None:
+                print(f"The key {key} collided with {self.buckets[index]}")
+                index = (index + 1) % self.bucket_size
+
+            self.buckets[index] = (key, value,)
 
     def __setitem__(self, key, value):
-        Dictionary.list_nodes.append((key, value))
-        hash_table = Hashtable(Dictionary.list_nodes)
+        while self.len_bucket > self.bucket_resize:
+            self.bucket_size *= 2
+        self.buckets = [[] for i in range(self.bucket_size)]
+        self._assign_buckets((key, value))
 
-    def __getitem__(self, key):
-        return self.key
+    def __getitem__(self, input_key):
+        hashed_value = hash(input_key)
+        index = hashed_value % self.bucket_size
+        while self.buckets[index] is not None:
+            key, value = self.buckets[index]
+            if key == input_key:
+                return value
+            index = (index + 1) % self.bucket_size
+
+    def __str__(self):
+        return pprint.pformat(self.buckets)
 
     def __len__(self):
-        return len(Dictionary.list_nodes)
-
-
-# name_data = Dictionary()
-
-# name_data[4] = 10
-
-# print(name_data[4])
-
-
-def the_same(something):
-    print(something)
-
-the_same([(123123, "awdawd"), (1231, "fweww")])
+        return self.len_bucket
