efeeb125da97310ca627a7837537507c893d5b7e
Yehor Kornev <dmekkindom00@gmail.com>
2023-02-18 15:21:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,31 @@
+from app.hashtable import Hashtable
+
+
 class Dictionary:
-    pass
+    list_nodes = []
+
+    def __init__(self, *args, **kwargs):
+        Dictionary.list_nodes += [kwargs]
+
+    def __setitem__(self, key, value):
+        Dictionary.list_nodes.append((key, value))
+        hash_table = Hashtable(Dictionary.list_nodes)
+
+    def __getitem__(self, key):
+        return self.key
+
+    def __len__(self):
+        return len(Dictionary.list_nodes)
+
+
+# name_data = Dictionary()
+
+# name_data[4] = 10
+
+# print(name_data[4])
+
+
+def the_same(something):
+    print(something)
+
+the_same([(123123, "awdawd"), (1231, "fweww")])
