38d3a8ba81999fb4d4c4b4c4330e3539e5f3a7d3
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-19 18:56:39 +0200

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -1,22 +1,27 @@
 from __future__ import annotations
+from typing import Hashable, Any
 
 
 class Node:
-    def __init__(self, key, value, hash_val=None):
+    def __init__(
+            self,
+            key: Hashable,
+            value: Any,
+            hash_val: int = None
+    ) -> None:
         self.key = key
         self.value = value
         self.hash = hash(key) if hash_val is None else hash_val
 
 
 class Dictionary:
-    def __init__(self, capacity=8, load_factor=2 / 3):
+    def __init__(self, capacity: int = 8, load_factor: int = 2 / 3) -> None:
         self.capacity = capacity
         self.load_factor = load_factor
         self.size = 0
         self.table = [[] for _ in range(self.capacity)]
 
-    def __setitem__(self, key, value):
-
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         hash_val = hash(key)
         store = self.table[hash_val % self.capacity]
         for node in store:
@@ -28,7 +33,7 @@ class Dictionary:
         if self.size >= self.capacity * self.load_factor:
             self.resize()
 
-    def resize(self):
+    def resize(self) -> None:
         self.capacity *= 2
         new_table = [[] for _ in range(self.capacity)]
 
@@ -38,7 +43,7 @@ class Dictionary:
                 new_table[node.hash % self.capacity].append(node)
         self.table = new_table
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Hashable) -> Any:
         hash_val = hash(key)
         store = self.table[hash_val % self.capacity]
         for node in store:
@@ -46,10 +51,10 @@ class Dictionary:
                 return node.value
         raise KeyError(key)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.size
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: Hashable) -> None:
         hash_val = hash(key)
         store = self.table[hash_val % self.capacity]
         for i, node in enumerate(store):
@@ -59,29 +64,29 @@ class Dictionary:
                 return
         raise KeyError(key)
 
-    def clear(self):
+    def clear(self) -> None:
         self.size = 0
         self.table = [[] for _ in range(self.capacity)]
 
-    def get(self, key, default=None):
+    def get(self, key: Hashable) -> Any:
         try:
             return self[key]
         except KeyError:
-            return default
+            return
 
-    def pop(self, key, default=None):
+    def pop(self, key: Hashable) -> Any:
         try:
             value = self[key]
             del self[key]
             return value
         except KeyError:
-            return default
+            return
 
-    def update(self, other):
+    def update(self, other: dict or Dictionary) -> None:
         for key, value in other.items():
             self[key] = value
 
-    def __iter__(self):
+    def __iter__(self) -> None:
         for store in self.table:
             for node in store:
                 yield node.key
