1f340a64b3a21e5a471fdccb2175143a70bff0f6
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-02-21 15:43:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,46 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.length = 0
+        self.size = 8
+        self.hash_table = [None] * self.size
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        if self.length > int(self.size * 2 / 3):
+            self.resize()
+        hashed = hash(key)
+        index = hashed % self.size
+        while True:
+            if not self.hash_table[index]:
+                self.hash_table[index] = [key, value, hashed]
+                self.length += 1
+                break
+            elif self.hash_table[index][0] == key and \
+                    self.hash_table[index][2] == hashed:
+                self.hash_table[index][1] = value
+                break
+            else:
+                index = (index + 1) % self.size
+
+    def __getitem__(self, key: Any) -> Any:
+        hashed = hash(key)
+        index = hashed % self.size
+        while self.hash_table[index]:
+            if self.hash_table[index][0] == key:
+                return self.hash_table[index][1]
+            index = (index + 1) % self.size
+        raise KeyError
+
+    def __len__(self) -> int:
+        return self.length
+
+    def resize(self) -> None:
+        self.size *= 2
+        self.length = 0
+        old_table = self.hash_table
+        self.hash_table = [None] * self.size
+        for item in old_table:
+            if item:
+                self.__setitem__(item[0], item[1])
