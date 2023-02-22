b91a6b07bb5befcf82f7806cd72c4403ccf00d29
Ruslan Yanyshyn <ruslan.yanyshyn@yahoo.com>
2023-02-22 14:01:11 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -2,22 +2,21 @@ from typing import Any, Hashable
 
 
 class Dictionary:
-    def __init__(self, capacity: int = 10, load_factor: float = 0.75) -> None:
+    def __init__(self, capacity: int = 10,) -> None:
         self.capacity = capacity
-        self.load_factor = load_factor
-        self.threshold = int(self.capacity * self.load_factor)
+        self.threshold = int(self.capacity * 0.75)
         self.size = 0
-        self.table = [[]] * self.capacity
+        self.table = [None] * self.capacity
 
     def resize(self) -> None:
         self.capacity *= 2
-        self.threshold = int(self.capacity * self.load_factor)
+        self.threshold = int(self.capacity * 0.75)
         self.size = 0
         old_table = self.table
         self.table = [None] * self.capacity
         for cell in old_table:
             if cell:
-                self.__setitem__(cell[0], cell[2])
+                self.__setitem__(cell[0], cell[1])
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
         if self.size > self.threshold:
@@ -26,26 +25,26 @@ class Dictionary:
         index = current_hash % self.capacity
         while True:
             if not self.table[index]:
-                self.table[index] = [key, current_hash, value]
+                self.table[index] = [key, value, current_hash]
                 self.size += 1
                 break
             if (
                 self.table[index][0] == key
-                and self.table[index][1] == current_hash
+                and self.table[index][2] == current_hash
             ):
-                self.table[index][2] = value
+                self.table[index][1] = value
                 break
             index = (index + 1) % self.capacity
 
-    def __getitem__(self, key: Hashable) -> list:
+    def __getitem__(self, key: Hashable) -> None:
         current_hash = hash(key)
         index = current_hash % self.capacity
         while self.table[index]:
             if (
-                self.table[index][1] == current_hash
+                self.table[index][2] == current_hash
                 and self.table[index][0] == key
             ):
-                return self.table[index][2]
+                return self.table[index][1]
             index = (index + 1) % self.capacity
         raise KeyError
 
