f5237e02de8b2bf984e2d157a7c95b3937cfe8b8
Andriy <andriykrav@gmail.com>
2023-02-23 22:22:07 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Any
+from typing import Any, Hashable
 
 
 class Dictionary:
@@ -11,27 +11,30 @@ class Dictionary:
     def __len__(self) -> int:
         return self.size
 
-    def __setitem__(self, key: Any, value: Any) -> Any:
+    def __setitem__(self, key: Hashable, value: Any) -> Any:
         index = hash(key) % self.capacity
         treshold = self.capacity * 2 / 3
         while self.node[index] is not None:
             if self.node[index].key == key:
                 self.node[index].value = value
-                return key, value
+                return
             index = (index + 1) % self.capacity
         if self.size > treshold:
-            self.capacity *= 2
-            items = [item for item in self.node if item is not None]
-            self.size = 0
-            self.node = [None] * self.capacity
-            for item in items:
-                self.__setitem__(item.key, item.value)
+            self.resize_hashtable()
             self.__setitem__(key=key, value=value)
-            return key, value
+            return
         self.size += 1
         self.node[index] = Node(key=key, value=value, hash_=hash(key))
 
-    def __getitem__(self, key: Any) -> None:
+    def resize_hashtable(self) -> None:
+        self.capacity *= 2
+        items = [item for item in self.node if item is not None]
+        self.size = 0
+        self.node = [None] * self.capacity
+        for item in items:
+            self.__setitem__(item.key, item.value)
+
+    def __getitem__(self, key: Hashable) -> None:
         index = hash(key) % self.capacity
         while self.node[index] is not None:
             if self.node[index].key == key:
