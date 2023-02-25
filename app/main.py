9da508212156872ab2775ca459541df41e4d8678
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-25 12:15:00 +0200

    change _resize method

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+from copy import deepcopy
 from typing import Any, Hashable
 
 INITIAL_CAPACITY = 8
@@ -21,8 +23,6 @@ class Dictionary:
         self._dict_values: list = []
         self._capacity: int = INITIAL_CAPACITY
         self._hash_table: list = [None] * self._capacity
-        for key, value in kwargs.items():
-            self.__setitem__(key, value)
 
     def __str__(self) -> str:
         items = []
@@ -44,7 +44,7 @@ class Dictionary:
 
         self._hash_table[index] = node
 
-        if self._length / self._capacity >= LOAD_FACTOR:
+        if self._length / self._capacity > LOAD_FACTOR:
             self._resize()
 
     def __getitem__(self, key: Hashable) -> Any:
@@ -90,25 +90,29 @@ class Dictionary:
         return self._dict_values
 
     def items(self) -> list:
-        return [(self.hash_table[i].key, self._hash_table[i].value) for i in
+        return [(self._hash_table[i].key, self._hash_table[i].value) for i in
                 range(len(self._hash_table)) if
                 self._hash_table[i] is not None]
 
     def clear(self) -> None:
-        self.hash_table = []
+        self._hash_table = []
         self._dict_keys = []
         self._dict_values = []
         self._length = 0
 
-    def get(self, key: Hashable) -> Any:
-        return self.__getitem__(key)
+    def get(self, key: Hashable, default: Any = None) -> Any:
+        try:
+            self.__getitem__(key)
+        except KeyError:
+            return default
 
-    def pop(self, _key: Any) -> Any:
-        if self._hash_table[hash(_key) % self._capacity] is None:
-            raise KeyError(_key)
-        key_value = self._hash_table[hash(_key) % self._capacity].value
-        self.__delitem__(_key)
-        return key_value
+    def pop(self, _key: Any, default: Any = None) -> Any:
+        try:
+            key_value = self[_key]
+            self.__delitem__(key_value)
+            return key_value
+        except KeyError:
+            return default
 
     def update(self, new_dict: Dictionary) -> None:
         for key, value in new_dict.items():
@@ -121,16 +125,10 @@ class Dictionary:
 
     def _resize(self) -> None:
         self._capacity *= RESIZE_FACTOR
-        new_table = [None] * self._capacity
+        old_table = self._hash_table
+        self._hash_table = [None] * self._capacity
+        self._length = 0
 
-        for node in self._hash_table:
+        for node in old_table:
             if node is not None:
-                index = node.hash_key % self._capacity
-
-                while (new_table[index] is not None
-                       and new_table[index].key != node.key):
-                    index = (index + 1) % self._capacity
-
-                new_table[index] = node
-
-        self._hash_table = new_table
+                self.__setitem__(node.key, node.value)
