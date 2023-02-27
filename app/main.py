4d1e5143d9ca1b2bb7e99d66033e9b78c372af77
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-02-28 01:33:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,59 @@
+from typing import Any, Hashable
+
+
 class Dictionary:
-    pass
+    def __init__(self, capacity: int = 8, load_factor: float = 0.66) -> None:
+        self.capacity = capacity
+        self.load_factor = load_factor
+        self.length = 0
+        self.limit = capacity * load_factor
+        self.hash_table = [[] for _ in range(capacity)]
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        if self.length > self.limit:
+            self.resize()
+        index = self._get_index(key)
+        for i, (k, h, v) in enumerate(self.hash_table[index]):
+            if k == key and h == hash(key):
+                self.hash_table[index][i] = (key, hash(key), value)
+                return
+        self.hash_table[index].append((key, hash(key), value))
+        self.length += 1
+
+    def __getitem__(self, key: Hashable) -> Any:
+        index = self._get_index(key)
+        for k, h, v in self.hash_table[index]:
+            if k == key and h == hash(key):
+                return v
+        raise KeyError(key)
+
+    def __delitem__(self, key: Hashable) -> None:
+        index = self._get_index(key)
+        for i, (k, h, v) in enumerate(self.hash_table[index]):
+            if k == key and h == hash(key):
+                del self.hash_table[index][i]
+                self.length -= 1
+                return
+        raise KeyError(key)
+
+    def __len__(self) -> int:
+        return self.length
+
+    def _get_index(self, key: Hashable) -> int:
+        index = hash(key) % self.capacity
+        while self.hash_table[index]:
+            k, h, v = self.hash_table[index][0]
+            if k == key and h == hash(key):
+                return index
+            index = (index + 1) % self.capacity
+        return index
+
+    def resize(self) -> None:
+        old_hash_table = self.hash_table
+        self.capacity *= 2
+        self.limit = self.capacity * self.load_factor
+        self.hash_table = [[] for _ in range(self.capacity)]
+        self.length = 0
+        for bucket in old_hash_table:
+            for k, h, v in bucket:
+                self.__setitem__(k, v)
