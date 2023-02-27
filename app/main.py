0258c9621b62c2ed8384482738706e3072f5d1d0
kuznetsb <kuznetsbog@gmail.com>
2023-02-27 13:19:08 +0200

    Fully completed dict

--- app/main.py
+++ app/main.py
@@ -23,8 +23,12 @@ class Dictionary:
 
     def __getitem__(self, key: Any) -> Any:
         index = hash(key) % self.capacity
-        if not self.hash_table[index]:
-            raise KeyError
+        if self.hash_table is None or not self.hash_table[index]:
+            if index + 1 > self.capacity:
+                index = 0
+            if not self.hash_table[index + 1]:
+                raise KeyError
+            index += 1
         while self.hash_table[index][0] != key:
             index += 1
             if index >= self.capacity:
@@ -33,6 +37,19 @@ class Dictionary:
 
     def __len__(self) -> int:
         return self.current_load
+
+    def __delitem__(self, key: Any) -> None:
+        index = hash(key) % self.capacity
+        if self.hash_table is None or not self.hash_table[index]:
+            raise KeyError
+        while self.hash_table[index][0] != key:
+            index += 1
+            if index >= self.capacity:
+                index = 0
+        self.hash_table[index] = None
+
+    def __iter__(self) -> object:
+        return iter({node[0]: node[2] for node in self.hash_table if node})
 
     def check_existing_key(self, node: tuple) -> bool:
         index = node[1] % self.capacity
@@ -46,8 +63,7 @@ class Dictionary:
         if self.current_load >= self.capacity * self.load_factor:
             self.capacity *= 2
             new_hash_table = [None] * self.capacity
-            for exist_node in [node for node in self.hash_table
-                               if node is not None]:
+            for exist_node in [node for node in self.hash_table if node]:
                 index = exist_node[1] % self.capacity
                 index = self.find_empty_node(new_hash_table, index)
                 new_hash_table[index] = exist_node
@@ -59,3 +75,36 @@ class Dictionary:
             if index >= self.capacity:
                 index = 0
         return index
+
+    def clear(self) -> None:
+        self.hash_table = None
+
+    def get(self, key: Any) -> Any:
+        index = hash(key) % self.capacity
+        if self.hash_table is None or not self.hash_table[index]:
+            if index + 1 > self.capacity:
+                index = 0
+            if not self.hash_table[index + 1]:
+                raise KeyError
+            index += 1
+        while self.hash_table[index][0] != key:
+            index += 1
+            if index >= self.capacity:
+                index = 0
+        return self.hash_table[index][2]
+
+    def pop(self, key: Any) -> Any:
+        index = hash(key) % self.capacity
+        if self.hash_table is None or not self.hash_table[index]:
+            raise KeyError
+        while self.hash_table[index][0] != key:
+            index += 1
+            if index >= self.capacity:
+                index = 0
+        value = self.hash_table[index][2]
+        self.hash_table[index] = None
+        return value
+
+    def update(self, new_dict: dict) -> None:
+        for key, value in new_dict.items():
+            self[key] = value
