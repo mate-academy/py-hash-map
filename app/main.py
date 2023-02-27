f48bb3be98d85ce8c9056c4ea2810eb4d15082f3
kuznetsb <kuznetsbog@gmail.com>
2023-02-27 23:52:23 +0200

    Refactored solution

--- app/main.py
+++ app/main.py
@@ -1,108 +1,97 @@
-from typing import Any
+from typing import Hashable, Any
 
 
 class Dictionary:
     def __init__(self) -> None:
-        self.capacity = 8
-        self.load_factor = 2 / 3
-        self.current_load = 0
-        self.hash_table = [None] * self.capacity
+        self._capacity = 8
+        self._load_factor = 2 / 3
+        self._current_load = 0
+        self._hash_table = [None] * self._capacity
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         node = (key, hash(key), value)
-        if self.check_existing_key(node):
+        if self._check_existing_key(node):
             return
 
-        self.current_load += 1
+        self._current_load += 1
 
-        self.check_for_resizing()
+        self._check_for_resizing()
 
-        index = hash(key) % self.capacity
-        index = self.find_empty_node(self.hash_table, index)
-        self.hash_table[index] = node
+        index = hash(key) % self._capacity
+        index = self._find_empty_node(self._hash_table, index)
+        self._hash_table[index] = node
 
-    def __getitem__(self, key: Any) -> Any:
-        index = hash(key) % self.capacity
-        if not self.hash_table or not self.hash_table[index]:
-            if index + 1 >= self.capacity:
-                index = 0
-            if not self.hash_table[index + 1]:
-                raise KeyError
+    def __getitem__(self, key: Hashable) -> Any:
+        index = hash(key) % self._capacity
+        if (
+                not self._hash_table[index]
+                and self._hash_table[(index + 1) % self._capacity]
+        ):
             index += 1
-        while self.hash_table[index][0] != key:
+            index %= self._capacity
+        while self._hash_table[index]:
+            if self._hash_table[index][0] == key:
+                return self._hash_table[index][2]
             index += 1
-            if index >= self.capacity:
-                index = 0
-        return self.hash_table[index][2]
+            index %= self._capacity
+        raise KeyError
 
     def __len__(self) -> int:
-        return self.current_load
+        return self._current_load
 
-    def __delitem__(self, key: Any) -> None:
-        index = hash(key) % self.capacity
-        if not self.hash_table or not self.hash_table[index]:
-            raise KeyError
-        while self.hash_table[index][0] != key:
+    def __delitem__(self, key: Hashable) -> None:
+        index = hash(key) % self._capacity
+        while self._hash_table[index]:
+            if self._hash_table[index][0] == key:
+                self._hash_table[index] = None
+                return
             index += 1
-            if index >= self.capacity:
-                index = 0
-        self.hash_table[index] = None
+            index %= self._capacity
+        raise KeyError
 
     def __iter__(self) -> object:
-        return iter({node[0]: node[2] for node in self.hash_table if node})
+        return iter(node for node in self._hash_table if node)
 
-    def check_existing_key(self, node: tuple) -> bool:
-        index = node[1] % self.capacity
-        for i in range(index, len(self.hash_table)):
-            if self.hash_table[i] and self.hash_table[i][0] == node[0]:
-                self.hash_table[i] = node
+    def _check_existing_key(self, node: tuple) -> bool:
+        index = node[1] % self._capacity
+        while self._hash_table[index]:
+            if self._hash_table[index][0] == node[0]:
+                self._hash_table[index] = node
                 return True
+            index += 1
+            index %= self._capacity
         return False
 
-    def check_for_resizing(self) -> None:
-        if self.current_load >= self.capacity * self.load_factor:
-            self.capacity *= 2
-            new_hash_table = [None] * self.capacity
-            for exist_node in [node for node in self.hash_table if node]:
-                index = exist_node[1] % self.capacity
-                index = self.find_empty_node(new_hash_table, index)
+    def _check_for_resizing(self) -> None:
+        if self._current_load >= self._capacity * self._load_factor:
+            self._capacity *= 2
+            new_hash_table = [None] * self._capacity
+            for exist_node in [node for node in self._hash_table if node]:
+                index = exist_node[1] % self._capacity
+                index = self._find_empty_node(new_hash_table, index)
                 new_hash_table[index] = exist_node
-            self.hash_table = new_hash_table
+            self._hash_table = new_hash_table
 
-    def find_empty_node(self, hash_table: list, index: int) -> int:
+    def _find_empty_node(self, hash_table: list, index: int) -> int:
         while hash_table[index]:
             index += 1
-            if index >= self.capacity:
-                index = 0
+            index %= self._capacity
         return index
 
     def clear(self) -> None:
-        self.hash_table = None
+        self._hash_table = None
 
-    def get(self, key: Any) -> Any:
-        index = hash(key) % self.capacity
-        if not self.hash_table or not self.hash_table[index]:
-            if index + 1 > self.capacity:
-                index = 0
-            if not self.hash_table[index + 1]:
-                raise KeyError
-            index += 1
-        while self.hash_table[index][0] != key:
-            index += 1
-            if index >= self.capacity:
-                index = 0
-        return self.hash_table[index][2]
-
-    def pop(self, key: Any) -> Any:
-        index = hash(key) % self.capacity
-        if not self.hash_table or not self.hash_table[index]:
+    def get(self, key: Hashable, default: Any = None) -> Any:
+        try:
+            return self[key]
+        except KeyError:
+            if default:
+                return default
             raise KeyError
-        while self.hash_table[index][0] != key:
-            index += 1
-            if index >= self.capacity:
-                index = 0
-        value = self.hash_table[index][2]
-        self.hash_table[index] = None
+
+    def pop(self, key: Hashable) -> Any:
+        value = self[key]
+        del self[key]
         return value
 
     def update(self, new_dict: dict) -> None:
