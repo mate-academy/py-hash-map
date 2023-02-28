77b36801de1f28de72a0a2fd7b9b804d1c3d734f
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-02-28 13:57:24 +0200

    Passed 12/14 tests

--- app/main.py
+++ app/main.py
@@ -19,8 +19,8 @@ class Node:
             raise TypeError(f"{key_value} is not hashable")
         self._key = key_value
 
-    def __hash__(self):
-        return hash(self.key)
+    # def __hash__(self):
+    #     return hash(self.key)
 
     def __repr__(self) -> str:
         return f"{self.key}: {self.value}"
@@ -34,33 +34,35 @@ class Dictionary:
         self.bucket = [None] * self.initial_capacity
         self.empty_indexes = list(range(self.initial_capacity))
 
+    def find_index(self, key: Hashable) -> int:
+        return hash(key) % self.initial_capacity
+
     def __setitem__(self, key: Hashable, value: object) -> None:
-        node = Node(key, value)
-        hash_table_index = node.__hash__() % self.initial_capacity
+        hash_table_index = self.find_index(key)
 
         # if index inside list is None:
         if self.bucket[hash_table_index] is None:
-            self.bucket[hash_table_index] = node
+            self.bucket[hash_table_index] = Node(key, value)
             self.empty_indexes.remove(hash_table_index)
 
         # if index inside is occupied
-        if hasattr(self.bucket[hash_table_index], "key"):
+        if self.bucket[hash_table_index] is not None:
             if self.bucket[hash_table_index].key == key:
-                self.bucket[hash_table_index].value = value
+                self.bucket[hash_table_index] = Node(key, value)
             else:
                 random_index = random.choice(self.empty_indexes)
                 self.empty_indexes.remove(random_index)
                 self.bucket[hash_table_index].next.append(random_index)
-                self.bucket[random_index] = node
+                self.bucket[random_index] = Node(key, value)
 
-        if len(self.empty_indexes) <= self.initial_capacity - self.load_factor:
+        if len(self.empty_indexes) + 1 <= self.initial_capacity - self.load_factor:
             self.resize()
 
     def __getitem__(self, key: Hashable) -> None:
         if not isinstance(key, Hashable):
             raise TypeError(f"{key} is not hashable")
 
-        hash_table_index = key.__hash__() % self.initial_capacity
+        hash_table_index = self.find_index(key)
 
         if self.bucket[hash_table_index] is None:
             raise KeyError("Current key does not exist")
@@ -68,9 +70,9 @@ class Dictionary:
         if self.bucket[hash_table_index].key == key:
             return self.bucket[hash_table_index].value
 
-        for node in self.bucket[hash_table_index].next:
-            if node.key == key:
-                return node.value
+        for index in self.bucket[hash_table_index].next:
+            if self.bucket[index].key == key:
+                return self.bucket[index].value
 
     def resize(self) -> None:
         self.initial_capacity *= 2
@@ -84,4 +86,15 @@ class Dictionary:
                 self.__setitem__(node.key, node.value)
 
     def __len__(self) -> int:
-        return len([elem for elem in self.bucket if elem])
+        return len(self.bucket) - len(self.empty_indexes)
+
+
+if __name__ == '__main__':
+    d = Dictionary()
+
+    d[1] = "one"
+    d[2] = "two"
+    d["str"] = "string"
+    d[6] = 6
+    d[8] = 8
+    print(d.bucket)
