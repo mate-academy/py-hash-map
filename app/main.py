f426b0fce5bbf68ac3a104fda9a8b548255b848c
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-02-28 17:32:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from typing import Hashable
 
+INITIAL_CAPACITY = 8
+RESIZE_THRESHOLD = 2 / 3
+CAPACITY_MULTIPLIER = 2
+
 
 class Node:
     def __init__(self, key: Hashable, value: any) -> None:
@@ -7,42 +11,38 @@ class Node:
         self.value = value
         self.next = None
 
+    def __str__(self) -> str:
+        return f"({self.key}, {self.value})"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
 
 class Dictionary:
-    def __init__(self, capacity: int = 8, load_factor: float = 2 / 3) -> None:
-        self.capacity = capacity
+    def __init__(self) -> None:
+        self.capacity = INITIAL_CAPACITY
         self.size = 0
-        self.load_factor = load_factor
-        self.table = [None] * capacity
+        self.load_factor = RESIZE_THRESHOLD
+        self.table = [None] * self.capacity
 
     def __setitem__(self, key: Hashable, value: any) -> None:
-        index = self.hash(key)
+        index = self._calculate_index(key)
         node = self.table[index]
-        if node is None:
-            self.table[index] = Node(key, value)
-            self.size += 1
-        else:
-            while node:
-                if node.key == key:
-                    node.value = value
-                    break
-                if node.next is None:
-                    node.next = Node(key, value)
-                    self.size += 1
-                    break
-                node = node.next
 
-        if self.size / self.capacity >= self.load_factor:
-            self.resize()
+        if node is None:
+            if self.size + 1 >= self.capacity * self.load_factor:
+                self.resize()
+                return self.__setitem__(key, value)
+            self.size += 1
+        self.table[index] = Node(key, value)
 
     def __getitem__(self, key: Hashable) -> str:
-        index = self.hash(key)
-        node = self.table[index]
-        while node:
-            if node.key == key:
-                return node.value
-            node = node.next
-        raise KeyError(f"Key {key} not found")
+        index = self._calculate_index(key)
+
+        if self.table[index] is None:
+            raise KeyError(f"Key {key} not found")
+
+        return self.table[index].value
 
     def __len__(self) -> int:
         return self.size
@@ -52,20 +52,14 @@ class Dictionary:
         self.size = 0
 
     def resize(self) -> None:
-        self.capacity *= 2
-        new_table = [None] * self.capacity
-        for node in self.table:
-            while node:
-                index = self.hash(node.key)
-                if new_table[index] is None:
-                    new_table[index] = Node(node.key, node.value)
-                else:
-                    current = new_table[index]
-                    while current.next:
-                        current = current.next
-                    current.next = Node(node.key, node.value)
-                node = node.next
-        self.table = new_table
+        old_hash_table = self.table
+        self.capacity *= CAPACITY_MULTIPLIER
+        self.table = [None] * self.capacity
+        self.size = 0
+
+        for node in old_hash_table:
+            if node is not None:
+                self.__setitem__(node.key, node.value)
 
     def __delitem__(self, key: Hashable) -> None:
         index = self.hash(key)
@@ -92,23 +86,19 @@ class Dictionary:
         except KeyError:
             return default
 
-    def pop(self) -> tuple:
-        for i, node in enumerate(self.table):
-            if node is not None:
-                self.table[i] = node.next
-                self.size -= 1
-                return node.key, node.value
-        raise KeyError("Dictionary is empty")
-
-    def update(self, other: dict) -> None:
-        for key, value in other.items():
-            self[key] = value
-
     def __iter__(self) -> any:
         for node in self.table:
             while node:
                 yield node.key
                 node = node.next
 
-    def hash(self, key: Hashable) -> int:
-        return hash(key) % self.capacity
+    def _calculate_index(self, key: Hashable) -> int:
+        index = hash(key) % self.capacity
+
+        while (self.table[index] is not None and self.table[index].key != key):
+            index = (index + 1) % self.capacity
+
+        return index
+
+    def __str__(self) -> str:
+        return str(self.table)
