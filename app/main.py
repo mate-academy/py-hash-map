7d6cb9acf684b013050ff8c91e1d65b2e26b5da7
Yehor Kornev <dmekkindom00@gmail.com>
2023-03-01 12:54:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class Dictionary:
             elements = []
         self._bucket_size = 8
         self._bucket_resize = self._bucket_size * 2 / 3
-        self.length = 0
+        self.__length = 0
         self._creating_buckets(len(elements))
         if len(elements):
             self._assign_buckets(elements)
@@ -20,11 +20,7 @@ class Dictionary:
         self._buckets = [[] for i in range(self._bucket_size)]
 
     def _assign_buckets(self, elements: List[tuple]) -> None:
-        for element in elements:
-            _key = element[0]
-            _value = None
-            if len(element) > 1:
-                _value = element[1]
+        for _key, _value in elements:
 
             if isinstance(_key, list | dict | set):
                 raise TypeError(
@@ -37,16 +33,27 @@ class Dictionary:
                 if self._buckets[index][0] == _key:
                     if len(self._buckets[index]) == 1:
                         break
-                    self.length -= 1
+                    self.__length -= 1
                     break
                 index = (index + 1) % self._bucket_size
 
-            if len(element) == 1:
-                self._buckets[index] = (_key,)
-                self.length -= 1
-
             self._buckets[index] = (_key, _value,)
-            self.length += 1
+            self.__length += 1
+
+    def _un_assign_buckets(self, _key: Any) -> None:
+        hashed_value = hash(_key)
+        index = hashed_value % self._bucket_size
+
+        while self._buckets[index]:
+            if self._buckets[index][0] == _key:
+                if len(self._buckets[index]) == 1:
+                    raise KeyError(f'There is no such key: "{_key}"')
+                break
+            index = (index + 1) % self._bucket_size
+        if not self._buckets[index]:
+            raise KeyError(f'There is no such key: "{_key}"')
+        self._buckets[index] = (_key,)
+        self.__length -= 1
 
     def _get_buckets_full(self) -> list[Any]:
         return [
@@ -61,12 +68,12 @@ class Dictionary:
         current_buckets = self._get_buckets_full()
         current_buckets += new_element
 
-        self._creating_buckets(self.length + len(new_element))
-        self.length = 0
+        self._creating_buckets(self.__length + len(new_element))
+        self.__length = 0
         self._assign_buckets(current_buckets)
 
     def __setitem__(self, _key: Any, _value: Any) -> None:
-        if self.length + 1 > self._bucket_resize:
+        if self.__length + 1 > self._bucket_resize:
             self._resize([(_key, _value)])
         self._assign_buckets([(_key, _value)])
 
@@ -85,14 +92,14 @@ class Dictionary:
             index = (index + 1) % self._bucket_size
 
     def __delitem__(self, input_key: Any) -> None:
-        self._assign_buckets([(input_key,)])
+        self._un_assign_buckets(input_key)
 
     def __str__(self) -> str:
         dict_str = "  {\n"
         for _key, _value in self._get_buckets_full():
             dict_str += f"    {_key}: {_value},\n"
         dict_str += "}"
-        return dict_str
+        return f"{self._buckets}"
 
     def __repr__(self) -> str:
         dict_repr = "{"
@@ -102,17 +109,17 @@ class Dictionary:
         return dict_repr
 
     def __len__(self) -> int:
-        return self.length
+        return self.__length
 
     def keys(self) -> list[Any]:
-        return [_key for _key, _value in self._get_buckets_full()]
+        return [_key for _key, _ in self._get_buckets_full()]
 
     def values(self) -> list[Any]:
-        return [_value for _key, _value in self._get_buckets_full()]
+        return [_value for _, _value in self._get_buckets_full()]
 
     def clear(self) -> None:
         self._buckets = [[] for i in range(self._bucket_size)]
-        self.length = 0
+        self.__length = 0
 
     def get(self, _key: Any) -> Any:
         _value = None
@@ -123,6 +130,6 @@ class Dictionary:
         return _value
 
     def update(self, elements: List[tuple]) -> None:
-        if self.length + len(elements) > self._bucket_resize:
+        if self.__length + len(elements) > self._bucket_resize:
             self._resize(elements)
         self._assign_buckets(elements)
