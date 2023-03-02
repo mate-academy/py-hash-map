22ae9d911004d4b4eea1e71b82c555e52dbbe178
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-03-02 14:15:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,57 +1,116 @@
-from typing import Any
+from typing import Hashable, Any
 
 
 class Dictionary:
-    def __init__(self) -> None:
-        self.dict_size = 8
-        self.dict_length = 0
-        self.dict_threshold = int(self.dict_size * 2 / 3)
-        self.hash_list = [[] * self.dict_size]
+    def __init__(
+            self,
+            initial_capacity: int = 8,
+            load_factor: float = 0.75
+    ) -> None:
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+        self.initial_capacity = initial_capacity
+        self.load_factor = load_factor
+        self.size = 0
+        self.table = [None] * initial_capacity
 
-        if self.dict_length > self.dict_threshold:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+
+        hash_val = hash(key)
+        index = hash_val % len(self.table)
+        node = self.table[index]
+
+        while node is not None:
+            if node[0] == key:
+                node[2] = value
+                return
+            node = node[1]
+
+        self.table[index] = [key, self.table[index], value]
+        self.size += 1
+
+        if self.size >= self.load_factor * len(self.table):
             self.resize()
 
-        current_hash = hash(key)
-        index = current_hash % self.dict_size
+    def __getitem__(self, key: Hashable) -> Any:
 
-        while True:
-            if not self.hash_list[index]:
-                self.hash_list[index] = [key, current_hash, value]
-                self.dict_length += 1
-                break
-            if (
-                self.hash_list[index][0] == key
-                and self.hash_list[index][1] == current_hash
-            ):
-                self.hash_list[index][2] = value
-                break
-            index = (index + 1) % self.dict_size
+        hash_val = hash(key)
+        index = hash_val % len(self.table)
+        node = self.table[index]
 
-    def __getitem__(self, key: Any) -> list:
-        current_hash = hash(key)
-        index = current_hash % self.dict_size
+        while node is not None:
+            if node[0] == key:
+                return node[2]
+            node = node[1]
 
-        while self.hash_list[index]:
-            if (
-                self.hash_list[index][1] == current_hash
-                and self.hash_list[index][0] == key
-            ):
-                return self.hash_list[index][2]
-            index = (index + 1) % self.dict_size
-        raise KeyError
+        raise KeyError(key)
 
     def __len__(self) -> int:
-        return self.dict_length
+        return self.size
 
     def resize(self) -> None:
-        self.dict_size *= 2
-        self.dict_threshold = int(self.dict_size * 2 / 3)
-        self.dict_length = 0
-        old_table = self.hash_list
-        self.hash_list = [[]] * self.dict_size
 
-        for item in old_table:
-            if item:
-                self.__setitem__(item[0], item[2])
+        new_capacity = len(self.table) * 2
+        new_table = [None] * new_capacity
+
+        for node in self.table:
+            while node is not None:
+                key, next_node, value = node
+                index = hash(key) % new_capacity
+                new_table[index] = [key, new_table[index], value]
+                node = next_node
+
+        self.initial_capacity = new_capacity
+        self.table = new_table
+
+    def __delitem__(self, key: Hashable) -> None:
+
+        hash_val = hash(key)
+        index = hash_val % len(self.table)
+        node = self.table[index]
+        prev_node = None
+
+        while node is not None:
+            if node[0] == key:
+                if prev_node is None:
+                    self.table[index] = node[1]
+                else:
+                    prev_node[1] = node[1]
+                self.size -= 1
+                return
+            prev_node = node
+            node = node[1]
+
+        raise KeyError(key)
+
+    def clear(self) -> None:
+
+        self.table = [None] * self.initial_capacity
+        self.size = 0
+
+    def get(self, key: Hashable, default: Any = None) -> Any:
+
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def pop(self, key: Hashable, default: Any = None) -> Any:
+
+        try:
+            value = self[key]
+            del self[key]
+            return value
+        except KeyError:
+            return default
+
+    def update(self, other_dict: Any) -> Any:
+
+        for key, value in other_dict.items():
+            self[key] = value
+
+    def __iter__(self) -> None:
+
+        for node in self.table:
+            while node is not None:
+                yield node[0]
+                node = node[1]
