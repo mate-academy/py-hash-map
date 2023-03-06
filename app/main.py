2554dd1c25a1271148b5f658315d1acfef62b239
Yevhen Kaskov <kaskov.e@gmail.com>
2023-03-06 17:58:44 +0200

    Solution like python dictionary style

--- app/main.py
+++ app/main.py
@@ -1,80 +1,50 @@
+from dataclasses import dataclass
 from typing import Hashable, Any
 
 
+@dataclass
+class Node:
+    key: Hashable
+    value: Any
+
+
 class Dictionary:
-    def __init__(self) -> None:
-        self.capacity = 8
-        self.table = [[] for _ in range(self.capacity)]
+    def __init__(self, capacity: int = 8) -> None:
+        self.capacity = capacity
+        self.hash_table: list[Node | list | None] = [None] * self.capacity
         self.size = 0
         self.load_factor = 0.67
 
     def __len__(self) -> int:
         return self.size
 
+    def __str__(self) -> str:
+        return str(self.hash_table)
+
+    def calculate_index(self, key: Hashable) -> int:
+        index = hash(key) % self.capacity
+        while self.hash_table[index] and self.hash_table[index].key != key:
+            index = (index + 1) % self.capacity
+        return index
+
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        if self.size >= len(self.table) * self.load_factor:
-            self.resize()
-        index = hash(key) % len(self.table)
-        bucket = self.table[index]
-        for i, (k, v) in enumerate(bucket):
-            if k == key:
-                bucket[i] = (key, value)
-                break
-        else:
-            bucket.append((key, value))
+        index = self.calculate_index(key)
+        if self.hash_table[index] is None:
+            if self.size >= self.load_factor * self.capacity:
+                self.resize()
+                return self.__setitem__(key, value)
             self.size += 1
+        self.hash_table[index] = Node(key, value)
 
     def __getitem__(self, key: Hashable) -> Any:
-        index = hash(key) % len(self.table)
-        bucket = self.table[index]
-        for k, v in bucket:
-            if k == key:
-                return v
-        raise KeyError(key)
+        index = self.calculate_index(key)
+        if self.hash_table[index] is None:
+            raise KeyError(key)
+        return self.hash_table[index].value
 
     def resize(self) -> None:
-        new_capacity = self.capacity * 2
-        new_table = [[] for _ in range(new_capacity)]
-        for bucket in self.table:
-            for k, v in bucket:
-                index = hash(k) % new_capacity
-                new_table[index].append((k, v))
-        self.table = new_table
-        self.capacity = new_capacity
-
-    def clear(self) -> None:
-        self.table = [[] for _ in range(self.capacity)]
-        self.size = 0
-
-    def __delitem__(self, key: Hashable) -> None:
-        index = hash(key) % len(self.table)
-        bucket = self.table[index]
-        for i, (k, v) in enumerate(bucket):
-            if k == key:
-                del bucket[i]
-                self.size -= 1
-            else:
-                raise KeyError(key)
-
-    def pop(self, key: Hashable, default: Any = None) -> Any:
-        index = hash(key) % len(self.table)
-        bucket = self.table[index]
-        for i, (k, v) in enumerate(bucket):
-            if k == key:
-                del bucket[i]
-                self.size -= 1
-                return v
-        if default:
-            return default
-        raise KeyError(key)
-
-    def update(self, other: dict) -> None:
-        if not isinstance(other, dict):
-            print("Data for update must be dictionary!")
-        for k, v in other.items():
-            self[k] = v
-
-    def __iter__(self) -> iter:
-        for bucket in self.table:
-            for k, v in bucket:
-                yield k
+        old_hash_table = self.hash_table
+        self.__init__(self.capacity * 2)
+        for node in old_hash_table:
+            if node is not None:
+                self.__setitem__(node.key, node.value)
