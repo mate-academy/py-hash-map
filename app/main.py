3cc18057b7702d7c89ff3aa6a84ad24a12d7c2db
Andy <andy77andy@ukr.net>
2023-03-25 20:28:03 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Dictionary:
     def __init__(self) -> None:
         self.length = 0
         self.capacity = 8
-        self.threshold = 2 / 3 * self.capacity
+        self.threshold = 0.67 * self.capacity
         self.bucket = [[]] * self.capacity
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
@@ -15,8 +15,8 @@ class Dictionary:
                 self.bucket[empty_slot] = [key, hash(key), value]
                 self.length += 1
                 break
-            if self.bucket[empty_slot][0] == key and \
-                    self.bucket[empty_slot][1] == hash(key):
+            if self.bucket[empty_slot][0] == key and (
+                    self.bucket[empty_slot][1] == hash(key)):
                 self.bucket[empty_slot][2] = value
                 break
             empty_slot = (empty_slot + 1) % self.capacity
@@ -26,8 +26,8 @@ class Dictionary:
     def __getitem__(self, key: Hashable) -> Any:
         empty_slot = hash(key) % self.capacity
         while self.bucket[empty_slot]:
-            if key == self.bucket[empty_slot][0] and \
-                    self.bucket[empty_slot][1] == hash(key):
+            if key == self.bucket[empty_slot][0] and (
+                    self.bucket[empty_slot][1] == hash(key)):
                 return self.bucket[empty_slot][2]
             empty_slot = (empty_slot + 1) % self.capacity
         raise KeyError(key)
@@ -37,8 +37,10 @@ class Dictionary:
 
     def resize(self) -> None:
         bucket1 = self.bucket
+        self.threshold *= 2
         self.capacity *= 2
-        self.bucket = [[] for _ in range(self.capacity)]
+        self.length = 0
+        self.bucket = [[]] * self.capacity
         for slot in bucket1:
             if slot:
                 self.__setitem__(slot[0], slot[2])
