e933290babcb853c5b9190cf3a3feffa92c18ceb
Vlad Tetruk <vladteteruk01@gmail.com>
2023-05-13 19:35:24 +0300

    use break

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Hashable, Any
 
 
 class Dictionary:
@@ -8,18 +8,19 @@ class Dictionary:
         self.hash_table = [None] * self.initial_capacity
         self.size = 0
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Hashable) -> None:
         index = hash(key) % self.initial_capacity
         while self.hash_table[index] is not None:
             if self.hash_table[index][0] == key:
                 self.hash_table[index][-1] = value
-                return value
+                break
             else:
                 index = (index + 1) % self.initial_capacity
-        self.hash_table[index] = [key, value]
-        self.size += 1
-        if self.size > int(self.initial_capacity * self.load_factor):
-            self.resize()
+        else:
+            self.hash_table[index] = [key, value]
+            self.size += 1
+            if self.size > int(self.initial_capacity * self.load_factor):
+                self.resize()
 
     def resize(self) -> None:
         nodes = [x for x in self.hash_table if x is not None]
@@ -29,7 +30,7 @@ class Dictionary:
         for node in nodes:
             self.__setitem__(node[0], node[1])
 
-    def __getitem__(self, key: Any) -> Any:
+    def __getitem__(self, key: Hashable) -> Any:
         index = hash(key) % self.initial_capacity
         while self.hash_table[index] is not None:
             if self.hash_table[index][0] == key:
