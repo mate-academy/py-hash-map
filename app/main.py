c0d428cf819703a28a3ec420c64677c21c94262f
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-05-15 20:41:00 +0300

    used different algorithm to step by nodes and easier way to count dict len

--- app/main.py
+++ app/main.py
@@ -14,57 +14,43 @@ class Dictionary:
         self.hash_table = [[] for i in range(self.capacity)]
         for key_value in args:
             key, value = key_value
-            current_node = hash(key) % self.capacity
-            node_step = 0
-            hash_node = self.hash_table[current_node]
+            index = hash(key) % self.capacity
+            hash_node = self.hash_table[index]
             while len(hash_node) != 0:
-                node_step += 1
-                if current_node + node_step >= self.capacity:
-                    node_step -= self.capacity
-                hash_node = self.hash_table[current_node + node_step]
+                index = (index + 1) % self.capacity
+                hash_node = self.hash_table[index]
             hash_node.append([hash(key), key, value])
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        if self.__len__() + 1 > self.capacity * self.load_factor:
+        if len(self) + 1 > self.capacity * self.load_factor:
             reinit_args = []
-            for i in range(self.capacity):
-                check_node = self.hash_table[i]
-                if len(check_node) != 0:
-                    reinit_args.append((check_node[0][1], check_node[0][2]))
+            for node in self.hash_table:
+                if len(node) != 0:
+                    reinit_args.append((node[0][1], node[0][2]))
             self.reinit(list(reinit_args))
-
-        current_node = hash(key) % self.capacity
-        node_step = 0
-        hash_node = self.hash_table[current_node]
+        index = hash(key) % self.capacity
+        hash_node = self.hash_table[index]
         while len(hash_node) != 0:
             if hash_node[0][1] == key:
                 hash_node[0][2] = value
                 break
-            node_step += 1
-            if current_node + node_step >= self.capacity:
-                node_step -= self.capacity
-            hash_node = self.hash_table[current_node + node_step]
+            index = (index + 1) % self.capacity
+            hash_node = self.hash_table[index]
         hash_node.append([hash(key), key, value])
 
     def __getitem__(self, key: Any) -> Any:
-        current_node = hash(key) % self.capacity
-        node_step = 0
-        hash_node = self.hash_table[current_node]
+        index = hash(key) % self.capacity
+        hash_node = self.hash_table[index]
         while len(hash_node) != 0:
             try:
                 if hash_node[0][0] == hash(key) and hash_node[0][1] == key:
                     return hash_node[0][2]
             except KeyError as error:
                 raise error
-            node_step += 1
-            if current_node + node_step >= self.capacity:
-                node_step -= self.capacity
-            hash_node = self.hash_table[current_node + node_step]
+            index = (index + 1) % self.capacity
+            hash_node = self.hash_table[index]
         raise KeyError
 
     def __len__(self) -> int:
-        result = 0
-        for i in range(self.capacity):
-            if len(self.hash_table[i]) != 0:
-                result += 1
+        result = sum(bool(node) for node in self.hash_table)
         return result
