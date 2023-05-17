1faf4fc24b0e5f21e00fb40f70f6fdac2fa8a17c
Maks Blazhko <makssimm1@gmail.com>
2023-05-17 18:10:54 +0200

    solution v1

--- app/main.py
+++ app/main.py
@@ -12,27 +12,26 @@ class Dictionary:
         self.buckets = [[] for _ in range(initial_capacity)]
 
     def __setitem__(self, key: Any, value: Any) -> None:
+        if self.size / self.capacity > self.load_factor:
+            self._resize()
         bucket_index = self._get_bucket_index(key)
         bucket = self.buckets[bucket_index]
 
-        for i, (k, _) in enumerate(bucket):
-            if k == key:
-                bucket[i] = (key, value)
+        for i, (stored_key, stored_value, stored_hash) in enumerate(bucket):
+            if stored_key == key:
+                bucket[i] = (key, value, hash(key))
                 return
 
-        bucket.append((key, value))
+        bucket.append((key, value, hash(key)))
         self.size += 1
-
-        if self.size / self.capacity > self.load_factor:
-            self._resize()
 
     def __getitem__(self, key: Any) -> Any:
         bucket_index = self._get_bucket_index(key)
         bucket = self.buckets[bucket_index]
 
-        for find_key, value in bucket:
-            if find_key == key:
-                return value
+        for stored_key, stored_value, stored_hash in bucket:
+            if stored_key == key:
+                return stored_value
 
         raise KeyError(key)
 
@@ -40,15 +39,34 @@ class Dictionary:
         return self.size
 
     def _get_bucket_index(self, key: Any) -> int:
-        return hash(key) % self.capacity
+        hash_value = hash(key)
+        initial_index = hash_value % self.capacity
+        bucket_index = initial_index
+
+        while bucket_index >= len(self.buckets):
+            self._resize()
+
+        bucket = self.buckets[bucket_index]
+
+        while bucket:
+            for stored_key, _, _ in bucket:
+                if stored_key == key:
+                    return bucket_index
+            bucket_index = (bucket_index + 1) % self.capacity
+            bucket = self.buckets[bucket_index]
+
+            if bucket_index == initial_index:
+                raise KeyError(key)
+
+        return bucket_index
 
     def _resize(self) -> None:
         self.capacity *= 2
         new_buckets = [[] for _ in range(self.capacity)]
 
         for bucket in self.buckets:
-            for key, value in bucket:
-                new_bucket_index = hash(key) % self.capacity
-                new_buckets[new_bucket_index].append((key, value))
+            for key, value, _ in bucket:
+                bucket_index = hash(key) % self.capacity
+                new_buckets[bucket_index].append((key, value, hash(key)))
 
         self.buckets = new_buckets
