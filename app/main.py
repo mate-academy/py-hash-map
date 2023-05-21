20c75e7036f67fb561614f178a8e26cd720aedc2
Evgen Shepelenko <ShepelenkoEV@gmail.com>
2023-05-21 21:52:38 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,2 +1,80 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.length = 0
+        self.hash_table: list = [None] * 8
+
+    def __len__(self) -> int:
+        return self.length
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        index_hash_table = hash(key) % len(self.hash_table)
+        if self.hash_table[index_hash_table] is None:
+            self.hash_table[index_hash_table] = (key, value)
+            self.length += 1
+        elif self.hash_table[index_hash_table][0] == key:
+            self.hash_table[index_hash_table] = (key, value)
+        else:
+            for i in range(1, len(self.hash_table)):
+                if self.hash_table[
+                    (index_hash_table + i)
+                    % len(self.hash_table)
+                ] is None:
+                    self.hash_table[
+                        (index_hash_table + i)
+                        % len(self.hash_table)
+                    ] = (key, value)
+                    self.length += 1
+                    break
+                elif self.hash_table[
+                    (index_hash_table + i)
+                    % len(self.hash_table)
+                ][0] == key:
+                    self.hash_table[
+                        (index_hash_table + i)
+                        % len(self.hash_table)
+                    ] = (key, value)
+                    break
+        if self.length > (len(self.hash_table) / 3 * 2):
+            cache_table = self.hash_table
+            self.length = 0
+            self.hash_table = list([None] * len(self.hash_table) * 2)
+            for element in cache_table:
+                if element is not None:
+                    index_hash_table = hash(element[0]) % len(self.hash_table)
+                    if self.hash_table[index_hash_table] is None:
+                        self.hash_table[index_hash_table] = element
+                        self.length += 1
+                    else:
+                        for i in range(1, len(self.hash_table)):
+                            if self.hash_table[
+                                (index_hash_table + i)
+                                % len(self.hash_table)
+                            ] is None:
+                                self.hash_table[
+                                    (index_hash_table + i)
+                                    % len(self.hash_table)
+                                ] = element
+                                self.length += 1
+                                break
+
+    def __getitem__(self, key: Any) -> Any:
+        index_hash_table = hash(key) % len(self.hash_table)
+        if self.hash_table[index_hash_table] is None:
+            raise KeyError
+        elif self.hash_table[index_hash_table][0] == key:
+            return self.hash_table[index_hash_table][1]
+        else:
+            for i in range(1, len(self.hash_table)):
+                if self.hash_table[
+                    (index_hash_table + i)
+                    % len(self.hash_table)
+                ][0] == key:
+                    return self.hash_table[
+                        (index_hash_table + i)
+                        % len(self.hash_table)
+                    ][1]
+                elif self.hash_table[index_hash_table][0] is None:
+                    pass  # KeyError
