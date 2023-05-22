378b0e623c7015d9a2c8a217a813f399026f53ef
Evgen Shepelenko <ShepelenkoEV@gmail.com>
2023-05-22 14:12:08 +0300

    'Solution4'

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class Dictionary:
         self.hash_table = list([None] * len(self.hash_table) * 2)
         for element in cache_table:
             if element is not None:
-                self.__setitem__(element[0], element[1])
+                self[element[0]] = element[1]
 
     def __setitem__(self, key: Any, value: Any) -> None:
         index_hash_table = hash(key) % len(self.hash_table)
@@ -25,29 +25,17 @@ class Dictionary:
         elif self.hash_table[index_hash_table][0] == key:
             self.hash_table[index_hash_table] = (key, value, hash(key))
         else:
-            i = 0
             while True:
-                i += 1
-                if self.hash_table[
-                    (index_hash_table + i)
-                    % len(self.hash_table)
-                ] is None:
-                    self.hash_table[
-                        (index_hash_table + i)
-                        % len(self.hash_table)
-                    ] = (key, value, hash(key))
+                index_hash_table += 1
+                index_hash_table %= len(self.hash_table)
+                if self.hash_table[index_hash_table] is None:
+                    self.hash_table[index_hash_table] = (key, value, hash(key))
                     self.length += 1
                     break
-                elif self.hash_table[
-                    (index_hash_table + i)
-                    % len(self.hash_table)
-                ][0] == key:
-                    self.hash_table[
-                        (index_hash_table + i)
-                        % len(self.hash_table)
-                    ] = (key, value, hash(key))
+                elif self.hash_table[index_hash_table][0] == key:
+                    self.hash_table[index_hash_table] = (key, value, hash(key))
                     break
-        if self.length > (len(self.hash_table) / 3 * 2):
+        if self.length > (len(self.hash_table) * (2 / 3)):
             self.table_resize()
 
     def __getitem__(self, key: Any) -> Any:
@@ -57,12 +45,8 @@ class Dictionary:
         elif self.hash_table[index_hash_table][0] == key:
             return self.hash_table[index_hash_table][1]
         else:
-            for i in range(1, len(self.hash_table)):
-                if self.hash_table[
-                    (index_hash_table + i)
-                    % len(self.hash_table)
-                ][0] == key:
-                    return self.hash_table[
-                        (index_hash_table + i)
-                        % len(self.hash_table)
-                    ][1]
+            while True:
+                index_hash_table += 1
+                index_hash_table %= len(self.hash_table)
+                if self.hash_table[index_hash_table][0] == key:
+                    return self.hash_table[index_hash_table][1]
