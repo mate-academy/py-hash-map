ab21276a3bf720ed8fe280d74e4d6179c8afaec8
Iryna Ishchenko <irynaira@gmail.com>
2023-06-02 17:15:49 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,12 +4,14 @@ from typing import Any
 class Dictionary:
     def __init__(self) -> None:
         self.capacity = 4
+        self.overflow_limit = self.capacity * 2 // 3
         self.len = 0
         self.container = [None] * self.capacity
 
     def resize(self, size: int) -> None:
         container = self.container
         self.capacity = size
+        self.overflow_limit = self.capacity * 2 // 3
         self.container = [None] * size
         self.len = 0
         for item in container:
@@ -19,34 +21,33 @@ class Dictionary:
     def _get_key_position(self, key: Any) -> int:
         key_hash = hash(key) % self.capacity
         position = key_hash
-        while \
-                (self.container[position] is not None) and \
-                (self.container[position][1] > position):
-            position += 1
-        while \
-                (position < self.capacity) and \
-                (self.container[position] is not None) and \
-                (self.container[position][1] < key_hash):
-            position += 1
-        if position >= self.capacity:
-            position = 0
+
+        def skip_looped_shift(container_row: tuple[Any, int, Any]) -> bool:
+            return container_row[1] > position
+
+        def skip_shifting_rows(container_row: tuple[Any, int, Any]) -> bool:
+            return container_row[1] < key_hash
+
+        def skip_collisions(container_row: tuple[Any, int, Any]) -> bool:
+            return (container_row[1] == key_hash) and \
+                   (container_row[0] != key)
+
+        for checker in (
+                skip_looped_shift,
+                skip_shifting_rows,
+                skip_collisions
+        ):
             while \
+                    (position < self.capacity) and \
                     (self.container[position] is not None) and \
-                    (self.container[position][1] < key_hash):
+                    checker(self.container[position]):
                 position += 1
-        while \
-                (position < self.capacity) and \
-                (self.container[position] is not None) and \
-                (self.container[position][1] == key_hash) and \
-                (self.container[position][0] != key):
-            position += 1
-        if position >= self.capacity:
-            position = 0
-            while \
-                    (self.container[position] is not None) and \
-                    (self.container[position][1] == key_hash) and \
-                    (self.container[position][0] != key):
-                position += 1
+            if position >= self.capacity:
+                position = 0
+                while \
+                        (self.container[position] is not None) and \
+                        checker(self.container[position]):
+                    position += 1
         return position
 
     def _move_up(self, position: int) -> None:
@@ -106,7 +107,7 @@ class Dictionary:
         elif self.container[position] is None:
             self.len += 1
         self.container[position] = new_item
-        if self.len > self.capacity * 2 / 3:
+        if self.len > self.overflow_limit:
             self.resize(self.capacity * 2)
 
     def __getitem__(self, key: Any) -> Any:
@@ -115,8 +116,7 @@ class Dictionary:
                 (self.container[position] is not None) and \
                 (self.container[position][0] == key):
             return self.container[position][2]
-        else:
-            raise KeyError(key)
+        raise KeyError(key)
 
     def __len__(self) -> int:
         return self.len
@@ -135,24 +135,15 @@ class Dictionary:
             raise KeyError(key)
 
     def get(self, key: Any, default: Any = None) -> Any:
-        position = self._get_key_position(key)
-        if \
-                (self.container[position] is not None) and \
-                (self.container[position][0] == key):
-            return self.container[position][2]
-        else:
+        try:
+            return self[key]
+        except KeyError:
             return default
 
     def pop(self, key: Any) -> Any:
-        position = self._get_key_position(key)
-        if \
-                (self.container[position] is not None) and \
-                (self.container[position][0] == key):
-            value = self.container[position][2]
-            self._move_up(position)
-            return value
-        else:
-            raise KeyError(key)
+        value = self[key]
+        del self[key]
+        return value
 
     def update(self, other: dict) -> None:
         for key in other:
