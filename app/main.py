7545bc6f382c27ce86d7c73604540f43347210ef
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-09 19:33:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,45 +3,59 @@ from typing import Hashable, Any
 
 class Dictionary:
     def __init__(self) -> None:
-        self.initial_capacity = 8
-        self.load_factor = 0.6
-        self.hash_table = [None] * self.initial_capacity
-        self.size = 0
-
-    def __setitem__(self, key: Hashable, value: Hashable) -> None:
-        index = hash(key) % self.initial_capacity
-        while self.hash_table[index] is not None:
-            if self.hash_table[index][0] == key:
-                self.hash_table[index][-1] = value
-                break
-            else:
-                index = (index + 1) % self.initial_capacity
-        else:
-            self.hash_table[index] = [key, value]
-            self.size += 1
-            if self.size > int(self.initial_capacity * self.load_factor):
-                self.resize()
+        self.load_factor = 2 / 3
+        self.capacity = 8
+        self.length = 0
+        self.hash_list = [[]] * self.capacity
+        self.threshold = int(self.capacity * self.load_factor)
 
     def resize(self) -> None:
-        nodes = [x for x in self.hash_table if x is not None]
-        self.initial_capacity *= 2
-        self.hash_table = [None] * self.initial_capacity
-        self.size = 0
-        for node in nodes:
-            self.__setitem__(node[0], node[1])
+        self.capacity *= 2
+        self.threshold = int(self.capacity * self.load_factor)
+        self.length = 0
+        old_hash_list = self.hash_list
+        self.hash_list = [[]] * self.capacity
+
+        for hash_key_value in old_hash_list:
+            if hash_key_value:
+                hash_, key, value = hash_key_value
+                self.__setitem__(key, value)
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+
+        if self.length > self.threshold:
+            self.resize()
+
+        current_hash = hash(key)
+        index = current_hash % self.capacity
+
+        while True:
+            if not self.hash_list[index]:
+                self.hash_list[index] = current_hash, key, value
+                self.length += 1
+                break
+
+            if (
+                    current_hash == self.hash_list[index][0]
+                    and key == self.hash_list[index][1]
+            ):
+                self.hash_list[index] = current_hash, key, value
+                break
+
+            index = (index + 1) % self.capacity
 
     def __getitem__(self, key: Hashable) -> Any:
-        index = hash(key) % self.initial_capacity
-        while self.hash_table[index] is not None:
-            if self.hash_table[index][0] == key:
-                return self.hash_table[index][1]
-            index = (index + 1) % self.initial_capacity
-        raise KeyError
+        current_hash = hash(key)
+        index = current_hash % self.capacity
+        while self.hash_list[index]:
+            if (
+                    current_hash == self.hash_list[index][0]
+                    and key == self.hash_list[index][1]
+            ):
+                return self.hash_list[index][2]
 
-    def __len__(self) -> int:
-        return self.size
+            index = (index + 1) % self.capacity
+        raise KeyError()
 
-    def clear(self) -> None:
-        self.initial_capacity = 8
-        self.hash_table = [None] * self.initial_capacity
-        self.size = 0
+    def __len__(self) -> any:
+        return self.length
