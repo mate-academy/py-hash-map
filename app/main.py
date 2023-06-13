8f397731a98fac4ef9235326768f950a6129f555
S-SMTN <cven28@gmail.com>
2023-06-13 23:07:04 +0300

    implemented custom dictionary

--- app/main.py
+++ app/main.py
@@ -8,22 +8,14 @@ class Dictionary:
         self.__capacity = 8
         self.clear()
 
-    @property
-    def capacity(self) -> int:
-        return self.__capacity
-
-    @property
-    def data(self) -> list[list]:
-        return self.__data
-
     def __capacity_resize(self) -> None:
         self.__capacity *= 2
 
     def __need_resize(self) -> bool:
-        return (self.__len__() + 1) / self.capacity > 2 / 3
+        return (self.__len__() + 1) / self.__capacity > 2 / 3
 
     def __rewrite_data(self) -> None:
-        data = copy(self.data)
+        data = copy(self.__data)
         self.clear()
         for cell in data:
             if cell[0]:
@@ -34,38 +26,35 @@ class Dictionary:
             self.__capacity_resize()
             self.__rewrite_data()
         key_hash = hash(key)
-        cell = key_hash % self.capacity
+        cell = key_hash % self.__capacity
         while True:
-            if not self.data[cell][0] or (
-                self.data[cell][0] == key_hash
-                and self.data[cell][1] == key
-            ):
-                self.data[cell] = [key_hash, key, value]
+            if not self.__data[cell][0] or self.__data[cell][1] == key:
+                self.__data[cell] = [key_hash, key, value]
                 return
-            cell = (cell + 1) % self.capacity
+            cell = (cell + 1) % self.__capacity
 
     def __getitem__(self, key: Any) -> Any:
         cell = self.__find_item(key)
-        return self.data[cell][2]
+        return self.__data[cell][2]
 
     def __find_item(self, key: Any) -> int:
         key_hash = hash(key)
-        cell = start = key_hash % self.capacity
+        cell = start = key_hash % self.__capacity
         while cell != start - 1:
-            if self.data[cell][1] == key:
+            if self.__data[cell][1] == key:
                 return cell
-            cell = (cell + 1) % self.capacity
+            cell = (cell + 1) % self.__capacity
         raise KeyError("There is no such key!")
 
     def __len__(self) -> int:
-        return len([1 for cell in self.data if cell[0]])
+        return len([1 for cell in self.__data if cell[0]])
 
     def clear(self) -> None:
-        self.__data = [[None, None, None] for _ in range(self.capacity)]
+        self.__data = [[None, None, None] for _ in range(self.__capacity)]
 
     def __delitem__(self, key: Any) -> None:
         cell = self.__find_item(key)
-        self.data[cell] = [None, None, None]
+        self.__data[cell] = [None, None, None]
 
     def get(self, key: Any, value: Any) -> Any:
         try:
@@ -81,7 +70,7 @@ class Dictionary:
             self.__setitem__(key, value)
 
     def __iter__(self) -> None:
-        cells = [i for i in self.data if i[0]]
+        cells = [i for i in self.__data if i[0]]
         current_element = 0
         while current_element < len(cells):
             yield cells[current_element][1], cells[current_element][2]
