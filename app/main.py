29ce1731491814d9bc200f801f3ba3c2e55d1661
Anton Komarov <anton_komarov_qa@ukr.net>
2023-06-20 22:08:44 +0300

    'Changed annotations, made order (key, hash, value)'

--- app/main.py
+++ app/main.py
@@ -1,22 +1,25 @@
-from typing import Any
+from typing import Hashable, Any
 
 
 class Dictionary:
     def __init__(self) -> None:
-        self.length: int = 0
-        self.capacity: int = 8
-        self.load_factor: float = 0.66
-        self.hash_table: list = [None] * self.capacity
+        self.length = 0
+        self.capacity = 8
+        self.load_factor = 0.66
+        self.hash_table = [None] * self.capacity
 
-    def __add_to_hash_table(self, key_hash: int, key: Any, value: Any) -> None:
+    def __add_to_hash_table(self,
+                            key: Hashable,
+                            key_hash: int,
+                            value: Any) -> None:
         _index = key_hash % self.capacity
         while True:
             if not self.hash_table[_index]:
-                self.hash_table[_index] = (key_hash, key, value)
+                self.hash_table[_index] = (key, key_hash, value)
                 self.length += 1
                 break
-            elif self.hash_table[_index][1] == key:
-                self.hash_table[_index] = (key_hash, key, value)
+            elif self.hash_table[_index][0] == key:
+                self.hash_table[_index] = (key, key_hash, value)
                 break
             _index += 1
             if _index == len(self.hash_table):
@@ -31,22 +34,19 @@ class Dictionary:
             if item:
                 self.__add_to_hash_table(*item)
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         if self.length == int(self.capacity * self.load_factor):
             self.__resize_hash_table()
         key_hash = hash(key)
-        self.__add_to_hash_table(key_hash, key, value)
+        self.__add_to_hash_table(key, key_hash, value)
 
-    def __getitem__(self, key: Any) -> Any:
+    def __getitem__(self, key: Hashable) -> Any:
         key_hash = hash(key)
         key_index = key_hash % self.capacity
-        if self.hash_table[key_index]:
-            if self.hash_table[key_index][1] == key:
+        while self.hash_table[key_index]:
+            if self.hash_table[key_index][0] == key:
                 return self.hash_table[key_index][2]
-            elif self.hash_table[key_index][1] != key:
-                for item in self.hash_table:
-                    if item and item[1] == key:
-                        return item[2]
+            key_index = (key_index + 1) % self.capacity
         raise KeyError(f"No key: {key} in dictionary")
 
     def __len__(self) -> int:
