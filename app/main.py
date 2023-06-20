10d3991a57b14772d1ad2f2612641e83d1794fcf
Oleksii Kolii <igravkompik@gmail.com>
2023-06-20 21:40:23 +0300

    refactored code

--- app/main.py
+++ app/main.py
@@ -13,12 +13,12 @@ class Dictionary:
             self.resize()
         index = self.get_index(key)
         while True:
-            if self.hash_table[index] is None:
-                self.hash_table[index] = (key, self.get_index(key), value)
+            if not self.hash_table[index]:
+                self.hash_table[index] = (key, hash(key), value)
                 break
             else:
                 if self.hash_table[index][0] == key:
-                    self.hash_table[index] = (key, self.get_index(key), value)
+                    self.hash_table[index] = (key, hash(key), value)
                     break
                 index = index + 1 if index < self.capacity - 1 else 0
 
@@ -26,7 +26,7 @@ class Dictionary:
         index = self.get_index(item)
         cnt = 0
         while cnt < self.capacity:
-            if self.hash_table[index] is not None:
+            if self.hash_table[index]:
                 if self.hash_table[index][0] == item:
                     return self.hash_table[index][2]
             index = index + 1 if index < self.capacity - 1 else 0
@@ -35,10 +35,10 @@ class Dictionary:
 
     def resize(self) -> None:
         old_table = self.hash_table
-        self.hash_table = [None] * (self.capacity * 2)
         self.capacity *= 2
+        self.hash_table = [None] * self.capacity
         for item in old_table:
-            if item is not None:
+            if item:
                 self.__setitem__(item[0], item[2])
 
     def __len__(self) -> int:
