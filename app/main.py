f316b8eb4f88fea93d65e7592259ce64bf8c40b8
YOUR_NAME <YOUR_EMAIL>
2023-06-21 14:37:05 +0300

    My dictionary (fist refactor commit)

--- app/main.py
+++ app/main.py
@@ -23,15 +23,15 @@ class Dictionary:
         return self.length
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        size = (len(self.hash_table) // 3) * 2
-        if size == self.length:
+        if self.length + 1 > self.capacity * RESIZE_THRESHOLD:
             self.resize_table()
+
         hash_key = hash(key)
         key_index = hash_key % self.capacity
+
         while self.hash_table[key_index]:
             if (self.hash_table[key_index].hash_data == hash_key
                     and self.hash_table[key_index].key == key):
-
                 self.hash_table[key_index].value = value
                 return
             else:
@@ -45,58 +45,35 @@ class Dictionary:
         self.capacity *= CAPACITY_MULTIPLIER
 
         new_hash_table: List[Node | None] = [None] * self.capacity
+        node_data = [node for node in self.hash_table if node is not None]
 
-        for node in self.hash_table:
-            if node:
-                key_index = node.hash_data % self.capacity
-                while new_hash_table[key_index]:
-                    if (new_hash_table[key_index].hash_data == node.hash_data
-                            and new_hash_table[key_index].key == node.key):
+        for node in node_data:
+            key_index = node.hash_data % self.capacity
+            while new_hash_table[key_index]:
+                if (new_hash_table[key_index].hash_data == node.hash_data
+                        and new_hash_table[key_index].key == node.key):
 
-                        self.hash_table[key_index].value = node.value
-                        return
-                    else:
-                        key_index = (key_index + 1) % self.capacity
+                    self.hash_table[key_index].value = node.value
+                    return
+                else:
+                    key_index = (key_index + 1) % self.capacity
 
-                new_hash_table[key_index] = node
-                self.length += 1
+            new_hash_table[key_index] = node
+            self.length += 1
 
         self.hash_table = new_hash_table
 
-    def __getitem__(self, key: Hashable) -> [Any, Exception]:
+    def __getitem__(self, key: Hashable) -> Any:
 
         key_hash = hash(key)
 
         index = key_hash % self.capacity
 
-        while self.hash_table[index] is not None and self.hash_table[index].key != key:
+        while (self.hash_table[index] is not None
+               and self.hash_table[index].key != key):
             index = (index + 1) % self.capacity
 
         if self.hash_table[index] is None:
             raise KeyError(f"Key {key} doesn't exist!")
 
         return self.hash_table[index].value
-
-
-d = Dictionary()
-
-d[1] = "A"
-print(d.hash_table)
-print(d.length)
-d[9] = "B"
-print(d.hash_table)
-print(d.length)
-d[1] = "NEW - AAAA"
-print(d.hash_table)
-print(d.length)
-d[4] = "D"
-print(d.hash_table)
-print(d.length)
-d[5] = "E"
-print(d.hash_table)
-print(d.length)
-d[6] = "D"
-print(d.hash_table)
-print(d.length)
-
-print(d[1])
