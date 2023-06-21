cb20abd0a6d4afe23b395696f564ff8836b46d3a
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-21 17:06:42 +0300

    Changed "_" to None and fix delitem

--- app/main.py
+++ app/main.py
@@ -13,13 +13,13 @@ class Dictionary:
         self.capacity = 8
         self.size = 0
         self.threshold = round(self.capacity * 2 / 3)
-        self.hash_table = ["_" for i in range(self.capacity)]
+        self.hash_table = [None for i in range(self.capacity)]
 
     def collision(self, node: Node, node_ind: int) -> int:
         curr_id = node_ind
         while True:
             if curr_id < len(self.hash_table):
-                if self.hash_table[curr_id] == "_":
+                if self.hash_table[curr_id] is None:
                     return curr_id
                 if self.hash_table[curr_id].key == node.key:
                     return curr_id
@@ -32,10 +32,10 @@ class Dictionary:
         self.capacity *= 2
         self.threshold = round(self.capacity * 2 / 3)
         old_table = self.hash_table.copy()
-        self.hash_table = ["_" for i in range(self.capacity)]
+        self.hash_table = [None for i in range(self.capacity)]
         self.size = 0
         for elem in old_table:
-            if elem != "_":
+            if elem is not None:
                 self.__setitem__(elem.key, elem.value)
 
     def __setitem__(self, key: Any, value: Any) -> None:
@@ -43,7 +43,7 @@ class Dictionary:
         self.size += 1
         if self.size <= self.threshold:
             ind = hash(new_node.key) % self.capacity
-            if self.hash_table[ind] != "_":
+            if self.hash_table[ind] is not None:
                 ind = self.collision(new_node, ind)
             self.hash_table[ind] = new_node
         else:
@@ -61,7 +61,7 @@ class Dictionary:
 
     def __getitem__(self, key: Any) -> Node:
         for node in self.hash_table:
-            if node != "_":
+            if node is not None:
                 if node.key == key:
                     return node.value
         raise KeyError
@@ -69,12 +69,14 @@ class Dictionary:
     def __len__(self) -> int:
         len_ = 0
         for node in self.hash_table:
-            if node != "_":
+            if node is not None:
                 len_ += 1
         return len_
 
     def clear(self) -> None:
-        self.hash_table = ["_" for i in range(self.capacity)]
+        self.size = 0
+        self.hash_table = [None for i in range(self.capacity)]
 
     def __delitem__(self, key: Any) -> None:
-        self.hash_table[key] = "_"
+        self.hash_table[key] = None
+        self.size -= 1
