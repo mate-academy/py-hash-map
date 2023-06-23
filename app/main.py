ede0d630dc6df3aa6c6a4cf8600f5aba00c246df
Mariia Horbova <gorbova21@gmail.com>
2023-06-23 13:20:34 +0300

    solution changed

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
-from typing import Any
+from typing import Hashable, Any
 
 
 class Dictionary:
-    def __init__(self,
-                 initial_capacity: int = 8,
-                 load_factor: float = 0.75
-                 ) -> None:
-        self.initial_capacity = initial_capacity
-        self.load_factor = load_factor
+    initial_capacity: int = 8
+    load_factor: float = 2 / 3
+
+    def __init__(self, capacity: int = initial_capacity) -> None:
+        self.initial_capacity = capacity
+        self.load_factor = Dictionary.load_factor
         self.size = 0
-        self.capacity = initial_capacity
+        self.capacity = self.initial_capacity
         self.table = [None] * self.capacity
 
     def __setitem__(self,
-                    key: int | float | str | bool | tuple,
+                    key: Hashable,
                     value: Any) -> None:
         index = self._get_index(key)
         if self.table[index] is None:
@@ -23,35 +23,30 @@ class Dictionary:
             if node[0] == key:
                 node[2] = value
                 return
-        nodes.append([key, self._hash(key), value])
+        nodes.append([key, hash(key), value])
         self.size += 1
         self._check_resize()
 
     def __getitem__(self,
-                    key: int | float | str | bool | tuple
+                    key: Hashable
                     ) -> Any:
         index = self._get_index(key)
         nodes = self.table[index]
-        if nodes is not None:
-            for node in nodes:
-                if node[0] == key:
-                    return node[2]
-        raise KeyError(key)
+        if not nodes:
+            raise KeyError(f"{key} not found")
+        for node in nodes:
+            if node[0] == key:
+                return node[2]
+
 
     def __len__(self) -> int:
         return self.size
 
     def _get_index(self,
-                   key: int | float | str | bool | tuple
+                   key: Hashable
                    ) -> int:
         hash_value = hash(key)
         return hash_value % self.capacity
-
-    @staticmethod
-    def _hash(key: Any) -> int:
-        if isinstance(key, list or dict or set):
-            raise TypeError(f"`{type(key)}` can not be the key")
-        return hash(key)
 
     def _check_resize(self) -> None:
         load_factor = self.size / self.capacity
@@ -59,17 +54,13 @@ class Dictionary:
             self._resize()
 
     def _resize(self) -> None:
-        new_capacity = self.capacity * 2
-        new_table = [None] * new_capacity
-        for nodes in self.table:
-            if nodes is not None:
-                for node in nodes:
-                    index = node[1] % new_capacity
-                    if new_table[index] is None:
-                        new_table[index] = []
-                    new_table[index].append(node)
-        self.capacity = new_capacity
-        self.table = new_table
+        old_hash_table = self.table
+
+        self.__init__(self.capacity * 2)
+
+        for node in old_hash_table:
+            if node is not None:
+                self.__setitem__(node[0], node[2])
 
     def clear(self) -> None:
         self.table = [None] * self.initial_capacity
