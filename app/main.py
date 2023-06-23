1402e21d05c6ec17a3eaf1f31e8ebe6f57e71ce1
Oleksii Kolii <igravkompik@gmail.com>
2023-06-23 14:43:42 +0300

    optimized setitem, fixed key annotation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Hashable
 
 
 class Dictionary:
@@ -8,28 +8,24 @@ class Dictionary:
         self.load_factor = 0.66
         self.hash_table = [None] * self.capacity
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         if self.hash_table.count(None) < int(self.capacity * self.load_factor):
             self.resize()
         index = self.get_index(key)
         while True:
-            if not self.hash_table[index]:
+            if not self.hash_table[index] or self.hash_table[index][0] == key:
                 self.hash_table[index] = (key, hash(key), value)
                 break
-            else:
-                if self.hash_table[index][0] == key:
-                    self.hash_table[index] = (key, hash(key), value)
-                    break
-                index = index + 1 if index < self.capacity - 1 else 0
+            index = (index + 1) % self.capacity
 
     def __getitem__(self, item: Any) -> Any:
         index = self.get_index(item)
         cnt = 0
         while cnt < self.capacity:
-            if self.hash_table[index]:
+            if self.hash_table[index] is not None:
                 if self.hash_table[index][0] == item:
                     return self.hash_table[index][2]
-            index = index + 1 if index < self.capacity - 1 else 0
+            index = (index + 1) % self.capacity
             cnt += 1
         raise KeyError
 
