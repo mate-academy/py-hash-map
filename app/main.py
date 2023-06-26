445fe6bd5f7dc70524e27609a08ef3ad6cfc35ea
Alex Yatsun <starcrafter4444@gmail.com>
2023-06-26 16:43:33 +0300

    Improvement 1

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Hashable, Any
 
 
 class Dictionary:
@@ -7,7 +7,7 @@ class Dictionary:
         self.capacity = 8
         self.hash_table = [None] * self.capacity
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         index = hash(key) % self.capacity
         if self.length >= round(self.capacity * 2 / 3) - 1:
             self.length = 0
@@ -17,12 +17,12 @@ class Dictionary:
             for node in hash_table:
                 if node:
                     self[node[0]] = node[2]
-        if self.hash_table[index] is None:
+        if not self.hash_table[index]:
             self.length += 1
             self.hash_table[index] = [key, hash(key), value]
         else:
             while True:
-                if self.hash_table[index] is None:
+                if not self.hash_table[index]:
                     self.length += 1
                     self.hash_table[index] = [key, hash(key), value]
                     break
@@ -32,18 +32,16 @@ class Dictionary:
                 index = index + 1
                 index = index % self.capacity
 
-    def __getitem__(self, key: Any) -> Any:
+    def __getitem__(self, key: Hashable) -> Any:
         index = hash(key) % self.capacity
         if self.hash_table[index] and self.hash_table[index][0] == key:
             return self.hash_table[index][2]
-        else:
-            for i in range(self.capacity + 1):
-                index = index + 1
-                index = index % self.capacity
-                if self.hash_table[index]:
-                    if self.hash_table[index][0] == key:
-                        return self.hash_table[index][2]
-            raise KeyError
+        for i in range(self.capacity + 1):
+            index = index + 1
+            index = index % self.capacity
+            if self.hash_table[index] and self.hash_table[index][0] == key:
+                return self.hash_table[index][2]
+        raise KeyError
 
     def __len__(self) -> int:
         return self.length
