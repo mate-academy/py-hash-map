ce8397e0940693d7d4ce9d7da4dd13bb678f0dca
Mariia Horbova <gorbova21@gmail.com>
2023-06-27 19:54:39 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,66 +1,77 @@
+from dataclasses import dataclass
 from typing import Hashable, Any
+
+INITIAL_CAPACITY = 8
+RESIZE_THRESHOLD = 2 / 3
+CAPACITY_MULTIPLIER = 2
+
+
+@dataclass
+class Node:
+    key: Hashable
+    value: Any
 
 
 class Dictionary:
-    initial_capacity: int = 8
-    load_factor: float = 2 / 3
-
-    def __init__(self, capacity: int = initial_capacity) -> None:
-        self.initial_capacity = capacity
-        self.load_factor = Dictionary.load_factor
+    def __init__(self, capacity: int = INITIAL_CAPACITY) -> None:
+        self.initial_capacity = INITIAL_CAPACITY
+        self.capacity = capacity
+        self.hash_table: list[Node | list | None] = [None] * self.capacity
         self.size = 0
-        self.capacity = self.initial_capacity
-        self.table = [None] * self.capacity
 
-    def __setitem__(self,
-                    key: Hashable,
-                    value: Any) -> None:
-        self._check_resize()
-        index = self._get_index(key)
-        if self.table[index] is None:
-            self.table[index] = []
-        nodes = self.table[index]
-        for node in nodes:
-            if node[0] == key:
-                node[2] = value
-                return
-        nodes.append([key, hash(key), value])
-        self.size += 1
+    @property
+    def current_max_size(self) -> float:
+        return RESIZE_THRESHOLD * self.capacity
 
-    def __getitem__(self,
-                    key: Hashable
-                    ) -> Any:
-        index = self._get_index(key)
-        nodes = self.table[index]
-        if not nodes:
-            raise KeyError(f"{key} not found")
-        for node in nodes:
-            if node[0] == key:
-                return node[2]
+    def _calculate_index(self, key: Hashable) -> int:
+        index = hash(key) % self.capacity
 
-    def __len__(self) -> int:
-        return self.size
+        while (
+            self.hash_table[index] is not None
+            and self.hash_table[index].key != key
+        ):
+            index = self._cyclic_increment(index)
 
-    def _get_index(self,
-                   key: Hashable
-                   ) -> int:
-        hash_value = hash(key)
-        return hash_value % self.capacity
+        return index
 
-    def _check_resize(self) -> None:
-        load_factor = self.size / self.capacity
-        if load_factor >= self.load_factor:
-            self._resize()
+    def _cyclic_increment(self, index: int) -> int:
+        return (index + 1) % self.capacity
 
-    def _resize(self) -> None:
-        old_hash_table = self.table
+    def __getitem__(self, key: Hashable) -> Any:
+        index = self._calculate_index(key)
+
+        if self.hash_table[index] is None:
+            raise KeyError(f"Cannot find value for key: {key}")
+
+        return self.hash_table[index].value
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        index = self._calculate_index(key)
+
+        if self.hash_table[index] is None:
+            if self.size + 1 >= self.current_max_size:
+                self.resize()
+                return self.__setitem__(key, value)
+
+            self.size += 1
+
+        self.hash_table[index] = Node(key, value)
+
+    def resize(self) -> None:
+        old_hash_table = self.hash_table
 
         self.__init__(self.capacity * 2)
 
         for node in old_hash_table:
             if node is not None:
-                self.__setitem__(node[0][0], node[0][2])
+                self.__setitem__(node.key, node.value)
+
+    def __str__(self) -> str:
+        return str(self.hash_table)
+
+    def __len__(self) -> int:
+        return self.size
 
     def clear(self) -> None:
-        self.table = [None] * self.initial_capacity
+        self.hash_table = [None] * self.initial_capacity
         self.size = 0
