dbe0c889d262645e8145c70c486687616771ba86
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-28 16:25:18 +0300

    methods 'pop' and 'update' were improved

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Hashable, Iterable
+from typing import Any, Hashable
 
 
 class Dictionary:
@@ -15,15 +15,18 @@ class Dictionary:
                and self.hash_table[index][0] != input_key):
             index = (index + 1) % self.capacity
         if not self.hash_table[index]:
-            raise KeyError
+            raise KeyError(f"The Dictionary object doesn't contain element "
+                           f"with key '{input_key}'")
         return self.hash_table[index][2]
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
+
         if len(self) == round(self.capacity * self.load_factor):
             self.resize()
+
         index = hash(key) % self.capacity
-        while (self.hash_table[index]
-               and self.hash_table[index][0] != key):
+
+        while (self.hash_table[index] and self.hash_table[index][0] != key):
             index += 1
             if index > self.capacity - 1:
                 index = 0
@@ -39,49 +42,62 @@ class Dictionary:
     def __delitem__(self, key: Hashable) -> None:
         if key in self:
             for i in range(self.capacity):
-                if self.hash_table[i]:
-                    if self.hash_table[i][0] == key:
-                        self.hash_table[i] = None
-                        break
+                if self.hash_table[i] and self.hash_table[i][0] == key:
+                    self.hash_table[i] = None
+                    break
         else:
-            raise KeyError
+            raise KeyError(f"The Dictionary object doesn't contain element "
+                           f"with key '{key}'")
 
     def resize(self) -> None:
         self.capacity *= 2
-        old_hash_table_items = [item for item in self.hash_table
-                                if item]
+        old_hash_table_items = [item for item in self.hash_table if item]
         self.hash_table = [None] * self.capacity
         for key, _hash, value in old_hash_table_items:
             self[key] = value
 
     def get(self, input_key: Hashable) -> Any:
         for item in self.hash_table:
-            if item:
-                if input_key == item[0]:
-                    return item[2]
+            if item and input_key == item[0]:
+                return item[2]
 
     def clear(self) -> None:
         self.__init__()
 
-    def update(self, other: Dictionary | Iterable = None) -> None:
+    def update(self, other: Dictionary | dict | list | tuple | set) -> None:
+
         if isinstance(other, Dictionary):
             for item in other.hash_table:
                 if item:
                     self[item[0]] = other[item[0]]
-        elif isinstance(other, Iterable):
+
+        elif (isinstance(other, list) or isinstance(other, tuple)
+              or isinstance(other, set)):
             for item in other:
-                self[item[0]] = other[item[0]]
+                if len(item) == 2:
+                    self[item[0]] = item[1]
+                else:
+                    raise ValueError(f"Length of item {item} must be 2!")
+
+        elif isinstance(other, dict):
+            for key, value in other.items():
+                self[key] = other[key]
+
         else:
-            raise ValueError
+            raise ValueError("Object to update must be "
+                             "Dictionary, list, tuple or set!")
 
     def pop(self, key: Hashable, default: Any = None) -> Any:
-        if key in self:
+
+        if self.get(key):
             for i in range(self.capacity):
                 if self.hash_table[i] and self.hash_table[i][0] == key:
                     popped_value = self.hash_table[i]
                     self.hash_table[i] = None
-                    return popped_value
+                    return popped_value[2]
+
         elif default:
             return default
-        else:
-            raise KeyError
+
+        raise KeyError(f"The Dictionary object doesn't contain element "
+                       f"with key '{key}'. Add default value for return")
