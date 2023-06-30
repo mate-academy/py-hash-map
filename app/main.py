0a0a06f9ee576957cdc85038a195500981a369fd
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-30 18:12:56 +0300

    Done!

--- app/main.py
+++ app/main.py
@@ -1,2 +1,68 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.stored_data = []
+
+    def __str__(self) -> str:
+        representation = "{"
+        for current_pair in self.stored_data:
+            key, value = current_pair
+            representation += f"'{key}': '{value}', "
+        representation = representation[:-2]
+        representation += "}"
+        return representation
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        for current_pair in self.stored_data:
+            if current_pair[0] == key:
+                current_pair[1] = value
+                return None
+
+        self.stored_data.append([key, value])
+
+    def __getitem__(self, key: Any) -> Any:
+        for current_pair in self.stored_data:
+            cur_key, cur_value = current_pair
+            if key == cur_key:
+                return cur_value
+
+        raise KeyError
+
+    def __len__(self) -> int:
+        return len(self.stored_data)
+
+    def clear(self) -> None:
+        self.stored_data = []
+
+    def __delitem__(self, key: Any) -> Any:
+        for current_pair in self.stored_data:
+            if key == current_pair[0]:
+                returned_value = current_pair[1]
+                self.stored_data.remove(current_pair)
+                return returned_value
+
+    def get(self, key: Any) -> Any:
+        return self.__getitem__(key)
+
+    def pop(self, key: Any) -> Any:
+        return self.__delitem__(key)
+
+    def update(self, appended_dictionary: dict) -> None:
+        if not isinstance(appended_dictionary, dict):
+            raise ValueError
+        for key, value in appended_dictionary.items():
+            validator = False
+            for current_pair in self.stored_data:
+                current_key, current_value = current_pair
+                if key == current_key:
+                    current_pair[1] = value
+                    validator = True
+                    break
+            if validator is False:
+                self.stored_data.append([key, value])
+
+    def __iter__(self) -> None:
+        keys_list = [current_pair[0] for current_pair in self.stored_data]
+        yield from keys_list
