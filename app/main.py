d85aa54bcf780a5c08b9e153c4f7960f0b33c0c3
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-07-03 23:44:13 +0300

    redefining most functions

--- app/main.py
+++ app/main.py
@@ -6,87 +6,87 @@ from typing import Union, Any, Iterator
 
 class Dictionary:
     def __init__(self) -> None:
-        self.buckets_table = [[] for _ in range(8)]
-        self.capacity = len(self.buckets_table)
-        self.resize_breakpoint = 2 / 3
-        self.old_buckets_table = None
+        self.hash_table = [[] for _ in range(8)]
+        self.collisions = []
+        self.old_hash_table = None
+        self.capacity = 8
 
-    def buckets_table_resize(self) -> None:
+    def resize(self) -> None:
+        if not len(self) / self.capacity > 2 / 3:
+            return
+
+        """Resize"""
         self.capacity *= 2
-        self.old_buckets_table = copy.deepcopy(self.buckets_table)
-        self.buckets_table = [[] for bucket in range(self.capacity)]
+        self.old_hash_table = copy.deepcopy(self.hash_table)
+        self.hash_table = [[] for bucket in range(self.capacity)]
+        self.collisions.clear()
 
-        for bucket in self.old_buckets_table:
+        for bucket in self.old_hash_table:
             if len(bucket):
-                index_by_new_hash = bucket[2] % self.capacity
-                if not len(self.buckets_table[index_by_new_hash]):
-                    self.buckets_table[index_by_new_hash] = bucket
-                else:
-                    self.buckets_table[self.buckets_table.index([])] = bucket
+                self.__setitem__(key=bucket[0], value=bucket[1])
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        try:
-            hash(key)
-        except TypeError:
+        if not isinstance(key, Hashable):
             raise TypeError(f"unhashable type: '{type(key).__name__}'")
 
-        k_v_pair = [key, value, hash(key)]
         index = hash(key) % self.capacity
+        item = [key, value, hash(key)]
 
-        if not self.buckets_table[index]:
-            self.buckets_table[index] = k_v_pair
-        else:
+        """Empty bucket"""
+        if not len(self.hash_table[index]):
+            self.hash_table[index] = item
+            self.resize()
+            return
 
-            while self.buckets_table[index][0] != key:
-                index = (index + 1) % self.capacity
+        """Reassign value"""
+        if self.hash_table[index][0] == key:
+            self.hash_table[index][1] = value
+            return
 
-                if not self.buckets_table[index]:
-                    self.buckets_table[index] = k_v_pair
-                    break
+        """Check old collisions"""
+        for collision in self.collisions:
+            if collision[0] == key:
+                collision_item_index = collision[1]
+                self.hash_table[collision_item_index][1] = value
+                return
 
-            if self.buckets_table[index][0] == key:
-                self.buckets_table[index] = k_v_pair
-
-        if ((self.capacity - self.buckets_table.count([]))
-                / self.capacity > self.resize_breakpoint):
-            self.buckets_table_resize()
+        """New collision"""
+        collision_item_index = self.hash_table.index([])
+        self.hash_table[collision_item_index] = item
+        self.collisions.append([key, collision_item_index])
+        self.resize()
 
     def __getitem__(self, key: Hashable) -> Any:
-        for bucket in self.buckets_table:
-            if len(bucket) and key == bucket[0]:
-                return bucket[1]
+        index = hash(key) % self.capacity
+
+        if len(self.hash_table[index]):
+            if self.hash_table[index][0] == key:
+                return self.hash_table[index][1]
+
+            for collision in self.collisions:
+                if collision[0] == key:
+                    index = collision[1]
+                    return self.hash_table[index][1]
 
         raise KeyError(key)
 
     def __len__(self) -> int:
-        return sum(1 for bucket in self.buckets_table if len(bucket))
+        return sum(1 for bucket in self.hash_table if len(bucket))
 
     def clear(self) -> None:
-        self.buckets_table = [[] for bucket in range(self.capacity)]
+        self.hash_table = [[] for bucket in range(self.capacity)]
 
     def __delitem__(self, key: Hashable) -> None:
-        for bucket in self.buckets_table:
+        for bucket in self.hash_table:
             if len(bucket) and bucket[0] == key:
                 bucket.clear()
 
     def get(self, key: Hashable, default: Any = None) -> Any:
-        for bucket in self.buckets_table:
-            if len(bucket) and key == bucket[0]:
-                return bucket[1]
-
-        return default
-
-    def update(self, other_dict: "Dictionary") -> None:
-        for bucket in other_dict.buckets_table:
-            if len(bucket):
-                self.__setitem__(bucket[0], bucket[1])
+        self.__getitem__(key)
 
     def pop(self, key: Hashable, default: Any = None) -> Union[None, Any]:
-        value = None
-        for bucket in self.buckets_table:
-            if len(bucket) and bucket[0] == key:
-                value = bucket[1]
-                bucket.clear()
+        value = self.__getitem__(key)
+        self.__delitem__(key)
 
         if default is not None:
             return default
@@ -96,18 +96,23 @@ class Dictionary:
 
         raise KeyError
 
+    def update(self, other_dict: "Dictionary") -> None:
+        for bucket in other_dict.hash_table:
+            if len(bucket):
+                self.__setitem__(bucket[0], bucket[1])
+
     def __iter__(self) -> Iterator:
-        return iter(bucket[0] for bucket in self.buckets_table if len(bucket))
+        return iter(bucket[0] for bucket in self.hash_table if len(bucket))
 
     def __repr__(self) -> str:
         return ("CUSTOM_HASH_TABLE: [KEY, VALUE, HASH(KEY)]\n"
                 + ("*" * 42) + "\n" + pprint.pformat(
-                    object=self.buckets_table,
+                    object=self.hash_table,
                     indent=0)[1:-1]
                 + "\n" + "CAPACITY: " + str(self.capacity) + "\n"
                 + "LEN: " + str(self.__len__())) + "\n" + ("*" * 42)
 
     def __str__(self) -> str:
         return "{" + ", ".join([bucket[0] + " : " + str(bucket[1])
-                                for bucket in self.buckets_table
+                                for bucket in self.hash_table
                                 if len(bucket)]) + "}"
