b890008dc8046d2d2e20b1921c88553a60bc0ca0
laskarj <koks.adekvat@gmail.com>
2023-07-09 14:47:21 +0200

    Imports sorted.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Dict, Hashable, List, Optional, Union, Iterator
+from typing import Any, Dict, Hashable, Iterator, List, Optional, Union
 
 
 class Node:
