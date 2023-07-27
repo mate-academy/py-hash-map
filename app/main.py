e45a0c27f8bd6eecc5a28afb28161a16c58beb2d
lubart2517 <lubomirvihvatniuk@gmail.com>
2023-07-28 01:04:29 +0300

    Implemented linked list logic for nodes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import copy
-from typing import Hashable, Any
+from typing import Hashable, Any, Iterable
 
 
 class Node:
@@ -12,6 +12,7 @@ class Node:
         self.key = key
         self.index = index
         self.value = value
+        self.neighbour = None
 
     def __repr__(self) -> str:
         return (f"Node with key {self.key}, "
@@ -20,85 +21,130 @@ class Node:
 
 class Dictionary:
     def __init__(self) -> None:
-        self.length = 0
+        self._length = 0
         self.hash_table: list[None | Node] = [None] * 8
         self.cur_hash_table: list = []
+        self.load_factor = 2 / 3
 
-    def set(
+    def _set(
             self,
             key: Hashable,
             value: Any,
             reset: bool = False
     ) -> None:
         """ Set new key/value pair to dict"""
-        key_index = hash(key) % len(self.hash_table)
-        while self.hash_table[key_index]:
-            if self.hash_table[key_index].key == key:
-                self.hash_table[key_index].value = value
-                return
-            key_index = (key_index + 1) % len(self.hash_table)
-
-        self.hash_table[key_index] = Node(key_index, key, value)
-        if not reset:
-            self.length += 1
-
-    def __setitem__(self, key: Hashable, value: Any) -> None:
-        """Magik method for Dictionary, add new pair to Dictionary
-        and controls count of cells in hash table"""
-        self.set(key, value)
-        if self.length > 0.66666 * len(self.hash_table):
-            self.double_hash_table()
-
-    def double_hash_table(self) -> None:
-        """Increases count of cells in hash table and set
-        new indexes for each key/value pair"""
-        self.cur_hash_table = copy.copy(self.hash_table)
-        self.hash_table = [None] * len(self.cur_hash_table) * 2
-        for elem in self.cur_hash_table:
-            if elem:
-                self.set(elem.key, elem.value, True)
-        self.cur_hash_table = []
-
-    def find_table_index(self, key: Hashable) -> int:
-        """Search key in hash table and returns its index
-        in hash table"""
         hash_key = hash(key)
         key_index = hash_key % len(self.hash_table)
-        counter = 0
-        while True:
-            if (self.hash_table[key_index]
-                    and self.hash_table[key_index].key == key):
+        if self.hash_table[key_index]:
+            current_node = self.hash_table[key_index]
+            while True:
+                # reassign value of key
+                if (hash(current_node.key) == hash_key
+                        and current_node.key == key):
+                    current_node.value = value
+                    return
+                else:
+                    # jump to next node in linked list
+                    if current_node.neighbour:
+                        current_node = current_node.neighbour
+                    # create new neighbour Node at this index
+                    else:
+                        current_node.neighbour = Node(key_index, key, value)
+                        if not reset:
+                            self._length += 1
+                        return
+        else:
+            self.hash_table[key_index] = Node(key_index, key, value)
+            if not reset:
+                self._length += 1
+            return
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        """Magic method for Dictionary, add new pair to Dictionary
+        and controls count of cells in the hash table"""
+        self._set(key, value)
+        if self._length > self.load_factor * len(self.hash_table):
+            self._double_hash_table()
+
+    def _double_hash_table(self) -> None:
+        """Increases count of cells in the hash table and set
+        new indexes for each key/value pair"""
+        self.cur_hash_table = copy.copy(self.hash_table)
+        self.hash_table = [None] * len(self.hash_table) * 2
+        for elem in self.cur_hash_table:
+            if elem:
+                self._set(elem.key, elem.value, True)
+                if elem.neighbour:
+                    current_node = elem.neighbour
+                    while True:
+                        self._set(current_node.key, current_node.value, True)
+                        if current_node.neighbour:
+                            current_node = current_node.neighbour
+                        else:
+                            break
+        self.cur_hash_table = []
+
+    def _find_table_index(self, key: Hashable) -> int | Node:
+        """Search key in the hash table nodes and returns its index
+        in the hash table or parent node"""
+        hash_key = hash(key)
+        key_index = hash_key % len(self.hash_table)
+        if self.hash_table[key_index]:
+            current_node = self.hash_table[key_index]
+            if (hash(current_node.key) == hash_key
+                    and current_node.key == key):
                 return key_index
-            key_index = (key_index + 1) % len(self.hash_table)
-            counter += 1
-            if counter == len(self.hash_table):
-                break
-        raise KeyError(f"This key `{key}` is not present here.")
+            if current_node.neighbour:
+                while True:
+                    if (hash(current_node.neighbour.key) == hash_key
+                            and current_node.neighbour.key == key):
+                        return current_node
+                    else:
+                        current_node = current_node.neighbour
+                        if not current_node.neighbour:
+                            raise KeyError(
+                                f"This key `{key}` is not present here."
+                            )
+        else:
+            raise KeyError(f"This key `{key}` is not present here.")
 
     def __getitem__(self, key: Hashable) -> Any:
         """ Returns value of given key"""
-        index = self.find_table_index(key)
-        return self.hash_table[index].value
+        index = self._find_table_index(key)
+        if isinstance(index, int):
+            return self.hash_table[index].value
+        return index.neighbour.value
 
     def __len__(self) -> int:
         """ Returns count of records in Dictionary"""
-        return self.length
+        return self._length
 
     def __repr__(self) -> str:
         """String representation"""
-
-        return f"{[node.__repr__() for node in self.hash_table if node]}"
+        nodes_list = [
+            node.__repr__() for node in self.__iter__(iter_nodes=True)
+        ]
+        return f"{nodes_list}"
 
     def __delitem__(self, key: Hashable) -> None:
-        """Removes key/value pair from hash table by given key"""
-        index = self.find_table_index(key)
-        self.hash_table[index] = None
-        self.length -= 1
+        """Removes key/value pair from the hash table by given key"""
+        index = self._find_table_index(key)
+        if isinstance(index, int):
+            if self.hash_table[index].neighbour:
+                self.hash_table[index] = self.hash_table[index].neighbour
+            else:
+                self.hash_table[index] = None
+        else:
+            if index.neighbour.neighbour:
+                index.neighbour = index.neighbour.neighbour
+            else:
+                index.neighbour = None
+        self._length -= 1
 
     def clear(self) -> None:
         """Clear all records in Dictionary"""
         self.hash_table = [None] * 8
-        self.length = 0
+        self._length = 0
 
     def get(self, key: Hashable, default: Any = None) -> Any:
         """ Returns value of given key"""
@@ -107,34 +153,49 @@ class Dictionary:
         except KeyError:
             return default
 
-    def pop(self, key: Hashable) -> Any:
+    def pop(self, key: Hashable, default: Any = None) -> Any:
         """ Deletes key/value pair
         and returns value of given key"""
-        index = self.find_table_index(key)
-        elem = self.hash_table[index].value
-        self.hash_table[index] = None
-        self.length -= 1
-        return elem
+        try:
+            elem = self.__getitem__(key)
+            self.__delitem__(key)
+            return elem
+        except KeyError:
+            if default:
+                return default
+            else:
+                raise
 
-    def update(self, *args) -> None:
+    def update(
+            self,
+            _m: Hashable | Iterable | None = None,
+            *args,
+            **kwargs
+    ) -> None:
         """Reassign value of given key or
         iter through iterable to add or update key/value pairs"""
-        if len(args) == 2:
-            key, value = args[0], args[1]
-            index = self.find_table_index(key)
-            self.hash_table[index].value = value
+        if _m:
+            if args:
+                key, value = _m, args[0]
+                self.__setitem__(key, value)
+            else:
+                iterable = _m
+                if isinstance(iterable, dict):
+                    for key, value in iterable.items():
+                        self.__setitem__(key, value)
+                    return
+                if hasattr(iterable, "__iter__"):
+                    for key, value in iterable:
+                        self.__setitem__(key, value)
         else:
-            iterable = args[0]
-            if isinstance(iterable, dict):
-                for key, value in iterable.items():
-                    self.__setitem__(key, value)
-                return
-            if hasattr(iterable, "__iter__"):
-                for key, value in iterable:
-                    self.__setitem__(key, value)
+            pass
 
-    def __iter__(self) -> Hashable:
-        """Iter through keys in hash table"""
+    def __iter__(self, iter_nodes: bool = False) -> Iterable:
+        """Iter through keys or nodes in the hash table"""
         for elem in self.hash_table:
             if elem:
-                yield elem.key
+                yield elem if iter_nodes else elem.key
+                cur_elem = elem.neighbour
+                while cur_elem:
+                    yield cur_elem if iter_nodes else cur_elem.key
+                    cur_elem = cur_elem.neighbour
