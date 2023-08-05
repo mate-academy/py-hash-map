e187fcf8b2b506f3d498fd5a3314236319e6e407
Iryna Ishchenko <irynaira@gmail.com>
2023-08-05 20:15:56 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,153 +1,112 @@
-from typing import Any
+from typing import Any, Optional, List, Tuple
+
+
+class Node:
+    def __init__(self, key: Any, value: Any) -> Any:
+        self.key = key
+        self.value = value
+        self.next: Optional[Node] = None
 
 
 class Dictionary:
-    def __init__(self) -> None:
-        self.capacity = 4
-        self.overflow_limit = self.capacity * 2 // 3
-        self.len = 0
-        self.container = [None] * self.capacity
-
-    def resize(self, size: int) -> None:
-        container = self.container
-        self.capacity = size
-        self.overflow_limit = self.capacity * 2 // 3
-        self.container = [None] * size
-        self.len = 0
-        for item in container:
-            if item is not None:
-                self[item[0]] = item[2]
-
-    def _get_key_position(self, key: Any) -> int:
-        key_hash = hash(key) % self.capacity
-        position = key_hash
-
-        def skip_looped_shift(container_row: tuple[Any, int, Any]) -> bool:
-            return container_row[1] > position
-
-        def skip_shifting_rows(container_row: tuple[Any, int, Any]) -> bool:
-            return container_row[1] < key_hash
-
-        def skip_collisions(container_row: tuple[Any, int, Any]) -> bool:
-            return (container_row[1] == key_hash) and \
-                   (container_row[0] != key)
-
-        for checker in (
-                skip_looped_shift,
-                skip_shifting_rows,
-                skip_collisions
-        ):
-            while \
-                    (position < self.capacity) and \
-                    (self.container[position] is not None) and \
-                    checker(self.container[position]):
-                position += 1
-            if position >= self.capacity:
-                position = 0
-                while \
-                        (self.container[position] is not None) and \
-                        checker(self.container[position]):
-                    position += 1
-        return position
-
-    def _move_up(self, position: int) -> None:
-        move_border = position + 1
-        while \
-                (move_border < self.capacity) and \
-                (self.container[move_border] is not None) and \
-                (self.container[move_border][1] < move_border):
-            move_border += 1
-        self.container[position:move_border - 1] = \
-            self.container[position + 1:move_border]
-        if \
-                (move_border < self.capacity) or \
-                (self.container[0] is None) or \
-                (self.container[0][1] == 0):
-            self.container[move_border - 1] = None
-        else:
-            self.container[-1] = self.container[0]
-            move_border = 1
-            while \
-                    (self.container[move_border] is not None) and \
-                    (self.container[move_border][1] != move_border):
-                move_border += 1
-            self.container[:move_border - 1] = self.container[1:move_border]
-            self.container[move_border - 1] = None
-        self.len -= 1
-
-    def _move_down(self, position: int) -> None:
-        move_border = position + 1
-        while \
-                (move_border < self.capacity) and \
-                (self.container[move_border] is not None):
-            move_border += 1
-        if move_border < self.capacity:
-            self.container[position + 1:move_border + 1] = \
-                self.container[position:move_border]
-        else:
-            last = self.container[-1]
-            self.container[position + 1:move_border + 1] = \
-                self.container[position:move_border]
-            move_border = 0
-            while self.container[move_border] is not None:
-                move_border += 1
-            self.container[1:move_border + 1] = self.container[:move_border]
-            self.container[0] = last
-        self.container[position] = None
-        self.len += 1
+    def __init__(
+            self,
+            initial_capacity: int = 10,
+            load_factor: float = 0.75
+    ) -> None:
+        self._capacity: int = initial_capacity
+        self._load_factor: float = load_factor
+        self._size: int = 0
+        self._table: List[Optional[Node]] = [None] * self._capacity
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        key_hash = hash(key) % self.capacity
-        new_item = (key, key_hash, value)
-        position = self._get_key_position(key)
-        if \
-                (self.container[position] is not None) and \
-                (self.container[position][0] != key):
-            self._move_down(position)
-        elif self.container[position] is None:
-            self.len += 1
-        self.container[position] = new_item
-        if self.len > self.overflow_limit:
-            self.resize(self.capacity * 2)
+        hash_code: int = self._get_hash_code(key)
+        index: int = hash_code % self._capacity
+
+        new_node: Node = Node(key, value)
+
+        if self._table[index] is None:
+            self._table[index] = new_node
+        else:
+            current: Node = self._table[index]
+            prev: Optional[Node] = None
+            while current is not None:
+                if current.key == key:
+                    current.value = value
+                    return
+                prev = current
+                current = current.next
+            prev.next = new_node
+
+        self._size += 1
+        if self._size >= self._capacity * self._load_factor:
+            self._resize_table()
 
     def __getitem__(self, key: Any) -> Any:
-        position = self._get_key_position(key)
-        if \
-                (self.container[position] is not None) and \
-                (self.container[position][0] == key):
-            return self.container[position][2]
-        raise KeyError(key)
+        hash_code: int = self._get_hash_code(key)
+        index: int = hash_code % self._capacity
+
+        current: Optional[Node] = self._table[index]
+        while current is not None:
+            if current.key == key:
+                return current.value
+            current = current.next
+
+        raise KeyError(f"Key '{key}' not found.")
 
     def __len__(self) -> int:
-        return self.len
-
-    def clear(self) -> None:
-        self.len = 0
-        self.container = [None] * self.capacity
+        return self._size
 
     def __delitem__(self, key: Any) -> None:
-        position = self._get_key_position(key)
-        if \
-                (self.container[position] is not None) and \
-                (self.container[position][0] == key):
-            self._move_up(position)
-        else:
-            raise KeyError(key)
+        hash_code: int = self._get_hash_code(key)
+        index: int = hash_code % self._capacity
 
-    def get(self, key: Any, default: Any = None) -> Any:
-        try:
-            return self[key]
-        except KeyError:
-            return default
+        prev: Optional[Node] = None
+        current: Optional[Node] = self._table[index]
+        while current is not None:
+            if current.key == key:
+                if prev is None:
+                    self._table[index] = current.next
+                else:
+                    prev.next = current.next
+                self._size -= 1
+                return
+            prev = current
+            current = current.next
 
-    def pop(self, key: Any) -> Any:
-        value = self[key]
-        del self[key]
-        return value
+        raise KeyError(f"Key '{key}' not found.")
 
-    def update(self, other: dict) -> None:
-        for key in other:
-            self[key] = other[key]
+    def _get_hash_code(self, key: Any) -> int:
+        return hash(key)
 
-    def __iter__(self) -> None:
-        return iter(item[0] for item in self.container if item is not None)
+    def _resize_table(self) -> None:
+        new_capacity: int = self._capacity * 2
+        new_table: List[Optional[Node]] = [None] * new_capacity
+
+        for i in range(self._capacity):
+            current: Optional[Node] = self._table[i]
+            while current is not None:
+                new_index: int = current.key.__hash__() % new_capacity
+                new_node: Node = Node(current.key, current.value)
+
+                if new_table[new_index] is None:
+                    new_table[new_index] = new_node
+                else:
+                    temp: Node = new_table[new_index]
+                    while temp.next is not None:
+                        temp = temp.next
+                    temp.next = new_node
+
+                current = current.next
+
+        self._table = new_table
+        self._capacity = new_capacity
+
+    def __repr__(self) -> str:
+        items: List[Tuple[Any, Any]] = []
+        for i in range(self._capacity):
+            current: Optional[Node] = self._table[i]
+            while current is not None:
+                items.append((current.key, current.value))
+                current = current.next
+        return f"Dictionary({', '.join([f'{k}: {v}' for k, v in items])})"
