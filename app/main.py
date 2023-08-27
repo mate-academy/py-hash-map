619375b71c9d9eadbe06f72d89a4556d6b86c4eb
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-27 14:04:45 +0300

    Final version

--- app/main.py
+++ app/main.py
@@ -3,41 +3,19 @@ from typing import Any
 
 class Node:
     def __init__(self, key: str, _hash: int, value: str) -> None:
-        self._key = key
-        self._hash = _hash
-        self._value = value
-
-    @property
-    def key(self) -> str:
-        return self._key
-
-    @key.setter
-    def key(self, key: str) -> None:
-        self._key = key
-
-    @property
-    def value(self) -> str:
-        return self._value
-
-    @value.setter
-    def value(self, value: str) -> None:
-        self._value = value
-
-    @property
-    def hash(self) -> int:
-        return self._hash
-
-    @hash.setter
-    def hash(self, _hash: int) -> None:
-        self._hash = _hash
+        self.key = key
+        self.hash = _hash
+        self.value = value
 
 
 class Dictionary:
-    def __init__(self) -> None:
-        self._length: int = 0
-        self._hash_table: list[Node | None] = [None] * 8
-        self._capacity: int = 8
-        self._load_factor: float = 2 / 3
+    def __init__(
+        self, initial_capacity: int = 8, load_factor: float = 2 / 3
+    ) -> None:
+        self._length = 0
+        self._capacity = initial_capacity
+        self._load_factor = load_factor
+        self._hash_table = [None] * self._capacity
 
     def _resize(self) -> None:
         old_hash_table = self._hash_table
@@ -46,57 +24,43 @@ class Dictionary:
         self._length = 0
         for node in old_hash_table:
             if node is not None:
-                self.__setitem__(node.key, node.value)
+                self._set_node(node)
+
+    def _set_node(self, node: Node) -> None:
+        index = node.hash % self._capacity
+        while (
+            self._hash_table[index] is not None
+            and self._hash_table[index].key != node.key
+        ):
+            index = (index + 1) % self._capacity
+        if self._hash_table[index] is None:
+            self._length += 1
+        self._hash_table[index] = node
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        index = hash(key) % self._capacity
-        if self._hash_table[index] is None:
-            self._hash_table[index] = Node(key, hash(key), value)
-            self._length += 1
-        elif self._hash_table[index].key == key:
-            self._hash_table[index].value = value
-        else:
-            while True:
-                index = (index + 1) % self._capacity
-                if self._hash_table[index] is None:
-                    self._hash_table[index] = Node(key, hash(key), value)
-                    self._length += 1
-                    break
-                elif self._hash_table[index].key == key:
-                    self._hash_table[index].value = value
-                    break
-
+        node = Node(key, hash(key), value)
         if (self._length / self._capacity) >= self._load_factor:
             self._resize()
+        self._set_node(node)
+
+    def _find_index(self, key: str) -> int:
+        index = hash(key) % self._capacity
+        while (
+            self._hash_table[index] is not None
+            and self._hash_table[index].key != key
+        ):
+            index = (index + 1) % self._capacity
+        return index
 
     def __getitem__(self, key: str) -> str:
-        index = hash(key) % self._capacity
+        index = self._find_index(key)
         if self._hash_table[index] is None:
             raise KeyError
-        elif self._hash_table[index].key != key:
-            while True:
-                index = (index + 1) % self._capacity
-                if self._hash_table[index] is None:
-                    raise KeyError
-                elif self._hash_table[index].key == key:
-                    return self._hash_table[index].value
-        else:
-            return self._hash_table[index].value
+        return self._hash_table[index].value
 
     def __delitem__(self, key: str) -> None:
-        index = hash(key) % self._capacity
-        if self._hash_table[index] is None:
-            return
-        elif self._hash_table[index].key != key:
-            while True:
-                index = (index + 1) % self._capacity
-                if self._hash_table[index] is None:
-                    return
-                elif self._hash_table[index].key == key:
-                    self._hash_table[index] = None
-                    self._length -= 1
-                    break
-        else:
+        index = self._find_index(key)
+        if self._hash_table[index] is not None:
             self._hash_table[index] = None
             self._length -= 1
 
@@ -110,16 +74,13 @@ class Dictionary:
     def pop(self) -> str | None:
         if self._length == 0:
             return None
-        else:
-            index = self._capacity - 1
-            while True:
-                if self._hash_table[index] is not None:
-                    self._length -= 1
-                    node = self._hash_table[index]
-                    self._hash_table[index] = None
-                    return node.value
-                else:
-                    index -= 1
+        index = self._capacity - 1
+        while self._hash_table[index] is None:
+            index -= 1
+        self._length -= 1
+        node = self._hash_table[index]
+        self._hash_table[index] = None
+        return node.value
 
     def __iter__(self) -> iter:
         for node in self._hash_table:
@@ -127,18 +88,10 @@ class Dictionary:
                 yield node.key
 
     def update(self, other: dict = None, **kwargs: str) -> None:
-        """
-        Update the dictionary with
-        key-value pairs from `other` or keyword arguments.
-        """
         if other is not None:
-            if hasattr(other, "items"):
-                for key, value in other.items():
-                    self.__setitem__(key, value)
-            else:
-                for key, value in other:
-                    self.__setitem__(key, value)
-
+            items = other.items() if hasattr(other, "items") else other
+            for key, value in items:
+                self.__setitem__(key, value)
         for key, value in kwargs.items():
             self.__setitem__(key, value)
 
