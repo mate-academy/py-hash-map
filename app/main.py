d3b92de3d46040bdc07cde8451319730f5a723c7
CaCtUs1k <denischernish19012004@gmail.com>
2023-08-30 18:53:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,58 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.capacity = 8
+        self.load_factor = 2 / 3
+        self.size = 0
+        self.storage = [None] * self.capacity
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        idx = hash(key) % self.capacity
+        while True:
+            if self.storage[idx] is None:
+                self.storage[idx] = [key, hash(key), value]
+                self.size += 1
+                break
+            if self.storage[idx] is not None:
+                if key == self.storage[idx][0]:
+                    self.storage[idx][2] = value
+                    break
+            idx = (idx + 1) % self.capacity
+        if self.size >= self.capacity * self.load_factor:
+            self.resize()
+
+    def __getitem__(self, key: Any) -> None:
+        for key_value_pair in self.storage:
+            if key_value_pair is not None and key_value_pair[0] == key:
+                return key_value_pair[2]
+        else:
+            raise KeyError(key)
+
+    def __len__(self) -> int:
+        return self.size
+
+    def resize(self) -> None:
+        old_storage = self.storage
+        self.capacity *= 2
+        self.size = 0
+        self.clear()
+        for item in old_storage:
+            if item is not None:
+                self.__setitem__(item[0], item[2])
+
+    def clear(self) -> None:
+        self.storage = [None] * self.capacity
+
+    def __delitem__(self, key: Any) -> None:
+        for idx in range(self.capacity):
+            if self.storage[idx] is not None and self.storage[idx][0] == key:
+                self.storage[idx] = None
+                break
+        else:
+            raise KeyError(key)
+
+    def pop(self, key: Any) -> None:
+        print(self.__getitem__(key))
+        self.__delitem__(key)
