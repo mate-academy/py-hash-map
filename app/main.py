790ba666941913ad47df053c03113a40955e8a0a
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-08-31 10:49:17 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,78 @@
+from typing import Hashable, Any
+
+
+class Node:
+    def __init__(self, key: Hashable, new_hash: int, value: str) -> None:
+        self.key = key
+        self.value = value
+        self.hash = new_hash
+
+    def __repr__(self) -> str:
+        return f"{self.key}: {self.value}"
+
+
 class Dictionary:
-    pass
+    def __init__(
+            self,
+            initial_capacity: int = 8,
+            load_factor: float = 2 / 3
+    ) -> None:
+        self.capacity: int = initial_capacity
+        self.load_factor = load_factor
+        self.size = 0
+        self._hash_list = [None] * self.capacity
+
+    def find_slot(self, key: Hashable) -> int:
+        new_hash = hash(key)
+        index = new_hash % self.capacity
+        while self._hash_list[index]:
+            if self._hash_list[index].key == key:
+                return index
+            index = (index + 1) % self.capacity
+        return index
+
+    def _resize(self) -> None:
+        self.capacity *= 2
+        old_hash_list = self._hash_list
+        self._hash_list = [None] * self.capacity
+        self.size = 0
+
+        for item in old_hash_list:
+            if item:
+                self.__setitem__(item.key, item.value)
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        new_hash = hash(key)
+        index = self.find_slot(key)
+        if self._hash_list[index] is None:
+            self.size += 1
+
+        self._hash_list[index] = Node(key, new_hash, value)
+
+        if self.size >= self.capacity * self.load_factor:
+            self._resize()
+
+    def __getitem__(self, key: Hashable) -> str:
+        index = self.find_slot(key)
+        if self._hash_list[index]:
+            return self._hash_list[index].value
+        raise KeyError
+
+    def __len__(self) -> int:
+        return self.size
+
+    def clear(self) -> None:
+        self.size = 0
+        self._hash_list = [None] * self.capacity
+
+    def __delitem__(self, key: Hashable) -> None:
+        index = self.find_slot(key)
+        self._hash_list[index] = None
+        self.size -= 1
+
+    def pop(self, key: Any) -> None:
+        try:
+            print(self.__getitem__(key))
+            self.__delitem__(key)
+        except KeyError:
+            raise KeyError(key)
