b09ed0360849b7629adb84d1ede2ac57c8ca55ac
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-31 11:03:12 +0300

    refactored a little

--- app/main.py
+++ app/main.py
@@ -23,31 +23,9 @@ class Dictionary:
         self._load_factor = load_factor
         self._hash_table = [None] * self._capacity
 
-    def _resize(self) -> None:
-        old_hash_table = self._hash_table
-        self._capacity *= 2
+    def clear(self) -> None:
         self._hash_table = [None] * self._capacity
         self._length = 0
-        for node in old_hash_table:
-            if node is not None:
-                self._set_node(node)
-
-    def _set_node(self, node: Node) -> None:
-        index = node.hash % self._capacity
-        while (
-            self._hash_table[index] is not None
-            and self._hash_table[index].key != node.key
-        ):
-            index = (index + 1) % self._capacity
-        if self._hash_table[index] is None:
-            self._length += 1
-        self._hash_table[index] = node
-
-    def __setitem__(self, key: Any, value: Any) -> None:
-        node = Node(key, hash(key), value)
-        if (self._length / self._capacity) >= self._load_factor:
-            self._resize()
-        self._set_node(node)
 
     def _find_index(self, key: str) -> int:
         index = hash(key) % self._capacity
@@ -57,6 +35,27 @@ class Dictionary:
         ):
             index = (index + 1) % self._capacity
         return index
+
+    def _set_node(self, node: Node) -> None:
+        index = self._find_index(node.key)
+        if self._hash_table[index] is None:
+            self._length += 1
+        self._hash_table[index] = node
+
+    def _resize(self) -> None:
+        old_hash_table = self._hash_table
+        self._capacity *= 2
+        self._hash_table = [None] * self._capacity
+        self._length = 0
+        for node in old_hash_table:
+            if node is not None:
+                self._set_node(node)
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        node = Node(key, hash(key), value)
+        if (self._length / self._capacity) >= self._load_factor:
+            self._resize()
+        self._set_node(node)
 
     def __getitem__(self, key: str) -> str:
         index = self._find_index(key)
@@ -69,10 +68,6 @@ class Dictionary:
         if self._hash_table[index] is not None:
             self._hash_table[index] = None
             self._length -= 1
-
-    def clear(self) -> None:
-        self._hash_table = [None] * self._capacity
-        self._length = 0
 
     def get(self, key: str) -> str:
         return self.__getitem__(key)
