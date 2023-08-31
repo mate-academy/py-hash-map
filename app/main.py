45c89d85b392bfa0bd0e3f8493f1191afdecbdda
Kristian Yendrek <kristianendreck4@gmail.com>
2023-08-31 11:38:42 +0300

    some improves

--- app/main.py
+++ app/main.py
@@ -4,52 +4,51 @@ from typing import Any, Hashable
 
 class Dictionary:
     def __init__(self) -> None:
-        self.capacity = 8
-        self.load_factor = 0.67
-        self.length = 0
-        self.hash_table = [None] * self.capacity
+        self._capacity = 8
+        self._load_factor = 0.67
+        self._length = 0
+        self._hash_table = [None] * self._capacity
 
     def __len__(self) -> int:
-        return self.length
+        return self._length
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        index = self.__index(key)
-        if self.hash_table[index] is None:
-            self.length += 1
-        self.hash_table[index] = (key, value, hash(key))
-        if self.length > self.capacity * self.load_factor:
+        if self._length > self._capacity * self._load_factor:
             self.__resize()
+        index = self.__index(key)
+        if not self._hash_table[index]:
+            self._length += 1
+        self._hash_table[index] = (key, value, hash(key))
 
     def __resize(self) -> None:
-        cells = [cell for cell in self.hash_table if cell]
-        self.capacity *= 2
-        self.length = 0
-        self.hash_table = [None] * self.capacity
+        cells = [cell for cell in self._hash_table if cell]
+        self._capacity *= 2
+        self._hash_table = [None] * self._capacity
+        self._length = 0
         for cell in cells:
             self[cell[0]] = cell[1]
 
     def __getitem__(self, key: Hashable) -> Any:
         index = self.__index(key)
-        if self.hash_table[index]:
-            if self.hash_table[index][0] == key:
-                return self.hash_table[index][1]
+        if self._hash_table[index]:
+            return self._hash_table[index][1]
         raise KeyError
 
     def __index(self, key: Hashable) -> int:
-        index = hash(key) % self.capacity
-        while self.hash_table[index] and self.hash_table[index][0] != key:
-            index = (index + 1) % self.capacity
+        index = hash(key) % self._capacity
+        while self._hash_table[index] and self._hash_table[index][0] != key:
+            index = (index + 1) % self._capacity
         return index
 
     def clear(self) -> None:
-        self.hash_table = [None] * self.capacity
-        self.length = 0
+        self._hash_table = [None] * self._capacity
+        self._length = 0
 
     def __delitem__(self, key: Hashable) -> None:
         index = self.__index(key)
-        if self.hash_table[index]:
-            self.hash_table[index] = None
-            self.length -= 1
+        if self._hash_table[index]:
+            self._hash_table[index] = None
+            self._length -= 1
 
     def get(self, key: Hashable, value: Any = None) -> Any:
         try:
@@ -68,17 +67,17 @@ class Dictionary:
             return value
 
     def __iter__(self) -> iter:
-        for cell in self.hash_table:
+        for cell in self._hash_table:
             if cell:
                 yield cell[0]
 
     def items(self) -> tuple:
-        for cell in self.hash_table:
+        for cell in self._hash_table:
             if cell:
                 yield cell[:2]
 
     def values(self) -> Any:
-        for cell in self.hash_table:
+        for cell in self._hash_table:
             if cell:
                 yield cell[1]
 
