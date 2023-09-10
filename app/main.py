af0516bdcffb5bf551f82fa4015c592ee351bebc
Elena Komarova <elenkomarova4@gmail.com>
2023-09-10 14:05:24 +0300

    'Solution_3'

--- app/main.py
+++ app/main.py
@@ -1,88 +1,45 @@
-from typing import Hashable, Any
+from typing import Any
 
 
 class Dictionary:
     def __init__(self) -> None:
         self.length = 0
         self.capacity = 8
-        self.load_factor = 2 / 3
         self.hash_table: list = [None] * self.capacity
+        self.load_factor = 2 / 3
 
-    def __len__(self) -> int:
-        return self.length
+    def __setitem__(self, key: Any, value: Any) -> None:
+        if self.length >= self.capacity * self.load_factor:
+            self.resize()
+        key_index = self.get_index(key)
+        if not self.hash_table[key_index]:
+            self.length += 1
+        self.hash_table[key_index] = (key, hash(key), value)
 
-    def __setitem__(self, key: Hashable, value: Any) -> None:
+    def __getitem__(self, item: Any) -> Any:
+        item_index = self.get_index(item)
+        if self.hash_table[item_index]:
+            return self.hash_table[item_index][2]
+        raise KeyError
 
-        index = self.get_index_for_key(key)
-
-        if self.hash_table[index] is not None:
-            for i, (existing_key, _, existing_value) \
-                    in enumerate(self.hash_table[index]):
-                if existing_key == key:
-                    self.hash_table[index][i] = (key, hash(key), value)
-                    break
-        else:
-            self.hash_table[index] = [(key, hash(key), value)]
-
-    def __getitem__(self, key: Hashable) -> Any:
-        index = self.get_index_for_key(key)
-        if self.hash_table[index] is None:
-            raise KeyError
-        return self.hash_table[index][2]
-
-    def get_index_for_key(self, key: Hashable) -> int:
+    def get_index(self, key: Any) -> int:
         index = hash(key) % self.capacity
-        while (self.hash_table[index] is not None
-               and self.hash_table[index][0] != key):
+        while self.hash_table[index] and self.hash_table[index][0] != key:
             index = (index + 1) % self.capacity
         return index
 
     def resize(self) -> None:
-        last_table = self.hash_table
+        hash_table = self.hash_table
+        self.capacity = self.capacity * 2
+        self.hash_table: list = [None] * self.capacity
         self.length = 0
-        self.capacity *= 2
-        self.hash_table = [None] * self.capacity
-        for element in last_table:
-            if element is not None:
-                key, _, value = element
-                index = self.get_index_for_key(key)
-                self.length += 1
-                self.hash_table[index] = element
+        for item in hash_table:
+            if item:
+                self.__setitem__(item[0], item[2])
 
-    def clear(self) -> None:
-        self.hash_table = [None] * self.capacity
-        self.length = 0
+    def __len__(self) -> int:
+        return self.length
 
-    def __delitem__(self, key: Hashable) -> None:
-        for index in range(len(self.hash_table)):
-            if (self.hash_table[index] is not None
-                    and self.hash_table[index][0] == key):
-                self.hash_table[index] = None
-                self.length -= 1
-                break
-
-    def get(self, key: Hashable, value: Any = None) -> Any:
-
-        for index in range(len(self.hash_table)):
-            if (self.hash_table[index] is not None
-                    and self.hash_table[index][0] == key):
-                return self.hash_table[index][2]
-        return value
-
-    def pop(self, key: Hashable, value: Any = None) -> Any:
-        for index in range(len(self.hash_table)):
-            if (self.hash_table[index] is not None
-                    and self.hash_table[index][0] == key):
-                result = self.hash_table[index][2]
-                self.hash_table[index] = None
-                self.length -= 1
-                return result
-        if value is not None:
-            return value
-        raise KeyError
-
-    def update(self, other_dict: "Dictionary") -> None:
-        for index in range(len(other_dict.hash_table)):
-            if other_dict.hash_table[index] is not None:
-                key, hash_key, value = other_dict.hash_table[index]
-                self.__setitem__(key, value)
+    def __str__(self) -> str:
+        result = {item[0]: item[2] for item in self.hash_table if item}
+        return f"{result}"
