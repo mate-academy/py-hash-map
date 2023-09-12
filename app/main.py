5cdbfa7635bb98dfd8244beb49af70faa6b49724
Marina <marishka28023@gmail.com>
2023-09-12 18:20:15 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -2,15 +2,25 @@ from typing import Hashable, Any
 
 
 class Node:
-    def __init__(self, key: Hashable, hash_value: int, value: Any) -> None:
+    def __init__(
+            self,
+            key: Hashable,
+            hash_value: int,
+            value: Any
+    ) -> None:
         self.key = key
         self.hash_value = hash_value
         self.value = value
+        self.next = None
 
 
 class Dictionary:
-    def __init__(self, capacity: int = 8, load_factor: float = 0.67) -> None:
-        self.capacity = capacity
+    def __init__(
+            self,
+            initial_capacity: int = 8,
+            load_factor: float = 0.67
+    ) -> None:
+        self.capacity = initial_capacity
         self.load_factor = load_factor
         self.size = 0
         self.table = [None] * self.capacity
@@ -19,28 +29,37 @@ class Dictionary:
         hash_value = hash(key)
         index = hash_value % self.capacity
 
-        if self.table[index] is None:
-            self.table[index] = [Node(key, hash_value, value)]
-        else:
-            for node in self.table[index]:
-                if node.key == key:
-                    node.value = value
-                    return
-            self.table[index].append(Node(key, hash_value, value))
-        self.size += 1
-
         if self.size >= self.capacity * self.load_factor:
             self._resize()
+
+        current = self.table[index]
+        prev = None
+
+        while current:
+            if current.key == key:
+                current.value = value
+                return
+            prev, current = current, current.next
+
+        new_node = Node(key, hash_value, value)
+        if prev:
+            prev.next = new_node
+        else:
+            self.table[index] = new_node
+
+        self.size += 1
 
     def __getitem__(self, key: Hashable) -> None:
         hash_value = hash(key)
         index = hash_value % self.capacity
+        current = self.table[index]
 
-        if self.table[index] is not None:
-            for node in self.table[index]:
-                if node.key == key:
-                    return node.value
-        raise KeyError(key)
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+
+        raise KeyError
 
     def __len__(self) -> int:
         return self.size
@@ -50,13 +69,16 @@ class Dictionary:
         new_table = [None] * new_capacity
 
         for i in range(self.capacity):
-            if self.table[i] is not None:
-                for node in self.table[i]:
-                    new_index = node.hash_value % new_capacity
-                    if new_table[new_index] is None:
-                        new_table[new_index] = [node]
-                    else:
-                        new_table[new_index].append(node)
+            current = self.table[i]
+            while current:
+                new_index = current.hash_value % new_capacity
+                new_node = Node(current.key, current.hash_value, current.value)
+                if new_table[new_index] is None:
+                    new_table[new_index] = new_node
+                else:
+                    new_node.next = new_table[new_index]
+                    new_table[new_index] = new_node
+                current = current.next
 
         self.table = new_table
         self.capacity = new_capacity
