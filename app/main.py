5c61e7e7e2e706369fd2b2c631cd230e51764aac
Artem <hartem472@gmail.com>
2023-09-16 18:55:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,50 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self, capacity: int = 8) -> None:
+        self.capacity = capacity
+        self.size = 0
+        self.table = [[] for _ in range(self.capacity)]
+
+    def get_hash(self, key: Any) -> int:
+        return hash(key) % self.capacity
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        hash_index = self.get_hash(key)
+        slot = self.table[hash_index]
+
+        for index, (existing_key, existing_value) in enumerate(slot):
+            if existing_key == key:
+                slot[index] = (key, value)
+                break
+        else:
+            slot.append((key, value))
+            self.size += 1
+
+        if self.size >= self.capacity * 2:
+            self.resize()
+
+    def __getitem__(self, key: Any) -> Any:
+        hash_index = self.get_hash(key)
+        slot = self.table[hash_index]
+
+        for existing_key, existing_value in slot:
+            if existing_key == key:
+                return existing_value
+
+        raise KeyError(f"Key '{key}' not found")
+
+    def __len__(self) -> int:
+        return self.size
+
+    def resize(self) -> None:
+        self.capacity *= 2
+        new_table = [[] for _ in range(self.capacity)]
+
+        for bucket in self.table:
+            for key, value in bucket:
+                hash_index = hash(key) % self.capacity
+                new_table[hash_index].append((key, value))
+
+        self.table = new_table
