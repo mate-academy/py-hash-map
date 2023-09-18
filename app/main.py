90613ae8c884ac4f4414ebcfad2ad8fc9712a4e4
serhii_loboda <serhiy.loboda@gmail.com>
2023-09-18 22:48:02 +0300

    Solution: Custom dict.

--- app/main.py
+++ app/main.py
@@ -1,2 +1,72 @@
+from __future__ import annotations
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.length = 0
+        self.hash_table: list = [None] * 8
+
+    def resize_hash_table(self) -> bool:
+        if self.length >= int(len(self.hash_table) * 2 / 3):
+            self.hash_table += [None] * len(self.hash_table)
+
+            return True
+        return False
+
+    def reindex_element_after_resize_table(self) -> None:
+        elements_before_resize = [
+            element
+            for element in self.hash_table
+            if isinstance(element, tuple)
+        ]
+        self.hash_table = [None] * len(self.hash_table)
+
+        for key, value in elements_before_resize:
+            self.insert_element_in_hash_table(key=key, value=value)
+
+    def insert_element_in_hash_table(self, key: Any, value: Any) -> None:
+        index_key = hash(key) % len(self.hash_table)
+
+        while True:
+            if self.hash_table[index_key] is None:
+                self.hash_table[index_key] = (key, value)
+                break
+            elif index_key == (len(self.hash_table) - 1) \
+                    and self.hash_table[index_key] is not None:
+                index_key = 0
+            else:
+                index_key += 1
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        for element in self.hash_table:
+            if isinstance(element, tuple) and (element[0] == key):
+                index_el = self.hash_table.index(element)
+                self.hash_table[index_el] = (key, value)
+                return
+
+        self.insert_element_in_hash_table(key=key, value=value)
+        self.length += 1
+
+        check_resize = self.resize_hash_table()
+        if check_resize:
+            self.reindex_element_after_resize_table()
+
+    def __getitem__(self, item: Any) -> Any | Exception:
+        for element in self.hash_table:
+            if isinstance(element, tuple):
+                if element[0] == item:
+                    return element[1]
+        else:
+            raise KeyError
+
+    def __len__(self) -> int:
+        return self.length
+
+    def __delitem__(self, key: Any) -> None:
+        for element in self.hash_table:
+            if isinstance(element, tuple):
+                if element[0] == key:
+                    index_element = self.hash_table.index(element)
+                    self.hash_table[index_element] = None
+                    self.length -= 1
--- app/point.py
+++ app/point.py
@@ -11,13 +11,13 @@ class Point:
             return False
         return self.x == other.x and self.y == other.y
 
-    def __hash__(self) -> int:
+    def __hash__(self) -> float:
         # Change the implementation of the hash to debug your code.
         # For example, you can return self.x + self.y as a hash
         # which is NOT a best practice, but you will be able to predict
         # a hash value by coordinates of the point and its index
         # in the hashtable as well
-        return hash((self.x, self.y))
+        return self.x + self.y
 
     @property
     def x(self) -> float:
