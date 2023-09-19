54ba5a526b1ff16e4360195e3306f8bc36847f95
jinjo <vitaly.tyagunov@gmail.com>
2023-09-19 10:52:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,22 +3,18 @@ from typing import Any
 
 class Dictionary:
     def __init__(self, elements: list[tuple[Any, Any]] = None) -> None:
-        self.indices = [-1 for _ in range(8)]
+        self.entries = [-1 for _ in range(8)]
         self.length = 0
-        self.entries = []
         if elements is not None:
-            self._assign_entries(elements)
-
-    def _assign_entries(self, elements: list[tuple]) -> None:
-        for key, value in elements:
-            self.__setitem__(key, value)
+            for key, value in elements:
+                self.__setitem__(key, value)
 
     @staticmethod
-    def _get_next_index(index: int, iterable: list[int]) -> int:
+    def _get_next_index(index: int, iterable: list[Any]) -> int:
         return (index + 1) % len(iterable)
 
     @staticmethod
-    def _get_index(key: Any, iterable: list[int]) -> int:
+    def _get_index(key: Any, iterable: list[Any]) -> int:
         return hash(key) % len(iterable)
 
     @staticmethod
@@ -26,35 +22,36 @@ class Dictionary:
             input_key: Any,
             input_hash: int,
             entries_key: Any,
-            entries_hash_: int
+            entries_hash: int
     ) -> bool:
-        return input_key == entries_key and entries_hash_ == input_hash
+        return input_key == entries_key and entries_hash == input_hash
 
-    def _resize_indices(self) -> None:
-        new_indices = [-1 for _ in range(len(self.indices) * 2)]
-        index_in_entries = 0
+    def _resize_entries(self) -> None:
+        new_entries = [-1 for _ in range(len(self.entries) * 2)]
 
         for entry in self.entries:
-            index = self._get_index(entry[1], new_indices)
+            if entry != -1:
+                index = self._get_index(entry[1], new_entries)
+                while new_entries[index] != -1:
+                    index = self._get_next_index(index, new_entries)
+                new_entries[index] = entry
 
-            while new_indices[index] != -1:
-                index = self._get_next_index(index, new_indices)
+        self.entries = new_entries
 
-            new_indices[index] = index_in_entries
-            index_in_entries += 1
-        self.indices = new_indices
+    def _need_resize_entries(self) -> bool:
+        return self.length / len(self.entries) > 0.6
 
     def __setitem__(self, input_key: Any, input_value: Any) -> None:
-        if self.length / len(self.indices) > 0.7:
-            self._resize_indices()
+        if self._need_resize_entries():
+            self._resize_entries()
 
         input_hash = hash(input_key)
-        index = self._get_index(input_key, self.indices)
+        index = self._get_index(input_key, self.entries)
 
-        while self.indices[index] != -1:
+        while self.entries[index] != -1:
 
             entries_hash, entries_key, entries_value = \
-                self.entries[self.indices[index]]
+                self.entries[index]
 
             if self._object_equal(
                     input_key,
@@ -62,23 +59,21 @@ class Dictionary:
                     entries_key,
                     entries_hash
             ):
-                self.entries[self.indices[index]][2] = input_value
+                self.entries[index][2] = input_value
                 return
             else:
-                index = (index + 1) % len(self.indices)
+                index = self._get_next_index(index, self.entries)
 
-        self.indices[index] = self.length
-        self.entries.append([input_hash, input_key, input_value])
+        self.entries[index] = [input_hash, input_key, input_value]
         self.length += 1
 
     def __getitem__(self, input_key: Any) -> Any:
-
         input_hash = hash(input_key)
-        index = self._get_index(input_key, self.indices)
+        index = self._get_index(input_key, self.entries)
 
-        while self.indices[index] != -1:
+        while self.entries[index] != -1:
             entries_hash, entries_key, entries_value = \
-                self.entries[self.indices[index]]
+                self.entries[index]
 
             if self._object_equal(
                     input_key,
@@ -87,8 +82,9 @@ class Dictionary:
                     entries_hash
             ):
                 return entries_value
-            index = self._get_next_index(index, self.indices)
-        raise KeyError
+            index = self._get_next_index(index, self.entries)
+
+        raise KeyError(input_key)
 
     def __len__(self) -> int:
         return self.length
