d1b6026e221be0c94dde478eca9d97e5d5489a40
Ostap <ostapbojko04@gmail.com>
2023-09-24 17:47:23 +0300

    Work_version(py_dictionary)

--- app/main.py
+++ app/main.py
@@ -1,2 +1,67 @@
+from typing import Any, List, Optional
+
+
 class Dictionary:
-    pass
+    def __init__(
+            self,
+            initial_capacity: int = 16,
+            load_factor: float = 0.75
+    ) -> None:
+        self.initial_capacity: int = initial_capacity
+        self.load_factor: float = load_factor
+        self.capacity: int = initial_capacity
+        self.size: int = 0
+        self.table: List[Optional[List[List[Any]]]] = [None] * self.capacity
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        if self.size >= self.load_factor * self.capacity:
+            self._resize()
+        hash_value: int = hash(key)
+        index: int = hash_value % self.capacity
+        if self.table[index] is None:
+            self.table[index] = []
+        for node in self.table[index]:
+            if node[0] == key:
+                node[2] = value
+                return
+        self.table[index].append([key, hash_value, value])
+        self.size += 1
+
+    def __getitem__(self, key: Any) -> Any:
+        hash_value: int = hash(key)
+        index: int = hash_value % self.capacity
+        if self.table[index] is not None:
+            for node in self.table[index]:
+                if node[0] == key:
+                    return node[2]
+        raise KeyError(key)
+
+    def __len__(self) -> int:
+        return self.size
+
+    def _resize(self) -> None:
+        self.capacity *= 2
+        new_table: List[Optional[List[List[Any]]]] = [None] * self.capacity
+        for index in range(len(self.table)):
+            if self.table[index] is not None:
+                for node in self.table[index]:
+                    new_index: int = node[1] % self.capacity
+                    if new_table[new_index] is None:
+                        new_table[new_index] = []
+                    new_table[new_index].append(node)
+        self.table = new_table
+
+
+if __name__ == "__main__":
+    class Point:
+        def __init__(self, x: int, y: int) -> None:
+            self.x: int = x
+            self.y: int = y
+
+        def __eq__(self, other: Any) -> bool:
+            if not isinstance(other, Point):
+                return False
+            return self.x == other.x and self.y == other.y
+
+        def __hash__(self) -> int:
+            return hash((self.x, self.y))
