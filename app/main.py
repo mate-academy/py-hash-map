d0d4152b15f93a74b9305d67b2a8143d44ed6459
Olgierrd <oolgierrd@gmail.com>
2023-10-02 12:45:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,49 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self, capacity: int = 8, load_factor: float = 0.66) -> None:
+        self.capacity = capacity
+        self.load_factor = load_factor
+        self.size = 0
+        self.table = [None] * capacity
+
+    def __setitem__(self, key: Any, value: Any) -> Any:
+        index = self._hash(key)
+        if self.table[index] is None:
+            self.table[index] = []
+        for entry in self.table[index]:
+            if entry[0] == key:
+                entry[1] = value
+                return
+        self.table[index].append([key, value])
+        self.size += 1
+        if self.size > self.capacity * self.load_factor:
+            self._resize()
+
+    def __getitem__(self, key: Any) -> Any:
+        index = self._hash(key)
+        if self.table[index] is not None:
+            for entry in self.table[index]:
+                if entry[0] == key:
+                    return entry[1]
+        raise KeyError(key)
+
+    def __len__(self) -> int:
+        return self.size
+
+    def _hash(self, key: Any) -> int:
+        return hash(key) % self.capacity
+
+    def _resize(self) -> None:
+        new_capacity = self.capacity * 2
+        new_table = [None] * new_capacity
+        for bucket in self.table:
+            if bucket is not None:
+                for key, value in bucket:
+                    new_index = hash(key) % new_capacity
+                    if new_table[new_index] is None:
+                        new_table[new_index] = []
+                    new_table[new_index].append([key, value])
+        self.table = new_table
+        self.capacity = new_capacity
