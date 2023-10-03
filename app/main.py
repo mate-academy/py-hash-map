482b358939121263f16408e5fe4745c066228879
Anton <anton380969658858@gmail.com>
2023-10-03 15:13:38 +0300

    created class Dictionary

--- app/main.py
+++ app/main.py
@@ -1,2 +1,82 @@
+from typing import Any, Union
+
+import copy
+
+ImmutableSequence = Union[int, float, str, tuple]
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.length = 0
+        self.hash_table = [None] * 8
+
+    def resize(self) -> None:
+        if self.__len__() + 1 >= 2 / 3 * len(self.hash_table):
+            old_hash_table = copy.copy(self.hash_table)
+            self.hash_table = [None] * len(self.hash_table) * 2
+            for item in old_hash_table:
+                if item:
+                    self.__setitem__(item[0], item[2])
+                    self.length -= 1
+
+    def __getitem__(self, key: ImmutableSequence) -> Any:
+        index = int(self.__hash__(key) % len(self.hash_table))
+        for i in range(len(self.hash_table)):
+            if self.hash_table[index] and self.hash_table[index][0] == key:
+                return self.hash_table[index][2]
+            index += 1
+            if index == len(self.hash_table):
+                index = 0
+        raise KeyError
+
+    def __setitem__(self, key: ImmutableSequence, value: Any) -> None:
+        self.resize()
+        hashed_key = self.__hash__(key)
+        index = hashed_key % len(self.hash_table)
+        if self.hash_table[index]:
+            for i in range(len(self.hash_table)):
+                if self.hash_table[index][0] == key:
+                    self.hash_table[index] = (key, hashed_key, value)
+                    break
+                index += 1
+                if index == len(self.hash_table):
+                    index = 0
+                if not self.hash_table[index]:
+                    self.hash_table[index] = (key, hashed_key, value)
+                    self.length += 1
+                    break
+        else:
+            self.hash_table[index] = (key, hashed_key, value)
+            self.length += 1
+
+    def __len__(self) -> int:
+        return self.length
+
+    def __hash__(self, key: ImmutableSequence) -> int:
+        if isinstance(key, str):
+            return sum(ord(char) for char in key)
+        return hash(key)
+
+    def clear(self) -> None:
+        self.length = 0
+        self.hash_table = [None] * 8
+
+    def __delitem__(self, key: ImmutableSequence) -> None:
+        self.hash_table[self.__hash__(key) % len(self.hash_table)] = None
+        self.length -= 1
+
+    def get(self, key: ImmutableSequence) -> Any:
+        return self.__getitem__(key)
+
+    def pop(self, key: ImmutableSequence) -> tuple:
+        item = self.hash_table[self.__hash__(key) % len(self.hash_table)]
+        self.hash_table[self.__hash__(key) % len(self.hash_table)] = None
+        self.length -= 1
+        return item
+
+    def update(self, other: dict) -> None:
+        for key, value in other.items():
+            self.__setitem__(key, value)
+
+    def __iter__(self) -> list:
+        return self.hash_table
