575fedda6193db5e99883cb3457e7bdbd35a8ad3
Maksim Stahovskij <maksss196@gmail.com>
2023-10-05 11:41:08 +0300

    py-dictionary

--- app/main.py
+++ app/main.py
@@ -1,58 +1,62 @@
-from app.point import Point
+from typing import Any, List, Tuple
+
+
 class Dictionary:
-    def __init__(self, size=8):
+
+    def __init__(self, size: int = 8) -> None:
         self.data = size
         self.buckets = [[] for _ in range(self.data)]
-        self.load_factor = 2/3
+        self.load_factor = 2 / 3
+        self.total_count = 0
 
-    def hash_data(self, key):
+    def hash_data(self, key: Any) -> int:
         return hash(key) % self.data
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: Any, value: Any) -> None:
         key_hash = self.hash_data(key)
         for i, (k, v) in enumerate(self.buckets[key_hash]):
             if k == key:
                 self.buckets[key_hash][i] = (key, value)
                 return
         self.buckets[key_hash].append((key, value))
-        self.__check_load_factor__()
+        self.total_count += 1
 
-
-    def __getitem__(self, key):
+    def __getitem__(self, key: Any) -> Any:
         key_hash = self.hash_data(key)
         for k, v in self.buckets[key_hash]:
             if k == key:
                 return v
-        raise KeyError(f'Key not found: {key}')
+        raise KeyError(f"Key not found: {key}")
 
-
-    def __len__(self):
+    def __len__(self) -> int:
         total_count = 0
         for bucket in self.buckets:
             total_count += len(bucket)
         return total_count
 
+    def __check_load_factor__(self) -> None:
+        if self.total_count > self.load_factor * self.data:
+            self.__resize__()
 
-    def __check_load_factor__(self):
-        total_count = 0
-        for bucket in self.buckets:
-            total_count += len(bucket)
-            if total_count > self.load_factor * self.data:
-                self.__resize__()
-
-    def __resize__(self):
+    def __resize__(self) -> None:
         new_size = 2 * self.data
-        new_bucket = [[] for _ in range(new_size)]
+        new_bucket: List[List[Tuple[Any, Any]]] = [[] for _ in range(new_size)]
 
         for bucket in self.buckets:
-            for key, value in bucket:
-                new_key_hash = self.hash_data(key)
-                new_bucket[new_key_hash].append((key, value))
+            for k, v in bucket:
+                new_key_hash = self.hash_data(k)
+                new_bucket[new_key_hash].append((k, v))
 
         self.data = new_size
         self.buckets = new_bucket
 
 
-my_dict = Dictionary()
-my_dict.__setitem__(key=3, value="staho")
-print(my_dict.__getitem__(key=3))
+if __name__ == "__main__":
+    items = [(f"Elements {i}", i) for i in range(8)]
+    dictionary = Dictionary()
+    for key, value in items:
+        dictionary[key] = value
+
+    for key, value in items:
+        print(dictionary[key] == value)
+    print(len(dictionary) == len(items))
