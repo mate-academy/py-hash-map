f6c4fff99d1860b85ffcb03a7abdb3e9a84b0206
Nadiia <nadiia.developer777@gmail.com>
2023-10-09 17:22:24 +0300

    Solution-six-py-dictionary

---
+++ app/point.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+
+class Point:
+    def __init__(self, x: float, y: float) -> None:
+        self._x = x
+        self._y = y
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Point):
+            return False
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self) -> int:
+        # For example, you can return self.x + self.y as a hash
+        # which is NOT a best practice, but you will be able to predict
+        # a hash value by coordinates of the point and its index
+        # in the hashtable as well
+        return hash((self.x, self.y))
+
+    @property
+    def x(self) -> float:
+        return self._x
+
+    @property
+    def y(self) -> float:
+        return self._y
