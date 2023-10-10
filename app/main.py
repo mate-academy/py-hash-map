cf6dc676c620311fcc29baeef525d677254d87f2
Anton Tkachuk <tkacuk2291@gmail.com>
2023-10-10 19:52:49 +0300

    Solution_x

--- app/main.py
+++ app/main.py
@@ -1,34 +1,45 @@
 class Dictionary:
     def __init__(self) -> None:
-        self.dicts = []
         self.capacity = 8
-        self.resize = None
+        self.hash_table = [None] * self.capacity
         self.lens = 0
 
     def __setitem__(self, key: int, value: int) -> None:
         hash_key = hash(key)
-        self.dicts.append([key, hash(key), value])
-        for i, (k, h, v) in enumerate(self.dicts):
-            if hash(key) == h and key == k and v != value:
-                self.dicts[i] = [key, hash_key, value]
-                return
+        dicts_index = hash_key % self.capacity
+        if self.lens >= self.capacity // 1.2:
+            self.resize()
+            dicts_index = hash_key % self.capacity
 
-        self.lens += 1
+        while True:
+            if self.hash_table[dicts_index] is None:
+                self.hash_table[dicts_index] = [key, hash_key, value]
+                self.lens += 1
+                break
+            elif self.hash_table[dicts_index][0] == key:
+                self.hash_table[dicts_index][2] = value
+                break
+            dicts_index = (dicts_index + 1) % self.capacity
 
-    def __getitem__(self, key: int) -> None:
+    def __getitem__(self, key: int) -> int:
         hash_key = hash(key)
-        for k, h, v in self.dicts:
-            if hash_key == h and key == k:
-                return v
-        raise KeyError(f"{key} , not found")
+        dicts_index = hash_key % self.capacity
+
+        for _ in range(self.capacity):
+            if self.hash_table[dicts_index] is None:
+                raise KeyError(f"{key} not found")
+            if self.hash_table[dicts_index][0] == key:
+                return self.hash_table[dicts_index][2]
+            dicts_index = (dicts_index + 1) % self.capacity
 
     def __len__(self) -> int:
         return self.lens
 
-    def resize_(self) -> None:
+    def resize(self) -> None:
         self.capacity = self.capacity * 2
-        old_table = self.dicts
-        self.dicts = [None] * self.capacity
+        old_table = self.hash_table
+        self.hash_table = [None] * self.capacity
+        self.lens = 0
         for item in old_table:
             if item:
                 self.__setitem__(item[0], item[2])
