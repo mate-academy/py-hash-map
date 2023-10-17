6a518ddf2f42a1fc213561ce4b4769c806426eec
struhanets <posseydon87@gmail.com>
2023-10-17 15:59:40 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -14,8 +14,7 @@ class Dictionary:
         self.capasity = capasity
         self.size = size
         # створюємо дефолтну пусту таблицю
-        self.table = [[] for _ in range(self.capasity)]
-        self.overload = round(self.capasity * self.load_factor)
+        self.table = [None] * self.capasity
 
     # в цьому методі вираховуємо індекс елемента,
     # за формулою, щоб не поіторювати цю формулу в інших методах
@@ -27,47 +26,39 @@ class Dictionary:
     # проходить перевірка чи немає колізії(комірка зайнята іншим елементом)
     # чи заміна елемента якщо мають одинакові ключі
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        hash_key = hash(key)
-        index_key = self.hash_func(key)
-        if self.size >= self.overload:
-            self.resize()
-            index_key = self.hash_func(key)
+        if self.size >= self.capasity * self.load_factor:
+            self.resize()  # зміна розміру після перегрузки таблиці
+        index = self.find_index(key)
+        if self.table[index] is None:
+            self.size += 1
+        self.table[index] = [key, value, hash(key)]
 
-            while True:
-                if not self.table[index_key]:
-                    self.table[index_key] = [key, value, hash_key]
-                    self.size += 1
-                else:
-                    if self.table[index_key][0] == key:
-                        self.table[index_key][1] = value
-                        break
-                    index_key = (index_key + 1) % self.capasity
-                    self.table[index_key] = [key, value, hash_key]
-                    self.size += 1
+    def find_index(self, key: Hashable) -> int:
+        index_key = self.hash_func(key)
+        while self.table[index_key] is not None and \
+                self.table[index_key][0] != key:
+            index_key += 1
+            index_key %= self.capasity
+        return index_key
 
     # прописуємо розширення таблиці при заповненні на 2/3
     def resize(self) -> None:
-        capasity_incris = self.capasity * 2
         old_table = self.table
-        self.table = [None] * capasity_incris
-        self.capasity = capasity_incris
+        self.capasity *= 2
+        self.table = [None] * self.capasity
+
         self.size = 0
         for item in old_table:
-            if item:
+            if item is not None:
                 self[item[0]] = item[1]
 
     # повернення значення при запиті по ключу,
     # якщо такого ключа немає, викидає помилку
     def __getitem__(self, key: Hashable) -> Any:
-        hash_key = hash(key)
-        index_key = self.hash_func(key)
-        while True:
-            if not self.table[index_key]:
-                raise KeyError(f"Key '{key}' not found in the dictionary")
-            elif self.table[index_key][0] == key and \
-                    self.table[index_key][2] == hash_key:
-                return self.table[index_key][1]
-            index_key = (index_key + 1) % self.capasity
+        index = self.find_index(key)
+        if self.table[index] is None:
+            raise KeyError(f"Key '{key}' not found in the dictionary")
+        return self.table[index][1]
 
     # довжина таблиці із заповненими елементами
     def __len__(self) -> int:
