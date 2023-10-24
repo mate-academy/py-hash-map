17a83d9fa34e99e147fb69413d508a181c10bcec
Manynik <s.onufriyenko@gmail.com>
2023-10-24 14:34:21 +0300

    Solution-6

--- app/main.py
+++ app/main.py
@@ -3,25 +3,24 @@ from typing import Any, Hashable
 
 class Dictionary:
     def __init__(self, capacity: int = 8) -> None:
-        self.data = capacity
-        self.buckets = [[] for _ in range(self.data)]
+        self.capacity = capacity
+        self.buckets = [[] for _ in range(self.capacity)]
         self.load_factor = 2 / 3
         self.total_count = 0
 
     def hash_data(self, key: Hashable) -> int:
-        return hash(key) % self.data
+        return hash(key) % self.capacity
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        if self.total_count >= self.data * self.load_factor:
-            self.__resize__(2 * self.data)
         key_hash = self.hash_data(key)
-        for index, (exist_key, exist_value) \
-                in enumerate(self.buckets[key_hash]):
+        for i, (exist_key, exist_value) in enumerate(self.buckets[key_hash]):
             if exist_key == key:
-                self.buckets[key_hash][index] = (key, value)
+                self.buckets[key_hash][i] = (key, value)
                 return
         self.buckets[key_hash].append((key, value))
         self.total_count += 1
+        if self.total_count >= self.capacity * self.load_factor:
+            self.__resize__()
 
     def __getitem__(self, key: Hashable) -> Any:
         key_hash = self.hash_data(key)
@@ -31,12 +30,10 @@ class Dictionary:
         raise KeyError(f"Key not found: {key}")
 
     def __len__(self) -> int:
-        total_count = 0
-        for bucket in self.buckets:
-            total_count += len(bucket)
-        return total_count
+        return self.total_count
 
-    def __resize__(self, new_size: int) -> None:
+    def __resize__(self) -> None:
+        new_size = 2 * self.capacity
         new_buckets = [[] for _ in range(new_size)]
 
         for bucket in self.buckets:
@@ -44,5 +41,5 @@ class Dictionary:
                 new_index = hash(key) % new_size
                 new_buckets[new_index].append((key, value))
 
-        self.data = new_size
+        self.capacity = new_size
         self.buckets = new_buckets
