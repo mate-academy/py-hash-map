c06e73d2b3798267905a9827eec6b8c388547ef2
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-28 12:31:57 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,73 +1,89 @@
-import random
-from typing import Any, Hashable
+from dataclasses import dataclass
+from typing import Hashable, Any
+
+
+@dataclass
+class Node:
+    key: Hashable
+    value: Any
 
 
 class Dictionary:
-    def __init__(self) -> None:
+    def __init__(
+            self,
+            capacity: int = 8,
+            resize_threshold: float = 2/3,
+            capacity_multiplier: int = 2
+    ) -> None:
+
+        self.capacity = capacity
+        self.resize_threshold = resize_threshold
+        self.capacity_multiplier = capacity_multiplier
+        self.hash_table: list[Node | None] = [None] * self.capacity
         self.length = 0
-        self.len_hash_table = 8
-        self.hash_table: list = [None] * self.len_hash_table
 
-    def __len__(self) -> int:
-        return self.length
+    @property
+    def current_max_size(self) -> float:
+        return self.resize_threshold * self.capacity
 
-    @staticmethod
-    def hash(value: Hashable) -> int:
-        random.seed(42)
-        return sum(ord(char) + random.randint(0, 10000) for char in value)
+    def _calculate_index(self, key: Hashable) -> int:
+        index = hash(key) % self.capacity
+
+        while (
+            self.hash_table[index] is not None and
+            self.hash_table[index].key != key
+        ):
+            index = (index + 1) % self.capacity
+
+        return index
+
+    def __getitem__(self, key: Hashable) -> Any:
+        index = self._calculate_index(key)
+
+        if self.hash_table[index] is None:
+            raise KeyError(f"Cannot find value for key: {key}")
+
+        return self.hash_table[index].value
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        index = self._calculate_index(key)
+
+        if self.hash_table[index] is None:
+            if self.length + 1 >= self.current_max_size:
+                self.resize()
+                return self.__setitem__(key, value)
+
+            self.length += 1
+
+        self.hash_table[index] = Node(key, value)
+
+    def __delitem__(self, key: Hashable) -> None:
+        index = self._calculate_index(key)
+
+        if self.hash_table[index] is None:
+            raise KeyError(f"Key not found: {key}")
+
+        self.hash_table[index] = None
+        self.length -= 1
 
     def resize(self) -> None:
 
-        threshold = int(self.len_hash_table * 0.66)
-        if self.length >= threshold:
+        old_hash_table = self.hash_table
 
-            self.len_hash_table *= 2
-            self.length = 0
-            new_hash_table = [None] * self.len_hash_table
+        self.__init__(self.capacity * self.capacity_multiplier)
 
-            for data in self.hash_table:
-                if data is not None:
-                    key, _, value = data
-                    hash_key = self.hash(key) % self.len_hash_table
-                    index_hash = hash_key
+        for node in old_hash_table:
+            if node is not None:
+                self.__setitem__(node.key, node.value)
 
-                    while new_hash_table[index_hash] is not None:
-                        index_hash += 1
-                        if index_hash >= self.len_hash_table:
-                            index_hash = 0
+    def get(self, key: Hashable, default: Any = None) -> Any:
+        try:
+            return self[key]
+        except KeyError:
+            return default
 
-                    new_hash_table[index_hash] = (key, hash_key, value)
-                    self.length += 1
-            self.hash_table = new_hash_table
+    def __str__(self) -> str:
+        return str(self.hash_table)
 
-    def __setitem__(self, key: Hashable, value: Any) -> None:
-
-        hash_key = self.hash(key)
-        index_hash = hash_key % self.len_hash_table
-
-        while self.hash_table[index_hash] is not None:
-            if self.hash_table[index_hash][0] == key:
-                self.hash_table[index_hash] = (key, hash_key, value)
-                return
-            index_hash += 1
-            if index_hash >= self.len_hash_table:
-                index_hash = 0
-
-        self.hash_table[index_hash] = (key, hash_key, value)
-        self.length += 1
-
-        self.resize()
-
-    def __getitem__(self, key: Hashable) -> Any:
-
-        hash_key = self.hash(key)
-        index_hash = hash_key % self.len_hash_table
-
-        while self.hash_table[index_hash][0] != key:
-            index_hash += 1
-            if index_hash >= self.len_hash_table:
-                index_hash = 0
-
-        output = self.hash_table[index_hash][1]
-
-        return output
+    def __len__(self) -> int:
+        return self.length
