f8e59552393901016a77a4a6d029516c28222015
Ihor Bachuk <igor.bachuk04@gmail.com>
2023-10-28 13:21:11 +0300

    update

--- app/main.py
+++ app/main.py
@@ -8,24 +8,22 @@ class Dictionary:
     ) -> None:
         self.length = 0
         self.capacity = 8
-        self.keys = set()
         self.hash_table: list = [None] * self.capacity
 
     def _resize(self) -> None:
         self.capacity *= 2
         self.length = 0
-        self.temp_table: list = self.hash_table
+        temp_table: list = self.hash_table
         self.hash_table = [None] * self.capacity
-        for cell in self.temp_table:
+        for cell in temp_table:
             if cell:
-                self.__setitem__(cell[0], cell[2])
-        del self.temp_table
+                self[cell[0]] = cell[2]
 
     def __setitem__(self, key: Any, value: Any) -> None:
         if self.length == int(self.capacity * (2 / 3)):
             self._resize()
         cell_number = hash(key) % self.capacity
-        for _ in range(self.capacity):
+        while True: # while loop > for loop
             if not self.hash_table[cell_number]:
                 self.hash_table[cell_number] = [key, hash(key), value]
                 self.length += 1
@@ -36,18 +34,18 @@ class Dictionary:
                 break
             else:
                 cell_number = (cell_number + 1) % self.capacity
-        self.keys.add(key)
 
     def __getitem__(self, item: Any) -> None:
-        if item not in self.keys:
-            raise KeyError
         cell_number = hash(item) % self.capacity
         for _ in range(self.capacity):
+            if self.hash_table[cell_number] is None:
+                break
             if (self.hash_table[cell_number][0] == item
                     and self.hash_table[cell_number][1] == hash(item)):
                 return self.hash_table[cell_number][2]
             else:
                 cell_number = (cell_number + 1) % self.capacity
+        raise KeyError
 
     def __len__(self) -> int:
         return self.length
