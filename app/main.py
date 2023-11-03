4da83558188947a4573876561b4bade75fc1641c
Sergii Pryima <spryima@gmail.com>
2023-11-03 17:18:39 +0200

    hash added

--- app/main.py
+++ app/main.py
@@ -14,14 +14,14 @@ class Dictionary:
 
         for node_list in self.hash_table:
             if node_list:
-                for key, value in node_list:
+                for key, hsh, value in node_list:
 
                     index = hash(key) % self.capacity
 
                     if new_table[index] is None:
-                        new_table[index] = [(key, value)]
+                        new_table[index] = [(key, hsh, value)]
                     else:
-                        new_table[index].append((key, value))
+                        new_table[index].append((key, hsh, value))
 
         self.hash_table = new_table
 
@@ -33,21 +33,21 @@ class Dictionary:
         index = hash(key) % self.capacity
 
         if self.hash_table[index] is None:
-            self.hash_table[index] = [(key, value)]
+            self.hash_table[index] = [(key, hash(key), value)]
             self.length += 1
         else:
-            for idx, (exist_key, _) in enumerate(self.hash_table[index]):
+            for idx, (exist_key, hsh, _) in enumerate(self.hash_table[index]):
                 if exist_key == key:
-                    self.hash_table[index][idx] = (key, value)
+                    self.hash_table[index][idx] = (key, hash(key), value)
                     break
             else:
-                self.hash_table[index].append((key, value))
+                self.hash_table[index].append((key, hash(key), value))
                 self.length += 1
 
     def __getitem__(self, key: Any) -> Any:
         index = hash(key) % self.capacity
         if self.hash_table[index] is not None:
-            for exist_key, value in self.hash_table[index]:
+            for exist_key, _, value in self.hash_table[index]:
                 if exist_key == key:
                     return value
         raise KeyError(key)
