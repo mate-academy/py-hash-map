df9c65bb1a764e8302d2db38cd2162dc1f8ec28c
Frezworx <frezworx@gmail.com>
2023-11-13 18:03:11 +0200

    Refactor code, add _resize_array function

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Hashable
 
 
 class Dictionary:
@@ -6,38 +6,46 @@ class Dictionary:
     def __init__(
             self,
             initial_size: int = 8,
+            expansion_factor: int = 2,
             load_factor: float = 0.7
     ) -> None:
         self._initial_size = initial_size
+        self.expansion_factor = expansion_factor
         self._load_factor = load_factor
         self._array_buckets = [[] for _ in range(initial_size)]
-        # self._length =
 
-    def _get_hash_code(self, key: int | float | str) -> int:
-        return hash(key) % self._initial_size
+    def _get_hash_code(self, key: Hashable) -> int:
+        return hash(key) % len(self._array_buckets)
 
-    def add_data(self, key: int | float | str, value: Any) -> None:
-        hash_code = self._get_hash_code(key)
-        bucket = self._array_buckets[hash_code]
-        for i, (key_, _) in enumerate(bucket):
+    def _resize_array(self) -> None:
+        old_array = self._array_buckets
+        new_size_array = len(old_array) * self.expansion_factor
+        self._array_buckets = [
+            [] for _ in range(new_size_array)
+        ]
+
+        for bucket in old_array:
+            for key, value in bucket:
+                index = hash(key) % new_size_array
+                self._array_buckets[index].append((key, value))
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        if self.__len__() >= len(self._array_buckets) * self._load_factor:
+            self._resize_array()
+
+        bucket = self._array_buckets[self._get_hash_code(key)]
+        for index, (key_, _) in enumerate(bucket):
             if key_ == key:
-                bucket[i] = (key, value)
+                bucket[index] = (key, value)
                 return
         bucket.append((key, value))
 
-    def get_data(self, key: int | float | str) -> Any:
-        hash_code = self._get_hash_code(key)
-        bucket = self._array_buckets[hash_code]
+    def __getitem__(self, key: Hashable) -> Any:
+        bucket = self._array_buckets[self._get_hash_code(key)]
         for key_, value_ in bucket:
             if key_ == key:
                 return value_
         raise KeyError(f"Key {key} not found")
-
-    def __setitem__(self, key: int | float | str, value: Any) -> None:
-        self.add_data(key, value)
-
-    def __getitem__(self, key: int | float | str) -> Any:
-        return self.get_data(key)
 
     def __repr__(self) -> str:
         return (
