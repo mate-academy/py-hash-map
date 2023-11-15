3d25ecf387a514475eb9edeffdce95048019747f
Vyacheslav Vasylenko <vvg04021968@gmail.com>
2023-11-15 09:11:34 +0200

    'Solution_dictionary'

--- app/main.py
+++ app/main.py
@@ -1,2 +1,53 @@
+from typing import Any, Hashable
+
+
 class Dictionary:
-    pass
+    # noinspection PyCompatibility
+    def __init__(self) -> None:
+        self.capacity = 8
+        self.load_factor = 2 / 3
+        self.size = 0
+        self.hash_table: list = [None] * 8
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        if self.size >= self.capacity * self.load_factor:
+            self.resize()
+        if self.hash_table[self.count_index(key)] is None:
+            self.size += 1
+        self.hash_table[self.count_index(key)] = [key, value, hash(key)]
+
+    def __getitem__(self, key: Hashable) -> Any:
+        if self.hash_table[self.count_index(key)] is None:
+            raise KeyError(key)
+        return self.hash_table[self.count_index(key)][1]
+
+    def __len__(self) -> int:
+        return self.size
+
+    def resize(self) -> None:
+        temp_table = self.hash_table
+        self.capacity *= 2
+        self.hash_table = [None] * self.capacity
+        self.size = self.__len__()
+        for item in temp_table:
+            if item:
+                key = item[0]
+                self.hash_table[self.count_index(key)] = item
+
+    def count_index(self, key: Hashable) -> int:
+        index = hash(key) % self.capacity
+        while self.hash_table[index] and self.hash_table[index][0] != key:
+            index += 1
+            index %= self.capacity
+        return index
+
+    def clear(self) -> None:
+        self.hash_table = [None] * self.capacity
+        self.size = 0
+
+    def __delitem__(self, key: Hashable) -> None:
+        index = hash(key) % self.capacity
+        if not self.hash_table[index]:
+            raise KeyError(key)
+        self.hash_table[index] = None
+        self.size -= 1
