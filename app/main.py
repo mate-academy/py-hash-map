4e011173cf16a7ea90cd90dc1bf6a26b8129d970
Alex <oleksa.shcherbakov@gmail.com>
2023-11-23 14:28:43 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from typing import Any
+
+
 class Dictionary:
     def __init__(self, capacity: int = 8) -> None:
         self.capacity = capacity
@@ -11,29 +13,26 @@ class Dictionary:
         while True:
             if self.storage[hash_key] != []:
                 hash_key += 1
-                if hash_key > self.capacity -1:
+                if hash_key > self.capacity - 1:
                     hash_key = 0
             else:
                 self.storage[hash_key] = [key, value, hash(key)]
                 self.size += 1
                 break
 
-
-
-    def resize(self):
+    def resize(self) -> None:
         if self.size > 2 / 3 * self.capacity:
-            self.capacity *=2
+            self.capacity *= 2
             old_elements = self.storage
             self.storage = [[] for _ in range(self.capacity)]
             self.size = 0
             for element in old_elements:
-             if element != []:
-                 self.__setitem__(element[2],element[1])
+                if element != []:
+                     self.__setitem__(element[2], element[1])
 
 
     def __getitem__(self, key: Any) -> Any:
         hash_key = hash(key) % self.capacity
-
         if not self.storage[hash_key]:
             raise KeyError
         else:
