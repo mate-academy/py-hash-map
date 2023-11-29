7ee6a4edce391b81cff081ed021d7144be71bb24
Volodymyr Ivanchuk <Volodymyr_Ivanchuk@jabil.com>
2023-11-29 14:53:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,23 +6,28 @@ class Dictionary:
     def __init__(self) -> None:
         self.capacity = 8
         self.hash_table = [None] * self.capacity
-        self.fulfillment = 0
+        self.size = 0
 
     def __get_index(self, key: Any) -> int:
         index = hash(key) % self.capacity
-        while (self.hash_table[index] is not None
-               and self.hash_table[index][0] != key):
+        while (
+            self.hash_table[index] is not None
+            and (
+                self.hash_table[index][0] != key
+                or self.hash_table[index][2] != hash(key)
+            )
+        ):
             index = (index + 1) % self.capacity
         return index
 
     def __setitem__(self, key: Any, value: Any) -> None:
         index = self.__get_index(key)
         if self.hash_table[index] is None:
-            self.fulfillment += 1
-            self.hash_table[index] = [key, value]
+            self.size += 1
+            self.hash_table[index] = [key, value, hash(key)]
         elif self.hash_table[index] is not None:
-            self.hash_table[index] = [key, value]
-        if self.fulfillment >= self.capacity * 2 / 3:
+            self.hash_table[index] = [key, value, hash(key)]
+        if self.size >= self.capacity * 2 / 3:
             self.__resize()
 
     def __getitem__(self, key: Any) -> Any:
@@ -32,7 +37,7 @@ class Dictionary:
         return self.hash_table[index][1]
 
     def __len__(self) -> int:
-        return self.fulfillment
+        return self.size
 
     def __resize(self) -> None:
         old_hash_table = self.hash_table.copy()
@@ -40,8 +45,8 @@ class Dictionary:
         self.__clear()
         for cell in old_hash_table:
             if cell is not None:
-                self.__setitem__(cell[0], cell[1])
+                self[cell[0]] = cell[1]
 
     def __clear(self) -> None:
         self.hash_table = [None] * self.capacity
-        self.fulfillment = 0
+        self.size = 0
