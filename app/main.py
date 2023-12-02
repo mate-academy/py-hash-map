cac59189365f91d622636b6b9fa48231335e1de4
Serhii <serhii.domik@gmail.com>
2023-12-02 14:39:20 +0200

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from typing import Any
+from typing import Any, Hashable
 
 
 class Node:
-    def __init__(self, key: int, value: Any) -> None:
+    def __init__(self, key: Hashable, value: Any) -> None:
         self.key = key
         self.value = value
+        self.hash = hash(key)
 
 
 class Dictionary:
@@ -14,17 +15,18 @@ class Dictionary:
         self.hash_table: list = [None] * self.capacity
         self.load_factor = 0.7
 
-    def _hash(self, key: int) -> int:
+    def _hash(self, key: Hashable) -> int:
         return hash(key) % self.capacity
 
     def __len__(self) -> int:
         return self.size
 
-    def __setitem__(self, key: int, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         index = self._hash(key)
 
         while self.hash_table[index] is not None:
-            if self.hash_table[index].key == key:
+            if (self.hash_table[index].hash == hash(key)
+                    and self.hash_table[index].key == key):
                 self.hash_table[index].value = value
                 return
             index = (index + 1) % self.capacity
@@ -35,13 +37,14 @@ class Dictionary:
         if self.size / self.capacity >= self.load_factor:
             self._resize()
 
-    def __getitem__(self, key: int) -> int:
+    def __getitem__(self, key: Hashable) -> int:
         index = self._hash(key)
         while self.hash_table[index] is not None:
-            if self.hash_table[index].key == key:
+            if (self.hash_table[index].hash == hash(key)
+                    and self.hash_table[index].key == key):
                 return self.hash_table[index].value
             index = (index + 1) % self.capacity
-        raise KeyError
+        raise KeyError(key)
 
     def _resize(self) -> None:
         self.capacity *= 2
@@ -56,10 +59,11 @@ class Dictionary:
 
         self.hash_table = new_table
 
-    def __delitem__(self, key: int) -> None:
+    def __delitem__(self, key: Hashable) -> None:
         index = self._hash(key)
         while self.hash_table[index].key != key:
-            if self.hash_table[index].key == key:
+            if (self.hash_table[index].hash == hash(key)
+                    and self.hash_table[index].key == key):
                 self.hash_table[index] = None
             index = (index + 1) % self.capacity
         self.size -= 1
