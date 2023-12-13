76aae440b1e157e64f1630927ce1b35309361187
MykolaMazhula <mykm3ua@gmail.com>
2023-12-13 12:39:09 +0100

    edited3

--- app/main.py
+++ app/main.py
@@ -1,22 +1,23 @@
 from __future__ import annotations
-from typing import Any
+from typing import Any, Hashable
 
 
 class Dictionary:
     def __init__(self) -> None:
         self.pairs = [[] for i in range(8)]
         self.next_index = 0
+        self.length = 0
 
-    def __setitem__(
-            self,
-            key: int | float | str | type | bool | tuple | frozenset,
-            value: Any
-    ) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         cell_index = hash(key) % len(self.pairs)
         step = 0
         for pair in self.pairs[cell_index:] + self.pairs[:cell_index]:
-            if len(pair) == 0 or pair[0] == key:
+            if len(pair) == 0:
+                self.length += 1
                 break
+            elif pair[0] == key:
+                break
+
             step += 1
         self.pairs[(cell_index + step) % len(self.pairs)] = [key, value]
         self.resize()
@@ -27,13 +28,11 @@ class Dictionary:
                 pair[0] : pair[1] for pair in self.pairs if len(pair) == 2
             }
             self.pairs = [[] for i in range(len(self.pairs) * 2)]
+            self.length = 0
             for key, value in _items_present.items():
                 self.__setitem__(key, value)
 
-    def __getitem__(
-            self,
-            key: int | float | str | type | bool | tuple | frozenset
-    ) -> Any:
+    def __getitem__(self, key: Hashable) -> Any:
         cell_index = hash(key) % len(self.pairs)
         for pair in self.pairs[cell_index:] + self.pairs[:cell_index]:
             if len(pair) == 2 and pair[0] == key:
@@ -41,50 +40,40 @@ class Dictionary:
         raise KeyError
 
     def __len__(self) -> int:
-        return sum(len(pair) == 2 for pair in self.pairs)
+        return self.length
 
     def clear(self) -> None:
         self.pairs = [[] for i in range(8)]
 
-    def __delitem__(
-            self,
-            key: int | float | str | type | bool | tuple | frozenset
-    ) -> None:
+    def __delitem__(self, key: Hashable,) -> None:
         is_key_exist = False
         cell_index = hash(key) % len(self.pairs)
-        if (len(self.pairs[cell_index]) == 2
-                and self.pairs[cell_index][0] == key):
-            self.pairs[cell_index] = []
-            is_key_exist = True
+        step = 0
+        for pair in self.pairs[cell_index:] + self.pairs[:cell_index]:
+            if len(pair) == 2 and pair[0] == key:
+                is_key_exist = True
+                break
+            step += 1
+        if is_key_exist:
+            self.pairs[(cell_index + step) % len(self.pairs)] = []
+            self.length -= 1
         else:
-            for pair in self.pairs[cell_index + 1:] + self.pairs[:cell_index]:
-                if len(pair) == 2 and pair[0] == key:
-                    pair = []
-                    is_key_exist = True
-        if not is_key_exist:
             raise KeyError
 
-    def get(
-            self,
-            key: int | float | str | type | bool | tuple | frozenset
-    ) -> Any:
+    def get(self, key: Hashable) -> Any:
         return self.__getitem__(key)
 
-    def pop(
-            self,
-            key: int | float | str | type | bool | tuple | frozenset
-    ) -> Any:
+    def pop(self, key: Hashable) -> Any:
         value = self.__getitem__(key)
         self.__delitem__(key)
         return value
 
     def update(self, iterable_item: dict | list | tuple) -> None:
         if isinstance(iterable_item, dict):
-            for key, value in iterable_item.items():
-                self.__setitem__(key, value)
-        else:
-            for pair in iterable_item:
-                self.__setitem__(pair[0], pair[1])
+            iterable_item = [(key, value)
+                             for key, value in iterable_item.items()]
+        for key, value in iterable_item:
+            self.__setitem__(key, value)
 
     def __iter__(self) -> Dictionary:
         self.next_index = 0
