2723137d6b45dd9756198dead2e39bdf84c61b1f
MykolaMazhula <mykm3ua@gmail.com>
2023-12-14 21:51:37 +0100

    no more temporary lists

--- app/main.py
+++ app/main.py
@@ -10,20 +10,27 @@ class Dictionary:
 
     def __get_index__(self, key: Hashable, action: str) -> int:
         cell_index = hash(key) % len(self.cells)
-        step = 0
-        is_key_exist = False
-        for items in self.cells[cell_index:] + self.cells[:cell_index]:
-            if action == "add":
-                if not items or items[0] == key:
-                    break
-            elif action == "get":
-                if items and items[0] == key:
-                    is_key_exist = True
-                    break
-            step += 1
-        if action == "get" and not is_key_exist:
+        is_key_exists = False
+
+        def find_key(sliced_list: list) -> int:
+            nonlocal is_key_exists
+            for index, items in enumerate(sliced_list):
+                if action == "add":
+                    if not items or items[0] == key:
+                        return index
+                elif action == "get":
+                    if items and items[0] == key:
+                        is_key_exists = True
+                        return index
+
+        _index = find_key(self.cells[cell_index:])
+        if _index is None:
+            _index = find_key(self.cells[:cell_index])
+        else:
+            _index += len(self.cells[:cell_index])
+        if action == "get" and not is_key_exists:
             raise KeyError(f"No such a key {key}")
-        return (cell_index + step) % len(self.cells)
+        return _index
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
         cell_index = self.__get_index__(key, "add")
@@ -31,17 +38,17 @@ class Dictionary:
         if not cell_content:
             self.length += 1
         self.cells[cell_index] = [key, value]
-        self.resize()
+        if len(self) > len(self.cells) / 3 * 2:
+            self.resize()
 
     def resize(self) -> None:
-        if len(self) > len(self.cells) / 3 * 2:
-            items_present = {
-                items[0] : items[1] for items in self.cells if items
-            }
-            self.cells = [[] for i in range(len(self.cells) * 2)]
-            self.length = 0
-            for key, value in items_present.items():
-                self.__setitem__(key, value)
+        items_present = {
+            items[0] : items[1] for items in self.cells if items
+        }
+        self.cells = [[] for i in range(len(self.cells) * 2)]
+        self.length = 0
+        for key, value in items_present.items():
+            self.__setitem__(key, value)
 
     def __getitem__(self, key: Hashable) -> Any:
         return self.cells[self.__get_index__(key, "get")][1]
