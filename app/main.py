a2986211b247b388dfc15699bb65ce729f1649bb
Vlad <mr.darmstadtium@gmail.com>
2023-12-14 20:20:15 +0100

    pass all tests

--- app/main.py
+++ app/main.py
@@ -45,9 +45,9 @@ class Dictionary:
         else:
             try:
                 while (
-                    self.table[index] is not None
-                    and self.table[index].key != key
-            ):
+                        self.table[index] is not None
+                        and self.table[index].key != key
+                ):
                     index += 1
                     if index > len(self.table):
                         index = 0
@@ -70,6 +70,7 @@ class Dictionary:
         old_table = copy.deepcopy(self.table)
         self.capacity *= 2
         self.table = [None] * self.capacity
+        self.size = 0
 
         for member in old_table:
             try:
@@ -130,3 +131,34 @@ class Dictionary:
             element_index = self.table.index(element)
             self.table[element_index] = None
             print(f"Item with key {key} was destroyed")
+
+
+l = [
+    (8, "8"),
+    (16, "16"),
+    (32, "32"),
+    (64, "64"),
+    (128, "128"),
+    ("one", 2),
+    ("two", 2),
+    (Point(1, 1), "a"),
+    ("one", 1),
+    ("one", 11),
+    ("one", 111),
+    ("one", 1111),
+    (145, 146),
+    (145, 145),
+    (145, -1),
+    ("two", 22),
+    ("two", 222),
+    ("two", 2222),
+    ("two", 22222),
+    (Point(1, 1), "A"),
+]
+
+my_dict = Dictionary()
+
+for element in l:
+    my_dict[element[0]] = element[1]
+    print("wroted_element: ", element)
+    print("dict_len: ", my_dict.__len__())
