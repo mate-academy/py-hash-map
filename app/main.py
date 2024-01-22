ee3a0f95a0855ab0ad5702e9667ecd77ed9272b0
Andrii Zubchenko <dissom1987@gmail.com>
2024-01-22 21:57:39 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -19,46 +19,43 @@ class Dictionary:
     def __post_init__(self) -> None:
         self.hash_table = [None] * self.initial_capacity
 
-    def line_stop(self) -> int:
+    def _line_stop(self) -> int:
         return int(self.initial_capacity * self.load_factor)
 
-    def get_index(self, key: Hashable) -> int:
+    def _get_index(self, key: Hashable) -> int:
         return hash(key) % self.initial_capacity
 
     @staticmethod
-    def get_hash(key: Hashable) -> int:
+    def _get_hash(key: Hashable) -> int:
         return hash(key)
 
     def __len__(self) -> int:
         return self.size
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        if self.size >= self.line_stop():
+        if self.size >= self._line_stop():
             self.resize()
 
-        hash_val = self.get_hash(key)
-        index = self.get_index(key)
+        hash_val = self._get_hash(key)
+        index = self._get_index(key)
 
-        if self.hash_table[index] is None:
-            self.hash_table[index] = Node(key, hash_val, value)
-            self.size += 1
-            return
-        while self.hash_table[index]:
-            if self.hash_table[index].key == key:
-                self.hash_table[index].value = value
-                return
-            index = (index + 1) % self.initial_capacity
+        while True:
             if self.hash_table[index] is None:
                 self.hash_table[index] = Node(key, hash_val, value)
                 self.size += 1
-                return
+                break
+
+            if self.hash_table[index].key == key:
+                self.hash_table[index].value = value
+                break
+
+            index = (index + 1) % self.initial_capacity
 
     def __getitem__(self, key: Hashable) -> Any:
 
         for node in self.hash_table:
-            if node:
-                if node.key == key:
-                    return node.value
+            if node and node.key == key:
+                return node.value
         raise KeyError(f"Key not found: {key}")
 
     def resize(self) -> None:
