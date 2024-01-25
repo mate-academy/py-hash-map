e6f4af347bab0c41ad555cb10628964409523799
Oleksandra <aleksandra@regdesk.co>
2024-01-25 21:22:02 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Hashable
 
 
 class Dictionary:
@@ -8,18 +8,19 @@ class Dictionary:
         self.hash_table = [None] * self.capacity
         self.length = 0
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         index = self.get_index(key)
 
         if not self.hash_table[index]:
+            self.length += 1
+
             if self.length >= int(self.capacity * self.load_factor):
                 self.resize()
                 index = self.get_index(key)
-            self.length += 1
 
         self.hash_table[index] = (key, hash(key), value)
 
-    def __getitem__(self, key: Any) -> Any:
+    def __getitem__(self, key: Hashable) -> Any:
         index = self.get_index(key)
 
         if not self.hash_table[index]:
@@ -32,17 +33,15 @@ class Dictionary:
 
     def resize(self) -> None:
         self.capacity *= 2
-        self.length = 0
         hash_table = self.hash_table
         self.hash_table = [None] * self.capacity
 
         for node in hash_table:
             if node:
                 key, hash_, value = node
-                self.length += 1
                 self.hash_table[self.get_index(key)] = (key, hash_, value)
 
-    def get_index(self, key: Any) -> int:
+    def get_index(self, key: Hashable) -> int:
         index = hash(key) % self.capacity
 
         while (self.hash_table[index]
