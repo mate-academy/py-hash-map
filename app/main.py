4b7fd2d75da4832859129f682935a34dfdd760c7
Yura Push <youdeimos1@gmail.com>
2024-02-04 17:47:10 +0200

    -code style changes -added check for hash in __getitem__

--- app/main.py
+++ app/main.py
@@ -1,54 +1,6 @@
 from typing import Any, Hashable
 
 
-# class Dictionary:
-#     def __init__(self) -> None:
-#         self.capacity = 8
-#         self.hashmap: list = [[] for _ in range(self.capacity)]
-#         self.length = 0
-#         self.load_factor = 0.75
-#
-#     def resize(self) -> None:
-#         self.capacity *= 2
-#         self.length = 0
-#         old_hashmap = self.hashmap
-#         self.hashmap = [[] for _ in range(self.capacity)]
-#
-#         for elements in old_hashmap:
-#             if len(elements) >= 1:
-#                 for element in elements:
-#                     self.__setitem__(element[0], element[1])
-#
-#     def __setitem__(self, key: Hashable, value: Any) -> None:
-#         hash_key = hash(key) % self.capacity
-#         element_exists = False
-#         self.length += 1
-#
-#         for index, element in enumerate(self.hashmap[hash_key]):
-#             if len(element) == 2 and element[0] == key:
-#                 self.hashmap[hash_key][index] = (key, value)
-#                 element_exists = True
-#                 self.length -= 1
-#                 break
-#
-#         if not element_exists:
-#             self.hashmap[hash_key].append((key, value))
-#
-#         if self.length / float(self.capacity) >= self.load_factor:
-#             self.resize()
-#
-#     def __getitem__(self, key: Hashable) -> Any:
-#         hash_key = hash(key) % self.capacity
-#
-#         for index, element in enumerate(self.hashmap[hash_key]):
-#             if element[0] == key:
-#                 return element[1]
-#         else:
-#             raise KeyError("Key doesn't exist.")
-#
-#     def __len__(self) -> int:
-#         return self.length
-
 class Dictionary:
     def __init__(self) -> None:
         self.capacity = 8
@@ -65,7 +17,7 @@ class Dictionary:
         for elements in old_hashmap:
             if len(elements) >= 1:
                 for element in elements:
-                    self.__setitem__(element[0], element[1])
+                    self[element[0]] = element[1]
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
         hash_key = hash(key)
@@ -82,18 +34,20 @@ class Dictionary:
                 break
 
         if not element_exists:
-            (self.hashmap[hash_key % self.capacity].
-             append((key, value, hash_key)))
+            self.hashmap[hash_key % self.capacity].append(
+                (key, value, hash_key)
+            )
 
         if self.length / float(self.capacity) >= self.load_factor:
             self.resize()
 
     def __getitem__(self, key: Hashable) -> Any:
-        hash_key = hash(key) % self.capacity
+        hash_key = hash(key)
 
-        for index, element in enumerate(self.hashmap[hash_key]):
-            if element[0] == key:
-                return element[1]
+        for index, (stored_key, stored_value, stored_hash) \
+                in enumerate(self.hashmap[hash_key % self.capacity]):
+            if stored_key == key and stored_hash == hash_key:
+                return stored_value
         else:
             raise KeyError("Key doesn't exist.")
 
