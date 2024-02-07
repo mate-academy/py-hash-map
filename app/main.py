ca9092122ab204cf4459023b4b8813e4871556dd
Nataliia Korzh <clarioruberia@gmail.com>
2024-02-07 17:33:50 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -14,18 +14,20 @@ class Dictionary:
         return self._length
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        hash_of_key = hash(key)
-        index_to_insert = self._find_available_cell(key, hash_of_key)
+        key_hash = hash(key)
+        index_insert = self._find_available_cell(key, key_hash)
+        threshold = int(self._capacity * 2 / 3)
 
-        if self._hash_table[index_to_insert] is None:
+        if self._hash_table[index_insert] is None:
             self._length += 1
-            self._resize()
+            if self._length >= threshold:
+                self._resize()
 
-        self._hash_table[index_to_insert] = (key, hash_of_key, value)
+        self._hash_table[index_insert] = (key, key_hash, value)
 
     def __getitem__(self, key: Hashable) -> Any:
-        hash_of_key = hash(key)
-        index = self._get_index_by_hash(hash_of_key)
+        key_hash = hash(key)
+        index = self._get_index_by_hash(key_hash)
 
         for _ in range(self._capacity):
             if (self._hash_table[index] is not None
@@ -37,27 +39,24 @@ class Dictionary:
         raise KeyError(f"{key} not exist!")
 
     def _resize(self) -> None:
-        threshold = int(self._capacity * 2 / 3)
-        if self._length >= threshold:
-            old_hash_table = [cell
-                              for cell in self._hash_table
-                              if cell is not None]
-            self._capacity *= 2
-            self._hash_table = [None] * self._capacity
-            for key, hash_of_key, value in old_hash_table:
-                index_to_insert = self._find_available_cell(key, hash_of_key)
-                self._hash_table[index_to_insert] = (key, hash_of_key, value)
 
-    def _find_available_cell(self, key: Hashable, hash_of_key: int) -> int:
-        available_cell_index = self._get_index_by_hash(hash_of_key)
+        old_hash_table = list(filter(None, self._hash_table))
+        self._capacity *= 2
+        self._hash_table = [None] * self._capacity
+        for key, key_hash, value in old_hash_table:
+            index_insert = self._find_available_cell(key, key_hash)
+            self._hash_table.__setitem__(index_insert, (key, key_hash, value))
+
+    def _find_available_cell(self, key: Hashable, key_hash: int) -> int:
+        available_cell_index = self._get_index_by_hash(key_hash)
 
         while self._is_cell_irrelevant_to_write_key(available_cell_index, key):
             available_cell_index = self._increment_index(available_cell_index)
 
         return available_cell_index
 
-    def _get_index_by_hash(self, hash_of_key: int) -> int:
-        return hash_of_key % self._capacity
+    def _get_index_by_hash(self, key_hash: int) -> int:
+        return key_hash % self._capacity
 
     def _increment_index(self, index: int) -> int:
         return (index + 1) % self._capacity
