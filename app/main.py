b04ee8914ae73158e9987596c0c6a99ca6288dd5
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2024-02-12 13:06:59 +0200

    Solution in developing

--- app/main.py
+++ app/main.py
@@ -1,2 +1,64 @@
+from __future__ import annotations
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.objects = []
+        self.capacity = 8
+        self.load_factor = 3 / 2
+
+        self.iter_counter = 0
+
+    def __len__(self) -> int:
+        return len(self.objects)
+
+    def clear(self) -> None:
+        self.objects = []
+
+    def __setitem__(self, key, value) -> None:
+        try:
+
+            if len(self.objects) == 0:
+                self.objects.append([key, value, hash(key)])
+            for index in range(len(self.objects)):
+                if self.objects[index][0] == key:
+                    self.objects[index] = [key, value, hash(key)]
+                    break
+                else:
+                    if index == len(self.objects) - 1:
+                        self.objects.append([key, value, hash(key)])
+        except Exception(KeyError):
+            raise KeyError
+
+    def __getitem__(self, item) -> None | str:
+        for element in self.objects:
+            if element[0] == item:
+                return element[1]
+        return None
+
+    def __delitem__(self, index) -> None:
+        self.objects.pop(index)
+
+    def get(self, index) -> None | str:
+        return self.__getitem__(index)
+
+    def pop(self, index=None) -> str:
+        if index is None:
+            value = self.objects[-1][1]
+            self.objects.pop()
+            return value
+        value = self.objects[index][1]
+        self.objects.pop(index)
+        return value
+
+    def update(self, pair) -> None:
+        key = [key for key in pair][0]
+        value = pair[key]
+
+        for index in range(len(self.objects)):
+            if self.objects[index][0] == key:
+                self.objects[index] = [key, value, hash(key)]
+                break
+            else:
+                if index == len(self.objects) - 1:
+                    self.objects.append([key, value, hash(key)])
