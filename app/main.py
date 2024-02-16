81d3cb1e6910d1ecf7e79d2d0b605312ffbd1863
Yura Push <youdeimos1@gmail.com>
2024-02-16 17:32:53 +0200

    -reworked resizing logic, to not modify self.length

--- app/main.py
+++ app/main.py
@@ -13,40 +13,38 @@ class Dictionary:
 
     def resize(self) -> None:
         self.capacity *= 2
-        self.length = 0
-        old_hashmap = self.hashmap
-        self.hashmap = [[] for _ in range(self.capacity)]
+        new_hashmap = [[] for _ in range(self.capacity)]
 
-        for elements in old_hashmap:
-            if len(elements) >= 1:
-                for element in elements:
-                    self[element[0]] = element[1]
+        for bucket in self.hashmap:
+            for key, value, hash_key in bucket:
+                new_hashmap[self.hash_key_index(key)].append(
+                    (key, value, hash_key)
+                )
+
+        self.hashmap = new_hashmap
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
         hash_key = hash(key)
+        bucket = self.hashmap[self.hash_key_index(key)]
 
         for index, (stored_key, stored_value, stored_hash) \
-                in enumerate(self.hashmap[self.hash_key_index(key)]):
+                in enumerate(bucket):
             if stored_hash == hash_key and stored_key == key:
-                self.hashmap[self.hash_key_index(key)][index] = \
-                    (key, value, hash_key)
+                bucket[index] = (key, value, hash_key)
                 self.length -= 1
                 break
-        else:
-            self.hashmap[self.hash_key_index(key)].append(
-                (key, value, hash_key)
-            )
 
+        bucket.append((key, value, hash_key))
         self.length += 1
 
         if self.length / float(self.capacity) >= self.LOAD_FACTOR:
             self.resize()
 
     def __getitem__(self, key: Hashable) -> Any:
-        for index, data_tuple \
-                in enumerate(self.hashmap[self.hash_key_index(key)]):
-            if data_tuple[0] == key:
-                return data_tuple[1]
+        bucket = self.hashmap[self.hash_key_index(key)]
+        for stored_key, stored_value, _ in bucket:
+            if stored_key == key:
+                return stored_value
         raise KeyError("Key doesn't exist.")
 
     def __len__(self) -> int:
