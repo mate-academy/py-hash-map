511a3b719a3a7c5fd71d7008bb158795976e93e9
Aeros72 <pashamelnyk2004@gmail.com>
2024-02-17 11:45:19 +0200

    Implemented py-dictionary

--- app/main.py
+++ app/main.py
@@ -1,2 +1,59 @@
+from typing import Any
+
+
+class Node:
+    def __init__(
+            self,
+            key: Any,
+            value: Any
+    ) -> None:
+        self.key = key
+        self.value = value
+        self.key_hash = hash(key)
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.load_factor = 0
+        self.capacity = 8
+        self.buckets = [None] * self.capacity
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        if self.load_factor >= self.capacity * 2 / 3:
+            self._resize(self.buckets)
+
+        index = hash(key) % self.capacity
+        while self.buckets[index] and self.buckets[index].key != key:
+            index = (index + 1) % self.capacity
+
+        if not self.buckets[index]:
+            self.load_factor += 1
+        self.buckets[index] = Node(key, value)
+
+    def __getitem__(self, input_key: Any) -> Any:
+        hash_key = hash(input_key)
+        index = hash_key % self.capacity
+
+        while self.buckets[index] is not None:
+            node = self.buckets[index]
+            if node.key == input_key:
+                return node.value
+            index = (index + 1) % self.capacity
+
+        raise KeyError()
+
+    def _resize(self, elements: list) -> None:
+        self.capacity *= 2
+        self.buckets = [None] * self.capacity
+
+        for node in elements:
+            if node is not None:
+                index = node.key_hash % self.capacity
+
+                while self.buckets[index] is not None:
+                    index = (index + 1) % self.capacity
+
+                self.buckets[index] = node
+
+    def __len__(self) -> int:
+        return self.load_factor
