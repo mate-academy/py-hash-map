61d5182c605b9166cfa8462685951752b1cdea8d
Alex Grytsai <a.grytsai1987@gmail.com>
2024-02-21 12:16:22 +0200

    Need fix bag with test resize bucket

--- app/point.py
+++ app/point.py
@@ -1,4 +1,5 @@
 from typing import Any
+from app.main import Dictionary
 
 
 class Point:
@@ -17,7 +18,7 @@ class Point:
         # which is NOT a best practice, but you will be able to predict
         # a hash value by coordinates of the point and its index
         # in the hashtable as well
-        return hash((self.x, self.y))
+        return hash((self.x + self.y))
 
     @property
     def x(self) -> float:
@@ -26,3 +27,40 @@ class Point:
     @property
     def y(self) -> float:
         return self._y
+
+
+# point = Point(2, 1)
+# point2 = Point(1, 2)
+# point3 = Point(0, 3)
+# dictionary = Dictionary()
+# dictionary["point"] = "ApointA" #1+
+# dictionary[point] = "point" #2
+# dictionary[point2] = "bbb" #3 +
+# dictionary["point5"] = 798978 #4
+# dictionary["point3"] = 5523 #5
+# dictionary["point4"] = "66666" #6
+
+# dictionary["point3"] = "AAAAAAAAA"
+# dictionary["point4"] = "FFFFFF"
+
+#
+# print(dictionary["point"])
+# print(dictionary[point])
+# print(dictionary[point2])
+# print(dictionary["point5"])
+# print(dictionary["point3"])
+# print(dictionary["point4"])
+#
+# print(dictionary)
+#
+# print(dictionary.__dict__)
+
+# def resize_bucket():
+#     items = [(f"Element {i}", i) for i in range(1000)]
+#     dictionary = Dictionary()
+#     for key, value in items:
+#         dictionary[key] = value
+#
+#     for key, value in items:
+#         assert dictionary[key] == value
+# resize_bucket()
