59b054c45af62a9bbd3305d415f818a21ee6a6d8
RomanHlodann <roman.hlodann@gmail.com>
2024-02-22 23:00:41 +0200

    improved code style

--- app/main.py
+++ app/main.py
@@ -1,60 +1,59 @@
-from typing import Any
+from typing import Any, Hashable
 
 
 class Dictionary:
     def __init__(self) -> None:
         self.length = 0
         self.hash_table: list = [None] * 8
+        self.capacity = 8
+
+    def get_index(self, key: Hashable) -> int:
+        return hash(key) % self.capacity
 
     def __setitem__(
             self,
-            key: int | float | str | tuple,
+            key: Hashable,
             value: Any
     ) -> None:
-        key_hash = hash(key)
-        if len(self) >= len(self.hash_table) * (2.0 / 3):
-            self.update()
-        index = key_hash % len(self.hash_table)
+        if self.length >= self.capacity * (2 / 3):
+            self.resize()
+        index = self.get_index(key)
         while True:
             if not self.hash_table[index]:
                 self.length += 1
             if not self.hash_table[index] or self.hash_table[index][0] == key:
-                self.hash_table[index] = (key, key_hash, value)
-                return
-            index += 1
-            if index >= len(self.hash_table):
-                index = 0
+                self.hash_table[index] = (key, hash(key), value)
+                break
+            index = (index + 1) % self.capacity
 
-    def __getitem__(self, key: int | float | str | tuple) -> Any:
+    def __getitem__(self, key: Hashable) -> Any:
         return self.hash_table[self.get(key)][2]
 
-    def get(self, key: int | float | str | tuple) -> int | KeyError:
-        key_hash = hash(key)
-        start_index = key_hash % len(self.hash_table)
+    def get(self, key: Hashable) -> int | KeyError:
+        start_index = self.get_index(key)
         index = start_index
         while True:
             if self.hash_table[index] and self.hash_table[index][0] == key:
                 return index
-            index += 1
-            if index >= len(self.hash_table):
-                index = 0
+            index = (index + 1) % self.capacity
             if index == start_index:
                 raise KeyError("Key was not found")
 
     def __len__(self) -> int:
         return self.length
 
-    def __delitem__(self, key: int | float | str | tuple) -> None:
+    def __delitem__(self, key: Hashable) -> None:
         index = self.get(key)
         self.hash_table[index] = None
         self.length -= 1
 
-    def update(self) -> None:
-        copy_hash_table = self.hash_table
-        self.hash_table = [None] * len(self) * 2
-        self.length = 0
+    def resize(self) -> None:
+        copy_hash_table = self.hash_table.copy()
+        self.hash_table += [None] * self.capacity
+        self.capacity *= 2
         for item in copy_hash_table:
             if item:
+                del self[item[0]]
                 self[item[0]] = item[2]
 
     def __iter__(self) -> tuple:
