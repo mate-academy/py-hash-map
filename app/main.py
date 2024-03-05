8f28416e08ece5eaaf27e851e53820db9bdaa0f2
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-03-05 11:31:11 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 from math import floor
-from typing import Any
-
-from app.point import Point
+from typing import Any, Iterable
 
 
 class Dictionary:
@@ -12,20 +10,20 @@ class Dictionary:
         self._length = 0
         self._hash_table: list = [None] * self._initial_capacity
 
-    def __setitem__(self, key, value) -> None:
-        def set_item(key_item: Any, value_item: Any) -> None:
-            hash_index = hash(key_item) % self._initial_capacity
-            cell_in_table = self._hash_table
-            while cell_in_table[hash_index] is not None:
-                if cell_in_table[hash_index][0] == key_item:
-                    break
-                if hash_index < self._initial_capacity - 1:
-                    hash_index += 1
-                else:
-                    hash_index = 0
+    def _find_hash_index(self, key_item: Any) -> int:
+        hash_index = hash(key_item) % self._initial_capacity
+        cell_in_table = self._hash_table
 
-            self._hash_table[hash_index] = [key_item, hash(key_item), value_item]
+        while cell_in_table[hash_index] is not None:
+            if cell_in_table[hash_index][0] == key_item:
+                break
+            if hash_index < self._initial_capacity - 1:
+                hash_index += 1
+            else:
+                hash_index = 0
+        return hash_index
 
+    def __setitem__(self, key: Any, value: Any) -> None:
         if self._resize <= self._length:
             self._initial_capacity *= 2
             self._resize = floor(self._initial_capacity * self._load_factor)
@@ -33,15 +31,21 @@ class Dictionary:
             temporary_hash_table = list(self._hash_table)
             self._hash_table = [None] * self._initial_capacity
 
-            for item in temporary_hash_table:
-                if item:
-                    set_item(item[0], item[2])
+            for cell_in_table in temporary_hash_table:
+                if cell_in_table:
+                    hash_index = self._find_hash_index(cell_in_table[0])
+                    self._hash_table[hash_index] = [
+                        cell_in_table[0],
+                        cell_in_table[1],
+                        cell_in_table[2]
+                    ]
             del temporary_hash_table
 
-        set_item(key, value)
+        hash_index = self._find_hash_index(key)
+        self._hash_table[hash_index] = [key, hash(key), value]
         self._length = self._initial_capacity - self._hash_table.count(None)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: Any) -> Any:
         hash_index = hash(item) % self._initial_capacity
 
         while self._hash_table[hash_index] is not None:
@@ -52,29 +56,57 @@ class Dictionary:
                     hash_index = 0
             else:
                 return self._hash_table[hash_index][2]
-        raise KeyError
+        raise KeyError(f"Key {item} not in the hash table") from None
+
+    def __delitem__(self, key: Any) -> None:
+        hash_index = self._find_hash_index(key)
+        self._hash_table[hash_index] = None
+        self._length -= 1
+
+    def clear(self) -> None:
+        self.__init__()
+
+    def get(self, key: Any, default: Any = None) -> Any:
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return default
+
+    def pop(self, key: Any, default: Any = None) -> list:
+        try:
+            result = self.__getitem__(key)
+        except KeyError as error:
+            if not default:
+                raise error
+            return default
+        else:
+            self.__delitem__(key)
+            return result
+
+    def update(self, iterable: Iterable) -> None:
+        if isinstance(iterable, Dictionary):
+            for item in iterable._hash_table:
+                if item is not None:
+                    self.__setitem__(item[0], item[2])
+        elif isinstance(iterable, (list, tuple)):
+            for item in iterable:
+                self.__setitem__(item[0], item[1])
+        else:
+            raise TypeError(f"Unsupported type for {iterable}. "
+                            f"Expected Dictionary, list, or tuple.")
+
+    def __iter__(self) -> Any:
+        for item in self._hash_table:
+            if item is not None:
+                yield item[0]
+
+    def __next__(self) -> Any:
+        raise StopIteration
+
+    def items(self) -> Any:
+        for item in self._hash_table:
+            if item is not None:
+                yield item[0], item[2]
 
     def __len__(self) -> int:
         return self._length
-
-# my_dict = Dictionary()
-# print(my_dict._hash_table)
-# my_dict[Point(5, 5)] = 50
-# print(my_dict._hash_table)
-# my_dict[Point(6, 5)] = 75
-# print(my_dict._hash_table)
-# my_dict[Point(4, 6)] = 46
-# print(my_dict._hash_table)
-# my_dict[Point(6, 5)] = 150
-# print(my_dict._hash_table)
-# print(my_dict[Point(4, 6)])
-# # my_dict[Point(4, 6)] = 100
-# # print(my_dict._hash_table)
-# # my_dict[Point(4, 6)] = 123
-# # print(my_dict._hash_table)
-# # my_dict[Point(15, 5)] = 5
-# # print(my_dict._hash_table)
-# print(len(my_dict))
-# print(my_dict._initial_capacity)
-# print(my_dict._resize)
-
