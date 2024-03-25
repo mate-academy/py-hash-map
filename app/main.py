ad279c7662777bb1470fc9a6e0fd09652581feac
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-03-25 12:15:04 +0200

    Solution5

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Hashable
+from typing import Hashable, Any
 
 
 class Dictionary:
@@ -7,7 +7,7 @@ class Dictionary:
         self.load_factor = 2 / 3
         self.table = [None] * self.capacity
 
-    def insert_into_hash_table(self, key: Hashable, value: any) -> None:
+    def insert_into_hash_table(self, key: Hashable, value: Any) -> None:
         hash_value = hash(key) % self.capacity
         while (
                 self.table[hash_value] is not None
@@ -16,12 +16,12 @@ class Dictionary:
             hash_value = (hash_value + 1) % self.capacity
         self.table[hash_value] = (key, hash_value, value)
 
-    def __setitem__(self, key: Hashable, value: any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         self.insert_into_hash_table(key, value)
         if len(self) >= self.load_factor * self.capacity:
             self._resize()
 
-    def __getitem__(self, key: Hashable) -> any:
+    def __getitem__(self, key: Hashable) -> Any:
         hash_value = hash(key) % self.capacity
         while self.table[hash_value] is not None:
             if self.table[hash_value][0] == key:
@@ -31,7 +31,11 @@ class Dictionary:
         raise KeyError(f"Key '{key}' not found in the dictionary")
 
     def __len__(self) -> int:
-        return sum(1 for entry in self.table if entry is not None)
+        numbers_of_elements_in_dict = 0
+        for element in self.table:
+            if element:
+                numbers_of_elements_in_dict += 1
+        return numbers_of_elements_in_dict
 
     def _resize(self) -> None:
         new_capacity = self.capacity * 2
