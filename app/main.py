84b5df0aeff2d5f8a845580c2931a8f975bcfd93
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-03-25 15:54:56 +0200

    Solution6

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ class Dictionary:
         self.capacity = 8
         self.load_factor = 2 / 3
         self.table = [None] * self.capacity
+        self.length = 0
 
     def insert_into_hash_table(self, key: Hashable, value: Any) -> None:
         hash_value = hash(key) % self.capacity
@@ -14,28 +15,31 @@ class Dictionary:
                 and self.table[hash_value][0] != key
         ):
             hash_value = (hash_value + 1) % self.capacity
-        self.table[hash_value] = (key, hash_value, value)
+        if (
+                self.table[hash_value] is not None
+                and self.table[hash_value][0] == key
+        ):
+            self.table[hash_value] = (key, hash_value, value)
+        else:
+            self.table[hash_value] = (key, hash_value, value)
+            self.length += 1
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
         self.insert_into_hash_table(key, value)
-        if len(self) >= self.load_factor * self.capacity:
+        if self.length >= self.load_factor * self.capacity:
             self._resize()
 
     def __getitem__(self, key: Hashable) -> Any:
         hash_value = hash(key) % self.capacity
-        while self.table[hash_value] is not None:
+        for _ in range(self.capacity):
+            if self.table[hash_value] is None:
+                raise KeyError(f"Ключ '{key}' не знайдено в словнику")
             if self.table[hash_value][0] == key:
                 return self.table[hash_value][2]
             hash_value = (hash_value + 1) % self.capacity
 
-        raise KeyError(f"Key '{key}' not found in the dictionary")
-
     def __len__(self) -> int:
-        numbers_of_elements_in_dict = 0
-        for element in self.table:
-            if element:
-                numbers_of_elements_in_dict += 1
-        return numbers_of_elements_in_dict
+        return self.length
 
     def _resize(self) -> None:
         new_capacity = self.capacity * 2
@@ -47,7 +51,6 @@ class Dictionary:
                 new_hash_value = hash(key) % new_capacity
                 while new_table[new_hash_value] is not None:
                     new_hash_value = (new_hash_value + 1) % new_capacity
-
                 new_table[new_hash_value] = (key, new_hash_value, value)
 
         self.table = new_table
