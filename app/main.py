90b3cc18130d8456a55b0fbceaed8065e887017b
Denys Kibalenko <dennieking87@gmail.com>
2024-04-06 12:13:08 +0200

    Solution fix DRY code

--- app/main.py
+++ app/main.py
@@ -19,7 +19,11 @@ class Dictionary:
 
     def _get_node_index(self, key: Hashable, capacity: int) -> int:
         """ Computes node index based on the key hash """
-        return hash(key) % capacity
+        node_index = hash(key) % capacity
+        while (self.hash_table[node_index]
+               and self.hash_table[node_index][0] != key):
+            node_index = (node_index + 1) % self.capacity
+        return node_index
 
     def _resize(self) -> None:
         new_capacity = self.capacity * 2
@@ -27,7 +31,7 @@ class Dictionary:
         for node in self.hash_table:
             if node:
                 key, *_ = node
-                new_node_index = self._get_node_index(key, new_capacity)
+                new_node_index = hash(key) % new_capacity
                 while new_hash_table[new_node_index]:
                     new_node_index = (new_node_index + 1) % new_capacity
                 new_hash_table[new_node_index] = (key, *_)
@@ -37,33 +41,29 @@ class Dictionary:
     def __setitem__(self, key: Hashable, value: Any) -> None:
         """ Stores a new key-value pair or updates existing. """
         node_index = self._get_node_index(key, self.capacity)
-        while self.hash_table[node_index]:
-            if self.hash_table[node_index][0] == key:
-                self.hash_table[node_index] = (key, hash(key), value)
-                return
-            node_index = (node_index + 1) % self.capacity
-        self.hash_table[node_index] = (key, hash(key), value)
-        self.size += 1
+        node = (key, hash(key), value)
+        if (self.hash_table[node_index]
+                and self.hash_table[node_index][0] == key):
+            self.hash_table[node_index] = node
+        else:
+            self.hash_table[node_index] = node
+            self.size += 1
         if self.size / self.capacity >= self.load_factor:
             self._resize()
 
     def __getitem__(self, key: Hashable) -> str | int:
         """ Looks up a value with the specified key """
         node_index = self._get_node_index(key, self.capacity)
-        while self.hash_table[node_index]:
-            if self.hash_table[node_index][0] == key:
-                return self.hash_table[node_index][2]
-            node_index = (node_index + 1) % self.capacity
+        if self.hash_table[node_index]:
+            return self.hash_table[node_index][2]
         raise KeyError(f"Key {key} doesn't exist.")
 
     def __delitem__(self, key: Hashable) -> None:
         """ Deletes item with the specified key """
         node_index = self._get_node_index(key, self.capacity)
-        while self.hash_table[node_index]:
-            if self.hash_table[node_index][0] == key:
-                self.hash_table[node_index] = None
-                self.size -= 1
-            node_index = (node_index + 1) % self.capacity
+        if self.hash_table[node_index]:
+            self.hash_table[node_index] = None
+            self.size -= 1
 
     def __len__(self) -> int:
         return self.size
@@ -71,7 +71,7 @@ class Dictionary:
     def get(self, key: Hashable, default: Any = None) -> Any:
         """ Returns a value of the specified key, otherwise default value """
         try:
-            return self[key]
+            return self.__getitem__(key)
         except KeyError:
             return default
 
