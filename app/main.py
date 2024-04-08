01e38c1859c0651034f5acd221e1c6e05a6c120e
kekW <panicua12@gmail.com>
2024-04-08 20:43:29 +0300

    added clear/get/__delitem__ methods

--- app/main.py
+++ app/main.py
@@ -2,73 +2,95 @@ from typing import Any, Hashable
 
 
 class Dictionary:
-    def __init__(self,
-                 hash_size: int = 8,
-                 resize_factor: float = 2 / 3,
-                 resize_multiplier: int = 2) -> None:
-        self.length = 0
-        self.hash_table: list = [None] * 8
-        self.hash_size = hash_size
-        self.resize_factor = resize_factor
-        self.resize_multiplier = resize_multiplier
+    def __init__(self) -> None:
+        self._length: int = 0
+        self._hash_table: list = [None] * 8
+        self._hash_size: int = 8
+        self._resize_factor: float | int = 2 / 3
+        self._resize_multiplier: int = 2
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
         self._hash_size_check_increase()
+        key_hash_position = hash(key) % self._hash_size
 
-        key_hash_position = hash(key) % self.hash_size
-
-        while self.hash_table[key_hash_position]:
-            if self.hash_table[key_hash_position][0] == key:
-                self.hash_table[key_hash_position][1] = value
+        while self._hash_table[key_hash_position]:
+            if self._hash_table[key_hash_position][0] == key:
+                self._hash_table[key_hash_position][1] = value
                 return print(f"value with key '{key}' were rewritten")
-            key_hash_position = (key_hash_position + 1) % self.hash_size
+            key_hash_position = (key_hash_position + 1) % self._hash_size
 
+        self._hash_table[key_hash_position] = [key, value]
+        self._length += 1
         print(f"'key:value = {key}:{value}' "
               f"is set to {key_hash_position} index in the Dictionary")
-        self.hash_table[key_hash_position] = [key, value]
-        self.length += 1
 
     def __getitem__(self, item: Hashable) -> Any | None:
-        key_hash_position = hash(item) % self.hash_size
-
-        if not self.hash_table[key_hash_position]:
-            raise KeyError("No such item in the Dictionary")
+        key_hash_position = hash(item) % self._hash_size
 
         # find item by key_hash_position and if it has the right key to it
-        while self.hash_table[key_hash_position]:
-            if self.hash_table[key_hash_position][0] == item:
-                return self.hash_table[key_hash_position][1]
-            key_hash_position = (key_hash_position + 1) % self.hash_size
+        while self._hash_table[key_hash_position]:
+            if self._hash_table[key_hash_position][0] == item:
+                return self._hash_table[key_hash_position][1]
+            key_hash_position = (key_hash_position + 1) % self._hash_size
 
-        raise KeyError("Key not found in the Dictionary")
+        raise KeyError("Key is not found in the Dictionary")
 
     def __len__(self) -> int:
-        return self.length
+        return self._length
 
-    # am I allowed to use over O(1) while creating a __repr__?
     def __repr__(self) -> str:
         result = [f"{item[0]}:{item[1]}"
-                  for item in self.hash_table
+                  for item in self._hash_table
                   if item is not None]
         return "{" + ", \n".join(result) + "}"
 
+    def __delitem__(self, key: Hashable) -> None:
+        key_hash_position = hash(key) % self._hash_size
+
+        while self._hash_table[key_hash_position]:
+            if self._hash_table[key_hash_position][0] == key:
+                self._hash_table[key_hash_position] = None
+                print(f"removed: {key} at {key_hash_position} position")
+                return
+            key_hash_position = (key_hash_position + 1) % self._hash_size
+
+        raise KeyError("Key is not found in the Dictionary")
+
+    def get(self, key: Hashable) -> Any | None:
+        """
+        Works same as __getitem__ but
+        doesn't return KeyError if key doesn't exist
+        """
+        try:
+            self.__getitem__(key)
+        except KeyError:
+            return None
+        else:
+            return self.__getitem__(key)
+
+    def clear(self) -> None:
+        self._hash_table = [None] * 8
+        self._hash_size = 8
+        self._length = 0
+
     def _hash_size_check_increase(self) -> None:
-        if self.__len__() == int(self.hash_size * self.resize_factor):
-            self.hash_size *= self.resize_multiplier
-            temp_hash_table = [None] * self.hash_size
+        """Supporting func to check and increase hash_table size when needed"""
+
+        if self.__len__() == int(self._hash_size * self._resize_factor):
+            self._hash_size *= self._resize_multiplier
+            temp_hash_table = [None] * self._hash_size
         else:
             return
 
-        for item in self.hash_table:
+        for item in self._hash_table:
             if item:
-                item_key_hash_position = hash(item[0]) % self.hash_size
+                item_key_hash_position = hash(item[0]) % self._hash_size
                 # check so they don't overwrite each-other
                 while temp_hash_table[item_key_hash_position]:
-                    item_key_hash_position = (
-                            (item_key_hash_position + 1) % self.hash_size
-                    )
+                    item_key_hash_position = ((item_key_hash_position + 1)
+                                              % self._hash_size)
                 temp_hash_table[item_key_hash_position] = item
-        self.hash_table = temp_hash_table
+        self._hash_table = temp_hash_table
         print(f"Dictionary size were increased "
-              f"from {self.hash_size // self.resize_multiplier} "
-              f"to {self.hash_size}")
+              f"from {self._hash_size // self._resize_multiplier} "
+              f"to {self._hash_size}")
