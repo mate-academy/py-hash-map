ccea28e0cf2c1fbece1366e9539037babe8bdcf3
bujhm <Bujhm99@gmail.com>
2024-04-12 06:01:56 +0300

    Add del method

--- app/main.py
+++ app/main.py
@@ -11,31 +11,31 @@ class Dictionary:
         self.load_factor = load_factor
         self.resize = resize
 
-    def __setitem__(self, key: Any, value: Any) -> None:
-        hash_value = hash(key)
-        if self.count / self.capacity > self.load_factor:
-            self.resizing()
-
-        index = hash_value % self.capacity
-        while self.items[index] and self.items[index][0] != key:
-            index += 1
-            index = 0 if index > self.capacity - 1 else index
-
-        if not self.items[index]:
-            self.count += 1
-        self.items[index] = (key, hash_value, value)
-
-    def __getitem__(self, key: Any) -> Any:
+    def _get_index(self, key: Any) -> int:
         hash_value = hash(key)
         index = hash_value % self.capacity
-
         while self.items[index]:
             if self.items[index][0] == key:
-                return self.items[index][2]
+                return index
             index = index + 1
             index = 0 if index > self.capacity - 1 else index
 
-        raise KeyError(f"Key '{key}' not found")
+        return index
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        if self.count / self.capacity > self.load_factor:
+            self.resizing()
+        index = self._get_index(key)
+
+        if not self.items[index]:
+            self.count += 1
+        self.items[index] = (key, hash(key), value)
+
+    def __getitem__(self, key: Any) -> Any:
+        index = self._get_index(key)
+        if not self.items[index]:
+            raise KeyError(f"Key '{key}' not found")
+        return self.items[index][2]
 
     def __len__(self) -> int:
         return self.count
@@ -48,6 +48,13 @@ class Dictionary:
         for item in temp:
             if item:
                 self.__setitem__(item[0], item[2])
+
+    def __delitem__(self, key: Any) -> None:
+        index = self._get_index(key)
+        if not self.items[index]:
+            raise KeyError(f"Key '{key}' not found")
+        self.items[index] = None
+        self.count -= 1
 
     def __repr__(self) -> str:
         return str({item[0]: item[2] for item in self.items if item})
