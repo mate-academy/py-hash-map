03c85ccc6032e3541ee0a4c6846a063d2bb8fbe4
Misha Goryn <mgoryn68@gmail.com>
2024-04-20 19:28:52 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,56 @@
+class Node:
+    def __init__(self, key, value) -> None:
+        self.key = key
+        self.value = value
+        self.next = None
+
+
 class Dictionary:
-    pass
+    def __init__(self, capacity=10, load_factor=0.7) -> None:
+        self.capacity = capacity
+        self.load_factor = load_factor
+        self.size = 0
+        self.table = [None] * capacity
+
+    def _hash(self, key) -> int:
+        return hash(key) % self.capacity
+
+    def __setitem__(self, key, value) -> None:
+        index = self._hash(key)
+        node = self.table[index]
+        while node is not None:
+            if node.key == key:
+                node.value = value
+                return
+            node = node.next
+        new_node = Node(key, value)
+        new_node.next = self.table[index]
+        self.table[index] = new_node
+        self.size += 1
+        if self.size > self.capacity * self.load_factor:
+            self._resize()
+
+    def __getitem__(self, key) -> None:
+        index = self._hash(key)
+        node = self.table[index]
+        while node is not None:
+            if node.key == key:
+                return node.value
+            node = node.next
+        raise KeyError(key)
+
+    def __len__(self) -> int:
+        return self.size
+
+    def _resize(self) -> None:
+        new_capacity = self.capacity * 2
+        new_table = [None] * new_capacity
+        for node in self.table:
+            while node:
+                index = hash(node.key) % new_capacity
+                new_node = Node(node.key, node.value)
+                new_node.next = new_table[index]
+                new_table[index] = new_node
+                node = node.next
+        self.table = new_table
+        self.capacity = new_capacity
