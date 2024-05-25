addf8c94afe555b670e1f22f4f20655f5a80cf55
daikj <tyutyunniksolomia@gmail.com>
2024-05-25 22:52:27 +0300

    implement py-dictionary

--- app/main.py
+++ app/main.py
@@ -1,2 +1,51 @@
+from typing import Hashable, Any
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.load_factor = 2 / 3
+        self. capacity = 8
+        self.storage = [None] * 8
+        self.length = 0
+
+    def _resize(self) -> None:
+        old_storage = self.storage
+        self.capacity *= 2
+        self.storage = [None] * self.capacity
+        self.length = 0
+
+        for bucket in old_storage:
+            if bucket:
+                for pair_k_v in bucket:
+                    self.__setitem__(pair_k_v[0], pair_k_v[1])
+
+    def _hash(self, key: Hashable) -> Any:
+        return hash(key) % self.capacity
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        if self.length + 1 > self.capacity * self.load_factor:
+            self._resize()
+
+        index = self._hash(key)
+        if self.storage[index] is None:
+            self.storage[index] = []
+
+        for i, pair_k_v in enumerate(self.storage[index]):
+            if pair_k_v[0] == key:
+                self.storage[index][i] = (key, value)
+                return
+        self.storage[index].append((key, value))
+        self.length += 1
+
+    def __getitem__(self, key: Hashable) -> None:
+        index = self._hash(key)
+
+        if self.storage[index] is not None:
+            for pair in self.storage[index]:
+                if pair[0] == key:
+                    return pair[1]
+            raise KeyError
+        raise KeyError
+
+    def __len__(self) -> int:
+        return self.length
