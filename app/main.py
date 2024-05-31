4fd14708ba476e23d7bb758328696091a3ce2228
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-05-31 21:15:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,79 @@
+from typing import Any, Hashable, Iterable
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.length = 0
+        self.hash_table: list = [None] * 8
+        self.__capacity = len(self.hash_table)
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        index = hash(key) % self.__capacity
+        while self.hash_table[index] and self.hash_table[index][0] != key:
+            index = (index + 1) % self.__capacity
+        if not self.hash_table[index]:
+            self.hash_table[index] = (key, hash(key), value)
+            self.length += 1
+        elif self.hash_table[index][0] == key:
+            self.hash_table[index] = (key, hash(key), value)
+        if self.length >= self.__capacity * 2 / 3:
+            self.__resize()
+
+    def __getitem__(self, key: Hashable) -> Any:
+        index = hash(key) % self.__capacity
+        while self.hash_table[index]:
+            if self.hash_table[index][0] == key:
+                return self.hash_table[index][2]
+            index = (index + 1) % self.__capacity
+        raise KeyError(f"Key {key} not in hash table")
+
+    def __len__(self) -> int:
+        return self.length
+
+    def clear(self) -> None:
+        self.hash_table = [None] * self.__capacity
+        self.length = 0
+
+    def __resize(self) -> None:
+        self.__capacity *= 2
+        new_hash_table = [None] * self.__capacity
+        for item in self.hash_table:
+            if item:
+                key, _hash, value = item
+                index = hash(key) % self.__capacity
+                while new_hash_table[index]:
+                    index = (index + 1) % self.__capacity
+                new_hash_table[index] = (key, hash(key), value)
+        self.hash_table = new_hash_table
+
+    def get(self, key: Hashable, default: Any = None) -> Any:
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def update(self, other: dict) -> None:
+        for key, value in other.items():
+            self[key] = value
+
+    def __iter__(self) -> Iterable:
+        for item in self.hash_table:
+            if item:
+                for key, _ in item:
+                    yield key
+
+    def __delitem__(self, key: Hashable) -> None:
+        for index, item in enumerate(self.hash_table):
+            if item and item[0] == key:
+                self.hash_table[index] = None
+                self.length -= 1
+                return
+        raise KeyError(f"Key {key} not in hash table")
+
+    def pop(self, key: Hashable, default: Any = None) -> Any:
+        try:
+            pop_value = self[key]
+            del self[key]
+            return pop_value
+        except KeyError:
+            return default
