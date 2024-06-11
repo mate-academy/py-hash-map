fe13a3bf60d9181568dcc711ad527d772e771d41
unlie9 <unlie.devops9@gmail.com>
2024-06-11 20:16:47 +0300

    solve node as iterator not complete 6 failed, 8 passed

--- app/main.py
+++ app/main.py
@@ -1,49 +1,96 @@
-from dataclasses import dataclass
 from typing import Hashable, Any
 from app.point import Point
+from dataclasses import dataclass
 
 
 @dataclass
 class Node:
-    key: Hashable
-    hash_: int
-    value: Any
+    key: int
+    hash: int
+    value: int
+    next: "Node" = None
+
+    def __iter__(self):
+        self.current = self
+        return self
+
+    def __next__(self):
+        if self.current is None:
+            raise StopIteration
+        else:
+            result = self.current
+            self.current = self.current.next
+            return result
 
 
 class Dictionary:
-    CAPACITY = 8
-    LOAD_FACTOR = 0.75
-
-    def __init__(self, capacity: int = CAPACITY):
+    def __init__(self):
+        self.size = 8
         self.length = 0
-        self.capacity = capacity
-        self.resize = int(self.capacity * self.LOAD_FACTOR)
-        self.hash_table = [] * self.capacity
+        self.load_factor = 0.66
+        self.hash_table = [None] * self.size
+        self.resize = int(self.size * self.load_factor)
 
-    def _get_index(self, key: Hashable):
-        hash_ = hash(key)
-        return hash_ % len(self.hash_table)
+    def _resize(self):
+        new_capacity = self.size * 2
+        new_hash_table = [None] * new_capacity
 
-    def __setitem__(self, key, value):
-        index = self._get_index(key)
-        self.hash_table[index] = value
+        for node in self.hash_table:
+            while node is not None:
+                new_index = self._get_index(node.key, new_capacity)
+                if new_hash_table[new_index] is None:
+                    new_hash_table[new_index] = node
+                else:
+                    cur_new_index = new_hash_table[new_index]
+                    while cur_new_index.next is not None:
+                        cur_new_index = cur_new_index.next
+                    cur_new_index.next = node
+
+                if node is not None and node.next is not None:
+                    node = node.next
+                else:
+                    break
+
+        self.hash_table = new_hash_table
+        self.size = new_capacity
+        self.resize = int(self.size * self.load_factor)
+
+    def _get_index(self, key: Hashable) -> int:
+        hash_key = hash(key)
+        return hash_key % self.size
 
     def __getitem__(self, key):
         index = self._get_index(key)
         node = self.hash_table[index]
-        return node
+
+        while node is not None:
+            if node.key == key:
+                return node.value
+            node = node.next
+        raise KeyError("This is not found, please, try again")
+
+    def __setitem__(self, key, value):
+        index = self._get_index(key)
+        node = Node(key, hash(key), value)
+
+        if self.hash_table[index] is None:
+            self.hash_table[index] = node
+            self.length += 1
+        else:
+            cur_index = self.hash_table[index]
+            while cur_index is not None:
+                if cur_index.key == key:
+                    cur_index.value = value
+                    return
+                if cur_index.key == key:
+                    break
+                cur_index = cur_index.next
+
+            cur_index.next = node
+            self.length += 1
+
+        if self.length >= self.resize:
+            self.resize()
 
     def __len__(self):
-        if not self.hash_table:
-            return 0
         return self.length
-
-    def clear(self):
-        self.hash_table.clear()
-        self.length = 0
-
-
-#   point = Point(3, 3)
-#   print(point.x == point.y)
-
-dict
