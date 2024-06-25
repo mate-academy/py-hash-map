a2d293a1ae6ce24bf7ac276b679faa6c2d1569fd
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-25 22:25:33 +0300

    Solution 25-06-2024 22-25

--- app/main.py
+++ app/main.py
@@ -8,24 +8,28 @@ class Dictionary:
         self.dict_data = [False for _ in range(8)]
         self.dict_len = 0
         self.free_paces_in_dict_data = [i for i in range(self.dict_capacity)]
+        self.need_to_extend = False
 
     def __setitem__(self, key, value):
         key_hash = hash(key)
         index_to_write = key_hash % self.dict_capacity
-        tepm_index = self.free_paces_in_dict_data[0]
 
         if not self.dict_data[index_to_write]:
             self.dict_data[index_to_write] = (key_hash, key, value)
+            self.free_paces_in_dict_data.remove(self.free_paces_in_dict_data[0])
+            self.dict_len += 1
 
-        else:
+        elif self.dict_data[index_to_write]:
             item_hash, item_key, item_value = self.dict_data[index_to_write]
 
-            if item_hash == hash(key) or item_key == key:
-                self.dict_data[index_to_write] = value
+            if item_hash == hash(key) and item_key == key:
+                self.dict_data[index_to_write] = (item_hash, item_key, value)
             else:
-                self.dict_data[tepm_index] = (key_hash, key, value)
-        self.free_paces_in_dict_data.remove(tepm_index)
-        self.dict_len += 1
+                temp_index = self.free_paces_in_dict_data[0]
+                self.dict_data[temp_index] = (key_hash, key, value)
+                self.free_paces_in_dict_data.remove(temp_index)
+                self.dict_len += 1
+        self.check_load_factor()
 
     def __getitem__(self, key):
         index = hash(key) % self.dict_capacity
@@ -42,52 +46,72 @@ class Dictionary:
         return self.dict_len
 
     def check_load_factor(self):
-        if self.dict_len >= self.load_factor:
+        if self.dict_len == self.load_factor:
+            self.need_to_extend = True
             self.expand_dict_data()
 
     def expand_dict_data(self):
-        expanded_capacity = self.dict_capacity * 2
-        expanded_dict_data = [False for _ in range(expanded_capacity)]
-        self.dict_len = 0
-        free_paces_in_dict_data = [i for i in range(expanded_capacity)]
-        for i, data_item in enumerate(self.dict_data):
-            if data_item:
-                key, value = data_item
-                key_hash = hash(key)
-                data_to_add = (key, value)
-                index = key_hash % expanded_capacity
-                if not expanded_dict_data[index]:
-                    expanded_dict_data[index] = data_to_add
-                    free_paces_in_dict_data.remove(index)
-                else:
-                    second_type_index = free_paces_in_dict_data[0]
-                    expanded_dict_data[second_type_index] = data_to_add
-                    free_paces_in_dict_data.remove(second_type_index)
-                self.dict_len += 1
-        self.dict_capacity = expanded_capacity
-        self.dict_data = expanded_dict_data
+        self.dict_capacity *= 2
         self.load_factor = int(self.dict_capacity * (2 / 3))
+        self.free_paces_in_dict_data = [_ for _ in range(self.dict_capacity)]
+        self.dict_len = 0
+        only_true_dict_data = [item for item in self.dict_data if item]
+        self.dict_data = [False for _ in range(self.dict_capacity)]
+
+        for item in only_true_dict_data:
+            item_hash, item_key, item_value = item
+            index_to_write = item_hash % self.dict_capacity
+            if not self.dict_data[index_to_write]:
+                self.dict_data[index_to_write] = (item_hash, item_key, item_value)
+            else:
+                temp_index = self.free_paces_in_dict_data[0]
+                self.dict_data[temp_index] = (item_hash, item_key, item_value)
+                self.free_paces_in_dict_data.remove(temp_index)
+            self.dict_len += 1
+            self.need_to_extend = False
 
 
 if __name__ == "__main__":
     items = [
-        (Point(0, 0), "origin"),
-        (Point(10, 10), "A"),
-        (Point(-10, 10), "B"),
-        (Point(0, 5), "C")
+        (8, "8"),
+        (16, "16"),
+        (32, "32"),
+        (64, "64"),
+        (128, "128"),
+        ("one", 2),
+        ("two", 2),
+        (Point(1, 1), "a"),
+        ("one", 1),
+        # ("one", 11),
+        # ("one", 111),
+        # ("one", 1111),
+        # (145, 146),
+        # (145, 145),
+        # (145, -1),
+        # ("two", 22),
+        # ("two", 222),
+        # ("two", 2222),
+        # ("two", 22222),
+        # (Point(1, 1), "A"),
     ]
-    pairs_after_adding = [
-        (Point(0, 0), "origin"),
-        (Point(10, 10), "A"),
-        (Point(-10, 10), "B"),
-        (Point(0, 5), "C")
-    ]
+    # pairs_after_adding = [
+    #     (8, "8"),
+    #     (16, "16"),
+    #     (32, "32"),
+    #     (64, "64"),
+    #     (128, "128"),
+    #     ("one", 1111),
+    #     ("two", 22222),
+    #     (145, -1),
+    #     (Point(1, 1), "A"),
+    # ]
 
     dictionary = Dictionary()
 
     for key, value in items:
         dictionary[key] = value
     print(dictionary.dict_data)
-    for key, value in pairs_after_adding:
-        assert dictionary[key] == value
+    print(len(dictionary), dictionary.dict_capacity, dictionary.load_factor)
+    # for key, value in pairs_after_adding:
+    #     assert dictionary[key] == value
     # assert len(dictionary) == len(pairs_after_adding)
