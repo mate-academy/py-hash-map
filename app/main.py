b7355e4e6a07ac3a6e39f1e6c91072c7d3268979
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-26 13:53:57 +0300

    Solution 26-06-2024 13-53

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from app.point import Point
+from typing import Any
 
 
 class Dictionary:
-    def __init__(self):
+    def __init__(self) -> None:
         self.dict_capacity = 8
         self.load_factor = int(self.dict_capacity * (2 / 3))
         self.dict_data = [False for _ in range(8)]
@@ -10,7 +10,7 @@ class Dictionary:
         self.free_paces_in_dict_data = [i for i in range(self.dict_capacity)]
         self.need_to_extend = False
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: (str, int, object, tuple), value: Any) -> None:
         key_hash = hash(key)
         index_to_write = key_hash % self.dict_capacity
 
@@ -31,7 +31,9 @@ class Dictionary:
                     if item:
                         sec_item_hash, sec_item_key, sec_item_value = item
                         if sec_item_hash == key_hash and sec_item_key == key:
-                            self.dict_data[i] = (sec_item_hash, sec_item_key, value)
+                            self.dict_data[i] = (sec_item_hash,
+                                                 sec_item_key,
+                                                 value)
                             need_to_continue = False
                             break
 
@@ -43,27 +45,31 @@ class Dictionary:
 
         self.check_load_factor()
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: (str, int, object, tuple)) -> Any:
         index = hash(key) % self.dict_capacity
+
+        if not self.dict_data[index]:
+            raise KeyError
+
         data_hash, data_key, data_value = self.dict_data[index]
 
         if key == data_key:
             return data_value
 
-        for data_item in [item for item in self.dict_data if item]:
+        for i, data_item in enumerate(item for item in self.dict_data if item):
             data_item_hash, data_item_key, data_item_value = data_item
             if data_item_key == key:
                 return data_item_value
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.dict_len
 
-    def check_load_factor(self):
+    def check_load_factor(self) -> None:
         if self.dict_len == self.load_factor:
             self.need_to_extend = True
             self.expand_dict_data()
 
-    def expand_dict_data(self):
+    def expand_dict_data(self) -> None:
         self.dict_capacity *= 2
         self.load_factor = int(self.dict_capacity * (2 / 3))
         self.free_paces_in_dict_data = [_ for _ in range(self.dict_capacity)]
@@ -75,50 +81,3 @@ class Dictionary:
             item_hash, item_key, item_value = item
             self.__setitem__(item_key, item_value)
             self.need_to_extend = False
-
-
-if __name__ == "__main__":
-    items = [
-        (8, "8"),
-        (16, "16"),
-        (32, "32"),
-        (64, "64"),
-        (128, "128"),
-        ("one", 2),
-        ("two", 2),
-        (Point(1, 1), "a"),
-        ("one", 1),
-        ("one", 11),
-        ("one", 111),
-        ("one", 1111),
-        (145, 146),
-        (145, 145),
-        (145, -1),
-        ("two", 22),
-        ("two", 222),
-        ("two", 2222),
-        ("two", 22222),
-        (Point(1, 1), "A"),
-    ]
-    pairs_after_adding = [
-        (8, "8"),
-        (16, "16"),
-        (32, "32"),
-        (64, "64"),
-        (128, "128"),
-        ("one", 1111),
-        ("two", 22222),
-        (145, -1),
-        (Point(1, 1), "A"),
-    ]
-
-    dictionary = Dictionary()
-
-    for key, value in items:
-        dictionary[key] = value
-
-    # print(len(dictionary), dictionary.dict_capacity, dictionary.load_factor)
-    print(dictionary.dict_data)
-    for key, value in pairs_after_adding:
-        assert dictionary[key] == value,(dictionary[key], value)
-    assert len(dictionary) == len(pairs_after_adding)
