1405fd5a11ae773a05836fb8ca00ce792dd45645
Andrew Chugreev <andrey.4.1985@gmail.com>
2024-08-02 12:16:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Hashable
 
 
 class Dictionary:
@@ -8,6 +8,8 @@ class Dictionary:
         self.table = [None, None, None] * self.capacity_table
 
     def __setitem__(self, key: Any, value: Any) -> None:
+        if not isinstance(key, Hashable):
+            raise TypeError("Key not hashable")
         key_hash = hash(key)
         index_table_cell = key_hash % self.capacity_table
         while True:
@@ -23,7 +25,7 @@ class Dictionary:
                 break
             index_table_cell += 1
 
-        if self.size_dict >= self.capacity_table * (2 / 3):
+        if self.size_dict > self.capacity_table * (2 / 3):
             self.resize_dict(self.table)
 
     def resize_dict(self, table_for_resize: list) -> None:
@@ -31,18 +33,17 @@ class Dictionary:
         self.size_dict = 0
         self.table = [None] * self.capacity_table
         for item in table_for_resize:
-            self.__setitem__(item[0], item[1])
+            if item:
+                self.__setitem__(item[0], item[1])
 
     def __getitem__(self, key: Any) -> Any:
         key_hash = hash(key)
         index_table_cell = key_hash % self.capacity_table
-        while True:
-            if (
-                    self.table[index_table_cell][0] == key
-                    and self.table[index_table_cell][2] == key_hash
-            ):
+        while self.table[index_table_cell]:
+            if self.table[index_table_cell][0] == key:
                 return self.table[index_table_cell][1]
             index_table_cell += 1
+        raise KeyError
 
     def __len__(self) -> int:
         return self.size_dict
