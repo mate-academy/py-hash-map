74d849a6ed5a784dd78049a8015847f3a54057b4
Danylo Skepskyi <kol230305@gmail.com>
2024-08-04 01:19:46 +0200

    Rework

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+from __future__ import annotations
 from typing import Any, Hashable
 
 
 class Dictionary:
-    def __init__(self, threshold: float = 2 / 3, **kwargs) -> None:
+    def __init__(self, threshold: float = 2 / 3) -> None:
         self.threshold = threshold
         self._table_size = 8
         self._size = 0
@@ -27,6 +28,16 @@ class Dictionary:
         self._size += 1
 
     def __getitem__(self, item: Hashable) -> Any:
+        return self._get_item(item)[1]
+
+    def __delitem__(self, key: Hashable) -> Any:
+        index, key = self._get_item(key)
+        self.hash_table[index] = []
+
+    def __len__(self) -> int:
+        return self._size
+
+    def _get_item(self, item: Hashable) -> tuple:
         item_hash = hash(item)
         index = item_hash % len(self.hash_table)
         if self.hash_table[index]:
@@ -34,11 +45,8 @@ class Dictionary:
                 if index == len(self.hash_table) - 1:
                     index = 0
                 index += 1
-            return self.hash_table[index][2]
+            return index, self.hash_table[index][2]
         raise KeyError(f"No such key '{item}' in a dictionary")
-
-    def __len__(self) -> int:
-        return self._size
 
     def _resize(self) -> None:
         new_table = [[] for _ in range(len(self.hash_table) * 2)]
@@ -63,3 +71,14 @@ class Dictionary:
             return self.__getitem__(item)
         except (KeyError, IndexError):
             return default_value
+
+    def pop(self, item: Hashable) -> None:
+        index, value = self._get_item(item)
+        self.hash_table[index] = []
+        return value
+
+    def update(self, other: Dictionary) -> None:
+        for element in other.hash_table:
+            if element:
+                key_hash, key, value = element
+                self[key] = value
