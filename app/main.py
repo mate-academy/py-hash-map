83e41f2a9b74da0b53ea6b0e636be6feda23b3aa
Alexandr Politov <apolitov83@gmail.com>
2024-08-12 17:49:34 +0300

    Solution Politov

--- app/main.py
+++ app/main.py
@@ -1,2 +1,12 @@
-class Dictionary:
-    pass
+from typing import Any
+
+
+class Dictionary(dict):
+    def __getitem__(self, key: str | int | float) -> Any:
+        return self.__dict__[key]
+
+    def __setitem__(self, key: str | int | float, value: Any) -> None:
+        self.__dict__[key] = value
+
+    def __len__(self) -> int:
+        return len(self.__dict__)
