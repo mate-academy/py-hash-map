ca94c86c8ed29652ffbb8e7c32240b90ca1290c8
Mikola Rozhansky <bymikola@gmail.com>
2024-08-15 00:38:35 +0300

    1

--- app/main.py
+++ app/main.py
@@ -1,2 +1,35 @@
 class Dictionary:
-    pass
+    hash_table = []
+    len_hash_table = 8
+
+    def __init__(self, value, key) -> None:
+        # self.value = value
+        # self.key = key
+        # self.hask_key = hash(key)
+
+    def __setitem__(self, key, value):
+        if len(self.hash_table) > (self.len_hash_table * 2) / 3:
+            self.len_hash_table *= 2
+
+        index = hash(key) % self.len_hash_table
+        while True:
+            if not (self.hash_table[index]):
+                break
+            index += 1
+
+        self.hash_table[index] = [key, value, hash(key)]
+
+    def __getitem__(self, key):
+        index = hash(key) % self.len_hash_table
+        while True:
+            if self.hash_table[index]:
+                break
+            index += 1
+        return self.hash_table[index]
+
+    def __len__(self):
+        _len = 0
+        for data in self.hash_table:
+            if data:
+                _len += 1
+        return _len
