fd5742f702fc7aa4043a208db242c0d28dd1cc3e
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-08-16 11:34:14 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
+from __future__ import annotations
+
 from copy import deepcopy
 from fractions import Fraction
 from typing import Any, Hashable
+
+
+class Node:
+    def __init__(self, key: Hashable, value: Any, hash_value: int, next_node: Node = None) -> None:
+        self.key = key
+        self.value = value
+        self.hash_value = hash_value
+        self.next = next_node
 
 
 class Dictionary:
@@ -10,7 +20,7 @@ class Dictionary:
         self._capacity = 8
         self._load_factor = Fraction(2, 3)
         self._threshold = self._capacity * self._load_factor
-        self._hash_table = [[] for _ in range(self._capacity)]
+        self._hash_table = [None] * self._capacity
         self._size = 0
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
@@ -18,33 +28,41 @@ class Dictionary:
             self._get_resized()
         calc_hash = hash(key)
         index = calc_hash % self._capacity
-        for cell in self._hash_table[index]:
-            if cell[0] == key:
-                self._hash_table[index].remove(cell)
-                self._size -= 1
-                break
-        self._hash_table[index].append((key, value, calc_hash))
+        node = self._hash_table[index]
+        prev_node = None
+
+        while node:
+            if node.key == key:
+                node.value = value
+                return
+            prev_node = node
+            node = node.next
+        new_node = Node(key, value, calc_hash)
+        if prev_node is None:
+            self._hash_table[index] = new_node
+        else:
+            prev_node.next = new_node
         self._size += 1
 
     def __len__(self) -> int:
         return self._size
 
     def __getitem__(self, key: Hashable) -> Any:
-        try:
-            index = hash(key) % self._capacity
-        except TypeError:
-            raise TypeError
-        for cell in self._hash_table[index]:
-            if cell[0] == key:
-                return cell[1]
+        index = hash(key) % self._capacity
+        node = self._hash_table[index]
+        while node:
+            if node.key == key:
+                return node.value
+            node = node.next
         raise KeyError
 
     def _get_resized(self) -> None:
         self._capacity = self._capacity * 2
         self._threshold = self._capacity * self._load_factor
-        copy_table = deepcopy(self._hash_table)
-        self._hash_table = [[] for i in range(self._capacity)]
+        copy_table = self._hash_table
+        self._hash_table = [None] * self._capacity
         self._size = 0
-        for ext_cell in copy_table:
-            for key, value, _ in ext_cell:
-                self[key] = value
+        for node in copy_table:
+            while node:
+                self[node.key] = node.value
+                node = node.next
