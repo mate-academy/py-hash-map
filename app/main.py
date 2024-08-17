0acd7e663a80de281b2256e16cd4a2cc0d66a164
Alexandr Politov <apolitov83@gmail.com>
2024-08-17 15:01:38 +0300

    class Node deleted

--- app/main.py
+++ app/main.py
@@ -1,22 +1,16 @@
 from typing import Any, Hashable
 
 
-class Node:
-    def __init__(self, key: Hashable, value: Any) -> None:
-        self.key = key
-        self.value = value
-
-
 class Dictionary:
     def __init__(self, capacity: int = 8) -> None:
         self.capacity = capacity
         self.size = 0
-        self.hash_table: list[Node | None] = [None] * self.capacity
+        self.hash_table: list = [None] * self.capacity
 
     def _calculate_index(self, key: Hashable) -> int:
         index = hash(key) % self.capacity
         while (self.hash_table[index] is not None
-               and self.hash_table[index].key != key):
+               and self.hash_table[index][1] != key):
             index = (index + 1) % self.capacity
         return index
 
@@ -25,7 +19,7 @@ class Dictionary:
         self.__init__(self.capacity * 2)
         for item in old_hash_table:
             if item is not None:
-                self.__setitem__(item.key, item.value)
+                self.__setitem__(item[1], item[-1])
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
         index = self._calculate_index(key)
@@ -36,13 +30,13 @@ class Dictionary:
                 return self.__setitem__(key, value)
             self.size += 1
 
-        self.hash_table[index] = Node(key, value)
+        self.hash_table[index] = (hash(key), key, value)
 
     def __getitem__(self, key: Hashable) -> Any:
         index = self._calculate_index(key)
         if self.hash_table[index] is None:
             raise KeyError(f"Key {key} is not in this dict")
-        return self.hash_table[index].value
+        return self.hash_table[index][-1]
 
     def __len__(self) -> int:
         return self.size
