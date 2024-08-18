a473a91643ee0a53c357287243bf4b032b6aab52
Danyil <dartomov@gmail.com>
2024-08-18 16:15:27 +0300

    Fixed_1

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from typing import Any, Hashable
+from fractions import Fraction
+
+
+class Node:
+    def __init__(self, key: Hashable, key_hash: int, value: Any) -> None:
+        self.key = key
+        self.key_hash = key_hash
+        self.value = value
 
 
 class Dictionary:
-    load_factor = 2 / 3
+    load_factor = Fraction(2, 3)
 
     def __init__(self) -> None:
         self.length = 0
@@ -14,12 +22,40 @@ class Dictionary:
             key: Hashable,
             value: Any
     ) -> None:
-        key_hash = hash(key)
-        items = [key, key_hash, value]
+
         if self.length >= round(self.capacity * self.load_factor):
             self.__hash_table_resize()
 
-        self.__put_key_value_to_a_hash_table(items)
+        self.__put_key_value_to_a_hash_table(key, value)
+
+    def __put_key_value_to_a_hash_table(
+        self,
+        key: Hashable,
+        value: Any,
+        ignore_counter_of_length: bool = False
+    ) -> Any:
+
+        index_in_hash_table = self.__find_index(key)
+        cell = self.hash_table[index_in_hash_table]
+        if not cell:
+            self.hash_table[index_in_hash_table] = Node(key=key,
+                                                        key_hash=hash(key),
+                                                        value=value
+                                                        app)
+            if not ignore_counter_of_length:
+                self.length += 1
+        else:
+            cell.value = value
+
+    def __find_index(self, key: Hashable) -> int:
+        index = hash(key) % self.capacity
+        while (
+                self.hash_table[index] is not None
+                and key != self.hash_table[index].key
+        ):
+            index = (index + 1) % self.capacity
+        print(index)
+        return index
 
     def __hash_table_resize(self) -> None:
         template_hash_table = self.hash_table
@@ -28,58 +64,24 @@ class Dictionary:
         for items in template_hash_table:
             if items:
                 self.__put_key_value_to_a_hash_table(
-                    items,
+                    items.key,
+                    items.value,
                     ignore_counter_of_length=True
                 )
 
-    def __put_key_value_to_a_hash_table(
-        self,
-        items: list[Hashable | int | Any],
-        ignore_counter_of_length: bool = False
-    ) -> Any:
-
-        key, key_hash, value = items
-        index_in_hash_table = key_hash % self.capacity
-
-        while True:
-            cell = self.hash_table[index_in_hash_table]
-            if not cell:
-                break
-            if key_hash == cell[1]:
-                if key == cell[0]:
-                    cell[2] = value
-                    return
-            index_in_hash_table = (index_in_hash_table + 1) % self.capacity
-
-        self.hash_table[index_in_hash_table] = items
-        if not ignore_counter_of_length:
-            self.length += 1
-
-    def __getitem__(self, item: Hashable) -> Any:
-        index_in_hash_table = hash(item) % self.capacity
-
-        while True:
-            cell = self.hash_table[index_in_hash_table]
-
-            if not cell:
-                raise KeyError("No such key found in this dictionary")
-            if item == cell[0]:
-                return cell[2]
-
-            index_in_hash_table = (index_in_hash_table + 1) % self.capacity
+    def __getitem__(self, key: Hashable) -> Any:
+        index_in_hash_table = self.__find_index(key)
+        if not self.hash_table[index_in_hash_table]:
+            raise KeyError("No such key found in this dictionary")
+        return self.hash_table[index_in_hash_table].value
 
     def __len__(self) -> int:
         return self.length
 
     def __delitem__(self, key: Hashable) -> None:
-        index_in_hash_table = hash(key) % self.capacity
-        times = 0
-        while times <= self.capacity:
-            cell = self.hash_table[index_in_hash_table]
-            if cell:
-                if key == cell[0]:
-                    self.hash_table[index_in_hash_table] = None
-                    break
-            times += 1
+        index_in_hash_table = self.__find_index(key)
+        if self.hash_table[index_in_hash_table] is None:
+            raise KeyError("No such key found in this dictionary")
 
-            index_in_hash_table = (index_in_hash_table + 1) % self.capacity
+        self.hash_table[index_in_hash_table] = None
+        self.length -= 1
