f9ce626a2b0b226e1858452e21728a11d81d860c
DSahalatyi <dsahalatyi@gmail.com>
2024-08-19 13:19:08 +0300

    fix requested changes

--- app/main.py
+++ app/main.py
@@ -2,12 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from fractions import Fraction
-from typing import Any, Hashable
-
-
-CAPACITY = 8
-LOAD_FACTOR = Fraction(2, 3)
-CAPACITY_MULTIPLIER = 2
+from typing import Any, Hashable, Iterable
 
 
 @dataclass
@@ -18,8 +13,15 @@ class Node:
 
 
 class Dictionary:
-    def __init__(self, capacity: int = CAPACITY) -> None:
-        self.capacity = capacity
+    INITIAL_CAPACITY = 8
+    LOAD_FACTOR = Fraction(2, 3)
+    CAPACITY_MULTIPLIER = 2
+
+    def __init__(self) -> None:
+        try:
+            self.capacity
+        except AttributeError:
+            self.capacity = Dictionary.INITIAL_CAPACITY
         self.hashtable: list[Node | None] = [None] * self.capacity
         self.size = 0
         self.keys = []
@@ -37,7 +39,7 @@ class Dictionary:
 
     @property
     def resize_threshold(self) -> Fraction:
-        return self.capacity * LOAD_FACTOR
+        return self.capacity * Dictionary.LOAD_FACTOR
 
     def __getitem__(self, key: Hashable) -> Any:
         index = self.get_index(key)
@@ -49,7 +51,9 @@ class Dictionary:
 
     def resize(self) -> None:
         old_hash_table = self.hashtable
-        self.__init__(self.capacity * CAPACITY_MULTIPLIER)
+
+        self.capacity *= Dictionary.CAPACITY_MULTIPLIER
+        self.__init__()
 
         for node in old_hash_table:
             if node is not None:
@@ -61,7 +65,7 @@ class Dictionary:
         if self.hashtable[index] is None:
             if self.size + 1 >= self.resize_threshold:
                 self.resize()
-                return self.__setitem__(key, value)
+                index = self.get_index(key)
             self.size += 1
             self.keys.append(key)
 
@@ -81,9 +85,7 @@ class Dictionary:
         self.size -= 1
 
     def clear(self) -> None:
-        for i in range(len(self.hashtable)):
-            if self.hashtable[i] is not None:
-                self.hashtable[i] = None
+        self.__init__()
         self.keys = []
 
     def get(self, key: Hashable, default: Any = None) -> Any:
@@ -94,40 +96,45 @@ class Dictionary:
 
         return self.hashtable[index]
 
-    def pop(self, key: Hashable, default: Any = None) -> Any:
+    _NO_DEFAULT = object()
+
+    def pop(self, key: Hashable, default: Any = _NO_DEFAULT) -> Any:
         index = self.get_index(key)
         node = self.hashtable[index]
-
         if node is None:
-            if default is not None:
+            if default is not Dictionary._NO_DEFAULT:
                 return default
             raise KeyError(f"{key}")
 
-        self.hashtable[index] = None
+        del self.hashtable[index]
         self.keys.remove(key)
         self.size -= 1
 
         return node.value
 
-    def update(self, other: Dictionary) -> None:
+    def update(self, other: Dictionary | Iterable) -> None:
         if isinstance(other, Dictionary):
             for node in other:
-                if node is not None:
-                    self.__setitem__(node.key, node.value)
+                self.__setitem__(node.key, node.value)
+
+        elif isinstance(other, Iterable):
+            for item in other:
+                if isinstance(item, (list, tuple)) and len(item) == 2:
+                    key, value = item
+                    self.__setitem__(key, value)
+                else:
+                    raise ValueError(
+                        "Iterable must contain pairs of (key, value)"
+                    )
         else:
             raise TypeError(
-                f"{other} is {type(other).__name__}, not a Dictionary"
+                f"Expected a dict or iterable of pairs, "
+                f"got {type(other).__name__}"
             )
 
     def __iter__(self) -> Dictionary:
         self.current_key = 0
-        return self
-
-    def __next__(self) -> Node:
         while self.current_key < len(self.keys):
-            index = self.get_index(
-                self.keys[self.current_key]
-            )
+            index = self.get_index(self.keys[self.current_key])
             self.current_key += 1
-            return self.hashtable[index]
-        raise StopIteration()
+            yield self.hashtable[index]
