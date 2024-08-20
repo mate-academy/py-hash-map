79eff2cbe6e7418682af36741a2bf4a015a34171
Stepan <oleksiukstepann@gmail.com>
2024-08-20 18:29:52 +0300

    "Solution 2.4"

--- app/main.py
+++ app/main.py
@@ -1,52 +1,55 @@
 from __future__ import annotations
 
 from fractions import Fraction
-from typing import Any, Iterator
+from typing import Any, Iterator, Iterable, Hashable, Optional
 
-INITIAL_CAPACITY = 8
-LOAD_FACTOR = Fraction(2, 3)
-CAPACITY_MULTIPLIER = 2
+NOT_PROVIDED = object()
 
 
 class Dictionary:
     class Node:
-        def __init__(self, key: Any, value: Any) -> None:
+        def __init__(self, key: Any, key_hash: int, value: Any) -> None:
             self.key = key
+            self.key_hash = key_hash
             self.value = value
 
     def __init__(self) -> None:
-        self.capacity = INITIAL_CAPACITY
-        self.hash_table = [None] * self.capacity
-        self.size = 0
+        self.capacity: int = 8
+        self.load_factor: Fraction = Fraction(2, 3)
+        self.hash_table: list[Dictionary.Node | None] = [None] * self.capacity
+        self.size: int = 0
 
-    def _calculate_index(self, key: Any) -> int:
-        index = hash(key) % self.capacity
+    def _calculate_index(self, key: Any, hash_key: int) -> int:
+        index = hash_key % self.capacity
 
         while (
-                self.hash_table[index] is not None
-                and self.hash_table[index].key != key
+            self.hash_table[index] is not None
+            and (
+                self.hash_table[index].key != key
+                or self.hash_table[index].key_hash != hash_key
+            )
         ):
             index = (index + 1) % self.capacity
 
         return index
 
     @property
-    def threshold(self) -> float:
-        return self.capacity * LOAD_FACTOR
+    def threshold(self) -> Fraction:
+        return self.capacity * self.load_factor
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        index = self._calculate_index(key)
+        index = self._calculate_index(key, hash(key))
 
         if self.hash_table[index] is None:
             if self.size + 1 >= self.threshold:
                 self.resize()
-                index = self._calculate_index(key)
+                return self.__setitem__(key, value)
             self.size += 1
 
-        self.hash_table[index] = Dictionary.Node(key, value)
+        self.hash_table[index] = Dictionary.Node(key, hash(key), value)
 
     def __getitem__(self, key: Any) -> Any:
-        index = self._calculate_index(key)
+        index = self._calculate_index(key, hash(key))
 
         if self.hash_table[index] is None:
             raise KeyError(f"Key {key} is not in the dict")
@@ -60,53 +63,43 @@ class Dictionary:
         self.__init__()
 
     def __delitem__(self, key: Any) -> None:
-        index = self._calculate_index(key)
-        loop_counter = 0
+        index = self._calculate_index(key, hash(key))
 
-        while self.hash_table[index] is None:
-            loop_counter += 1
-            index = (index + 1) % self.capacity
-            if loop_counter > self.capacity:
-                raise KeyError(f"Cannot find value for key: {key}")
+        if self.hash_table[index] is None:
+            raise KeyError(f"Cannot find value for key: {key}")
 
         self.hash_table[index] = None
         self.size -= 1
 
-        if (
-                self.size < self.threshold // 2
-                and self.capacity > INITIAL_CAPACITY
-        ):
-            self.resize()
-
-    def get(self, key: Any, default: Any = None) -> Any:
+    def get(self, key: Hashable, default: Optional[Any] = None) -> Any:
         try:
             return self[key]
         except KeyError:
             return default
 
-    def pop(self, key: Any, default: Any = None) -> Any:
-        value = self.get(key, default)
+    def pop(self, key: Hashable, default: Optional[Any] = NOT_PROVIDED) -> Any:
+        try:
+            value = self[key]
+        except KeyError:
+            if default is not NOT_PROVIDED:
+                return default
+            else:
+                raise
+
         del self[key]
         return value
 
-    def update(self, other: Iterator[Any]) -> None:
+    def update(self, other: Dictionary | Iterable) -> None:
         for node in other:
             if node:
-                self.__setitem__(node.key, node.value)
+                self[node.key] = node.value
 
     def __iter__(self) -> Iterator[Any]:
-        return (node if node else None for node in self.hash_table)
+        return (node for node in self.hash_table if node)
 
     def resize(self) -> None:
         old_dict = self.hash_table
-        if self.size + 1 >= self.threshold:
-            self.capacity *= CAPACITY_MULTIPLIER
-        elif self.size < self.threshold // 2:
-            self.capacity //= CAPACITY_MULTIPLIER
-
+        self.capacity *= 2
         self.hash_table = [None] * self.capacity
         self.size = 0
-
-        for node in old_dict:
-            if node:
-                self.__setitem__(node.key, node.value)
+        self.update(old_dict)
