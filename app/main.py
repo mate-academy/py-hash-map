ca7223607ac0e60a2f0e3e2e777c06868e85261c
Yakiv Lobach <lobachyakiv@gmail.com>
2024-08-20 12:14:43 +0300

    first_try small changes add changes add more changes

--- app/main.py
+++ app/main.py
@@ -1,23 +1,23 @@
-from typing import Any, Hashable
-
-
-class Node:
-    def __init__(self, key: Hashable, hash_key: int, value: Any) -> None:
-        self.key = key
-        self.hash_key = hash_key
-        self.value = value
+from typing import Any, Hashable, Optional, List, Iterable, Union, Tuple
 
 
 class Dictionary:
+    class _Node:
+        def __init__(self, key: Hashable, value: Any) -> None:
+            self.key = key
+            self.hash_key = hash(key)
+            self.value = value
+
     def __init__(
             self,
             initial_capacity: int = 8,
-            load_factor: float = 0.75
+            load_factor: float = 2 / 3
     ) -> None:
-        self._capacity = initial_capacity
-        self._load_factor = load_factor
-        self._size = 0
-        self._buckets = [[] for _ in range(self._capacity)]
+        self._capacity: int = initial_capacity
+        self._load_factor: float = load_factor
+        self._size: int = 0
+        self._buckets: (
+            List)[Optional[Dictionary._Node]] = [None] * self._capacity
 
     def _get_index(self, key: Hashable) -> int:
         return hash(key) % self._capacity
@@ -26,71 +26,81 @@ class Dictionary:
         if self._size / self._capacity >= self._load_factor:
             self._resize()
         index = self._get_index(key)
-        for node in self._buckets[index]:
-            if node.key == key:
-                node.value = value
+        while self._buckets[index] is not None:
+            if self._buckets[index].key == key:
+                self._buckets[index].value = value
                 return
-        self._buckets[index].append(Node(key, hash(key), value))
+            index = (index + 1) % self._capacity
+        self._buckets[index] = self._Node(key, value)
         self._size += 1
 
     def __getitem__(self, key: Hashable) -> Any:
         index = self._get_index(key)
-        for node in self._buckets[index]:
-            if node.key == key:
-                return node.value
-        raise KeyError(f"KeyError: '{key}' not found in Dictionary")
+        while self._buckets[index] is not None:
+            if self._buckets[index].key == key:
+                return self._buckets[index].value
+            index = (index + 1) % self._capacity
+        raise KeyError(f'KeyError: "{key}" not found in Dictionary')
 
     def __len__(self) -> int:
         return self._size
 
     def _resize(self) -> None:
         new_capacity = self._capacity * 2
-        new_buckets = [[] for _ in range(new_capacity)]
-        for bucket in self._buckets:
-            for node in bucket:
+        new_buckets: List[Optional[Dictionary._Node]] = [None] * new_capacity
+        for node in self._buckets:
+            if node is not None:
                 index = node.hash_key % new_capacity
-                new_buckets[index].append(node)
+                while new_buckets[index] is not None:
+                    index = (index + 1) % new_capacity
+                new_buckets[index] = node
         self._buckets = new_buckets
         self._capacity = new_capacity
 
     def clear(self) -> None:
-        self._buckets = [[] for _ in range(self._capacity)]
+        self._buckets = [None] * self._capacity
         self._size = 0
 
     def __delitem__(self, key: Hashable) -> None:
         index = self._get_index(key)
-        for i, node in enumerate(self._buckets[index]):
-            if node.key == key:
-                del self._buckets[index][i]
+        while self._buckets[index] is not None:
+            if self._buckets[index].key == key:
+                self._buckets[index] = None
                 self._size -= 1
                 return
-        raise KeyError(f"KeyError: '{key}' not found in Dictionary")
+            index = (index + 1) % self._capacity
+        raise KeyError(f'KeyError: "{key}" not found in Dictionary')
 
-    def get(self, key: Hashable, default: Any = None) -> Any:
+    def get(self, key: Hashable, default: Optional[Any] = None) -> Any:
         try:
             return self[key]
         except KeyError:
             return default
 
-    def pop(self, key: Hashable, default: Any = None) -> Any:
-        index = self._get_index(key)
-        for i, node in enumerate(self._buckets[index]):
-            if node.key == key:
-                value = node.value
-                del self._buckets[index][i]
-                self._size -= 1
-                return value
-        if default is not None:
-            return default
-        raise KeyError(f"KeyError: '{key}' not found in Dictionary")
+    def pop(self, key: Hashable, default: Optional[Any] = None) -> Any:
+        try:
+            value = self[key]
+            del self[key]
+            return value
+        except KeyError:
+            if default is not None:
+                return default
+            raise
 
-    def update(self, other: dict) -> None:
+    def update(
+            self,
+            other: Union[dict, "Dictionary", Iterable[Tuple[Hashable, Any]]]
+    ) -> None:
         if other is None:
             return
-        for key, value in other.items():
-            self[key] = value
+        if isinstance(other, dict) or isinstance(other, Dictionary):
+            for key, value in other.items():
+                self[key] = value
+        else:
+            for key, value in other:
+                self[key] = value
 
-    def __iter__(self) -> Any:
-        for bucket in self._buckets:
-            for node in bucket:
+    def __iter__(self) -> Iterable[Hashable]:
+        for node in self._buckets:
+            if node is not None:
                 yield node.key
