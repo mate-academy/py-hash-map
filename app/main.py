cf59c2ada8684dd69a8c0c0a18de693990c7ed3c
Alexandr Politov <apolitov83@gmail.com>
2024-08-20 17:28:38 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,24 @@
+from fractions import Fraction
 from typing import Any, Hashable
 
 
 class Dictionary:
+
+    class Node:
+
+        def __init__(self, key: Hashable, value: Any) -> None:
+            self.key = key
+            self.value = value
+
     def __init__(self, capacity: int = 8) -> None:
         self.capacity = capacity
         self.size = 0
-        self.hash_table: list = [None] * self.capacity
+        self.hash_table: list[Dictionary.Node | None] = [None] * self.capacity
 
     def _calculate_index(self, key: Hashable) -> int:
         index = hash(key) % self.capacity
         while (self.hash_table[index] is not None
-               and self.hash_table[index][1] != key):
+               and self.hash_table[index].key != key):
             index = (index + 1) % self.capacity
         return index
 
@@ -19,24 +27,23 @@ class Dictionary:
         self.__init__(self.capacity * 2)
         for item in old_hash_table:
             if item is not None:
-                self.__setitem__(item[1], item[-1])
+                self.__setitem__(item.key, item.value)
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
-        index = self._calculate_index(key)
+        if self.size + 1 >= self.capacity * Fraction(2, 3):
+            self.resize()
 
+        index = self._calculate_index(key)
         if self.hash_table[index] is None:
-            if self.size + 1 >= self.capacity * 2 / 3:
-                self.resize()
-                return self.__setitem__(key, value)
             self.size += 1
 
-        self.hash_table[index] = (hash(key), key, value)
+        self.hash_table[index] = Dictionary.Node(key, value)
 
     def __getitem__(self, key: Hashable) -> Any:
         index = self._calculate_index(key)
         if self.hash_table[index] is None:
             raise KeyError(f"Key {key} is not in this dict")
-        return self.hash_table[index][-1]
+        return self.hash_table[index].value
 
     def __len__(self) -> int:
         return self.size
