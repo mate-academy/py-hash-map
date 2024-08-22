074c2dcfa39adbf0c9fbd06f02956a809209dfef
Ivan Shakhman <shakhmanivan@gmail.com>
2024-08-22 13:36:50 +0300

    RE re re - solution

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
-import math
 from dataclasses import dataclass
+from fractions import Fraction
 from typing import Hashable, Any, Iterator
 
 
-@dataclass
-class Node:
-    key: Hashable
-    obj_hash: hash
-    value: Any
-
-
 class Dictionary:
-    def __init__(self, capacity: int = 8) -> None:
-        self.capacity = capacity
+    @dataclass
+    class Node:
+        key: Hashable
+        obj_hash: hash
+        value: Any
+
+    def __init__(self) -> None:
+        self.capacity = 8
         self.size = 0
-        self.hash_table: list[Node | None] = [None] * self.capacity
+        self.hash_table: list[Dictionary.Node | None] = [None] * self.capacity
 
     def _calculate_index(self, key: Hashable) -> int:
         index = hash(key) % self.capacity
@@ -28,28 +27,25 @@ class Dictionary:
         return index
 
     @property
-    def current_max_size(self) -> int:
-        return math.floor(self.capacity * 2 / 3)
+    def current_max_size(self) -> Fraction:
+        return self.capacity * Fraction(2 , 3)
 
     def _resize(self) -> None:
         old_hash_table = self.hash_table
-
-        self.__init__(self.capacity * 2)
-
+        self.capacity *= 2
+        self.size = 0
+        self.hash_table = [None] * self.capacity
         for node in old_hash_table:
             if node:
-                self.__setitem__(node.key, node.value)
+                self[node.key] = node.value
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
+        if self.size > self.current_max_size:
+            self._resize()
         index = self._calculate_index(key)
-
-        if self.hash_table[index] is None:
-            if self.size + 1 >= self.current_max_size:
-                self._resize()
-                return self.__setitem__(key, value)
+        if not self.hash_table[index]:
             self.size += 1
-
-        self.hash_table[index] = Node(key, hash(key), value)
+        self.hash_table[index] = Dictionary.Node(key, hash(key), value)
 
     def __getitem__(self, item: Any) -> Any:
         index = self._calculate_index(item)
@@ -88,28 +84,36 @@ class Dictionary:
         temp = None
         index = self._calculate_index(key)
         if self.hash_table[index]:
-            temp = self.hash_table[index]
+            temp = self.hash_table[index].value
             self.hash_table[index] = None
         return temp
 
     def keys(self) -> list[Hashable] | None:
         keys = []
         for node_key in self.hash_table:
-            keys.append(node_key.key)
+            if node_key:
+                keys.append(node_key.key)
         return keys if keys else None
 
-    def values(self) -> Any:
+    def values(self) -> list[Any] | None:
         values = []
         for node_value in self.hash_table:
-            values.append(node_value.value)
+            if node_value:
+                values.append(node_value.value)
         return values if values else None
 
-    def items(self) -> tuple[Hashable, Any] | tuple[None, None]:
-        return self.keys(), self.values()
+    def items(self) -> list[tuple[Hashable, Any]] | None:
+        items_ = []
+        for key, value in self.values(), self.items():
+            if key and value:
+                items_.append((key, value))
+        return items_ if items_ else None
 
     def update(self, other: object) -> None:
-        if isinstance(other, Dictionary):
+        if isinstance(other, Dictionary) or type(other) is dict:
             for key, value in other.items():
                 self.hash_table[key] = value
+        elif type(other) is Dictionary.Node:
+            self.hash_table[other.key] = other.value
         else:
             raise TypeError("Update data must be Dictionary!")
