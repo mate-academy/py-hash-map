9384bc7a88f0df2ffb43ab809b81d2c2878ccbbb
Misha Havryliuk <misharob31415@gmail.com>
2024-08-23 13:32:02 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,27 +1,51 @@
-from typing import Any
+from dataclasses import dataclass
+from typing import Any, Hashable, Optional
 
 
 class Dictionary:
-    def __init__(self, capacity: int = 10) -> None:
-        self.table = [[] for _ in range(capacity)]
+    @dataclass
+    class Node:
+        key: Hashable
+        value: Any
 
-    def _hash(self, key: str) -> int:
-        return hash(key) % len(self.table)
+    def __init__(self, capacity: int = 8) -> None:
+        self.capacity = capacity
+        self.table: list[Optional[Dictionary.Node]] = [None] * self.capacity
+        self.size = 0
+
+    def _find_index(self, key: Hashable) -> int:
+        index = hash(key) % self.capacity
+        while (
+            self.table[index] is not None
+            and self.table[index].key != key
+        ):
+            index = (index + 1) % self.capacity
+        return index
+
+    def resize(self) -> None:
+        old_hash_table = self.table
+        self.capacity *= 2
+        self.table = [None] * self.capacity
+        self.size = 0
+
+        for node in old_hash_table:
+            if node is not None:
+                self.__setitem__(node.key, node.value)
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        index = self._hash(key)
-        for kv in self.table[index]:
-            if kv[0] == key:
-                kv[1] = value
-                return
-        self.table[index].append([key, value])
+        index = self._find_index(key)
+        if self.table[index] is None:
+            if self.size + 1 >= self.capacity * (2 / 3):
+                self.resize()
+                index = self._find_index(key)
+            self.size += 1
+        self.table[index] = self.Node(key, value)
 
     def __getitem__(self, key: Any) -> Any:
-        index = self._hash(key)
-        for kv in self.table[index]:
-            if kv[0] == key:
-                return kv[1]
-        raise KeyError(f"Key {key} not found")
+        index = self._find_index(key)
+        if self.table[index] is None:
+            raise KeyError(f'Cannot find value for key: {key}')
+        return self.table[index].value
 
     def __len__(self) -> int:
-        return sum(len(bucket) for bucket in self.table)
+        return self.size
