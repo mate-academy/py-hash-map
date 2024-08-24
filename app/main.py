57b3abf3ceca1b314b1118fab36409b11f9bb77f
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-08-24 16:03:54 +0300

    'made_requested_changes'

--- app/main.py
+++ app/main.py
@@ -1,39 +1,43 @@
+from __future__ import annotations
+
 from fractions import Fraction
 from typing import Hashable, Any
-
-
-class Node:
-    def __init__(self, key: Hashable, value: Any) -> None:
-        self.key = key
-        self.value = value
 
 
 class Dictionary:
     LOAD_FACTOR = Fraction(2, 3)
 
-    def __init__(self, capacity: int = 8) -> None:
-        self.capacity = capacity
+    class Node:
+        def __init__(self, key: Hashable, value: Any) -> None:
+            self.key = key
+            self.value = value
+
+    def __init__(self) -> None:
+        self.capacity = 8
         self.size = 0
-        self.hash_table: list[Node | None] = [None] * self.capacity
+        self.hash_table: list[Dictionary.Node | None] = [None] * self.capacity
 
     def _resize(self) -> None:
         old_hash_table = self.hash_table
 
-        self.__init__(self.capacity * 2)
+        self.capacity *= 2
+        self.size = 0
+        self.hash_table: list[Dictionary.Node | None] = [None] * self.capacity
 
-        for slot in old_hash_table:
-            if slot:
-                self.__setitem__(slot.key, slot.value)
+        for node in old_hash_table:
+            if node:
+                self[node.key] = node.value
 
-    def _calculate_index(self, key: Hashable, setting: bool = True) -> int:
+    def _calculate_index(self, key: Hashable) -> int:
         index = hash(key) % self.capacity
 
-        while True:
-            if not self.hash_table[index]:
-                return index if setting else -1
-            if self.hash_table[index].key == key:
-                return index
+        while (
+            self.hash_table[index] is not None
+            and self.hash_table[index].key != key
+        ):
             index = (index + 1) % self.capacity
+
+        return index
 
     def __setitem__(self, key: Hashable, value: any) -> None:
         if self.size > self.capacity * self.LOAD_FACTOR:
@@ -42,11 +46,11 @@ class Dictionary:
         index = self._calculate_index(key)
         if not self.hash_table[index]:
             self.size += 1
-        self.hash_table[index] = Node(key, value)
+        self.hash_table[index] = Dictionary.Node(key, value)
 
     def __getitem__(self, key: Hashable) -> any:
-        index = self._calculate_index(key, False)
-        if index == -1:
+        index = self._calculate_index(key)
+        if self.hash_table[index] is None:
             raise KeyError(f"Key {key} not found")
         return self.hash_table[index].value
 
@@ -54,4 +58,5 @@ class Dictionary:
         return self.size
 
     def clear(self) -> None:
-        self.__init__(self.capacity)
+        self.size = 0
+        self.hash_table: list[Dictionary.Node | None] = [None] * self.capacity
