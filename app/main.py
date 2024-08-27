bff3c6b8641808e952705a8a8f9caf71d7c3251e
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-27 09:07:09 +0300

    first

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
-from typing import Hashable, Any
+from typing import Hashable, Any, Optional, List
 from fractions import Fraction
-
 
 class Dictionary:
     CAPACITY = 8
@@ -17,9 +16,9 @@ class Dictionary:
             self.key_hash = hash(self.key)
 
     def __init__(self) -> None:
-        self.capacity = self.CAPACITY
-        self.size = 0
-        self.buckets = [None] * self.capacity
+        self.capacity: int = self.CAPACITY
+        self.size: int = 0
+        self.buckets: List[Optional[Dictionary.Node]] = [None] * self.capacity
 
     def calculate_index(self, key: Hashable) -> int:
         index = hash(key) % self.capacity
@@ -30,18 +29,10 @@ class Dictionary:
             index = (index + 1) % self.capacity
         return index
 
-    def __setitem__(self, key: Hashable, value: Any) -> None:
-        if self.size >= self.max_size:
-            self.resize()
-
-        index = self.calculate_index(key)
-        if self.buckets[index] is None:
-            self.size += 1
-        self.buckets[index] = self.Node(key, value)
-
-    def resize(self) -> None:
+    def resize(self, use_multiplier: bool = True) -> None:
         old_buckets = self.buckets
-        self.capacity *= self.INCREASE_MULTIPLIER
+        if use_multiplier:
+            self.capacity *= self.INCREASE_MULTIPLIER
         self.buckets = [None] * self.capacity
         self.size = 0
 
@@ -52,6 +43,15 @@ class Dictionary:
     @property
     def max_size(self) -> int:
         return int(self.capacity * self.LOAD_FACTOR)
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        if self.size >= self.max_size:
+            self.resize()
+
+        index = self.calculate_index(key)
+        if self.buckets[index] is None:
+            self.size += 1
+        self.buckets[index] = self.Node(key, value)
 
     def __getitem__(self, key: Hashable) -> Any:
         index = self.calculate_index(key)
@@ -73,14 +73,8 @@ class Dictionary:
         self.buckets[index] = None
         self.size -= 1
 
-        self.rehash()
+        if self.size <= self.max_size // self.INCREASE_MULTIPLIER:
+            self.resize(use_multiplier=False)
 
     def rehash(self) -> None:
-        old_buckets = [
-            bucket for bucket in self.buckets if bucket is not None
-        ]
-        self.buckets = [None] * self.capacity
-        self.size = 0
-
-        for node in old_buckets:
-            self.__setitem__(node.key, node.value)
+        self.resize(use_multiplier=False)
