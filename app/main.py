f696bbff862d655d2d77d9e96417862b89dc8d3c
Mikola Rozhansky <bymikola@gmail.com>
2024-08-29 20:01:29 +0300

    14 passed - new1

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Hashable
 
 
 class Dictionary:
@@ -8,15 +8,13 @@ class Dictionary:
         self.hash_table = [None] * 8
         self.length_hash_table = 8
 
-    def update(self) -> None:
+    def _resize(self) -> None:
         self.length_hash_table *= 2
         index = 0
         old_hash = self.hash_table
         self.hash_table = [None] * self.length_hash_table
 
         for data in old_hash:
-            print("Data", data)
-
             if data is not None:
                 key, value = data[0], data[1]
                 index = hash(key) % self.length_hash_table
@@ -31,10 +29,10 @@ class Dictionary:
 
                 self.hash_table[index] = [key, value, hash(key)]
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
 
         if self.length > self.length_hash_table * 2 / 3:
-            self.update()
+            self._resize()
 
         index = hash(key) % self.length_hash_table
 
@@ -54,7 +52,7 @@ class Dictionary:
 
         self.hash_table[index] = [key, value, hash(key)]
 
-    def __getitem__(self, key: Any) -> Any:
+    def __getitem__(self, key: Hashable) -> Any:
         index = hash(key) % self.length_hash_table
         try:
             while True:
