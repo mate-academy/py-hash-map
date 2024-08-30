38414ad58dadb71df834ae705013826006c74f26
Anton Kudriavtsev <bryanfinchnzt@gmail.com>
2024-08-30 01:14:00 -0700

    Replaced -1 by None

--- app/main.py
+++ app/main.py
@@ -5,17 +5,17 @@ class Dictionary:
     def __init__(self) -> None:
         self.length = 8
         self.load_factor = 2 / 3
-        self.dictionary = [-1 for _ in range(8)]
+        self.dictionary = [None for _ in range(8)]
         self.insert_elem = 0
 
     def resize(self) -> None:
-        self.dictionary += [-1 for _ in range(self.length)]
+        self.dictionary += [None for _ in range(self.length)]
         self.length *= 2
 
     def __setitem__(self, key: Hashable, value: Any) -> None:
         hash_code = hash(key)
         index = hash_code % self.length
-        if self.dictionary[index] == -1:
+        if self.dictionary[index] is None:
             self.dictionary[index] = (key, hash_code, value)
             self.insert_elem += 1
         elif (hash_code == self.dictionary[index][1]) and (
@@ -26,7 +26,7 @@ class Dictionary:
             need_location = True
             while need_location:
                 index = (index + 1) % self.length
-                if self.dictionary[index] == -1:
+                if self.dictionary[index] is None:
                     self.dictionary[index] = (key, hash_code, value)
                     need_location = False
                     self.insert_elem += 1
@@ -46,7 +46,7 @@ class Dictionary:
         index = hash_code % self.length
         for i in range(self.length):
             index = (hash_code + i) % self.length
-            if self.dictionary[index] != -1:
+            if self.dictionary[index] is not None:
                 if self.dictionary[index][0] == key:
                     return self.dictionary[index][2]
         raise KeyError("There is no such key.")
