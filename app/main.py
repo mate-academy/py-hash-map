c696a838b052042eb252c07a7aa22cd8caf35fc6
Halyna Rohatska <rohatskahr@gmail.com>
2024-09-04 17:02:14 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -35,9 +35,12 @@ class Dictionary:
     def current_max_size(self) -> int:
         return int(self.capacity * self.resize_threshold)
 
-    def resize(self) -> None:
+    def resize(self, multiplier: int = None) -> None:
+        if multiplier is None:
+            multiplier = self.capacity_multyplier
+
         old_hash_table = self.hash_table
-        self.capacity *= self.capacity_multyplier
+        self.capacity *= multiplier
         self.size = 0
         self.hash_table = [None] * self.capacity
 
@@ -49,14 +52,11 @@ class Dictionary:
         hash_value = hash(key)
         index = self._calculate_index(key, hash_value)
 
-        while self.hash_table[index] is not None:
-            if self.hash_table[index].key == key:
-                self.hash_table[index].value = value
-                return
-            index = (index + 1) % self.current_max_size
-
-        self.hash_table[index] = Dictionary.Node(key, value, hash_value)
-        self.size += 1
+        if self.hash_table[index] is not None:
+            self.hash_table[index].value = value
+        else:
+            self.hash_table[index] = Dictionary.Node(key, value, hash_value)
+            self.size += 1
 
         if self.size >= self.current_max_size:
             self.resize()
@@ -74,23 +74,13 @@ class Dictionary:
         hash_value = hash(key)
         index = self._calculate_index(key, hash_value)
 
-        while True:
-            if self.hash_table[index] is None:
-                raise KeyError(f"Cannot find value for key: {key}")
-            elif self.hash_table[index].key == key:
-                break
-            index = (index + 1) % self.current_max_size
+        if self.hash_table[index] is None or self.hash_table[index].key != key:
+            raise KeyError(f"Cannot find value for key: {key}")
 
         self.hash_table[index] = None
         self.size -= 1
 
-        next_index = (index + 1) % self.current_max_size
-        while self.hash_table[next_index] is not None:
-            node_to_rehash = self.hash_table[next_index]
-            self.hash_table[next_index] = None
-            self.size -= 1
-            self.__setitem__(node_to_rehash.key, node_to_rehash.value)
-            next_index = (next_index + 1) % self.current_max_size
+        self.resize(multiplier=1)
 
     def get(self, key: Hashable, default: Optional[Any] = None) -> Any:
         try:
@@ -105,14 +95,18 @@ class Dictionary:
         return str(self.hash_table)
 
     def clear(self) -> None:
-        self.__init__()
+        self.hash_table = [None] * self.capacity
+        self.size = 0
 
-    def pop(self, key: Hashable, default: Optional[Any] = None) -> Any:
-        try:
-            value = self[key]
-            del self[key]
-            return value
-        except KeyError:
-            if default is not None:
-                return default
-            return None
+    def pop(self, key: Hashable, default: Optional[Any] = object()) -> Any:
+        hash_value = hash(key)
+        index = self._calculate_index(key, hash_value)
+
+        if self.hash_table[index] is None or self.hash_table[index].key != key:
+            if default is object():
+                raise KeyError(f"Cannot find value for key: {key}")
+            return default
+
+        value = self.hash_table[index].value
+        self.__delitem__(key)
+        return value
