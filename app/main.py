bc26e8d56db3a4d874f0004d716970326a508411
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-09-05 00:02:43 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,75 +1,39 @@
-from dataclasses import dataclass
+from copy import deepcopy
 from typing import Hashable, Any
 
 
-INITIAL_CAPACITY = 8
-RESIZE_THRESHOLD = 2 / 3
-CAPACITY_MULTIPLIER = 2
-
-
-@dataclass
-class Node:
-    key: Hashable
-    value: Any
-
-
 class Dictionary:
-    def __init__(self, capacity: int) -> None:
-        self.capacity = capacity
+
+    class Node:
+
+        def __init__(self, key: Hashable, value: Any, hash_key: int) -> None:
+            self.key = key
+            self.value = value
+            self.hash_key = hash_key
+
+    def __init__(self) -> None:
+        self.load_factor = 0.667
+        self.number_of_elements = 0
+        self.capacity = 8
         self.size = 0
-        self.hash_table: list[Node | None] = [None] * self.capacity
+        self.hash_table: list[tuple | None] = [None] * self.capacity
 
-    def _calculate_index(self, key: float) -> None:
-        index = hash(key) % self.capacity
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        if self.size >= self.capacity * self.load_factor:
+            self.resize()
 
-        while (
-            self.hash_table[index] is not None
-            and self.hash_table[index].key != key
-        ):
-            index = (index + 1) % self.capacity
-
-        return index
-
-    @property
-    def current_max_size(self) -> float:
-        return self.capacity * RESIZE_THRESHOLD
-
-    def resize(self) -> None:
-        old_hash_table = self.hash_table
-
-        self.__init__(self.capacity * CAPACITY_MULTIPLIER)
-
-        for node in old_hash_table:
-            if node is not None:
-                self.__setitem__(node.key, node.value)
-
-    def __setitem__(self, key: float, value: float) -> None:
-        index = self._calculate_index(key)
-
+        index = self.find_index(key)
         if self.hash_table[index] is None:
-            if self.size + 1 >= self.current_max_size:
-                self.resize()
-                return self.__setitem__(key, value)
             self.size += 1
 
-        self.hash_table[index] = Node(key, value)
+        self.hash_table[index] = Dictionary.Node(key, value, hash(key))
 
-    def __getitem__(self, key: Hashable) -> None:
-        index = self._calculate_index(key)
-
+    def __getitem__(self, key: Hashable) -> Any:
+        index = self.find_index(key)
         if self.hash_table[index] is None:
-            raise KeyError("Cannot find")
+            raise KeyError(f"Value for key '{key}' not found")
 
         return self.hash_table[index].value
-
-    def __delitem__(self, key: Hashable) -> None:
-        index = self._calculate_index(key)
-
-        if self._calculate_index[index] is None:
-            raise KeyError("Cannot find")
-
-        self.hash_table[index] = None
-        self.size -= 1
 
     def get(self, key: Hashable, default: Any = None) -> None:
         try:
@@ -82,3 +46,22 @@ class Dictionary:
 
     def __str__(self) -> str:
         return str(self.hash_table)
+
+    def resize(self) -> None:
+        self.capacity = self.capacity * 2
+        self.size = 0
+        copy_arr: list = deepcopy(self.hash_table)
+        self.hash_table: list[None | Dictionary.Node] = [
+            None for _ in range(self.capacity)]
+
+        for values in copy_arr:
+            if values is not None:
+                self.__setitem__(values.key, values.value)
+
+    def find_index(self, key: Hashable) -> int:
+        index = hash(key) % self.capacity
+        while (self.hash_table[index] is not None
+                and self.hash_table[index].key != key):
+            index = (index + 1) % self.capacity
+
+        return index
