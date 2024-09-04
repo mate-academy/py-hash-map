5116688d24be19924dd60e228934273d84773a8b
Vlada <vlada.belayy@mail.com>
2024-09-05 00:14:57 +0300

    changes

--- app/main.py
+++ app/main.py
@@ -1,48 +1,66 @@
+from typing import Any, Hashable, List, Optional
+from fractions import Fraction
+
+
 class Node:
-    def __init__(self, key: object, value: object) -> None:
+    def __init__(self, key: Hashable, value: Any) -> None:
         self.key = key
         self.value = value
         self.hash = hash(key)
 
 
 class Dictionary:
-    def __init__(self, capacity: int = 16, load_factor: float = 0.75) -> None:
-        self.capacity = capacity
+    def __init__(self) -> None:
+        self.capacity = 8
         self.size = 0
-        self.load_factor = load_factor
-        self.table = [[] for _ in range(capacity)]
+        self.load_factor = Fraction(2, 3)
+        self.table: List[Optional[List[Node]]] = [None] * self.capacity
 
     def _resize(self) -> None:
         old_table = self.table
         self.capacity *= 2
-        self.table = [[] for _ in range(self.capacity)]
+        self.table = [None] * self.capacity
         self.size = 0
         for bucket in old_table:
-            for node in bucket:
-                self[node.key] = node.value
+            if bucket:
+                for node in bucket:
+                    self.__setitem__(node.key, node.value)
 
-    def _hash_index(self, key: object) -> int:
+    def _hash_index(self, key: Hashable) -> int:
         return hash(key) % self.capacity
 
-    def __setitem__(self, key: object, value: object) -> None:
+    def _find_node(
+        self,
+        bucket: Optional[List[Node]],
+        key: Hashable
+    ) -> Optional[Node]:
+        if bucket:
+            for node in bucket:
+                if node.key == key:
+                    return node
+        return None
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         if self.size / self.capacity > self.load_factor:
             self._resize()
 
         index = self._hash_index(key)
-        bucket = self.table[index]
-        for node in bucket:
-            if node.key == key:
-                node.value = value
-                return
-        bucket.append(Node(key, value))
-        self.size += 1
+        if self.table[index] is None:
+            self.table[index] = []
 
-    def __getitem__(self, key: object) -> object:
+        node = self._find_node(self.table[index], key)
+        if node:
+            node.value = value
+        else:
+            self.table[index].append(Node(key, value))
+            self.size += 1
+
+    def __getitem__(self, key: Hashable) -> Any:
         index = self._hash_index(key)
         bucket = self.table[index]
-        for node in bucket:
-            if node.key == key:
-                return node.value
+        node = self._find_node(bucket, key)
+        if node:
+            return node.value
         raise KeyError(f"Key {key} not found")
 
     def __len__(self) -> int:
