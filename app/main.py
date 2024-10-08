a81ccc1874a445a89107101750c519d2b20df371
dmytro <dmysamo@gmail.com>
2024-10-08 15:28:37 +0300

    dictionary

--- app/main.py
+++ app/main.py
@@ -1,2 +1,90 @@
+from collections.abc import Iterator
+from dataclasses import dataclass
+
+
+@dataclass
+class Node:
+    key: object
+    hash_: object
+    value: object
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.size = 0
+        self.load_factor = 2 / 3
+        self.capacity = 8
+        self.hash_table: list = [None] * self.capacity
+
+    def get_cell(self, key: object) -> int:
+        return hash(key) % self.capacity
+
+    def update(self) -> None:
+        current_nodes = [node for node in self.hash_table if node]
+        self.capacity *= 2
+        self.clear()
+        for node in current_nodes:
+            self.__setitem__(node.key, node.value)
+
+    def get(self, key: object) -> None | Node:
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return None
+
+    def pop(self, key: object) -> Node:
+        result = self.__getitem__(key)
+        self.__delitem__(key)
+        return result
+
+    def clear(self) -> None:
+        self.size = 0
+        self.hash_table: list = [None] * self.capacity
+
+    def __setitem__(self, key: object, value: object) -> None:
+        cell = self.get_cell(key)
+        while node := self.hash_table[cell]:
+            if node.key == key:
+                node.value = value
+                return
+            cell += 1
+            if cell == self.capacity:
+                cell = 0
+
+        self.hash_table[cell] = Node(
+            key,
+            hash(key),
+            value
+        )
+        self.size += 1
+        if self.size == round(self.capacity * self.load_factor):
+            self.update()
+
+    def __getitem__(self, key: object) -> Node:
+        cell = self.get_cell(key)
+        if not self.hash_table[cell]:
+            raise KeyError
+        while node := self.hash_table[cell]:
+            if node.key == key:
+                return node.value
+            cell += 1
+            if cell == self.capacity:
+                cell = 0
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __delitem__(self, key: object) -> None:
+        cell = self.get_cell(key)
+        while node := self.hash_table[cell]:
+            if node.key == key:
+                node_index = self.hash_table.index(node)
+                self.hash_table[node_index] = None
+                self.size -= 1
+                return
+            cell += 1
+            if cell == self.capacity:
+                cell = 0
+
+    def __iter__(self) -> Iterator:
+        return iter(self.hash_table)
