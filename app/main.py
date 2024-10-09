a2365af19bf9e2b5b8339f171497e185438a8f4a
Lev <lev2099@gmail.com>
2024-10-09 16:02:56 +0300

    replace key: Any --> key: Hashable

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from typing import Any, List, Tuple
+from typing import Any, List, Tuple, Hashable
 
 
 class Dictionary:
     def __init__(self) -> None:
         self.my_dict = []
 
-    def __setitem__(self, key: Any, value: Any) -> None:
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         key_hash = self._hash(key)
 
         for i, (k, v) in enumerate(self.my_dict):
@@ -14,7 +14,7 @@ class Dictionary:
                 return
         self.my_dict.append((key_hash, (key, value)))
 
-    def __getitem__(self, key: Any) -> Any:
+    def __getitem__(self, key: Hashable) -> Any:
         key_hash = self._hash(key)
 
         for k, v in self.my_dict:
@@ -26,7 +26,7 @@ class Dictionary:
     def __len__(self) -> int:
         return len(self.my_dict)
 
-    def __delitem__(self, key: Any) -> None:
+    def __delitem__(self, key: Hashable) -> None:
         key_hash = self._hash(key)
         for i, (h, (k, v)) in enumerate(self.my_dict):
             if h == key_hash and k == key:
@@ -35,7 +35,7 @@ class Dictionary:
 
         raise KeyError(f"Key '{key}' not found")
 
-    def __contains__(self, key: Any) -> bool:
+    def __contains__(self, key: Hashable) -> bool:
         key_hash = self._hash(key)
         return any(h == key_hash and k == key for h, (k, v) in self.my_dict)
 
@@ -48,7 +48,7 @@ class Dictionary:
     def values(self) -> List[Any]:
         return [v for _, (k, v) in self.my_dict]
 
-    def _hash(self, key: Any) -> int:
+    def _hash(self, key: Hashable) -> int:
         if isinstance(key, (list, dict)):
             raise TypeError("Unhashable type: 'list' or 'dict'")
         return hash(key)
@@ -56,14 +56,14 @@ class Dictionary:
     def clear(self) -> None:
         self.my_dict.clear()
 
-    def get(self, key: Any, default: Any = None) -> Any:
+    def get(self, key: Hashable, default: Any = None) -> Any:
         key_hash = self._hash(key)
         for k, v in self.my_dict:
             if k == key_hash and key == v[0]:
                 return v[1]
         return default
 
-    def pop(self, key: Any, default: Any = None) -> Any:
+    def pop(self, key: Hashable, default: Any = None) -> Any:
         key_hash = self._hash(key)
         for i, (h, (k, v)) in enumerate(self.my_dict):
             if h == key_hash and k == key:
