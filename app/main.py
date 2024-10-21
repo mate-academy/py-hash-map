beb4565f05d415311e8a0b8cc4ed83eebdb489aa
user <nikitaskrypets@gmail.com>
2024-10-21 16:01:26 +0300

    upgraded code logic

--- app/main.py
+++ app/main.py
@@ -1,69 +1,56 @@
 class Dictionary:
     def __init__(self) -> None:
         self.capacity = 8
-        self.nodes = [[]] * 8
+        self.nodes = [[] for _ in range(8)]
+        self.length = 0
+
+    def resize(self) -> None:
+        self.capacity *= 2
+        old_nodes = self.nodes
+        self.nodes = [[] for _ in range(self.capacity)]
+        self.length = 0
+
+        for node in old_nodes:
+            if node:
+                self.__setitem__(node[0], node[1])
 
     def __setitem__(self, key: any, value: any) -> None:
+        bucket = self.nodes[hash(key) % self.capacity]
+        for node in bucket:
+            if node[0] == key:
+                node[1] = value
+                return
 
-        if self.__len__() >= self.capacity * 2 / 3:
-            self.capacity *= 2
-            old_nodes = self.nodes
-            self.nodes = [[]] * self.capacity
-
-            for node in old_nodes:
-                if node:
-                    if not self.nodes[node[2] % self.capacity]:
-                        self.nodes[node[2] % self.capacity] = node
-                    else:
-                        for i in range(0, self.capacity):
-                            if not self.nodes[i]:
-                                self.nodes[i] = node
-                                break
-
-        node = self.nodes[hash(key) % self.capacity]
-        if node:
-            if not node[0] == key:
-                for i in range(0, self.capacity):
-                    if not self.nodes[i] or self.nodes[i][0] == key:
-                        self.nodes[i] = [key, value, hash(key)]
-                        break
-            else:
-                self.nodes[hash(key) % self.capacity] = [key, value, hash(key)]
-        else:
-            self.nodes[hash(key) % self.capacity] = [key, value, hash(key)]
+        bucket.append([key, value, hash(key)])
+        self.length += 1
 
     def __getitem__(self, key: any) -> any:
-        node = self.nodes[hash(key) % self.capacity]
-        if node:
-            if node[0] != key:
-                for i in range(0, self.capacity):
-                    if self.nodes[i] and self.nodes[i][0] == key:
-                        return self.nodes[i][1]
-            return node[1]
+        bucket = self.nodes[hash(key) % self.capacity]
+        for node in bucket:
+            if node[0] == key:
+                return node[1]
         raise KeyError("There is no such key in the dictionary")
 
     def __len__(self) -> int:
-        return len(self.nodes) - self.nodes.count([])
+        return self.length
 
     def __delitem__(self, key: any) -> None:
-        node = self.nodes[hash(key) % self.capacity]
-        if node:
-            if node[0] != key:
-                for i in range(0, self.capacity):
-                    if self.nodes[i] and self.nodes[i][0] == key:
-                        self.nodes[i] = []
+        bucket = self.nodes[hash(key) % self.capacity]
+        for node in bucket:
+            if node[0] == key:
+                node[1] = []
+                self.length -= 1
 
-    def pop(self, key: any) -> any:
-        res = self[key]
-        self.__delitem__(key)
-        return res
+    def clear(self):
+        self.__init__()
 
-    def clear(self) -> None:
-        self.capacity = 8
-        self.nodes = [[]] * 8
-
-    def get(self, key: any) -> any:
+    def get(self, key: any) -> None:
         try:
-            self[key]
+            return self.__getitem__(key)
         except KeyError:
             return None
+
+    def pop(self, key):
+        result = self.__getitem__(key)
+        self.__delitem__(key)
+        return result
