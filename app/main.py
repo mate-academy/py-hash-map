c72d470261e07fae499357158638fd56188f4951
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-10-23 18:15:35 +0300

    Added get | __delitem__ | clear methods

--- app/main.py
+++ app/main.py
@@ -78,10 +78,46 @@ class Dictionary:
                 and self.storage[index_][2] == hash_):
             return self.storage[index_][1]
 
-        for i in range(self.capacity):
+        for i in range(1, self.capacity):
             new_index = self.collision(key, index_)
             if self.storage[new_index] is None:
                 raise KeyError("No such key exists.")
             if (self.storage[new_index][0] == key
                     and self.storage[new_index][2] == hash_):
                 return self.storage[new_index][1]
+
+    def clear(self) -> None:
+        """This method returns the dictionary to its original form."""
+        self.capacity = 8
+        self.storage = [None] * self.capacity
+        self.current_length = 0
+
+    def __delitem__(self, key: Any) -> None:
+        """This method removes an element by the specified key."""
+        hash_, index_ = self.get_index_hash(key)
+
+        if self.storage[index_] is None:
+            raise KeyError("No such key exists.")
+
+        if self.storage[index_][0] == key and self.storage[index_][2] == hash_:
+            self.storage[index_] = None
+            self.current_length -= 1
+            return f"The item {key} has been successfully removed."
+
+        for i in range(1, self.capacity):
+            new_index = self.collision(key, index_)
+            if self.storage[new_index] is None:
+                raise KeyError("No such key exists.")
+
+            if (self.storage[new_index][0] == key
+                    and self.storage[new_index][2] == hash_):
+                self.storage[new_index] = None
+                self.current_length -= 1
+                return f"The item {key} has been successfully removed."
+
+    def get(self, key: Any, user_value: Any = None) -> None | Any:
+        """This method returns the value by key or None | user value."""
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return user_value
