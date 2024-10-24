e02eb940ab7b995a16e974a745e29e15d0ab3820
Dima Havrylenko <94nj111@gmail.com>
2024-10-24 12:03:13 +0300

    dictionary solution (Node dataclass + minor fixes)

--- app/main.py
+++ app/main.py
@@ -2,35 +2,44 @@ from __future__ import annotations
 
 from typing import Any, Hashable, Iterator
 
+from dataclasses import dataclass
+
+
+@dataclass
+class Node:
+    hash: int
+    key: Hashable
+    value: Any
+
 
 class Dictionary:
 
     def __init__(self) -> None:
-        self.__len = 8
-        self.dictionary = [None] * self.__len
+        self.__size = 0
+        self.__capacity = 8
+        self._hash_table: list[Node | None] = [None] * self.__capacity
 
-    def __setitem__(self, key: Hashable, *value: Any) -> None:
-        if len(value) == 0:
-            raise TypeError("expected 2 arguments, got 1")
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         key_hash = hash(key)
-        hash_index = self.__collision_to_write(key)
-        if len(value) == 1:
-            value = value[0]
-        self.dictionary[hash_index] = (key, value, key_hash)
+        hash_index = self.__index_to_write(key)
+        if self._hash_table[hash_index] is None:
+            self.__size += 1
+        self._hash_table[hash_index] = Node(hash=key_hash, key=key, value=value)
 
-        if len(self) > int(self.__len * (2 / 3)):
+        if len(self) > int(self.__capacity * (2 / 3)):
             self.__resize()
 
     def __getitem__(self, key: Hashable) -> Any:
-        hash_index = self.__collision_to_read(key)
-        return self.dictionary[hash_index][1]
+        hash_index = self.__index_to_read(key)
+        return self._hash_table[hash_index].value
 
     def __len__(self) -> int:
-        return sum(1 for item in self.dictionary if item is not None)
+        return self.__size
 
     def __delitem__(self, key: Hashable) -> None:
-        hash_index = self.__collision_to_read(key)
-        self.dictionary[hash_index] = None
+        hash_index = self.__index_to_read(key)
+        self._hash_table[hash_index] = None
+        self.__size -= 1
 
     def __repr__(self) -> str:
         str_ = "{"
@@ -40,9 +49,9 @@ class Dictionary:
 
     def __iter__(self) -> Iterator:
         list_ = []
-        for i in range(self.__len):
-            if self.dictionary[i] is not None:
-                list_.append(self.dictionary[i][0])
+        for i in range(self.__capacity):
+            if self._hash_table[i] is not None:
+                list_.append(self._hash_table[i].key)
         return iter(list_)
 
     def get(self, key: Hashable, default_value: Any = None) -> Any:
@@ -53,64 +62,53 @@ class Dictionary:
             return default_value
 
     def pop(self, key: Hashable, *default_value: Any) -> Any:
-        value = None
-        value_switched = False
-        if len(default_value) == 1:
-            value = default_value[0]
-            value_switched = True
         if len(default_value) > 1:
             raise TypeError("Expected at most two argument")
         try:
-            hash_index = self.__collision_to_read(key)
-            value = self.dictionary[hash_index]
-            self.dictionary[hash_index] = None
+            hash_index = self.__index_to_read(key)
+            value = self._hash_table[hash_index]
+            self.__delitem__(key)
+            self.__size -= 1
             return value
         except KeyError:
-            if value_switched:
+            try:
+                value = default_value[0]
                 return value
-            raise
+            except IndexError:
+                raise
 
     def update(self, other: dict | Dictionary) -> None:
         for key in other:
             self[key] = other.get(key)
 
     def clear(self) -> None:
-        self.dictionary = [None] * self.__len
+        self.__size = 0
+        self._hash_table = [None] * self.__capacity
 
-    def __collision_to_write(self, key: Hashable) -> int:
-        key_hash = hash(key)
-        hash_index = key_hash % self.__len
-        for i in range(hash_index, self.__len):
-            if (self.dictionary[i] is None
-                    or self.dictionary[i][0] == key
-                    and self.dictionary[i][2] == key_hash):
-                return i
-        for i in range(0, hash_index):
-            if (self.dictionary[i] is None
-                    or self.dictionary[i][0] == key
-                    and self.dictionary[i][2] == key_hash):
-                return i
+    def __index_to_write(self, key: Hashable) -> int:
+        hash_index = hash(key) % self.__capacity
+        for i in range(hash_index, self.__capacity + hash_index):
+            index = i % self.__capacity
+            if (self._hash_table[index] is None
+                    or self._hash_table[index].key == key
+                    and self._hash_table[index].hash == hash(key)):
+                return index
 
-    def __collision_to_read(self, key: Hashable) -> int:
-        key_hash = hash(key)
-        hash_index = key_hash % self.__len
-        for i in range(hash_index, self.__len):
-            if (self.dictionary[i] is not None
-                    and self.dictionary[i][0] == key
-                    and self.dictionary[i][2] == key_hash):
-                return i
-        for i in range(0, hash_index):
-            if (self.dictionary[i] is not None
-                    and self.dictionary[i][0] == key
-                    and self.dictionary[i][2] == key_hash):
-                return i
+    def __index_to_read(self, key: Hashable) -> int:
+        hash_index = hash(key) % self.__capacity
+        for i in range(hash_index, self.__capacity + hash_index):
+            index = i % self.__capacity
+            if (self._hash_table[index] is not None
+                    and self._hash_table[index].key == key
+                    and self._hash_table[index].hash == hash(key)):
+                return index
         raise KeyError
 
     def __resize(self) -> None:
-        old_dictionary = self.dictionary
-        self.__len *= 2
-        self.dictionary = [None] * self.__len
-        for el in old_dictionary:
-            if el is not None:
-                key, value = el[:2]
-                self[key] = value
+        old_hash_table = self._hash_table
+        self.__capacity *= 2
+        self._hash_table = [None] * self.__capacity
+        self.__size = 0
+        for node in old_hash_table:
+            if node is not None:
+                self[node.key] = node.value
