d2d4668d62e6b56e2dc513b839e536a78f0cdb18
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2024-11-23 13:03:13 +0200

    Fix resize() method and add other methods

--- app/main.py
+++ app/main.py
@@ -4,43 +4,87 @@ from typing import Any, Hashable
 class Dictionary:
     def __init__(self) -> None:
         self.length = 0
-        self.hash_table: list = [None] * 8
+        self.hash_table: list[None | tuple[Hashable, hash, Any]] = [None] * 8
         self.load_factor = 2 / 3
-
-    def __setitem__(self, key: Hashable, value: Any) -> None:
-        hashed_key = hash(key)
-        index = hashed_key % len(self.hash_table)
-        while True:
-            if not self.hash_table[index]:
-                if (self.length + 1 > self.load_factor
-                        * len(self.hash_table)):
-                    self.resize()
-                    index = hashed_key % len(self.hash_table)
-                    continue
-                self.length += 1
-                self.hash_table[index] = (key, hashed_key, value)
-                break
-            elif self.hash_table[index][0] == key:
-                self.hash_table[index] = (key, hashed_key, value)
-                break
-            index = (index + 1) % len(self.hash_table)
-
-    def __getitem__(self, key: Hashable) -> Any:
-        index = hash(key) % len(self.hash_table)
-        counter = 0
-        while counter < len(self):
-            if (self.hash_table[index]
-                    and self.hash_table[index][0] == key):
-                return self.hash_table[index][2]
-            index = (index + 1) % len(self.hash_table)
-            counter += 1
-        raise KeyError
+        self.capacity = 2
 
     def __len__(self) -> int:
         return self.length
 
+    @staticmethod
+    def add_item_to_table(
+            table: list[None | tuple[Hashable, hash, Any]],
+            key: Hashable,
+            value: Any
+    ) -> None:
+        hash_ = hash(key)
+        index = hash_ % (len(table) - 1)
+        while True:
+            if not table[index] or table[index][0] == key:
+                table[index] = key, hash_, value
+                break
+            index += 1
+            if index == len(table):
+                index = 0
+
     def resize(self) -> None:
-        table = self.hash_table
-        self.hash_table = [None] * len(self.hash_table) * 2
+        new_hash_table = [None] * len(self.hash_table) * self.capacity
+        for item in self:
+            self.add_item_to_table(
+                new_hash_table,
+                item,
+                self.__getitem__(item)
+            )
+        self.hash_table = new_hash_table
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        if len(self) > self.load_factor * len(self.hash_table):
+            self.resize()
+        if key not in self:
+            self.length += 1
+        self.add_item_to_table(self.hash_table, key, value)
+
+    def __getitem__(self, item: Hashable) -> Any:
+        if item not in self:
+            raise KeyError
+        index = hash(item) % (len(self.hash_table) - 1)
+        while True:
+            if self.hash_table[index] and self.hash_table[index][0] == item:
+                return self.hash_table[index][2]
+            index += 1
+            if index == len(self.hash_table):
+                index = 0
+
+    def __delitem__(self, key: Hashable) -> None:
+        if key not in self:
+            raise KeyError
+        index = hash(key) % (len(self.hash_table) - 1)
+        while True:
+            if self.hash_table[index] and self.hash_table[index][0] == key:
+                self.hash_table[index] = None
+                self.length -= 1
+                break
+            index += 1
+            if index == len(self.hash_table):
+                index = 0
+
+    def clear(self) -> None:
+        self.hash_table = [None] * len(self.hash_table)
         self.length = 0
-        [self.__setitem__(bucket[0], bucket[2]) for bucket in table if bucket]
+
+    def get(self, key: Hashable, value: Any) -> Any:
+        if key in self:
+            return self.__getitem__(key)
+        return value
+
+    def pop(self, key: Hashable) -> Any:
+        output = self.__getitem__(key)
+        self.__delitem__(key)
+        return output
+
+    def update(self, other: dict) -> None:
+        for key in other:
+            self.__setitem__(key, other.__getitem__(key))
+
+    def __iter__(self) -> iter:
+        return iter(item[0] for item in self.hash_table if item)
