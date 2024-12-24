41016ce1adc9ca5943f386dcb5c6bfa70717c14f
Trukhanov Danylo <trukhanov.danya@gmail.com>
2024-12-24 13:41:40 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from typing import Any, Hashable
 from dataclasses import dataclass
 
+
 @dataclass
 class Node:
     key: Hashable
     value: Any
+
 
 class Dictionary:
     def __init__(self) -> None:
@@ -14,16 +16,16 @@ class Dictionary:
         self.threshold = int(self.capacity * self.load_factor)
         self.size = 0
 
-    def _index(self, key):
+    def _index(self, key: Hashable) -> int:
         index = hash(key) % self.capacity
         while (
-            self.hash_table[index] is not None and
-            self.hash_table[index].key != key
+            self.hash_table[index] is not None
+            and self.hash_table[index].key != key
         ):
             index = (index + 1) % self.capacity
         return index
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: Hashable, value: Any) -> None:
         if self.size == self.threshold:
             self._resize()
 
@@ -34,17 +36,16 @@ class Dictionary:
 
         self.hash_table[index] = Node(key, value)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Hashable) -> Any:
         index = self._index(key)
         if self.hash_table[index] is None:
             raise KeyError("Not found")
         return self.hash_table[index].value
 
-
-    def __len__(self):
+    def __len__(self) -> int:
         return self.size
 
-    def _resize(self):
+    def _resize(self) -> None:
         old_table = self.hash_table
         self.size = 0
         self.capacity *= 2
@@ -54,4 +55,3 @@ class Dictionary:
         for node in old_table:
             if node is not None:
                 self.__setitem__(node.key, node.value)
-
