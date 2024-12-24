b39235aa6431578d656a05320fb9abddd01a5bf9
4ikkamony <compbanderloga@gmail.com>
2024-12-24 13:05:31 +0200

    Final version

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from __future__ import annotations
-from math import floor, log
-from typing import Hashable, Any, Iterator
+from typing import Hashable, Any, Iterator, Iterable
+
+from dataclasses import dataclass
 
 
-PairType = tuple[Hashable, Any]
-BucketType = list[PairType]
+@dataclass
+class Pair:
+    key: Hashable
+    value: Any
+    key_hash: int
+
+
+BucketType = list[Pair]
 
 
 class Dictionary:
@@ -16,9 +23,6 @@ class Dictionary:
             capacity: int = DEFAULT_CAPACITY,
             load_factor_limit: float = 0.75
     ) -> None:
-        if capacity < Dictionary.DEFAULT_CAPACITY:
-            capacity = Dictionary._next_power_of_two(capacity)
-
         self._buckets: list[BucketType] = [[] for _ in range(capacity)]
 
         self._load_factor_limit = load_factor_limit
@@ -27,17 +31,18 @@ class Dictionary:
         if self._load_factor() >= self._load_factor_limit:
             self._resize_and_rehash()
 
-        bucket = self._buckets[self._index(key)]
+        index = self._index(key)
+        bucket = self._buckets[index]
         try:
-            index, _ = self._find_pair(bucket, key)
-            bucket[index] = (key, value)
+            index_in_bucket, _ = self._find_pair(bucket, key)
+            bucket[index_in_bucket] = Pair(key, value, index)
         except KeyError:
-            bucket.append((key, value))
+            bucket.append(Pair(key, value, index))
 
     def __getitem__(self, key: Hashable) -> Any:
         bucket = self._buckets[self._index(key)]
         _, pair = self._find_pair(bucket, key)
-        return pair[1]
+        return pair.value
 
     def __delitem__(self, key: Hashable) -> None:
         bucket = self._buckets[self._index(key)]
@@ -58,24 +63,15 @@ class Dictionary:
     def __iter__(self) -> Iterator:
         yield from self.keys()
 
-    @classmethod
-    def from_dict(cls, dictionary: dict) -> Dictionary:
-        my_dictionary = cls(len(dictionary))
-
-        for key, value in dictionary.items():
-            my_dictionary[key] = value
-
-        return my_dictionary
-
     def get(self, key: Hashable, default: Any = None) -> Any:
         try:
             return self[key]
         except KeyError:
             return default
 
-    def items(self) -> list[PairType]:
+    def items(self) -> list[tuple[Hashable, Any]]:
         return [
-            pair for bucket in self._buckets
+            (pair.key, pair.value) for bucket in self._buckets
             for pair in bucket
         ]
 
@@ -83,27 +79,50 @@ class Dictionary:
         return [pair[1] for pair in self.items()]
 
     def keys(self) -> list[Hashable]:
-        return [pair[0] for pair in self.items()]
+        return [pair[0]for pair in self.items()]
 
-    def pop(self) -> Any:
-        ...
+    def pop(self, key: Hashable, default: Any = None) -> Any:
+        try:
+            value = self[key]
+            self.__delitem__(key)
+            return value
+        except KeyError:
+            return default
 
-    def update(self) -> None:
-        ...
+    def update(
+            self,
+            iterable: Iterable[
+                tuple[Hashable, Any]
+            ] | Dictionary | dict | None = None,
+            **kwargs,
+    ) -> None:
+
+        if isinstance(iterable, Dictionary | dict):
+            for key, value in iterable.items():
+                self[key] = value
+        elif isinstance(iterable, Iterable):
+            for item in iterable:
+                if not isinstance(item, tuple) and len(item) != 2:
+                    raise TypeError(
+                        f"{type(iterable)} must contain tuples "
+                        f"of key-value pairs"
+                    )
+                self[item[0]] = item[1]
+        else:
+            raise TypeError(f"{type(iterable)} is not supported")
+
+        for key, value in kwargs:
+            self[key] = value
 
     def clear(self) -> None:
-        ...
+        self._buckets = [[] for _ in range(self._capacity())]
 
     @staticmethod
-    def _find_pair(bucket: BucketType, key: Hashable) -> tuple[int, PairType]:
+    def _find_pair(bucket: BucketType, key: Hashable) -> tuple[int, Pair]:
         for index, pair in enumerate(bucket):
-            if pair[0] == key:
+            if pair.key == key:
                 return index, pair
         raise KeyError(f"{key} not in dict")
-
-    @staticmethod
-    def _next_power_of_two(number: int) -> int:
-        return 2 ** (floor(log(number) / log(2)) + 1)
 
     def _capacity(self) -> int:
         return len(self._buckets)
