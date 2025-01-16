8e142905f22eff9e5926e06ccd986fbeb3b35a66
yevpy <eugene2piskun@gmail.com>
2025-01-16 18:05:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,37 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self, capacity: int = 8) -> None:
+        self.capacity = capacity
+        self.size = 0
+        self.table = [None] * self.capacity
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        index = hash(key) % self.capacity
+
+        if self.table[index] is None:
+            self.table[index] = []
+
+        for pair in self.table[index]:
+            if pair[0] == key:
+                pair[1] = value
+                return
+
+        self.table[index].append([key, value])
+        self.size += 1
+
+    def __getitem__(self, item: Any) -> None:
+        index = hash(item) % self.capacity
+
+        if self.table[index] is None:
+            raise KeyError(f"Key '{item}' not found")
+
+        for pair in self.table[index]:
+            if pair[0] == item:
+                return pair[1]
+
+        raise KeyError(f"Key '{item}' not found")
+
+    def __len__(self) -> int:
+        return self.size
