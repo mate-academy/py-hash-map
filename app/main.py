51140629e9455d5c9e1e8cba968a15b18846a104
Tymofii Khlistik <tima0323199@gmail.com>
2025-02-07 11:40:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,60 @@
 class Dictionary:
-    pass
+    def __init__(
+            self,
+            initial_capacity: int = 8,
+            load_factor: None = 2 / 3
+    ) -> None:
+        self.capacity = initial_capacity
+        self.load_factor = load_factor
+        self.size = 0
+        self.buckets = [None] * self.capacity
+
+    class Node:
+        def __init__(self, key: None, value: None) -> None:
+            self.key = key
+            self.value = value
+            self.hash = hash(key)
+
+    def _hash_index(self, key: None) -> hash:
+        return hash(key) % self.capacity
+
+    def __setitem__(
+            self, key: [str, int, float],
+            value: [str, int, float]
+    ) -> None:
+        if self.size / self.capacity >= self.load_factor:
+            self._resize()
+
+        index = self._hash_index(key)
+
+        while self.buckets[index] is not None:
+            if self.buckets[index].key == key:
+                self.buckets[index].value = value
+                return
+            index = (index + 1) % self.capacity
+
+        self.buckets[index] = self.Node(key, value)
+        self.size += 1
+
+    def __getitem__(self, key: [int, str, float]) -> None:
+        index = self._hash_index(key)
+
+        while self.buckets[index] is not None:
+            if self.buckets[index].key == key:
+                return self.buckets[index].value
+            index = (index + 1) % self.capacity
+
+        raise KeyError(f"Key {key} not found")
+
+    def __len__(self) -> int:
+        return self.size
+
+    def _resize(self) -> None:
+        old_buckets = self.buckets
+        self.capacity *= 2
+        self.buckets = [None] * self.capacity
+        self.size = 0  # Перезапис розміру
+
+        for node in old_buckets:
+            if node is not None:
+                self[node.key] = node.value
