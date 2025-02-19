7f986fded045b28e81d881ae113c2eaa67754de1
Marek Smolana <msmolana@gmail.com>
2025-02-19 15:19:41 +0100

    draft

--- app/main.py
+++ app/main.py
@@ -1,2 +1,65 @@
 class Dictionary:
-    pass
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.table = [None] * capacity
+        pass
+
+    def _hash(self, key):
+        return hash(key) % self.capacity
+
+    def __setitem__(self, key: any, value: any) -> None:
+        index = self._hash(key)
+
+        if self.table[index] is None:
+            self.table[index] = Node(key, value)
+            self.size += 1
+        else:
+            current = self.table[index]
+            while current:
+                if current.key == key:
+                    current.value = value
+                    return
+                current = current.next
+            new_node = Node(key, value)
+            new_node.next = self.table[index]
+            self.table[index] = new_node
+            self.size += 1
+
+    def __getitem__(self, key: any) -> any:
+        index = self._hash(key)
+        current = self.table[index]
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+        raise KeyError(key)
+
+    def __len__(self) -> int:
+        return self.size
+
+    def clear(self) -> None:
+        self.table = [None] * self.capacity
+
+    def __delitem__(self, key: any) -> None:
+        index = self._hash(key)
+        previous = None
+        current = self.table[index]
+        while current:
+            if current.key == key:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.table[index] = current.next
+                self.size -= 1
+                return
+            previous = current
+            current = current.next
+        raise KeyError(key)
+
+
+class Node:
+    def __init__(self, key: any, value: any) -> None:
+        self.key = key
+        self.value = value
+        self.next = None
