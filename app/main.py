493f4440abceae022da00d21132eeaa1b4f7a941
Mazurkav <aleshkamazurkevich@gmail.com>
2025-02-28 14:45:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,55 +1,58 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
 class Dictionary:
-    def __init__(self, initial_capacity=8, load_factor=0.75):
+    def __init__(self, initial_capacity=10):
         self.capacity = initial_capacity
-        self.load_factor = load_factor
         self.size = 0
         self.buckets = [None] * self.capacity
 
-    class Node:
-        def __init__(self, key, value):
-            self.key = key
-            self.value = value
-            self.hash = hash(key)
-            self.next = None
-
     def __setitem__(self, key, value):
-        if self.size / self.capacity >= self.load_factor:
-            self._resize()
-
         index = hash(key) % self.capacity
-        if not self.buckets[index]:
-            self.buckets[index] = self.Node(key, value)
+        if self.buckets[index] is None:
+            self.buckets[index] = Node(key, value)
         else:
-            node = self.buckets[index]
-            while True:
-                if node.key == key:
-                    node.value = value  # Update value
+            current = self.buckets[index]
+            while current:
+                if current.key == key:
+                    current.value = value
                     return
-                if not node.next:
+                if current.next is None:
                     break
-                node = node.next
-            node.next = self.Node(key, value)
-
+                current = current.next
+            current.next = Node(key, value)
         self.size += 1
+        if self.size / self.capacity > 0.7:
+            self._resize()
 
     def __getitem__(self, key):
         index = hash(key) % self.capacity
-        node = self.buckets[index]
-        while node:
-            if node.key == key:
-                return node.value
-            node = node.next
-        raise KeyError(f"Key {key} not found")
+        current = self.buckets[index]
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+        raise KeyError(f"Key '{key}' not found")
 
     def __len__(self):
         return self.size
 
     def _resize(self):
-        old_buckets = self.buckets
-        self.capacity *= 2
-        self.buckets = [None] * self.capacity
-        self.size = 0
-        for node in old_buckets:
-            while node:
-                self[key] = node.value
-                node = node.next
+        new_capacity = self.capacity * 2
+        new_buckets = [None] * new_capacity
+        for i in range(self.capacity):
+            current = self.buckets[i]
+            while current:
+                new_index = hash(current.key) % new_capacity
+                if new_buckets[new_index] is None:
+                    new_buckets[new_index] = Node(current.key, current.value)
+                else:
+                    new_current = new_buckets[new_index]
+                    while new_current.next:
+                        new_current = new_current.next
+                    new_current.next = Node(current.key, current.value)
+                current = current.next
+        self.buckets = new_buckets
+        self.capacity = new_capacity
